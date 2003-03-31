a46a4a61a5ee8b403c2b0dfe5d4398685799327e
Gregor <Gregor@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2003-03-31 12:07:35 +0000

    no message

--- orange/OrangeWidgets/OWParallelCoordinates.py
+++ orange/OrangeWidgets/OWParallelCoordinates.py
@@ -17,6 +17,7 @@ from OWParallelGraph import *
 from OData import *
 
 class OWParallelCoordinates(OWWidget):
+    settingsList=[]
     def __init__(self,parent=None):
         OWWidget.__init__(self,
         parent,
@@ -56,28 +57,67 @@ class OWParallelCoordinates(OWWidget):
     
         self.connect(self.options, PYSIGNAL("canvasColorChange(QColor &)"), self.setCanvasColor)
 
-        #add controls to self.controlArea widget 
+        #add controls to self.controlArea widget
         self.selClass = QVGroupBox(self.controlArea)
         self.selout = QVGroupBox(self.space)
+        self.attrOrder = QVGroupBox(self.space)
         self.selClass.setTitle("Class attribute")
         self.selout.setTitle("Shown attributes")
+        self.attrOrder.setTitle("Attribute order")
+
         self.classCombo = QComboBox(self.selClass)
         self.showContinuousCB = QCheckBox('show continuous', self.selClass)
         self.connect(self.showContinuousCB, SIGNAL("clicked()"), self.setClassCombo)
 
-
         self.attributesLB = QListBox(self.selout)
         self.attributesLB.setSelectionMode(QListBox.Multi)
+
+        self.attributesOrderLB = QListBox(self.attrOrder)
+        self.attrButtonGroup = QHButtonGroup(self.attrOrder)
+        self.buttonUPAttr = QPushButton("Attr UP", self.attrButtonGroup)
+        self.buttonDOWNAttr = QPushButton("Attr DOWN", self.attrButtonGroup)
+
+        
         #connect controls to appropriate functions
-        self.connect(self.classCombo, SIGNAL('activated ( const QString & )'), self.classAttributeChange)
-        self.connect(self.attributesLB, SIGNAL("selectionChanged()"), self.showSelectedAttributes)
+        self.connect(self.classCombo, SIGNAL('activated ( const QString & )'), self.updateGraph)
+        self.connect(self.attributesLB, SIGNAL("selectionChanged()"), self.updateGraph)
+
+        self.connect(self.buttonUPAttr, SIGNAL("clicked()"), self.moveAttrUP)
+        self.connect(self.buttonDOWNAttr, SIGNAL("clicked()"), self.moveAttrDOWN)
 
         self.repaint()
+
+    # move selected attribute in "Attribute Order" list one place up
+    def moveAttrUP(self):
+        for i in range(self.attributesOrderLB.count()):
+            if self.attributesOrderLB.isSelected(i) and i != 0:
+                text = self.attributesOrderLB.text(i)
+                self.attributesOrderLB.removeItem(i)
+                self.attributesOrderLB.insertItem(text, i-1)
+                self.attributesOrderLB.setSelected(i-1, TRUE)
+        self.updateGraph()
+
+    # move selected attribute in "Attribute Order" list one place down  
+    def moveAttrDOWN(self):
+        count = self.attributesOrderLB.count()
+        for i in range(count-2,-1,-1):
+            if self.attributesOrderLB.isSelected(i):
+                text = self.attributesOrderLB.text(i)
+                self.attributesOrderLB.removeItem(i)
+                self.attributesOrderLB.insertItem(text, i+1)
+                self.attributesOrderLB.setSelected(i+1, TRUE)
+        self.updateGraph()
+
+    def updateGraph(self):
+        self.updateGraphData(self.getAttributeList(), str(self.classCombo.currentText()))
+
 
     def setCanvasColor(self, c):
         self.GraphCanvasColor = str(c.name())
         self.graph.setCanvasColor(c)
 
+
+    # receive new data and update all fields
     def cdata(self, data):
         self.data = data
         self.graph.setData(data)
@@ -91,18 +131,11 @@ class OWParallelCoordinates(OWWidget):
 
         
         self.setAttributeList(self.data.data.domain)
-        self.showSelectedAttributes()
+        self.setAttributeOrderList(self.data.data.domain)
+        self.updateGraph()
 
-    def showSelectedAttributes(self):
-        attributes = []
-        for i in range(self.attributesLB.numRows()):
-            if self.attributesLB.isSelected(i):
-                attributes.append(str(self.attributesLB.text(i)))
-        
-        self.graph.updateData(attributes, str(self.classCombo.currentText()))
-        self.graph.replot()
-        self.repaint()
-   
+  
+    # set combo box values with attributes that can be used for coloring the data
     def setClassCombo(self):
         exText = str(self.classCombo.currentText())
         self.classCombo.clear()
@@ -121,26 +154,50 @@ class OWParallelCoordinates(OWWidget):
                 self.classCombo.setCurrentItem(i)
                 return
         self.classCombo.setCurrentItem(0)
-        self.showSelectedAttributes()
 
 
+    # set attribute list
     def setAttributeList(self, list):
         self.attributeList = list
 
         self.attributesLB.clear()
-        if len(self.attributeList) == 0: return
+        if len(self.attributeList) == 0:
+            return
         for item in list:
             self.attributesLB.insertItem(item.name)
 
         self.attributesLB.selectAll(TRUE)
 
+    # set attribute order list
+    def setAttributeOrderList(self, list):
+        self.attributeOrderList = list
+
+        self.attributesOrderLB.clear()
+        if len(self.attributeOrderList) == 0:
+            return
+        for item in list:
+            self.attributesOrderLB.insertItem(item.name)
+
+
+    # new class attribute was selected - update the graph
     def classAttributeChange(self, newClass):
         attributes = []
         for i in range(self.attributesLB.numRows()):
             if self.attributesLB.isSelected(i):
                 attributes.append(str(self.attributesLB.text(i)))
 
-        self.graph.updateData(attributes, str(self.classCombo.currentText()))
+        self.updateGraph()
+
+
+    def getAttributeList (self):
+        list = []
+        for i in range(self.attributesOrderLB.count()):
+            list.append(str(self.attributesOrderLB.text(i)))
+        return list
+
+
+    def updateGraphData(self, attributes, className):
+        self.graph.updateData(attributes, className)
         self.graph.replot()
         self.repaint()
 
