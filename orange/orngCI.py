6db18fce2ec1ad4cc083dc300567953927b5933b
janezd <janez.demsar@fri.uni-lj.si>
2003-05-05 16:09:03 +0000

    no message

--- orange/orngCI.py
+++ orange/orngCI.py
@@ -320,7 +320,7 @@ class AttributeRedundanciesRemover:
 
     for attr in ordered:
       newattr = inducer(data, [attr], weight)[0]
-      if len(newattr.values) < len(attr.values):
+      if len(newattr.values)==1 or (not self.noMinimization and (len(newattr.values) < len(attr.values))):
         newset = filter(lambda x: x!=attr, data.domain.attributes)
         if len(newattr.values)>1:
           newset.append(newattr)
