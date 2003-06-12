fa04b3112eb41359742b3678456751715d5e2b39
janezd <janez.demsar@fri.uni-lj.si>
2003-06-12 12:38:53 +0000

    no message

---
+++ source/include/pythag.cpp
@@ -0,0 +1,13 @@
+#include <math.h>
+#define NRANSI
+#include "nrutil.h"
+
+float pythag(float a, float b)
+{
+	float absa,absb;
+	absa=fabs(a);
+	absb=fabs(b);
+	if (absa > absb) return absa*sqrt(1.0+SQR(absb/absa));
+	else return (absb == 0.0 ? 0.0 : absb*sqrt(1.0+SQR(absa/absb)));
+}
+#undef NRANSI
