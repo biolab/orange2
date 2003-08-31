169495c6a2c99b277741f87ef1b87345784d3cd7
Gregor <Gregor@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2003-08-31 14:05:59 +0000

    basic class that allows to save settings and do signaling. To be used as a base class for all dialogs.

---
+++ orange/OrangeWidgets/OWBaseWidget.py
@@ -0,0 +1,292 @@
+#
+# OWWidget.py
+# Orange Widget
+# A General Orange Widget, from which all the Orange Widgets are derived
+#
+
+import sys
+import ConfigParser,os
+from string import *
+import cPickle
+from OWTools import *
+from OWAboutX import *
+
+class OWBaseWidget(QWidget):
+    def __init__(
+    self,
+    parent=None,
+    title="Qt Orange BaseWidget",
+    description="This a base for OWWidget. It encorporates saving, loading settings and signal processing.",
+    wantSettings=FALSE,
+    wantGraph = FALSE, 
+    wantAbout = TRUE,
+    icon="OrangeWidgetsIcon.png",
+    logo="OrangeWidgetsLogo.png",
+    ):
+        """
+        Initialization
+        Parameters: 
+            title - The title of the\ widget, including a "&" (for shortcut in about box)
+            description - self explanatory
+            wantSettings - display settings button or not
+            icon - the icon file
+            logo - the logo file
+        """
+        fullIcon = sys.prefix + "/lib/site-packages/Orange/OrangeWidgets/icons/" + icon
+        logo = sys.prefix + "/lib/site-packages/Orange/OrangeWidgets/icons/" + logo
+        self.widgetDir = sys.prefix + "/lib/site-packages/Orange/OrangeWidgets/"
+        self.title = title.replace("&","")
+        self.captionTitle=title.replace("&","")
+
+        # if we want the widget to show the title then the title must start with "Qt"
+        if self.captionTitle[:2].upper != "QT":
+            self.captionTitle = "Qt " + self.captionTitle
+            
+        QWidget.__init__(self,parent)
+        #list of all active connections to this widget
+        self.connections=[]
+        #list of inputs - should list all the channels that can be received
+        self.inputs=[]
+        self.multipleInputs=[]
+        #list of outputs - should list all the channels that can be emited
+        self.outputs=[]
+        #the map with settings
+        if not hasattr(self, 'settingsList'):
+            self.__class__.settingsList = []
+        #is widget enabled?
+        self.enabled=TRUE
+        #the title
+        self.setCaption(self.captionTitle)
+        self.setIcon(QPixmap(fullIcon))
+        #about box
+        self.about=OWAboutX(title,description,fullIcon,logo)
+        self.buttonBackground=QVBox(self)
+        if wantSettings:
+            self.settingsButton=QPushButton("&Settings",self.buttonBackground)
+        if wantGraph:
+            self.graphButton=QPushButton("&Save Graph",self.buttonBackground)
+        if wantAbout:
+            self.aboutButton=QPushButton("&About",self.buttonBackground)
+            self.connect(self.aboutButton,SIGNAL("clicked()"),self.about.show)
+
+        #if parent==None:
+        #    self.closeButton=QPushButton("&Close",self.buttonBackground)
+        
+        #if parent==None:
+        #    self.connect(self.closeButton,SIGNAL("clicked()"),self.close)
+        self.mainArea=QWidget(self)
+#        self.mainArea.setBackgroundColor(Qt.white)
+        self.controlArea=QVBox(self)
+        self.space=QVBox(self)
+#        self.controlArea.setBackgroundColor(Qt.white)
+#        self.connect(self,SIGNAL("exit"),self.saveSettings)
+        self.linkBuffer={}
+        
+    def link(self,source,channel):
+        """
+        Link a widget to this widget
+        Parameters:
+            source - source widget
+            cnannel - name of the channel and the function it connects to
+        """
+        #what to do with those tree:classifier signals?!
+        
+        #check if this channel exist
+        if channel not in self.inputs and channel not in self.multipleInputs:
+            return
+        if channel not in self.connections:
+            self.connections.append([channel,source])
+            #there should be a function with the same name as the signal 
+            #in the destination class, otherwise this won't work!
+            #We could check if the destination functions exists.. to be done.
+            if self.enabled:                                
+#                print "self."+channel
+                if (channel in self.inputs or channel in self.multipleInputs):
+                    self.connect(source,PYSIGNAL(channel),self.receive)
+                    source.resend(channel)
+                else:
+                    print "Cannot link: the channel %s does not exist" % channel
+        
+    def unlink(self,source,channel):
+        """
+        Destroy link from a widget to this widget
+        Parameters:
+            source - source widget
+            cnannel - name of the channel and the function it connects to
+        """
+        try:
+            self.connections.remove([channel,source])
+#            self.disconnect(source,PYSIGNAL(channel),eval("self."+channel))
+            self.disconnect(source,PYSIGNAL(channel),self.receive)
+        except:
+            pass
+            
+    def send(self,channel,data):
+        """
+        Send data over a channel
+            channel - the channel name
+            data - the data to be sent, must be a tuple!
+        Saves some typing compared to QObject.emit. 
+        Only sends if connected.
+        """
+#        print "sending", data, "over channel", channel
+#        self.emit(PYSIGNAL(channel),(data,))
+        #save the data for later
+        self.linkBuffer[channel]=data
+        self.emit(PYSIGNAL(channel),(data,channel,id(self)))
+        
+    def resend(self,channel):
+        """
+        resends the data that was last sent through the channel
+        """
+        if channel in self.linkBuffer:
+            self.send(channel,self.linkBuffer[channel])
+    
+    def receive(self,zdata,channel,source):
+        """
+        Receives data over a channel. Passes it to the right function
+            data - the data
+            channel - the name of the channel
+            source - the source of the channel
+        """      
+        if channel in self.inputs:
+            eval("self."+channel)(zdata)
+        elif channel in self.multipleInputs:
+            eval("self."+channel)(zdata,source)
+        else:
+            print "Error: this channel does not exist as input!" #impossible
+    
+    def getConnections(self):
+        """
+        Gets all connections
+        Returns the list with all connections
+        """
+        return self.connections
+        
+    def setEnabled(self,enable):
+        """
+        Set this widget in enabled or disabled state
+        enable: TRUE - widget is enabled 
+                FALSE - widget is disabled
+        """
+        #setEnabled(FALSE) removes all links in connections
+        #setEnabled(TRUE) restores all this links
+        #we'll see if this works
+        self.enabled=enable
+        for connection in self.connections:
+            if enabled:
+#                self.connect(connection[0],PYSIGNAL(connection[1]),eval("self."+connection[1]))
+                self.connect(connection[0],PYSIGNAL(connection[1]),self.receive)
+                #resend in case something went wrong
+                self.resend(connection[1],connection[0])
+            else:
+#                self.disconnect(connection[0],PYSIGNAL(connection[1]),eval("self."+connection[1]))
+                self.disconnect(connection[0],PYSIGNAL(connection[1]),self.receive)
+        
+    def setSettings(self,settings):
+        """
+        Set all settings
+        settings - the map with the settings
+        """
+        self.__dict__.update(settings)
+
+    def getSettings(self):
+        """
+        Get all settings
+        returns map with all settings
+        """
+        return dict([(x, getattr(self, x, None)) for x in settingsList])
+   
+    def loadSettings(self, file = None):
+        """
+        Loads settings from the widget's .ini file
+        """
+        if hasattr(self, "settingsList"):
+            if file==None:
+                file = self.widgetDir + self.title + ".ini"
+            if type(file) == str:
+                if os.path.exists(file):
+                    file = open(file, "r")
+                    settings = cPickle.load(file)
+                    file.close()
+                else:
+                    settings = {}
+            else:
+                settings = cPickle.load(file)
+            
+            self.__dict__.update(settings)
+
+        
+    def saveSettings(self, file = None):
+        if hasattr(self, "settingsList"):
+            settings = dict([(name, getattr(self, name)) for name in self.settingsList])
+            if file==None:
+                file = self.widgetDir + self.title + ".ini"                
+            if type(file) == str:
+                file = open(file, "w")
+                cPickle.dump(settings, file)
+                file.close()
+            else:
+                cPickle.dump(settings, file)
+
+    def loadSettingsStr(self, str):
+        """
+        Loads settings from string str which is compatible with cPickle
+        """
+        if hasattr(self, "settingsList"):
+            settings = cPickle.loads(str)
+            self.__dict__.update(settings)
+
+    def saveSettingsStr(self):
+        """
+        return settings in string format compatible with cPickle
+        """
+        str = ""
+        if hasattr(self, "settingsList"):
+            settings = dict([(name, getattr(self, name)) for name in self.settingsList])
+            str = cPickle.dumps(settings)
+        return str
+
+    # this function is only intended for derived classes to send appropriate signals when all settings are loaded
+    def activateLoadedSettings(self):
+        pass
+        
+    def addInput(self,inpu,singleSignal=TRUE):
+        """
+        Adds an input.
+        Should be called for all functions that can be inputs (link destinations).
+        """
+        if singleSignal:
+            self.inputs.append(inpu)
+        else:
+            self.multipleInputs.append(inpu)
+    
+    def addOutput(self,output):
+        """
+        Adds an output.
+        Should be called for all the outputs (link sources) that can be generated.
+        """
+        self.outputs.append(output)
+        
+    def getInputs(self):
+        """
+        Get the names of all inputs
+        """
+        return self.inputs
+        
+    def getOutputs(self):
+        """
+        Get the names of all outputs
+        """
+        return self.outputs
+
+    def setOptions(self):
+        pass
+
+if __name__ == "__main__":  
+    a=QApplication(sys.argv)
+    oww=OWBaseWidget()
+    a.setMainWidget(oww)
+    oww.show()
+    a.exec_loop()
+    oww.saveSettings()
