2f66aeae9231100656b41090a96a549eede850cb
blaz <blaz.zupan@fri.uni-lj.si>
2003-11-28 09:09:20 +0000

    no message

--- orange/OrangeWidgets/OWGUI.py
+++ orange/OrangeWidgets/OWGUI.py
@@ -6,6 +6,7 @@
 # for an example
 
 from qt import *
+import qwt
 
 ##############################################################################
 
@@ -89,26 +90,31 @@ def button(widget, master, text, callback, disabled=0):
     master.connect(btn, SIGNAL("clicked()"), callback)
     return btn
 
-# TreeTab, self, 'Baseline for Line Width', ['No Dependency', 'Root Node', 'Parent Node'], 'LineWidthDep')
+# btnLabels is a list of either char strings or pixmaps
 def radioButtonsInBox(widget, master, groupLabel, btnLabels, value, tooltips=None, callback=None):
     bg = QVButtonGroup(groupLabel, widget)
     bg.setRadioButtonExclusive(1)
     for i in range(len(btnLabels)):
-        w = QRadioButton(btnLabels[i], bg)
+        if type(btnLabels[i])==type("string"):
+            w = QRadioButton(btnLabels[i], bg)
+        else:
+            w = QRadioButton(str(i), bg)
+            w.setPixmap(btnLabels[i])
         w.setOn(getattr(master, value) == i)
         if tooltips:
+            print 'ttt', tooltips
             QToolTip.add(w, tooltips[i])
     master.connect(bg, SIGNAL("clicked(int)"), ValueCallback(master, value))
     if callback:
         master.connect(bg, SIGNAL("clicked(int)"), FunctionCallback(master, callback))
 #        self.connect(self.options.spreadButtons, SIGNAL("clicked(int)"), self.setSpreadType)
-    return bg
 
 def hSlider(widget, master, value, box=None, minValue=0.0, maxValue=1.0, step=0.1, callback=None, ticks=0):
     if box:
         sliderBox = QHButtonGroup(box, widget)
     else:
-        sliderBox = QHGroupGroup(widget)
+        #sliderBox = QHGroupGroup(widget)
+        sliderBox = widget
     slider = QSlider(minValue, maxValue, step, getattr(master, value), QSlider.Horizontal, sliderBox)
     if ticks:
         slider.setTickmarks(QSlider.Below)
@@ -124,6 +130,62 @@ def hSlider(widget, master, value, box=None, minValue=0.0, maxValue=1.0, step=0.
     if callback:
         master.connect(slider, SIGNAL("valueChanged(int)"), FunctionCallback(master, callback))
 
+import math
+def qwtHSlider(widget, master, value, box=None, minValue=1, maxValue=10, step=0.1, precision=1, callback=None, logarithmic=0, ticks=0, maxWidth=80):
+    init = getattr(master, value)
+    if box:
+        sliderBox = QHButtonGroup(box, widget)
+    else:
+        sliderBox = widget
+
+    hb = QHBox(sliderBox)
+    if ticks:
+        slider = qwt.QwtSlider(hb, "", Qt.Horizontal, qwt.QwtSlider.Bottom, qwt.QwtSlider.BgSlot)
+    else:
+        slider = qwt.QwtSlider(hb, "", Qt.Horizontal, qwt.QwtSlider.None, qwt.QwtSlider.BgSlot)
+    slider.setScale(minValue, maxValue, logarithmic) # the third parameter for logaritmic scale
+    slider.setScaleMaxMinor(10)
+    slider.setThumbWidth(20)
+    slider.setThumbLength(12)
+    slider.setMaximumSize(maxWidth,40)
+    if logarithmic:
+        slider.setRange(math.log10(minValue), math.log10(maxValue), step)
+        slider.setValue(math.log10(init))
+    else:
+        slider.setRange(minValue, maxValue, step)
+        slider.setValue(init)
+        
+    format = "%s%d.%df" % ("%", precision+3, precision)
+    
+    label = QLabel(hb)
+    label.setText(format % minValue)
+    width1 = label.sizeHint().width()
+    label.setText(format % maxValue)
+    width2 = label.sizeHint().width()
+    label.setFixedSize(max(width1, width2), label.sizeHint().height())
+    label.setText(format % init)
+
+    if logarithmic:    
+        master.connect(slider, SIGNAL("valueChanged(double)"), ValueCallback(master, value, f=lambda x: 10**x))
+        master.connect(slider, SIGNAL("valueChanged(double)"), SetLabelCallback(master, label, format=format, f=lambda x: 10**x))
+    else:
+        master.connect(slider, SIGNAL("valueChanged(double)"), ValueCallback(master, value))
+        master.connect(slider, SIGNAL("valueChanged(double)"), SetLabelCallback(master, label, format=format))
+    if callback:
+        master.connect(slider, SIGNAL("valueChanged(double)"), FunctionCallback(master, callback))
+
+def comboBox(widget, master, value, label=None, items=None, tooltip=None, callback=None):
+    box = QHGroupBox(label, widget)
+    if tooltip: QToolTip.add(box, tooltip)
+    combo = QComboBox(box)
+    for i in items:
+        combo.insertItem(i)
+    combo.setCurrentItem(getattr(master, value))
+    master.connect(combo, SIGNAL("activated(int)"), ValueCallback(master, value))
+    if callback:
+        master.connect(combo, SIGNAL("activated(int)"), FunctionCallback(master, callback))
+
+
 ##############################################################################
 
 class ValueCallback:
@@ -131,10 +193,21 @@ class ValueCallback:
         self.widget = widget
         self.attribute = attribute
         self.f = f
-        print "widget", widget
         widget.callbackDeposit.append(self)
     def __call__(self, value):
         setattr(self.widget, self.attribute, self.f and self.f(value) or value)
+
+class SetLabelCallback:
+    def __init__(self, widget, label, format = "%5.2f", f = None):
+        self.widget = widget
+        self.label = label
+        self.format = format
+        self.f = f
+        widget.callbackDeposit.append(self)
+    def __call__(self, value):
+        if self.f:
+            value = self.f(value)
+        self.label.setText(self.format % value)
 
 class FunctionCallback:
     def __init__(self, master, f, widget=None, id=None, getwidget=None):
