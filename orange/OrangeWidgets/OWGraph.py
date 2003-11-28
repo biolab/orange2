739bfc0bd19b490133c45cdb528c02913f788c3c
Gregor <Gregor@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2003-11-28 17:01:56 +0000

    - moved saving images to OWDlgs

--- orange/OrangeWidgets/OWGraph.py
+++ orange/OrangeWidgets/OWGraph.py
@@ -11,8 +11,8 @@ from OWTools import *
 from qwt import *
 from Numeric import *
 from OWBaseWidget import *
-import popen2
 import time
+from OWDlgs import OWChooseImageSizeDlg
 
 class subBarQwtPlotCurve(QwtPlotCurve):
     def __init__(self, parent = None, text = None):
@@ -136,94 +136,6 @@ class HiddenScaleDraw(QwtScaleDraw):
     def label(self, value):
         return QString.null
 
-class OWChooseImageSizeDlg(OWBaseWidget):
-    settingsList = ["selectedSize", "customX", "customY", "saveAllSizes"]
-    def __init__(self, graph, *args):
-        OWBaseWidget.__init__(self, None, "Image size", "Set size of output image", TRUE, FALSE, FALSE, modal = TRUE)
-
-        self.graph = graph
-        self.selectedSize = 0
-        self.customX = 400
-        self.customY = 400
-        self.saveAllSizes = 0
-
-        self.loadSettings()
-        
-        self.space=QVBox(self)
-        self.grid=QGridLayout(self)
-        self.grid.addWidget(self.space,0,0)
-        self.group = QVGroupBox("Image size", self.space)
-        self.imageSize = QButtonGroup(5, Qt.Vertical, self.group)
-        self.imageSize.setFrameStyle(QFrame.NoFrame)
-
-        self.sizeOriginal = QRadioButton('Original size', self.imageSize)
-        self.size400 = QRadioButton('400 x 400', self.imageSize)
-        self.size600 = QRadioButton('600 x 600', self.imageSize)
-        self.size800 = QRadioButton('800 x 800', self.imageSize)
-        self.custom  = QRadioButton('Custom:', self.imageSize)
-        self.boxX = QHBox(self.group)
-        self.boxY = QHBox(self.group)
-        self.customWidth = QLabel('Width:', self.boxX)
-        self.xSize = QLineEdit(self.boxX)
-        self.customHeight = QLabel('Height:', self.boxY)
-        self.ySize = QLineEdit(self.boxY)
-        self.sizeOriginal.setChecked(1)
-        self.allSizes = QCheckBox("Save all sizes", self.group)
-        
-        self.printButton = QPushButton("Print", self.space)
-        self.okButton = QPushButton("OK", self.space)
-        self.cancelButton = QPushButton("Cancel", self.space)
-        self.connect(self.printButton, SIGNAL("clicked()"), self.printPic)
-        self.connect(self.okButton, SIGNAL("clicked()"), self.accept)
-        self.connect(self.cancelButton, SIGNAL("clicked()"), self.reject)
-                
-        if self.saveAllSizes == 1: self.allSizes.setChecked(1)
-
-        if self.selectedSize == 0: self.sizeOriginal.setChecked(1)
-        elif self.selectedSize == 1: self.size400.setChecked(1)
-        elif self.selectedSize == 2: self.size600.setChecked(1)
-        elif self.selectedSize == 3: self.size600.setChecked(1)
-        self.xSize.setText(str(self.customX))
-        self.ySize.setText(str(self.customY))
-
-    def accept(self):
-        if self.sizeOriginal.isChecked(): self.selectedSize = 0
-        elif self.size400.isChecked(): self.selectedSize = 1
-        elif self.size600.isChecked(): self.selectedSize = 2
-        elif self.size800.isChecked(): self.selectedSize = 3
-        self.customX = int(str(self.xSize.text()))
-        self.customY = int(str(self.ySize.text()))
-        self.saveAllSizes = self.allSizes.isChecked()
-        self.saveSettings()
-        QDialog.accept(self)
-
-
-    def printPic(self):
-        printer = QPrinter()
-
-        if self.sizeOriginal.isChecked(): size = self.size()
-        elif self.size400.isChecked(): size = QSize(400,400)
-        elif self.size600.isChecked(): size = QSize(600,600)
-        elif self.size800.isChecked(): size = QSize(800,800)
-        elif self.custom.isChecked():  size = QSize(int(str(self.xSize.text())), int(str(self.ySize.text())))
-        buffer = QPixmap(size)
-
-        if printer.setup():
-            painter = QPainter(printer)
-            metrics = QPaintDeviceMetrics(printer)
-            height = metrics.height() - 2*printer.margins().height()
-            width = metrics.width() - 2*printer.margins().width()
-            pageKvoc = width / float(height)
-            sizeKvoc = size.width() / float(size.height())
-            if pageKvoc < sizeKvoc:
-                rect = QRect(printer.margins().width(),printer.margins().height(), width, height*pageKvoc/sizeKvoc)
-            else:
-                rect = QRect(printer.margins().width(),printer.margins().height(), width*sizeKvoc/pageKvoc, height)
-            self.graph.printPlot(painter, rect)
-            painter.end()
-            
-
-
 
 class OWGraph(QwtPlot):
     def __init__(self, parent = None, name = None):
@@ -275,60 +187,13 @@ class OWGraph(QwtPlot):
         self.__dict__.update(settings)
 
     def saveToFile(self):
-        sizeDlg = OWChooseImageSizeDlg(self, self, "", TRUE)
+        sizeDlg = OWChooseImageSizeDlg(self)
         sizeDlg.exec_loop()
-        if sizeDlg.result() != QDialog.Accepted: return
 
-        if sizeDlg.sizeOriginal.isChecked(): size = self.size()
-        elif sizeDlg.size400.isChecked(): size = QSize(400,400)
-        elif sizeDlg.size600.isChecked(): size = QSize(600,600)
-        elif sizeDlg.size800.isChecked(): size = QSize(800,800)
-        elif sizeDlg.custom.isChecked():  size = QSize(int(str(sizeDlg.xSize.text())), int(str(sizeDlg.ySize.text())))
-        else:
-            print "error"
-            return
-
-        qfileName = QFileDialog.getSaveFileName(self.lastSaveDirName + "graph.png","Portable Network Graphics (*.PNG);;Windows Bitmap (*.BMP);;Graphics Interchange Format (*.GIF)", None, "Save to..", "Save to..")
-        fileName = str(qfileName)
-        if fileName == "": return
-        (fil,ext) = os.path.splitext(fileName)
-        ext = ext.replace(".","")
-        if ext == "":	
-        	ext = "PNG"  	# if no format was specified, we choose png
-        	fileName = fileName + ".png"
-        ext = ext.upper()
-
-
-        dirName, shortFileName = os.path.split(fileName)
-        self.lastSaveDirName = dirName + "/"
-
-        if not sizeDlg.allSizes.isChecked():
-            self.saveToFileDirect(fileName, ext, size)
-        else:
-            if not os.path.isdir(dirName + "\\400\\"): os.mkdir(dirName + "\\400\\")
-            if not os.path.isdir(dirName + "\\600\\"): os.mkdir(dirName + "\\600\\")
-            if not os.path.isdir(dirName + "\\800\\"): os.mkdir(dirName + "\\800\\")
-            if not os.path.isdir(dirName + "\\Original\\"): os.mkdir(dirName + "\\Original\\")
-            self.saveToFileDirect(dirName + "\\400\\" + shortFileName, ext, QSize(400,400))
-            self.saveToFileDirect(dirName + "\\600\\" + shortFileName, ext, QSize(600,600))
-            self.saveToFileDirect(dirName + "\\800\\" + shortFileName, ext, QSize(800,800))
-            self.saveToFileDirect(dirName + "\\Original\\" + shortFileName, ext, self.size())
-        
     def saveToFileDirect(self, fileName, ext, size = QSize()):
-        if os.path.exists(fileName):
-            res = QMessageBox.information(self,'Save picture','File already exists. Overwrite?','Yes','No', QString.null,0,1)
-            if res == 1: return
-
-        #print fileName
-        if size.isEmpty(): buffer = QPixmap(self.size()) # any size can do, now using the window size
-        else:              buffer = QPixmap(size)
-        #buffer = QPixmap(size)
-        painter = QPainter(buffer)
-        painter.fillRect(buffer.rect(), QBrush(Qt.white)) # make background same color as the widget's background
-        self.printPlot(painter, buffer.rect())
-        painter.end()
-        buffer.save(fileName, ext)
-
+        sizeDlg = OWChooseImageSizeDlg(self)
+        sizeDlg.saveToFileDirect(fileName, ext, size)
+        
     def setYLlabels(self, labels):
         "Sets the Y-axis labels on the left."
         if (labels <> None):
@@ -522,74 +387,13 @@ class OWGraph(QwtPlot):
 
     def setCanvasColor(self, c):
         self.setCanvasBackground(c)
-        self.repaint()
+        self.repaint()   
 
-
-    def gracePlot(self, pause=5):
-        """Clone the plot into Grace for very high quality hard copy output.
-
-        Know bug: Grace does not scale the data correctly when Grace cannot
-        cannot keep up with gracePlot.  This happens when it takes too long
-        to load Grace in memory (exit the Grace process and try again) or
-        when 'pause' is too short.
-        """
-        g = GracePlotter(debug = 0)
-        g('subtitle "%s"' % self.title())
-
-        # curves
-        keys = self.curveKeys()
-        for key in keys:
-            index = keys.index(key)
-            curve = self.curve(key)
-            if not curve.enabled():
-                continue
-            g('s%s legend "%s"' % (index, curve.title()))
-            if curve.symbol().style():
-                g('s%s symbol 1;'
-                  's%s symbol size 0.4;'
-                  's%s symbol fill pattern 1'
-                  % (index, index, index))
-            if curve.style():
-                g('s%s line linestyle 1' % index)
-            else:
-                g('s%s line linestyle 0' % index)
-            for i in range(curve.dataSize()):
-                g('%s.s%s point %g, %g'
-                  % ("g0", index, curve.x(i), curve.y(i)))
-
-        # finalize
-        g('redraw')
-        
-    # gracePlot()
-
-
-class GracePlotter:
-    def __init__ (self, sleep = 5, debug = None):
-        self.debug = debug
-        r, self.p, e = popen2.popen3 ("grace -nosafe -noask -dpipe 0") 
-        #self.command("view xmin 0.15")
-        #self.command("view xmax 0.85")
-        #self.command("view ymin 0.15")
-        #self.command("view ymax 0.85")
-        #self.flush()
-        time.sleep(sleep) 
-
-    def command(self, cmd):
-        if self.debug: print cmd
-        self.p.write(cmd + '\n')
-        self.flush()
-
-    def flush(self):
-        self.p.flush()
-
-    def __call__(self, cmd):
-        self.command(cmd)
- 
 
 if __name__== "__main__":
     #Draw a simple graph
     a = QApplication(sys.argv)
-    c = OWChooseImageSizeDlg()
+    c = OWChooseImageSizeDlg(None)
     """
     c = OWGraph()
     c.setXlabels(['red','green','blue','light blue', 'dark blue', 'yellow', 'orange', 'magenta'])
