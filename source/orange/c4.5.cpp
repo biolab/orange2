65f373de8b8eeb6fcd52a6eef3bb02ce12df3cb6
janezd <janez.demsar@fri.uni-lj.si>
2004-01-21 15:52:12 +0000

    - c4.5 now supported on MacOSx (again)

--- source/orange/c4.5.cpp
+++ source/orange/c4.5.cpp
@@ -67,6 +67,116 @@ void *pc45data;
 
 extern PyObject *orangeModule;
 
+#if defined _MSC_VER
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+
+const char *dynloadC45(char buf[], char *bp)
+{
+  #ifdef _DEBUG
+  strcpy(bp, "\\c45_d.dll");
+  #else
+  strcpy(bp, "\\c45.dll");
+  #endif
+
+  HINSTANCE c45Dll = LoadLibrary(buf);
+  if (!c45Dll)
+    return "cannot load c45.dll";
+
+//  char funcname[258];
+  
+  //snprintf(funcname, sizeof(funcname), "%s", "c45Data");
+  pc45data = GetProcAddress(c45Dll, "c45Data");
+
+  //snprintf(funcname, sizeof(funcname), "%s", "learn");
+  c45learn = (learnFunc *)(GetProcAddress(c45Dll, "learn"));
+
+  //snprintf(funcname, sizeof(funcname), "%s", "guarded_collect");
+  c45garbage = (garbageFunc *)(GetProcAddress(c45Dll, "guarded_collect"));
+ 
+  if (!pc45data || !c45learn || !c45garbage)
+    return "c45.dll is invalid";
+
+  return NULL;
+}
+
+#elif defined LINUX
+
+#include <dlfcn.h>
+
+const char *dynloadC45(char buf[], char *bp)
+{ 
+  #ifdef _DEBUG
+  strcpy(bp, "/c45_d.so");
+  #else
+  strcpy(bp, "/c45.so");
+  #endif
+
+  void *handle = dlopen(buf, 0 /*dlopenflags*/);
+  if (handle == NULL)
+    return dlerror();
+  
+//  getDataFunc *p = (getDataFunc *) dlsym(handle, "_Z10getc45Datav");
+  pc45data = dlsym(handle, "c45Data");
+  c45learn = (learnFunc *) dlsym(handle, "learn");
+  c45garbage = (garbageFunc *) dlsym(handle, "guarded_collect");
+  
+  if (!pc45data || !c45learn || !c45garbage)
+    return "c45.so is invalid (required functions are not found)";
+
+  return NULL;
+}
+
+#elif defined DARWIN
+
+#include <mach-o/dyld.h>
+#include <string.h>
+
+const char *dynloadC45(char pathname[], char *bp)
+{
+  NSObjectFileImageReturnCode rc;
+  NSObjectFileImage image;
+  NSModule newModule;
+  NSSymbol theSym;
+  
+  #ifdef _DEBUG
+  strcpy(bp, "/c45_d.so");
+  #else
+  strcpy(bp, "/c45.so");
+  #endif
+  
+    rc = NSCreateObjectFileImageFromFile(pathname, &image);
+    if (rc != NSObjectFileImageSuccess)
+      return "Cannot load c45.so";
+      
+    newModule = NSLinkModule(image, pathname, NSLINKMODULE_OPTION_BINDNOW | \
+                                              NSLINKMODULE_OPTION_RETURN_ON_ERROR | \
+                                              NSLINKMODULE_OPTION_PRIVATE); 
+    if (!newModule)
+      return "Cannot link module c45.so";
+      
+  theSym = NSLookupSymbolInModule(newModule, "_c45Data");
+  pc45data = theSym ? NSAddressOfSymbol(theSym) : NULL;
+  
+  theSym = NSLookupSymbolInModule(newModule, "_learn");
+  c45learn = theSym ? NSAddressOfSymbol(theSym) : NULL;
+
+  theSym = NSLookupSymbolInModule(newModule, "_guarded_collect");
+  c45garbage = theSym ? NSAddressOfSymbol(theSym) : NULL;
+
+  if (!pc45data || !c45learn || !c45garbage)
+    return "invalid c45.so (cannot find the necessary symbols)";
+
+  return NULL;
+ }
+    
+#else
+
+const char *dynloadC45()
+{ return "C45Loader", "c45 is not supported on this platform"; }
+
+#endif
 
 #ifdef IGNORE
 #undef IGNORE
