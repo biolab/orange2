26c9eceb5eeec65d717ff77a2824d1efab81ed99
martinm <martinm@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2004-02-14 11:19:09 +0000

    - included presentation of Bayes

--- orange/OrangeWidgets/OWLin_Results.py
+++ orange/OrangeWidgets/OWLin_Results.py
@@ -18,7 +18,7 @@
 from qttable import *
 from OData import *
 from OWWidget import *
-import math
+import math, orngLR
 
 class OWLin_Results(OWWidget):
     def __init__(self,parent=None):
@@ -38,8 +38,7 @@ shows regression coeficients of attributes estimated in a regression learner
         self.data=None
 
         #list inputs and outputs
-        #self.addInput("classifier")
-        self.inputs = [("Classifier", orange.Classifier, self.classifier, 0)]
+        self.inputs=[("Classifier", orange.Classifier, self.classifier, 1), ("Examples", ExampleTable, self.cdata, 1)]
         
         #GUI
         
@@ -68,7 +67,6 @@ shows regression coeficients of attributes estimated in a regression learner
 
     def sort(self,col):
         "Sort the column col"
-#        print col
         if col==self.sortby:
             samecolumn=TRUE
         else:
@@ -85,7 +83,6 @@ shows regression coeficients of attributes estimated in a regression learner
                     x=float(str(self.table.text(i,col)))
                     for j in range(i+1,self.table.numRows()):
                         y=float(str(self.table.text(j,col)))
-#                        print x,y, cmp(x,y)
                         if x>y:
 #                            print "swap!"
                             self.swapRows(i,j)
@@ -113,25 +110,90 @@ shows regression coeficients of attributes estimated in a regression learner
             self.classifier=classifier
         self.showResults()
 
+    def cdata(self,data):
+        self.data = data
+        if data == None and self.classifier and isinstance(self.classifier, orange.BayesClassifier): 
+            self.table.setNumRows(0)
+        self.showResults()
+
     def showResults(self):
+        # thisd subroutine computes standard error of estimated beta /for the time being use it only with discrete data
+        def err(e, priorError, key, data):
+            inf = 0.0
+            sume = e[0]+e[1]
+            for d in data:
+                if d[at]==key:
+                    inf += (e[0]*e[1]/sume/sume)
+            inf = max(inf, 0.00000001)
+            var = 1/inf - priorError*priorError
+            return (math.sqrt(var))
+
         if self.classifier == None:
             return
 
-        self.table.setNumRows(len(self.classifier.domain.attributes)+1)
-                              
-        self.table.setText(0,0,"Constant")        
-        self.table.setText(0,1,str(round(self.classifier.beta[0],2)))        
-        self.table.setText(0,2,str(round(self.classifier.beta_se[0],2)))        
-        self.table.setText(0,3,str(round(self.classifier.wald_Z[0],2)))        
-        self.table.setText(0,4,str(abs(round(self.classifier.P[0],2))))
-                              
-        for i in range(len(self.classifier.domain.attributes)):
-            self.table.setText(i+1,0,str(self.classifier.domain.attributes[i].name))        
-            self.table.setText(i+1,1,str(round(self.classifier.beta[i+1],2)))        
-            self.table.setText(i+1,2,str(round(self.classifier.beta_se[i+1],2)))        
-            self.table.setText(i+1,3,str(round(self.classifier.wald_Z[i+1],2)))        
-            self.table.setText(i+1,4,str(abs(round(self.classifier.P[i+1],2))))
-            self.table.setText(i+1,5,str(round(math.exp(self.classifier.beta[i+1]),2)))
+        if isinstance(self.classifier, orange.LogRegClassifier):
+            self.table.setNumRows(len(self.classifier.domain.attributes)+1)
+            self.table.setText(0,0,"Constant")        
+            self.table.setText(0,1,str(round(self.classifier.beta[0],2)))        
+            self.table.setText(0,2,str(round(self.classifier.beta_se[0],2)))        
+            self.table.setText(0,3,str(round(self.classifier.wald_Z[0],2)))        
+            self.table.setText(0,4,str(abs(round(self.classifier.P[0],2))))
+                                  
+            for i in range(len(self.classifier.domain.attributes)):
+                self.table.setText(i+1,0,str(self.classifier.domain.attributes[i].name))        
+                self.table.setText(i+1,1,str(round(self.classifier.beta[i+1],2)))        
+                self.table.setText(i+1,2,str(round(self.classifier.beta_se[i+1],2)))        
+                self.table.setText(i+1,3,str(round(self.classifier.wald_Z[i+1],2)))        
+                self.table.setText(i+1,4,str(abs(round(self.classifier.P[i+1],2))))
+                self.table.setText(i+1,5,str(round(math.exp(self.classifier.beta[i+1]),2)))
+        elif isinstance(self.classifier, orange.BayesClassifier) and self.data and self.data.domain == self.classifier.domain:
+            numrows = 1
+            for at in self.classifier.domain.attributes:
+                if at.varType == orange.VarTypes.Discrete:
+                    numrows += len(at.values)
+                else:
+                    numrows +=1
+            cl = self.classifier
+            classVal = self.classifier.domain.classVar.values
+            
+            self.table.setNumRows(numrows)
+            
+            dist1 = max(0.00000001, cl.distribution[classVal[1]])
+            dist0 = max(0.00000001, cl.distribution[classVal[0]])
+            prior = dist0/dist1
+            logprior = math.log(prior)
+            sumd = dist1+dist0
+            priorError = math.sqrt(1/((dist1*dist0/sumd/sumd)*len(self.data)))
+            
+
+            self.table.setText(0,0,"Constant")        
+            self.table.setText(0,1,str(round(logprior,2)))        
+            self.table.setText(0,2,str(round(priorError,2)))        
+            self.table.setText(0,3,str(round(logprior/priorError,2)))        
+            self.table.setText(0,4,str(abs(round(orngLR.lchisqprob(math.pow(logprior/priorError,2),1),2))))
+
+            row = 1                                  
+            for at in self.classifier.domain.attributes:
+                if at.varType == orange.VarTypes.Discrete:
+                    for cd in cl.conditionalDistributions[at].keys():
+                        self.table.setText(row,0,str(at.name+"="+cd))
+                        conditional0 = max(cl.conditionalDistributions[at][cd][classVal[0]], 0.00000001)
+                        conditional1 = max(cl.conditionalDistributions[at][cd][classVal[1]], 0.00000001)
+                        beta = math.log(conditional0/conditional1/prior)
+                        se = err(cl.conditionalDistributions[at][cd], priorError, cd, self.data) # standar error of beta 
+                        
+                        self.table.setText(row,1,str(round(beta,2)))        
+                        self.table.setText(row,2,str(round(se,2)))        
+                        self.table.setText(row,3,str(round(beta/se,2)))        
+                        self.table.setText(row,4,str(abs(round(orngLR.lchisqprob(math.pow(beta/se,2),1),2))))
+                        self.table.setText(row,5,str(round(math.exp(beta),2)))
+                        row += 1
+                else:
+                    self.table.setText(row,0,str(at.name))
+                    self.table.setText(row, 1, str("Not discrete attribute!"))
+                    row+=1
+        else:
+            return
 
         self.table.adjustColumn(0)        
 
