af69b4a09b9e0ccf141793a310d79acde3852d30
tomazc <tomazc@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2004-05-07 11:57:13 +0000

    no message

--- install-scripts/macinstallation.sh
+++ install-scripts/macinstallation.sh
@@ -14,8 +14,17 @@ mv *.so compiledOrange
 ## check out orange source and modules
 rm -Rf orange doc
 cvs -d :pserver:tomazc@estelle.fri.uni-lj.si:/d//cvs export -r HEAD orange
-mv orange/doc .
+mv orange/doc Orange\ Doc
 
+# remove files we don't want in the installation
+rm orange/OrangeWidgets/Visualize/OWLinViz.py
+rm orange/OrangeWidgets/Visualize/OWLinVizGraph.py
+rm orange/OrangeWidgets/Classify/OWCalibratedClassifier.py
+rm orange/OrangeWidgets/Data/OWExampleBuilder.py
+rm orange/OrangeWidgets/Data/OWSubsetGenerator.py
+rm orange/OrangeWidgets/OWLin_Results.py
+rm orange/OrangeWidgets/Other/OWITree.py
+rm -Rf orange/OrangeWidgets/Genomics
 
 ## after compiling orange, move it out of the path and into the orange directory
 cp compiledOrange/*.so orange
@@ -24,14 +33,14 @@ rm -Rf build
 python makeapplication.py --resource=orange build
 cp emptyDiskImages/* ~/Desktop
 open build
-open ~/Desktop/orange.dmg
+open ~/Desktop/Orange.dmg
 echo "drag orange application into orange mount drive"
 echo "resize and convert with the Disk Copy utility"
 open /Applications/Utilities/Disk\ Copy.app
 cat
 
 open . 
-open ~/Desktop/orangeDoc.dmg
+open ~/Desktop/Orange\ Doc.dmg
 echo "drag documentation into orange documentation mount drive"
 echo "resize and convert with the Disk Copy utility"
 open /Applications/Utilities/Disk\ Copy.app
--- install-scripts/makeapplication.py
+++ install-scripts/makeapplication.py
@@ -3,7 +3,7 @@ import bundlebuilder
 ##bundlebuilder.USE_ZIPIMPORT=None
 
 bundlebuilder.buildapp(
-    name='canvas.app',     # what to build                                 
+    name='Orange.app',     # what to build                                 
     mainprogram='orange/OrangeCanvas/orngCanvas.py',      # your app's main()                             
     argv_emulation=0,           # drag&dropped filenames show up in sys.argv    
     iconfile='orange.icns',      # file containing your app's icons              
@@ -11,7 +11,7 @@ bundlebuilder.buildapp(
     includeModules=['sip', 'ConfigParser', 'cPickle', 'cStringIO', 'LinearAlgebra', 'operator', 'math', 'whrandom', 'qtcanvas', 'qttable', 'Numeric', 'qwt', 'xml', 'xml.dom', 'xml.dom.minidom'],          # list of additional Modules to force in        
     includePackages=[],         # list of additional Packages to force in
     additionalPaths=['orange', 'orange/OrangeCanvas', 'orange/OrangeWidgets'],
-    libs=['/Developer/qt.old/lib/libqt.3.dylib'],                             # list of shared libs or Frameworks to include       
+    libs=['/Developer/qt/lib/libqt.3.dylib'],                             # list of shared libs or Frameworks to include       
 )
 
 ### end of makeapplication.py
