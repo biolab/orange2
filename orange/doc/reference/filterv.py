d86bcf5ddb953c77019055e0c5e8502c2ba47831
janezd <janez.demsar@fri.uni-lj.si>
2004-07-19 13:00:30 +0000

    *** empty log message ***

--- orange/doc/reference/filterv.py
+++ orange/doc/reference/filterv.py
@@ -2,6 +2,8 @@ import orange
 
 data = orange.ExampleTable("lenses")
 
+############ THIS IS WHAT YOU CAN DO WITH DISCRETE ATTRIBUTES
+
 print "\nYoung or presbyopic with astigmatism"
 fya = orange.Filter_values(domain = data.domain)
 fya["age"] = "young"
@@ -40,4 +42,116 @@ fcon = orange.Filter_disjunction([fya, fr])
 print "\n\nYoung and presbyopic asticmatic examples and examples that have reduced tear rate\n"
 for ex in fcon(data):
     print ex
+
+
+############ THIS IS WHAT YOU CAN DO WITH CONTINUOUS ATTRIBUTES
+
+data = orange.ExampleTable("iris")
+
+fcont = orange.Filter_values(domain = data.domain)
+fcont[0] = (orange.ValueFilter.Equal, 4.59999999999999) # This is to check that rounding errors don't hurt
+print "\n\nThe first attribute equals 4.6"
+for ex in fcont(data):
+    print ex
+
+fcont[0] = (orange.ValueFilter.Less, 4.6)
+print "\n\nThe first attribute is less than 4.6"
+for ex in fcont(data):
+    print ex
+
+fcont[0] = (orange.ValueFilter.LessEqual, 4.6)
+print "\n\nThe first attribute is less than or equal to 4.6"
+for ex in fcont(data):
+    print ex
+
+fcont[0] = (orange.ValueFilter.Greater, 7.6)
+print "\n\nThe first attribute is greater than 7.6"
+for ex in fcont(data):
+    print ex
+
+fcont[0] = (orange.ValueFilter.GreaterEqual, 7.6)
+print "\n\nThe first attribute is greater than or equal to 7.6"
+for ex in fcont(data):
+    print ex
+
+fcont[0] = (orange.ValueFilter.Between, 4.6, 5.0)
+print "\n\nThe first attribute is between to 4.5 and 5.0"
+for ex in fcont(data):
+    print ex
+
+fcont[0] = (orange.ValueFilter.Outside, 4.6, 7.5)
+print "\n\nThe first attribute is between to 4.5 and 5.0"
+for ex in fcont(data):
+    print ex
+
+
+############ THIS IS WHAT YOU CAN DO WITH STRING ATTRIBUTES
+
+data.domain.addmeta(orange.newmetaid(), orange.StringVariable("name"))
+for ex in data:
+    ex["name"] = str(ex.getclass())
+
+fstr = orange.Filter_values(domain = data.domain)
+fstr["name"] = "Iris-setosa"
+print "\n\nSetosae"
+d = fstr(data)
+print "%i examples, starting with %s" % (len(d), d[0])
+
+fstr["name"] = ["Iris-setosa", "Iris-virginica"]
+print "\n\nSetosae and virginicae"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.Less, "Iris-versicolor")
+print "\n\nLess than versicolor"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.LessEqual, "Iris-versicolor")
+print "\n\nLess or equal versicolor"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.Greater, "Iris-versicolor")
+print "\n\nGreater than versicolor"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.GreaterEqual, "Iris-versicolor")
+print "\n\nGreater or equal versicolor"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.Between, "Iris-versicolor", "Iris-virginica")
+print "\n\nGreater or equal versicolor"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.Contains, "ers")
+print "\n\nContains 'ers'"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.NotContains, "ers")
+print "\n\nDoesn't contains 'ers'"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.BeginsWith, "Iris-ve")
+print "\n\nBegins with 'Iris-ve'"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.EndsWith, "olor")
+print "\n\nBegins with 'Iris-ve'"
+d = fstr(data)
+print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
+
+fstr["name"] = (orange.Filter_values.EndsWith, "a"*50)
+print "\n\nBegins with '%s'" % ("a"*50)
+d = fstr(data)
+if not len(d):
+    print "<empty table>"
+else:
+    print "%i examples, starting with %s\n  finishing with %s" % (len(d), d[0], d[-1])
 
