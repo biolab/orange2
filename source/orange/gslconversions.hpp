97d55cda235501b71f0943c21c0553571e057fec
janezd <janez.demsar@fri.uni-lj.si>
2004-10-15 13:52:36 +0000

    - filestr2val and val2filestr now also pass the Example under construction as an argument

--- source/orange/gslconversions.hpp
+++ source/orange/gslconversions.hpp
@@ -2,8 +2,6 @@
 #define __GSLCONVERSIONS_HPP
 
 
-#ifndef NO_GSL
-
 #define GSL_FREE(O,tpe) { if (O) { gsl_##tpe##_free(O); O = NULL; }}
 
 #include "gsl/gsl_matrix.h"
@@ -14,9 +12,6 @@
 WRAPPER(ExampleGenerator)
 void exampleGenerator2gsl(PExampleGenerator egen, const int &weightID, const char *contents, const int &multiTreatment,
                           gsl_matrix *&X, gsl_vector *&y, gsl_vector *&w, int &rows, int &columns);
-
-#endif // NO_GSL
-
 
 void parseMatrixContents(PExampleGenerator egen, const int &weightID, const char *contents, const int &multiTreatment,
                          bool &hasClass, bool &classVector, bool &weightVector, bool &classIsDiscrete, int &columns,
