8125900a3c36d3be4fed898b369092e6f36dd53d
tomazc <tomazc@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2004-10-25 15:58:46 +0000

    including numarray and MA for HeatMap and Epistasis Analysis

--- install-scripts/mac/makeapplication.py
+++ install-scripts/mac/makeapplication.py
@@ -8,8 +8,8 @@ bundlebuilder.buildapp(
     argv_emulation=0,           # drag&dropped filenames show up in sys.argv    
     iconfile='orange.icns',      # file containing your app's icons              
     standalone=1,               # make this app self contained.                 
-    includeModules=['sip', 'ConfigParser', 'cPickle', 'cStringIO', 'LinearAlgebra', 'operator', 'math', 'whrandom', 'qtcanvas', 'qttable', 'Numeric', 'qwt', 'xml', 'xml.dom', 'xml.dom.minidom'],          # list of additional Modules to force in        
-    includePackages=[],         # list of additional Packages to force in
+    includeModules=['sip', 'ConfigParser', 'cPickle', 'cStringIO', 'LinearAlgebra', 'operator', 'math', 'whrandom', 'qtcanvas', 'qttable', 'Numeric', 'MA', 'numarray', 'qwt', 'xml', 'xml.dom', 'xml.dom.minidom'],          # list of additional Modules to force in
+    includePackages=['numarray', 'Numeric', 'MA'],         # list of additional Packages to force in
     additionalPaths=['orange', 'orange/OrangeCanvas', 'orange/OrangeWidgets'],
     libs=['/Developer/qt/lib/libqt.3.dylib'],                             # list of shared libs or Frameworks to include       
 )
