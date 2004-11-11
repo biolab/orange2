8640e9e84ce9b42262e4e0ce44d9cfb6df4d7808
janezd <janez.demsar@fri.uni-lj.si>
2004-11-11 09:36:48 +0000

    *** empty log message ***

---
+++ source/orange/wml/WmlDelaunay2.h
@@ -0,0 +1,156 @@
+// Magic Software, Inc.
+// http://www.magic-software.com
+// http://www.wild-magic.com
+// Copyright (c) 2004.  All Rights Reserved
+//
+// The Wild Magic Library (WML) source code is supplied under the terms of
+// the license agreement http://www.magic-software.com/License/WildMagic.pdf
+// and may not be copied or disclosed except in accordance with the terms of
+// that agreement.
+
+#ifndef WMLDELAUNAY2_H
+#define WMLDELAUNAY2_H
+
+// The Delaunay triangulation method is a modification of code written by 
+// Dave Watson.  It uses an algorithm described in
+//
+//     Watson, D.F., 1981, Computing the n-dimensional Delaunay 
+//     tessellation with application to Voronoi polytopes: 
+//     The Computer J., 24(2), p. 167-172.
+
+#include "WmlVector2.h"
+
+namespace Wml
+{
+
+template <class Real>
+class WML_ITEM Delaunay2
+{
+public:
+    // Construction and destruction.  In the first constructor,
+    // Delaunay2 accepts ownership of the input array and will delete
+    // it during destruction.  The second constructor is designed to allow
+    // sharing of the network (note that the reference argument is not passed
+    // as 'const').  Any other network that shares this object will not delete
+    // the data in this object.
+    Delaunay2 (int iVertexQuantity, Vector2<Real>* akVertex);
+    Delaunay2 (Delaunay2& rkNet);
+    virtual ~Delaunay2 ();
+    bool IsOwner () const;
+
+    // vertices
+    int GetVertexQuantity () const;
+    const Vector2<Real>& GetVertex (int i) const;
+    const Vector2<Real>* GetVertices () const;
+    Real GetXMin () const;
+    Real GetXMax () const;
+    Real GetXRange () const;
+    Real GetYMin () const;
+    Real GetYMax () const;
+    Real GetYRange () const;
+
+	void ComputeClassBoundaries();
+
+
+    // edges
+    class WML_ITEM Edge
+    {
+    public:
+        // vertices forming edge
+        int m_aiVertex[2];
+
+        // triangles sharing edge
+        int m_aiTriangle[2];
+    };
+
+    int GetEdgeQuantity () const;
+    const Edge& GetEdge (int i) const;
+    const Edge* GetEdges () const;
+
+	const Edge* 
+
+
+    // triangles
+    class WML_ITEM Triangle
+    {
+    public:
+        // vertices, listed in counterclockwise order
+        int m_aiVertex[3];
+
+        // adjacent triangles,
+        //   adj[0] points to triangle sharing edge (ver[0],ver[1])
+        //   adj[1] points to triangle sharing edge (ver[1],ver[2])
+        //   adj[2] points to triangle sharing edge (ver[2],ver[0])
+        int m_aiAdjacent[3];
+    };
+
+    int GetTriangleQuantity () const;
+    Triangle& GetTriangle (int i);
+    const Triangle& GetTriangle (int i) const;
+    Triangle* GetTriangles ();
+    const Triangle* GetTriangles () const;
+
+    // extra triangles (added to boundary)
+    int GetExtraTriangleQuantity () const;
+    Triangle& GetExtraTriangle (int i);
+    const Triangle& GetExtraTriangle (int i) const;
+    Triangle* GetExtraTriangles ();
+    const Triangle* GetExtraTriangles () const;
+
+    // helper functions
+    static void ComputeBarycenter (const Vector2<Real>& rkV0,
+        const Vector2<Real>& rkV1, const Vector2<Real>& rkV2,
+        const Vector2<Real>& rkP, Real afBary[3]);
+
+    static bool InTriangle (const Vector2<Real>& rkV0,
+        const Vector2<Real>& rkV1, const Vector2<Real>& rkV2,
+        const Vector2<Real>& rkTest);
+
+    static void ComputeInscribedCenter (const Vector2<Real>& rkV0,
+        const Vector2<Real>& rkV1, const Vector2<Real>& rkV2,
+        Vector2<Real>& rkCenter);
+
+    // tweaking parameters
+    static Real& Epsilon ();  // default = 0.00001
+    static Real& Range ();    // default = 10.0
+    static int& TSize ();        // default = 75
+
+protected:
+    // for sharing
+    bool m_bOwner;
+
+    // vertices
+    int m_iVertexQuantity;
+    Vector2<Real>* m_akVertex;
+    Real m_fXMin, m_fXMax, m_fXRange;
+    Real m_fYMin, m_fYMax, m_fYRange;
+
+    // edges
+    int m_iEdgeQuantity;
+    Edge* m_akEdge;
+	
+	// edges that connect points with different class values
+	int m_iBoundaryEdgeQuantity;
+	Edge* m_aiBoundaryEdge;
+	
+
+    // triangles
+    int m_iTriangleQuantity;
+    Triangle* m_akTriangle;
+
+    // extra triangles to support interpolation on convex hull of vertices
+    int m_iExtraTriangleQuantity;
+    Triangle* m_akExtraTriangle;
+
+    // tweaking parameters
+    static Real ms_fEpsilon;
+    static Real ms_fRange;
+    static int ms_iTSize;
+};
+
+typedef Delaunay2<float> Delaunay2f;
+typedef Delaunay2<double> Delaunay2d;
+
+}
+
+#endif
---
+++ source/orange/wml/WmlWinSystem.cpp
@@ -0,0 +1,48 @@
+// Magic Software, Inc.
+// http://www.magic-software.com
+// http://www.wild-magic.com
+// Copyright (c) 2004.  All Rights Reserved
+//
+// The Wild Magic Library (WML) source code is supplied under the terms of
+// the license agreement http://www.magic-software.com/License/WildMagic.pdf
+// and may not be copied or disclosed except in accordance with the terms of
+// that agreement.
+
+#include <windows.h>
+#include "WmlSystem.h"
+using namespace Wml;
+
+//----------------------------------------------------------------------------
+void System::EndianCopy (int iSize, const void* pvSrc, void* pvDst)
+{
+    memcpy(pvDst,pvSrc,iSize);
+}
+//----------------------------------------------------------------------------
+void System::EndianCopy (int iSize, int iQuantity, const void* pvSrc,
+    void* pvDst)
+{
+    memcpy(pvDst,pvSrc,iSize*iQuantity);
+}
+//----------------------------------------------------------------------------
+unsigned int System::MakeRGB (unsigned char ucR, unsigned char ucG,
+    unsigned char ucB)
+{
+    return (ucR | (ucG << 8) | (ucB << 16) | (0xFF << 24));
+}
+//----------------------------------------------------------------------------
+unsigned int System::MakeRGBA (unsigned char ucR, unsigned char ucG,
+    unsigned char ucB, unsigned char ucA)
+{
+    return (ucR | (ucG << 8) | (ucB << 16) | (ucA << 24));
+}
+//----------------------------------------------------------------------------
+double System::GetTime ()
+{
+    // 64-bit quantities
+    LARGE_INTEGER iFrequency, iCounter;
+
+    QueryPerformanceFrequency(&iFrequency);
+    QueryPerformanceCounter(&iCounter);
+    return ((double)iCounter.QuadPart)/((double)iFrequency.QuadPart);
+}
+//----------------------------------------------------------------------------
