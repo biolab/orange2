47ea4d514bc3974d330135a81a7d0860795414a0
tomazc <tomazc@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2005-01-06 10:33:01 +0000

    *** empty log message ***

--- install-scripts/mac/callmacinstallation.sh
+++ install-scripts/mac/callmacinstallation.sh
@@ -1,5 +1,5 @@
 if ! ~/install-scripts/mac/macinstallation.sh $1 $2 $3 $4 $5 &> output.log; then
-#  mail -s "Mac: ERROR compiling Orange" janez.demsar@fri.uni-lj.si < output.log
+  mail -s "Mac: ERROR compiling Orange" janez.demsar@fri.uni-lj.si < output.log
   mail -s "Mac: ERROR compiling Orange" tomaz.curk@fri.uni-lj.si < output.log
   cat output.log
   echo -e "\n\nERROR compiling, see log above"
