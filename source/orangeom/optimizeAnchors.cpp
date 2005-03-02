27108dcea5882fb117bd003cae45370ef47f528a
janezd <janez.demsar@fri.uni-lj.si>
2005-03-02 00:16:36 +0000

    - fixed some mistakes in derivation of formulae for orange.optimizeAnchors

---
+++ source/orangeom/optimizeAnchors.cpp
@@ -0,0 +1,204 @@
+#include "orange_api.hpp"
+#include "examplegen.hpp"
+
+//#include "rconversions.hpp"
+
+inline double sqr(const double &x)
+{ return x*x; }
+
+template<class T>
+inline void tomax(T &x, const T &y)
+{ if (y>x)
+    x = y;
+}
+
+typedef struct {double x, y; } TPoint;
+
+PyObject *optimizeAnchors(PyObject *, PyObject *args, PyObject *keywords) PYARGS(METH_VARARGS, "(scaledData, classes, anchors[, attractG=1.0, repelG=-1.0]) -> new-anchors")
+{
+  PyTRY
+    PyObject *scaledData;
+    PyObject *pyclasses;
+    PyObject *anchors;
+    PyObject *pyattrIndices;
+    double attractG = 1.0, repelG = -1.0;
+    int steps = 1;
+
+    if (!PyArg_ParseTuple(args, "OOOO|ddi:optimizeAnchors", &scaledData, &pyclasses, &anchors, &pyattrIndices, &attractG, &repelG, &steps))
+      return NULL;
+
+    if (!PyList_Check(scaledData) || !PyList_Check(pyclasses) || !PyList_Check(anchors))
+      PYERROR(PyExc_TypeError, "scaled data, classes and anchors should be given a lists", PYNULL);
+
+    int nAttrs = PyList_Size(anchors);
+    int nExamples = PyList_Size(scaledData);
+    int i, ii, u, v;
+    int maxcl;
+
+    TPoint *anci, *anc = (TPoint *)malloc(nAttrs * sizeof(TPoint)), *ance = anc + nAttrs;
+    TPoint *danci, *danc = (TPoint *)malloc(nAttrs * sizeof(TPoint)), *dance = danc + nAttrs;
+    TPoint *ptsi, *pts = (TPoint *)malloc(nExamples * sizeof(TPoint)), *ptse = pts + nExamples;
+    PyObject **lli, **ll = (PyObject **)malloc(nAttrs * sizeof(PyObject *));
+    int *classesi, *classes = (int *)malloc(nExamples * sizeof(int)), *classese = classes + nExamples;
+    double *Xi, *X = (double *)malloc(nExamples * nAttrs * sizeof(double));
+    double *sumi, *sum = (double *)malloc(nExamples * sizeof(double)), *sume = sum + nExamples;
+    TPoint *Ki, *K = (TPoint *)malloc(nExamples * sizeof(TPoint)), *Ke = K + nExamples;
+    int *aii, *attrIndices = (int *)malloc(nAttrs * sizeof(int)), *aie = attrIndices + nAttrs;
+
+    for(anci = anc, aii = attrIndices, lli = ll, i = 0; i < nAttrs; i++, anci++, aii++, lli++) {
+      *lli = NULL;
+      PyArg_ParseTuple(PyList_GetItem(anchors, i), "dd|O", &anci->x, &anci->y, lli);
+      *aii = PyInt_AsLong(PyList_GetItem(pyattrIndices, i));
+    }
+
+    maxcl = 0;
+    for(sumi = sum, classesi = classes, Xi = X, i = 0; i < nExamples;
+        sumi++,     classesi++,                 i++) {
+      *classesi = PyInt_AsLong(PyList_GetItem(pyclasses, i));
+      tomax(maxcl, *classesi);
+
+      PyObject *ex = PyList_GetItem(scaledData, i);
+      *sumi = 0.0;
+      for(aii = attrIndices; aii < aie; aii++) {
+        const double p = PyFloat_AsDouble(PyList_GetItem(ex, *aii));
+        *Xi++ = p;
+        *sumi += p;
+      }
+      if (*sumi == 0.0)
+        *sumi = 1.0;
+    }
+
+
+    while (steps--) {
+      for(sumi = sum, Xi = X, ptsi = pts; sumi != sume; sumi++, ptsi++) {
+        ptsi->x = ptsi->y = 0.0;
+        for(anci = anc; anci != ance; anci++, Xi++) {
+          ptsi->x += *Xi * anci->x;
+          ptsi->y += *Xi * anci->y;
+        }
+        ptsi->x /= *sumi;
+        ptsi->y /= *sumi;
+      }
+
+
+/* XXX   TO OPTIMIZE:
+       - use pointers instead of indexing
+       - sort the examples by classes so you don't have to compare them all the time
+       - if attractive force are 0, you can gain some time by checking only the examples from different classes
+*/
+                     
+      for(Ki = K; Ki != Ke; Ki++)
+        Ki->x = Ki-> y = 0.0;
+
+      for(danci = danc; danci != dance; danci++)
+        danci->x = danci->y = 0.0;
+
+      for(u = 0; u < nExamples; u++) {
+        for(v = u+1; v < nExamples; v++) {
+          const double duv = classes[u] == classes[v] ? attractG : repelG;
+          if (duv == 0.0)
+            continue;
+
+          double ruv = sqr(pts[u].x - pts[v].x) + sqr(pts[u].y - pts[v].y);
+          if (ruv < 1e-15)
+            ruv = 1e-15;
+
+          //const double druv = duv / exp(0.3 * log(ruv));
+          //const double druv = duv / sqrt(ruv);
+          const double druv = duv / ruv;
+
+          const double druvx = druv * (pts[u].x - pts[v].x);
+          K[u].x += druvx;
+          K[v].x -= druvx;
+
+          const double druvy = druv * (pts[u].y - pts[v].y);
+          K[u].y += druvy;
+          K[v].y -= druvy;
+        }
+
+        K[u].x /= sum[u];
+        K[u].y /= sum[u];
+
+        double *Xu = X + u * nAttrs;
+        for(i = 0; i < nAttrs; i++, Xu++) {
+          double ex = *Xu/sum[u];
+          danc[i].x -= K[u].x * ex;
+          danc[i].y -= K[u].y * ex;
+        }
+      }
+
+
+  // Scale the changes - normalize the jumps
+      double scaling = 1e10;
+      for(anci = anc, danci = danc; danci != dance; anci++, danci++) {
+        double maxdr = 0.1 * sqrt(sqr(anci->x) + sqr(anci->y));
+        double dr = sqrt(sqr(danci->x) + sqr(danci->y));
+        if ((maxdr > 1e-5) && (dr > 1e-5)) {
+          if (scaling * dr > maxdr)
+            scaling = maxdr / dr;
+        }
+      }
+
+      for(danci = danc; danci != dance; danci++) {
+        danci->x *= scaling;
+        danci->y *= scaling;
+      }
+
+
+  // Move anchors
+      for(anci = anc, danci = danc; danci != dance; danci++, anci++) {
+        anci->x += danci->x;
+        anci->y += danci->y;
+      }
+
+ 
+  //Centering and scaling
+      double aax = 0.0, aay = 0.0;
+      for(anci = anc; anci != ance; anci++) {
+        aax += anci->x;
+        aay += anci->y;
+      }
+
+      aax /= nAttrs;
+      aay /= nAttrs;
+
+      for(anci = anc; anci != ance; anci++) {
+        anci->x -= aax;
+        anci->y -= aay;
+      }
+
+
+      double maxr = 0.0;
+      for(anci = anc; anci != ance; anci++) {
+        const double r = sqr(anci->x) + sqr(anci->y);
+        if (r > maxr)
+          maxr = r;
+      }
+
+      if (maxr > 0) {
+        maxr = sqrt(maxr);
+        for(anci = anc; anci != ance; anci++) {
+          anci->x /= maxr;
+          anci->y /= maxr;
+        }
+      }
+    }
+
+    anchors = PyList_New(nAttrs);
+    for(i = 0, anci = anc, lli = ll;i < nAttrs; lli++, i++, anci++)
+      PyList_SetItem(anchors, i, *ll ? Py_BuildValue("ddO", anci->x, anci->y, *lli) : Py_BuildValue("dd", anci->x, anci->y));
+      
+    free(anc);
+    free(danc);
+    free(pts);
+    free(ll);
+    free(classes);
+    free(X);
+    free(sum);
+    free(K);
+    free(aii);
+
+    return anchors;
+      
+  PyCATCH;
+}
