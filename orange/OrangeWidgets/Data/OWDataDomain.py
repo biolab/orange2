19c1c8d59670665669ae3d1fbadeb833a37971a4
peterjuv <peterjuv@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2005-06-09 23:48:55 +0000

    fixed interface: flexible size class listBox shrinked to a single line fixed output ExampleWithClass: it returns None if class is removed

--- orange/OrangeWidgets/Data/OWDataDomain.py
+++ orange/OrangeWidgets/Data/OWDataDomain.py
@@ -22,9 +22,6 @@ class OWDataDomain(OWWidget):
     def __init__(self,parent = None, signalManager = None):
         OWWidget.__init__(self, parent, signalManager, "Data Domain") #initialize base class
 
-        buttonSize = QSize(40, 30)
-        upDownButtonSize = QSize(37,30)
-        
         # set member variables
         self.data = None
         self.internalSelectionUpdateFlag = 0
@@ -36,114 +33,76 @@ class OWDataDomain(OWWidget):
         self.inputs = [("InputData", ExampleTable, self.onDataInput)]
         self.outputs = [("OutputData", ExampleTable), ("OutputDataWithClass", ExampleTableWithClass)]
 
-        self.space.setMinimumSize(QSize(650,500))
-        self.hbox = QHBox(self.space)
-        self.hbox.setSpacing(10)
+        # GUI
+        self.mainArea.setFixedWidth(1)
+        ca=QFrame(self.controlArea)
+        ca.adjustSize()
+        gl=QGridLayout(ca,4,3,5)
 
-        self.vbox1 = QVBox(self.hbox)
-        self.vbox1.setSpacing(10)
-        
-        self.vframe2 = QFrame(self.hbox)
-        self.vframe2Layout = QGridLayout(self.vframe2, 9, 1, 0, 0)
-        self.vframe3 = QFrame(self.hbox)
-        self.vframe3Layout = QBoxLayout(self.vframe3,QBoxLayout.TopToBottom,10,5)
-
-        #set up leftmost column
-        self.inputAttributesNameBox = QVGroupBox(self.vbox1)
-        self.inputAttributesNameBox.setTitle('Available Attributes')
-        self.inputAttributesNameBox.setMinimumSize(QSize(200,100))
-        self.inputAttributesNameBox.setMaximumSize(QSize(100,1000))        
-        self.inputAttributesList = QListBox(self.inputAttributesNameBox,'InputAttributes')
+        # available attributes
+        boxAvail = QVGroupBox(ca)
+        boxAvail.setTitle('Available Attributes')
+        gl.addMultiCellWidget(boxAvail, 0,2,0,0)
+        self.inputAttributesList = QListBox(boxAvail,'InputAttributes')
         self.inputAttributesList.setSelectionMode(QListBox.Extended)
         self.connect(self.inputAttributesList, SIGNAL('selectionChanged()'), self.onInputAttributesSelectionChange)
         self.connect(self.inputAttributesList, SIGNAL('currentChanged(QListBoxItem*)'), self.onInputAttributesCurrentChange)
 
-
-        #set up middle column
-        self.horizontalAttributesFrame = QFrame(self.vframe2)
-        self.vframe2Layout.addMultiCellWidget(self.horizontalAttributesFrame,0,3, 0,0,0)
-        self.horizontalAttributesFrameLayout = QHBoxLayout(self.horizontalAttributesFrame, 5, 5)
-        
-        self.attributesButton = OWGUI.button(self.horizontalAttributesFrame, self, ">",self.onAttributesButtonClicked)        
-        self.attributesButton.setMaximumSize(buttonSize)
-        self.horizontalAttributesFrameLayout.addWidget(self.attributesButton, 0, Qt.AlignLeft)
-        self.attributesNameBox = QVGroupBox(self.horizontalAttributesFrame)
-        self.attributesNameBox.setTitle('Attributes')
-        self.attributesNameBox.setMinimumSize(QSize(200,200))        
-        self.horizontalAttributesFrameLayout.addWidget(self.attributesNameBox, 0, Qt.AlignLeft)        
-        self.attributesList = QListBox(self.attributesNameBox,'Attributes')
+        # attributes
+        vbAttr = QVBox(ca)
+        gl.addWidget(vbAttr, 0,1)
+        self.attributesButtonUp = OWGUI.button(vbAttr, self, "Up", self.onAttributesButtonUpClick)
+        self.attributesButton = OWGUI.button(vbAttr, self, ">",self.onAttributesButtonClicked)        
+        self.attributesButtonDown = OWGUI.button(vbAttr, self, "Down", self.onAttributesButtonDownClick)
+        boxAttr = QVGroupBox(ca)
+        boxAttr.setTitle('Attributes')
+        gl.addWidget(boxAttr, 0,2)
+        self.attributesList = QListBox(boxAttr,'Attributes')
         self.attributesList.setSelectionMode(QListBox.Extended)
         self.connect(self.attributesList, SIGNAL('selectionChanged()'), self.onAttributesSelectionChange)
-        self.connect(self.attributesList, SIGNAL('currentChanged(QListBoxItem*)'), self.onAttributesCurrentChange)                
+        self.connect(self.attributesList, SIGNAL('currentChanged(QListBoxItem*)'), self.onAttributesCurrentChange)
+        self.connect(self.attributesList, SIGNAL('doubleClicked(QListBoxItem*)'), self.onAttributesDoubleClick)
 
-        self.verticalAttributeFrame = QFrame(self.horizontalAttributesFrame)
-        self.horizontalAttributesFrameLayout.addWidget(self.verticalAttributeFrame, 0, Qt.AlignLeft)        
-        self.verticalAttributeFrameLayout = QVBoxLayout(self.verticalAttributeFrame,5,5)
-        self.attributesButtonUp = OWGUI.button(self.verticalAttributeFrame, self, "Up", self.onAttributesButtonUpClick)
-        self.attributesButtonUp.setMaximumSize(upDownButtonSize)        
-        self.verticalAttributeFrameLayout.addWidget(self.attributesButtonUp, 0, Qt.AlignTop)
-        self.attributesButtonDown = OWGUI.button(self.verticalAttributeFrame, self, "Down", self.onAttributesButtonDownClick)
-        self.attributesButtonDown.setMaximumSize(upDownButtonSize)
-        self.verticalAttributeFrameLayout.addWidget(self.attributesButtonDown, 0, Qt.AlignTop)
-        self.verticalAttributeFrameLayout.addStretch(100)
-
-        self.horizontalAttributesFrameLayout.addStretch(100)             
-        
-        self.horizontalClassFrame = QFrame(self.vframe2)
-        self.vframe2Layout.addMultiCellWidget(self.horizontalClassFrame,4,5,0,0,0)
-        self.horizontalClassFrameLayout = QHBoxLayout(self.horizontalClassFrame, 5, 5)  
-        self.classButton = OWGUI.button(self.horizontalClassFrame, self, ">", self.onClassButtonClicked)
-        self.classButton.setMaximumSize(buttonSize)
-        self.horizontalClassFrameLayout.addWidget(self.classButton, 0, Qt.AlignLeft)                
-        self.classNameBox = QVGroupBox(self.horizontalClassFrame)
-        self.classNameBox.setTitle('Class')
-        self.classNameBox.setMinimumSize(QSize(200,50))        
-        self.classNameBox.setMaximumSize(QSize(200,50))
-        self.horizontalClassFrameLayout.addWidget(self.classNameBox, 0, Qt.AlignLeft)           
-        self.classList = QListBox(self.classNameBox,'ClassAttribute')  
+        # class        
+        self.classButton = OWGUI.button(ca, self, ">", self.onClassButtonClicked)
+        gl.addWidget(self.classButton, 1,1)
+        boxClass = QVGroupBox(ca)
+        boxClass.setTitle('Class')
+        boxClass.setFixedHeight(46)
+        gl.addWidget(boxClass, 1,2)
+        self.classList = QListBox(boxClass,'ClassAttribute')
         self.classList.setSelectionMode(QListBox.Extended)
         self.connect(self.classList, SIGNAL('selectionChanged()'), self.onClassSelectionChange)
         self.connect(self.classList, SIGNAL('currentChanged(QListBoxItem*)'), self.onClassCurrentChange)
-
-        self.horizontalClassFrameLayout.addStretch(200)
-
-        self.horizontalMetaFrame = QFrame(self.vframe2)
-        self.vframe2Layout.addMultiCellWidget(self.horizontalMetaFrame,6,9,0,0,0)          
-        self.horizontalMetaFrameLayout = QHBoxLayout(self.horizontalMetaFrame, 5, 5)
+        self.connect(self.classList, SIGNAL('doubleClicked(QListBoxItem*)'), self.onClassDoubleClick)
         
-        self.metaButton = OWGUI.button(self.horizontalMetaFrame, self, ">",self.onMetaButtonClicked)
-        self.metaButton.setMaximumSize(buttonSize)
-        self.horizontalMetaFrameLayout.addWidget(self.metaButton, 0, Qt.AlignLeft)        
-        self.metaNameBox = QVGroupBox(self.horizontalMetaFrame)
-        self.metaNameBox.setTitle('Meta Attributes')
-        self.metaNameBox.setMinimumSize(QSize(200,200))
-        self.horizontalMetaFrameLayout.addWidget(self.metaNameBox, 0, Qt.AlignLeft)          
-        self.metaList = QListBox(self.metaNameBox,'MetaAttributes')
+        # meta
+        vbMeta = QVBox(ca)
+        gl.addWidget(vbMeta, 2,1)
+        self.metaButtonUp = OWGUI.button(vbMeta, self, "Up", self.onMetaButtonUpClick)
+        self.metaButton = OWGUI.button(vbMeta, self, ">",self.onMetaButtonClicked)
+        self.metaButtonDown = OWGUI.button(vbMeta, self, "Down", self.onMetaButtonDownClick)
+        boxMeta = QVGroupBox(ca)
+        boxMeta.setTitle('Meta Attributes')
+        gl.addWidget(boxMeta, 2,2)
+        self.metaList = QListBox(boxMeta,'MetaAttributes')
         self.metaList.setSelectionMode(QListBox.Extended)
         self.connect(self.metaList, SIGNAL('selectionChanged()'), self.onMetaSelectionChange)
         self.connect(self.metaList, SIGNAL('currentChanged(QListBoxItem*)'), self.onMetaCurrentChange)         
+        self.connect(self.metaList, SIGNAL('doubleClicked(QListBoxItem*)'), self.onMetaDoubleClick)
         
-        self.verticalMetaFrame = QFrame(self.horizontalMetaFrame)
-        self.horizontalMetaFrameLayout.addWidget(self.verticalMetaFrame, 0, Qt.AlignLeft)        
-        self.verticalMetaFrameLayout = QVBoxLayout(self.verticalMetaFrame,5,5)
-        self.metaButtonUp = OWGUI.button(self.verticalMetaFrame, self, "Up", self.onMetaButtonUpClick)
-        self.metaButtonUp.setMaximumSize(upDownButtonSize)        
-        self.verticalMetaFrameLayout.addWidget(self.metaButtonUp, 0, Qt.AlignTop)
-        self.metaButtonDown = OWGUI.button(self.verticalMetaFrame, self, "Down", self.onMetaButtonDownClick)
-        self.metaButtonDown.setMaximumSize(upDownButtonSize)      
-        self.verticalMetaFrameLayout.addWidget(self.metaButtonDown, 0, Qt.AlignTop)
-        self.verticalMetaFrameLayout.addStretch(50)
-        self.horizontalMetaFrameLayout.addStretch(200)        
-        
-        #set up rightmost column
-        self.applyButton = OWGUI.button(self.vframe3, self, "Apply", callback = self.setOutput)
-        self.applyButton.setEnabled(False)        
-        self.vframe3Layout.addWidget(self.applyButton,0,Qt.AlignTop)
-        self.resetButton = OWGUI.button(self.vframe3, self, "Reset", callback = self.reset)
-        self.vframe3Layout.addWidget(self.resetButton,0,Qt.AlignTop)
+        # apply/reset buttons
+        boxApply = QHBox(ca)
+        gl.addMultiCellWidget(boxApply, 3,3,0,2)
+        self.applyButton = OWGUI.button(boxApply, self, "Apply", callback = self.setOutput)
+        self.applyButton.setEnabled(False)
+        self.applyButton.setMaximumWidth(101)
+        self.resetButton = OWGUI.button(boxApply, self, "Reset", callback = self.reset)
+        self.resetButton.setMaximumWidth(101)
 
-        self.vframe3Layout.addStretch(200)
-        
+        self.resize(400,480)       
+
+
     ############################################################################################################################################################
     ## Data input and output management ########################################################################################################################
     ############################################################################################################################################################
@@ -170,31 +129,35 @@ class OWDataDomain(OWWidget):
                 self.metaList.insertItem(self.createListItem(attr.varType, attr.name))
 
             self.setInputAttributesListElements()
-            self.setOutput()
 
+        self.setOutput()
         self.updateInterfaceState()
         
     def setOutput(self):
-        self.applyButton.setEnabled(False)
-        attributes = [];
-        for i in range(0, self.attributesList.count()):
-            attributes.append(self.data.domain[str(self.attributesList.text(i))])
-            
-        #create domain without class attribute
-        if self.classList.count()>0:
-            domain = orange.Domain(attributes, self.data.domain[str(self.classList.text(0))],self.data.domain)
-        else:
-            domain = orange.Domain(attributes, None,self.data.domain)
-        for i in range(0,self.metaList.count()):
-            domain.addmeta(orange.newmetaid(), self.data.domain[str(self.metaList.text(i))])
-        newdata = orange.ExampleTable(domain, self.data)
-        newdata.name = self.data.name
-        self.send("OutputData", newdata)
+        if self.data:
+            self.applyButton.setEnabled(False)
+            attributes = [];
+            for i in range(0, self.attributesList.count()):
+                attributes.append(self.data.domain[str(self.attributesList.text(i))])
+                
+            #create domain without class attribute
+            if self.classList.count()>0:
+                domain = orange.Domain(attributes, self.data.domain[str(self.classList.text(0))],self.data.domain)
+            else:
+                domain = orange.Domain(attributes, None,self.data.domain)
+            for i in range(0,self.metaList.count()):
+                domain.addmeta(orange.newmetaid(), self.data.domain[str(self.metaList.text(i))])
+            newdata = orange.ExampleTable(domain, self.data)
+            newdata.name = self.data.name
+            self.send("OutputData", newdata)
 
-        #create domaing with  class atribute
-        if self.classList.count()>0:
-            self.send("OutputDataWithClass", newdata)
+            #create domaing with  class atribute
+            if self.classList.count()>0:
+                self.send("OutputDataWithClass", newdata)
+            else:
+                self.send("OutputDataWithClass", None)
         else:
+            self.send("OutputData", None)
             self.send("OutputDataWithClass", None)
         
     def reset(self):
@@ -227,7 +190,17 @@ class OWDataDomain(OWWidget):
         self.handleListSelectionChange(self.metaList)
 
     def onMetaCurrentChange(self, item):
-        self.handleListSelectionChange(self.metaList)        
+        self.handleListSelectionChange(self.metaList)
+
+
+    def onAttributesDoubleClick(self, item):
+        self.onAttributesButtonClicked()
+
+    def onClassDoubleClick(self, item):
+        self.onClassButtonClicked()
+
+    def onMetaDoubleClick(self, item):
+        self.onMetaButtonClicked()
 
     ############################################################################################################################################################
     ## Interface state management - updates interface elements based on selection in list boxes ################################################################
