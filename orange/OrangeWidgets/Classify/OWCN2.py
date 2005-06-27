67ac6581bcc495a2de783c28cc552e3e18299667
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2005-06-27 12:20:29 +0000

    *** empty log message ***

--- orange/OrangeWidgets/Classify/OWCN2.py
+++ orange/OrangeWidgets/Classify/OWCN2.py
@@ -19,7 +19,7 @@ class OWCN2(OWWidget):
     def __init__(self, parent=None, signalManager=None):
         OWWidget.__init__(self,parent,signalManager,"CN2")
         #OWWidget.__init__(self,parent,"Rules")
-        self.inputs=[("ExampleTable", orange.ExampleTable, self.dataset)]
+        self.inputs=[("ExampleTable", ExampleTable, self.dataset)]
         self.outputs=[("Learner", orange.Learner),("Classifier",orange.Classifier),("CN2UnorderedClassifier", orngCN2.CN2UnorderedClassifier)]
         self.QualityButton=0
         self.CoveringButton=0
@@ -38,24 +38,33 @@ class OWCN2(OWWidget):
         labelWidth=150
         self.learnerName=OWGUI.lineEdit(self.controlArea, self, "LearnerName", box="Learner/Classifier Name",tooltip="Name to be used by other widgets to identify yor Learner/Classifier")
         self.learnerName.setText("Rule classifier")
-        self.ruleQualityGroup=OWGUI.widgetBox(self.controlArea, self)
-        self.ruleQualityGroup.setTitle("Rule Quality Estimation")
+        self.ruleQualityBG=QVButtonGroup(self.controlArea)
+        self.ruleQualityBG.setTitle("Rule Quality Estimation")
         self.ruleValidationGroup=OWGUI.widgetBox(self.controlArea, self)
         self.ruleValidationGroup.setTitle("Pre-Prunning(LRS)")
 
         OWGUI.spin(self.controlArea, self, "BeamWidth", 1, 100, box="Beam Width", tooltip=" Specify the width of the search beam")
 
-        self.coveringAlgGroup=OWGUI.widgetBox(self.controlArea, self)
-        self.coveringAlgGroup.setTitle("Covering algorithm settings")
-
+        self.coveringAlgBG=QVButtonGroup(self.controlArea)
+        self.coveringAlgBG.setTitle("Covering algorithm settings")
+        """
         self.ruleQualityBG=OWGUI.radioButtonsInBox(self.ruleQualityGroup, self, "QualityButton",
                             btnLabels=["Laplace","m-estimate","WRACC"],
                             box="Rule quality", callback=self.qualityButtonPressed,
                             tooltips=["Laplace rule evaluator", "m-estimate rule evaluator",
                             "WRACC rule evaluator"])
-
         self.mSpin=Spin=OWGUI.spin(self.ruleQualityGroup, self, "m", 0, 100, label="m",
                 orientation="horizontal", labelWidth=labelWidth-100, tooltip="m value for m estimate rule evaluator")
+        """
+        
+        QRadioButton("Laplace",self.ruleQualityBG)
+        g=QHBox(self.ruleQualityBG)
+        b=QRadioButton("m-estimate",g)
+        self.ruleQualityBG.insert(b)
+        self.mSpin=OWGUI.spin(g,self,"m",0,100)
+        self.mSpin.setDisabled(1)
+        QRadioButton("WRACC",self.ruleQualityBG)
+        self.connect(self.ruleQualityBG,SIGNAL("released(int)"),self.qualityButtonPressed)
         
         a=OWGUI.doubleSpin(self.ruleValidationGroup, self, "Alpha", 0, 1,0.05, label="Alpha",
                 orientation="horizontal", labelWidth=labelWidth)
@@ -66,6 +75,7 @@ class OWCN2(OWWidget):
                 orientation="horizontal", labelWidth=labelWidth, tooltip=
                 "Maximum number of conditions in the left\npart of the rule (use 0 for dont care)")
         
+        """
         self.coveringAlgBG=OWGUI.radioButtonsInBox(self.coveringAlgGroup, self, "CoveringButton",
                             btnLabels=["Exclusive covering ","Weighted Covering"],
                             tooltips=["Each example will only be used once\n for the construction of a rule",
@@ -74,7 +84,15 @@ class OWCN2(OWWidget):
         self.weightSpin=OWGUI.doubleSpin(self.coveringAlgGroup, self, "Weight",0, 0.95,0.05,label= "Weight",
                 orientation="horizontal", labelWidth=labelWidth, tooltip=
                 "Multiplication constant by which the weight of\nthe example will be reduced")
- 
+        """
+
+        QRadioButton("Exclusive covering",self.coveringAlgBG)
+        g=QHBox(self.coveringAlgBG)
+        b=QRadioButton("Weighted covering",g)
+        self.coveringAlgBG.insert(b)
+        self.weightSpin=OWGUI.doubleSpin(g,self,"Weight",0,0.95,0.05)
+        self.weightSpin.setDisabled(1)
+        self.connect(self.coveringAlgBG,SIGNAL("released(int)"),self.coveringAlgButtonPressed)
         #layout=QVBoxLayout(self.controlArea)
         #layout.add(self.ruleQualityGroup)
         #layout.add(self.ruleValidationGroup)
@@ -85,6 +103,8 @@ class OWCN2(OWWidget):
         self.Alpha=float(self.Alpha)
         self.Weight=float(self.Weight)
 
+        self.ruleQualityBG.setButton(self.QualityButton)
+        self.coveringAlgBG.setButton(self.CoveringButton)
         self.qualityButtonPressed()
         self.coveringAlgButtonPressed()
         self.controlArea.setMinimumWidth(300)
@@ -138,14 +158,14 @@ class OWCN2(OWWidget):
             self.send("CN2UnorderedClassifier",None)
 
 
-    def qualityButtonPressed(self):
+    def qualityButtonPressed(self,id=0):
         id=self.QualityButton=self.ruleQualityBG.id(self.ruleQualityBG.selected())
         if id==1:
             self.mSpin.setDisabled(0)
         else:
             self.mSpin.setDisabled(1)
 
-    def coveringAlgButtonPressed(self):
+    def coveringAlgButtonPressed(self,id=0):
         id=self.CoveringButton=self.coveringAlgBG.id(self.coveringAlgBG.selected())
         if id==1:
             self.weightSpin.setDisabled(0)
