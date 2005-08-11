2bfaa4cabd5be8de5d69b19f1d078b0c2f46d3d7
jurem <jurem@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2005-08-11 10:58:19 +0000

    *** empty log message ***

--- install-scripts/linux/testOrange.sh
+++ install-scripts/linux/testOrange.sh
@@ -1,16 +1,15 @@
 TAG=HEAD
 
-rm -f orange.pth
-find `pwd`/orange -name '*' -type d | grep -v CVS | grep -v source > orange.pth
+#rm -f orange.pth
+#find `pwd`/orange -name '*' -type d | grep -v CVS | grep -v source > orange.pth
 
-rm -Rf regressionTests
-cvs -q -d :pserver:cvso@estelle.fri.uni-lj.si:/CVS checkout -r $TAG -f regressionTests
+cvs -q -d :pserver:cvso@estelle.fri.uni-lj.si:/CVS checkout -d /home/orange/test_download/install/orange -r $TAG -f regressionTests
 
 # fix the path for the regression test from ../doc to ../orange/doc
-cat regressionTests/xtest.py | sed s/"os.chdir(\"..\/doc\")"/"os.chdir(\"..\/orange\/doc\")"/ > regressionTests/new.py
-mv -f regressionTests/new.py regressionTests/xtest.py
+cat /home/orange/test_download/install/orange/xtest.py | sed s/"os.chdir(\"..\/doc/\")"/"os.chdir(\"..\/doc\/orange\")"/ > /home/orange/test_download/install/orange/new.py
+mv -f /home/orange/test_download/install/orange/new.py /home/orange/test_download/install/orange/xtest.py
 
-cd regressionTests
+cd /home/orange/test_download/install/orange/
 if ! python ./xtest.py test; then
 	cd ..
 	exit 1
