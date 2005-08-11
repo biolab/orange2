52daadddcd8f0cd012e28704355844460be68881
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2005-08-11 11:24:06 +0000

    *** empty log message ***

--- install-scripts/linux/downloadinstallregress.sh
+++ install-scripts/linux/downloadinstallregress.sh
@@ -7,15 +7,17 @@ rm -rf /home/orange/test_download
 mkdir /home/orange/test_download
 cd /home/orange/test_download
 
-tar zxpvf /mnt/estelleDownload/${SOURCE_SNAPSHOT}
+echo "Untaring from /mnt/estelleDownload/${SOURCE_SNAPSHOT}" &> output.log
+tar zxpvf /mnt/estelleDownload/${SOURCE_SNAPSHOT} >> output.log 2>&1
 
 cd orange
 
-if ! python setup.py compile &> output.log; then
-  mail -s "Linux: ERROR compiling Orange" janez.demsar@fri.uni-lj.si < output.log
-  mail -s "Linux: ERROR compiling Orange" tomaz.curk@fri.uni-lj.si < output.log
-  mail -s "Linux: ERROR compiling Orange" jurem@flextronics.si < output.log 
-  cat output.log
+if ! python setup.py compile >> ../output.log 2>&1 ; then
+  cat compiling.log >> ../output.log
+  mail -s "Linux: ERROR compiling Orange" janez.demsar@fri.uni-lj.si < ../output.log
+  mail -s "Linux: ERROR compiling Orange" tomaz.curk@fri.uni-lj.si < ../output.log
+  mail -s "Linux: ERROR compiling Orange" jurem@flextronics.si < ../output.log 
+  cat ../output.log
   echo -e "\n\nERROR compiling, see log above"
   
   /home/orange/umount_estelleDownload
@@ -25,24 +27,24 @@ fi
 python setup.py install --orangepath=/home/orange/test_download/install &> install.log
 
 echo -e "\n\n"
-echo -e "\n\nStarting regression tests...\n\n" >> output.log
+echo -e "\n\nStarting regression tests...\n\n" >> ../output.log
 
-if ! ~/testOrange.sh >> output.log 2>&1; then
+if ! ~/testOrange.sh >> ../output.log 2>&1; then
   cd /home/orange/test_download/orange
-  cat output.log >> install.log
-  mv install.log output.log
-  mail -s "Linux: ERROR regression tests (compile OK) Orange" janez.demsar@fri.uni-lj.si < output.log
-  mail -s "Linux: ERROR regression tests (compile OK) Orange" tomaz.curk@fri.uni-lj.si < output.log
-  mail -s "Linux: ERROR regression tests (compile ok) Orange" jurem@flextronics.si < output.log 
-  cat output.log
+  cat ../output.log >> install.log
+  mv install.log ../output.log
+  mail -s "Linux: ERROR regression tests (compile OK) Orange" janez.demsar@fri.uni-lj.si < ../output.log
+  mail -s "Linux: ERROR regression tests (compile OK) Orange" tomaz.curk@fri.uni-lj.si < ../output.log
+  mail -s "Linux: ERROR regression tests (compile ok) Orange" jurem@flextronics.si < ../output.log 
+  cat ../output.log
   echo regression tests failed
 else
   cd /home/orange/test_download/orange
-  cat output.log >> install.log
-  mv install.log output.log
-  mail -s "Linux: Orange compiled successfully" tomaz.curk@fri.uni-lj.si < output.log
-  mail -s "Linux: Orange compiled successfully" jurem@flextronics.si < output.log 
-  cat output.log
+  cat ../output.log >> install.log
+  mv install.log ../output.log
+  mail -s "Linux: Orange compiled successfully" tomaz.curk@fri.uni-lj.si < ../output.log
+  mail -s "Linux: Orange compiled successfully" jurem@flextronics.si < ../output.log 
+  cat ../output.log
   echo compiling was successful
 fi
 
