876092b7ff5383f0c6d329c4540a13083dae2058
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2005-10-10 12:58:46 +0000

    *** empty log message ***

--- install-scripts/linux/compileRegress.sh
+++ install-scripts/linux/compileRegress.sh
@@ -1,12 +1,13 @@
 cd /home/orange/install-scripts
 cvs update
 chmod +x linux/testOrange.sh
+chmod +x linux/compileRegress.sh
 
 # prepare sources
 cd /home/orange/daily
 rm -Rf orange 
-cvs -d :pserver:cvso@estelle.fri.uni-lj.si:/cvs checkout -d orange orange
-cvs -d :pserver:cvso@estelle.fri.uni-lj.si:/cvs checkout -d orange/source source
+cvs -q -d :pserver:cvso@estelle.fri.uni-lj.si:/cvs checkout -d orange orange
+cvs -q -d :pserver:cvso@estelle.fri.uni-lj.si:/cvs checkout -d orange/source source
 cp /home/orange/install-scripts/linux/setup.py /home/orange/daily/orange
 
 # build
@@ -19,6 +20,7 @@ echo `date` > ../output.log
 if ! python setup.py compile >> ../output.log 2>&1 ; then
   cat compiling.log >> ../output.log
   mail -s "Linux: ERROR compiling Orange" tomaz.curk@fri.uni-lj.si < ../output.log
+  mail -s "Linux: ERROR compiling Orange" jurem@insilica.com < ../output.log
   cat ../output.log
   echo -e "\n\nERROR compiling, see log above"
   exit 1
@@ -29,6 +31,7 @@ cd /home/orange/daily/orange
 if ! python setup.py install --orangepath=/home/orange/daily/test_install > install.log 2>&1 ; then
   cat install.log >> ../output.log
   mail -s "Linux: ERROR installing Orange" tomaz.curk@fri.uni-lj.si < ../output.log
+  mail -s "Linux: ERROR installing Orange" jurem@insilica.com < ../output.log
   cat ../output.log
   echo -e "\n\nERROR installing, see log above"
   exit 1
@@ -40,12 +43,14 @@ if ! /home/orange/install-scripts/linux/testOrange.sh > regress.log 2>&1 ; then
   cd /home/orange/daily/orange
   cat regress.log >> ../output.log
   mail -s "Linux: ERROR regression tests (compile OK) Orange" tomaz.curk@fri.uni-lj.si < ../output.log
+  mail -s "Linux: ERROR regression tests (compile OK) Orange" jurem@insilica.com  < ../output.log
   cat ../output.log
   echo regression tests failed
 else
   cd /home/orange/daily/orange
   cat regress.log >> ../output.log
   mail -s "Linux: Orange compiled successfully" tomaz.curk@fri.uni-lj.si < ../output.log
+  mail -s "Linux: Orange compiled successfully" jurem@insilica.com < ../output.log
   cat ../output.log
   echo compiling was successful
 fi
--- install-scripts/linux/testOrange.sh
+++ install-scripts/linux/testOrange.sh
@@ -1,20 +1,15 @@
 TAG=HEAD
 
-#rm -f orange.pth
-#find `pwd`/orange -name '*' -type d | grep -v CVS | grep -v source > orange.pth
-
-cvs -q -d :pserver:cvso@estelle.fri.uni-lj.si:/CVS checkout -d /home/orange/test_download/install/orange -r $TAG -f regressionTests
+cd /home/orange/daily/test_install/orange
+cvs -q -d :pserver:cvso@estelle.fri.uni-lj.si:/cvs checkout -d . -r $TAG -f regressionTests
 
 # fix the path for the regression test from ../doc to ../orange/doc
-cat /home/orange/test_download/install/orange/xtest.py | sed s/"os.chdir(\"..\/doc/\")"/"os.chdir(\"..\/doc\/orange\")"/ > /home/orange/test_download/install/orange/new.py
-mv -f /home/orange/test_download/install/orange/new.py /home/orange/test_download/install/orange/xtest.py
+cat xtest.py | sed s/"\.\.\/doc"/"\.\.\/doc\/orange"/ > new.py
+mv -f new.py xtest.py
 
-cd /home/orange/test_download/install/orange/
-if ! python ./xtest.py test; then
-	cd ..
+if ! python xtest.py test; then
 	exit 1
 else
-	cd ..
 	exit 0
 fi
 
