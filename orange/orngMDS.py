5c1ed8247149efd002b831396a7cac089236986f
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2005-10-11 08:27:59 +0000

    Changed the orngMDS.py to use the c++ implementation of the mds class. Not compatible with the old module !!!!

--- orange/orngMDS.py
+++ orange/orngMDS.py
@@ -1,44 +1,16 @@
-#
-# A Trashy (but working) MDS Library 
-#
-# CVS Info: $Id$
-#
-# V 1.01
-#   - fixed the scaling bug in constructMatrixFromProx() (H. Harpending)
-#
-# V 1.0
-#
-# Aleks Jakulin (jakulin@acm.org) 2001-2002
-#
-#
-
+import orange
+import orangemds
 from math import *
 from Numeric import *
 from LinearAlgebra import *
-import RandomArray
-import copy
-from time import clock
-import orangemds
 
-# this function converts a bottom-triangular pseudo-matrix [[1],[2,3]]
-# into a proper matrix [[0,1,2],[1,0,3],[2,3,0]]
-def constructMatrixFromProx(mat):
-    #print "converting..."
-    diss = array([0.0])
-    ts = len(mat)+1
-    diss = resize(diss,(ts,ts))
-    mi = 1e500
-    for i in range(1,ts):
-        for j in range(i):
-            r = mat[i-1][j]
-            mi = min(r,mi)
-            diss[j][i] = diss[i][j] = r
-    # normalize
-    if mi < 0:
-        mi *= 1.1
-        diss -= mi
-        diss += mi*identity(ts)
-    return diss
+KruskalStress=orangemds.KruskalStress()
+SammonStress=orangemds.SammonStress()
+SgnSammonStress=orangemds.SgnSammonStress()
+SgnRelStress=orangemds.SgnRelStress()
+
+PointList=orangemds.FloatListList
+FloatListList=orangemds.FloatListList
 
 def _mycompare((a,aa),(b,bb)):
     if a==b:
@@ -47,93 +19,50 @@ def _mycompare((a,aa),(b,bb)):
         return -1
     else:
         return 1
-
-
-def KruskalStress(correct, current, weight=1):
-    d = current-correct
-    return weight*d*d
-
-def SammonStress(correct, current, weight=1):
-    d = current - correct
-    return weight*(d*d)/max(1e-6,current)
-
-# signed Sammon's stress
-def SgnSammonStress(correct, current, weight=1):
-    d = current-correct
-    return weight*d/max(1e-6,current)
-
-# signed relative stress
-def SgnRelStress(correct, current, weight=1):
-    # negative if too close in diagram, positive if too far in diagram
-    d = current-correct
-    return weight*d/max(1e-6,correct)
-
-default_stress = SgnRelStress
-
+            
 class MDS:
-    # initialize with a dissimilarity matrix
-    def __init__(self, dissimilarity_matrix, dimensions = 2):
-        self.O = copy.deepcopy(dissimilarity_matrix)
-        self.Orig = copy.deepcopy(dissimilarity_matrix)
-        self.n = len(self.O)
-        self.setDim(dimensions)
-        self.W = None
-        # initialize the points randomly
-        self.X = RandomArray.random(shape=[self.n,dimensions])
-        self.dist=RandomArray.random(shape=[self.n, self.n])
-        self.stress=RandomArray.random(shape=[self.n, self.n])
-        self.freshD = 0
+    def __init__(self, *args):
+        self.mds=orangemds.MDS(*args)
+        self.originalDistances=orange.SymMatrix([m for m in self.distances])
 
-    def setDim(self, dim):
-        self.dim = dim
+    def __getattr__(self, name):
+        #print "getattr",name
+        if name in ["points", "projectedDistances", "distances" ,"stress", "progressCallback", "n", "dim", "avgStress"]:
+            #print "rec:",name            
+            return self.__dict__["mds"].__dict__[name]
+        else:
+            return object.__getattribute__(self, "__dict__")[name]
 
-    # update the self.dist distance matrix with euclidean distances
+    def __setattr__(self, name, value):
+        #print "setattr"
+        if name=="points":
+            for i in range(len(value)):
+                for j in range(len(value[i])):
+                    self.mds.points[i][j]=value[i][j]
+            return
+            
+        if name in ["projectedDistances", "distances" ,"stress", "progressCallback"]:
+            self.mds.__setattr__(name, value)
+        else:
+            self.__dict__[name]=value
+            
+    def SMACOFstep(self):
+        self.mds.SMACOFstep()
+
     def getDistance(self):
-        if not self.freshD:
-            self.freshD = 1
-            """
-            self.dist = resize(array([0.0]),(self.n,self.n))
-            for i in xrange(1,self.n):
-                for j in xrange(i):
-                    s = 0.0
-                    #for k in range(self.dim):
-                    #    s += (self.X[i][k]-self.X[j][k])**2
-                    v=self.X[i]-self.X[j]
-                    s=dot(v,v)
-                    s = sqrt(s)
-                    self.dist[i][j] = s
-                    self.dist[j][i] = s
-            """
-            orangemds.getDistance(self.X,self.dist)
-    
-    def getStress(self,stressf=default_stress):
-        self.getDistance()
-        map={ KruskalStress:0, SammonStress:1, SgnSammonStress:2, SgnRelStress:3}
-        try:
-            if not isinstance(stressf, int):
-                i=map[stressf]
-            else:
-                i=stressf
-            self.arr=orangemds.getStress(self.O,self.dist,self.stress,i)
-        except:
-            self.stress = resize(array([0.0]),(self.n,self.n))
-            self.arr = []
-            total = 0.0
-            for i in xrange(1,self.n):
-                for j in xrange(i):
-                    r = stressf(self.O[i][j],self.dist[i][j])
-                    self.stress[i][j] = r
-                    self.stress[j][i] = r
-                    self.arr.append((r,(i,j)))
-                    total += abs(r)
-            self.arr.sort(_mycompare)
-            print "avg. stress: ",total/(self.n*self.n)
-        
-        
-        
+        self.mds.getDistance()
+
+    def getStress(self, stressFunc=SgnRelStress):
+        self.mds.getStress(stressFunc)
+
+    def optimize(iter, stressFunc=SgnRelStress, eps=1e-3, progeressCallback=None):
+        self.mds.progressCallback=progressCallback
+        self.mds.optimize(iter, stressFunc, eps)
+
     def Torgerson(self):
         # Torgerson's initial approximation
-        B = matrixmultiply(self.O,self.O)
+        O=array([m for m in self.distances])
+        B = matrixmultiply(O,O)
         
         # double-center B
         cavg = sum(B)/(self.n+0.0)      # column sum
@@ -156,7 +85,7 @@ class MDS:
         Lt = take(L,idx)   # take those singular values
         Ut = take(U,idx,1) # take those columns that are enabled
         Dt = identity(self.dim)*sqrt(Lt)  # make a diagonal matrix, with squarooted values
-        self.X = matrixmultiply(Ut,Dt)
+        self.points = orangemds.FloatListList(matrixmultiply(Ut,Dt))
         self.freshD = 0
         
 #        D = identity(self.n)*sqrt(L)  # make a diagonal matrix, with squarooted values
@@ -172,21 +101,21 @@ class MDS:
         o = []
         for i in xrange(1,self.n):
             for j in xrange(i):
-                o.append((self.Orig[i][j],(i,j)))
+                o.append((self.originalDistances[i,j],(i,j)))
         o.sort(_mycompare)
         # find the ties in o, and construct the d vector sorting in order within ties
         d = []
         td = []
         uv = [] # numbers of consecutively tied o values
         (i,j) = o[0][1]
-        distnorm = self.dist[i][j]*self.dist[i][j]
-        td = [self.dist[i][j]] # fetch distance
+        distnorm = self.projectedDistances[i,j]*self.projectedDistances[i,j]
+        td = [self.projectedDistances[i,j]] # fetch distance
         for l in xrange(1,len(o)):
             # copy now sorted distances in an array
             # but sort distances within a tied o
             (i,j) = o[l][1]
-            cd = self.dist[i][j]
-            distnorm += self.dist[i][j]*self.dist[i][j]
+            cd = self.projectedDistances[i,j]
+            distnorm += self.projectedDistances[i,j]*self.projectedDistances[i,j]
             if o[l][0] != o[l-1][0]:
                 # differing value, flush
                 sum = reduce(lambda x,y:x+y,td)+0.0
@@ -228,103 +157,18 @@ class MDS:
         for i in d:
             for j in range(i[1]):
                 (ii,jj) = o[k][1]
-                self.O[ii][jj] = f*i[2]
-                self.O[jj][ii] = f*i[2]
+                self.distances[ii,jj] = f*i[2]
                 k += 1
         assert(len(o) == k)
         self.freshD = 0
         return effect
 
-    # perform one step of simple gradient descent SMACOF 
-    def SMACOFstep(self):
-        # compute the R (n*n) matrix
-        self.getDistance()
-        st=clock()
-        """
-        R = resize(array([0.0]),(self.n,self.n))
-        sumv = array([0.0]*self.n)
-        for i in xrange(self.n):
-            for j in xrange(self.n):
-                if i != j:
-                    if self.dist[i][j] > 1e-6:
-                        s = 1.0/self.dist[i][j]
-                    else:
-                        s = 0.0
-                    t = self.O[i][j]*s
-                    R[i][j] = -t
-                    sumv[i] += t
-                    
-        for i in range(self.n):
-            R[i][i] = sumv[i]
-        # compute the iteration step
-        self.X = matrixmultiply(R,self.X)/(self.n+0.0)
-        """
-        orangemds.SMACOFStep(self.dist, self.O, self.X)
-        #print "SMACOF", clock()-st
-        self.freshD = 0
-
-    def SMACOFstepSlow(self):
-        # compute the R (n*n) matrix
-        self.getDistance()
-        R = resize(array([0.0]),(self.n,self.n))
-        sumv = array([0.0]*self.n)
-        for i in xrange(self.n):
-            for j in xrange(self.n):
-                if i != j:
-                    if self.dist[i][j] > 1e-6:
-                        s = 1.0/self.dist[i][j]
-                    else:
-                        s = 0.0
-                    t = self.O[i][j]*s
-                    t = 1+(t-1)*0.1
-                    R[i][j] = -t
-                    sumv[i] += t
-                    
-        for i in range(self.n):
-            R[i][i] = sumv[i]
-        # compute the iteration step
-        self.X = matrixmultiply(R,self.X)/(self.n+0.0)
-        self.freshD = 0
-
-    def SMACOFstepLocal(self):
-        # compute the R (n*n) matrix
-        self.getDistance()
-        R = resize(array([0.0]),(self.n,self.n))
-        sumv = array([0.0]*self.n)
-        
-        near = array([1e10]*self.n)
-        for i in xrange(self.n):
-            for j in xrange(self.n):
-                if i!=j:
-                    near[i]=min(near[i],self.O[i][j])
-
-        for i in xrange(self.n):
-            for j in xrange(self.n):
-                if i != j:
-                    if self.dist[i][j] > 1e-6:
-                        s = 1.0/self.dist[i][j]
-                    else:
-                        s = 0.0
-                    t = self.O[i][j]*s
-                    t = 1+(t-1)*((near[i]+near[j])/(self.O[i][j]*2.0))
-                    R[i][j] = -t
-                    sumv[i] += t
-                    
-        for i in range(self.n):
-            R[i][i] = sumv[i]
-        # compute the iteration step
-        self.X = matrixmultiply(R,self.X)/(self.n+0.0)
-        self.freshD = 0
-
-
-# Weighted MDS
+"""
 class WMDS(MDS):
-    def setWeights(self, wei):
-        self.freshD = 0
-        self.W = copy.deepcopy(wei)
-        self.Wsum = sum(sum(wei))
+    def setWeights(self, weights):
+        self.W=weights
+        self.Wsum=sum(sum(self.W))
 
-        # create the prefix matrix for SMACOF
         V = resize(array([0.0]),(self.n,self.n))
         sumv = array([0.0]*self.n)
         for i in range(1,self.n):
@@ -358,22 +202,14 @@ class WMDS(MDS):
         self.iV = generalized_inverse(V)
         #print self.M
 
-    def getStress(self,stressf=default_stress):
-        self.getDistance()        
-        self.stress = resize(array([0.0]),(self.n,self.n))
-        self.arr = []
-        total = 0.0
-        for i in xrange(1,self.n):
-            for j in xrange(i):
-                r = stressf(self.O[i][j],self.dist[i][j],self.W[i][j])
-                self.stress[i][j] = r
-                self.stress[j][i] = r
-                self.arr.append((r,(i,j)))
-                total += abs(r)
-        self.arr.sort(_mycompare)
-        print "avg. W stress: ",total/(self.n*self.n)
-        #print self.arr
-        
+    def getStress(self, stressf=SgnRelStress):
+        total=0.0
+        for i in range(self.n):
+            for j in range(i):
+                r=self.stress[i,j]=stressf(self.distances[i,j], self.projectedDistances[i,j], self.W[i,j])
+                total+=abs(r)
+        self.avgStress=total/(self.n*self.n)
+
     def LSMT(self):
         # optimize the distance transformation
         # build vector o
@@ -384,22 +220,22 @@ class WMDS(MDS):
             for j in xrange(i):
                 # skip the distances we don't care about
                 if self.W[i][j] > 1e-6:
-                    o.append((self.Orig[i][j],(i,j)))
+                    o.append((self.originalDistances[i,j],(i,j)))
         o.sort(_mycompare)
         # find the ties in o, and construct the d vector sorting in order within ties
         d = []
         td = []
         uv = [] # numbers of consecutively tied o values
         (i,j) = o[0][1]
-        distnorm = self.W[i][j]*self.dist[i][j]*self.dist[i][j]
-        td = [self.W[i][j]*self.dist[i][j]] # weighted distance
+        distnorm = self.W[i][j]*self.projectedDistances[i,j]*self.projectedDistances[i,j]
+        td = [self.W[i][j]*self.projectedDistances[i,j]] # weighted distance
         ld = [self.W[i][j]] # weights
         for l in xrange(1,len(o)):
             # copy now sorted distances in an array
             # but sort distances within a tied o
             (i,j) = o[l][1]
-            cd = self.W[i][j]*self.dist[i][j]
-            distnorm += self.W[i][j]*self.dist[i][j]*self.dist[i][j]
+            cd = self.W[i][j]*self.projectedDistances[i,j]
+            distnorm += self.W[i][j]*self.projectedDistances[i,j]*self.projectedDistances[i,j]
             if o[l][0] != o[l-1][0]:
                 # differing value, flush
                 sum = reduce(lambda x,y:x+y,td)+0.0
@@ -445,35 +281,10 @@ class WMDS(MDS):
         for i in d:
             for j in xrange(len(i[3])): # note the use of the length of the array of distances
                 (ii,jj) = o[k][1]
-                self.O[ii][jj] = f*i[2]
-                self.O[jj][ii] = f*i[2]
+                self.distances[ii,jj] = f*i[2]
                 k += 1
         assert(len(o) == k) # all values must be transformed
         self.freshD = 0
         return effect
-
-    def SMACOFstep(self):
-        # compute the R (n*n) matrix
-        self.getDistance()
-        R = resize(array([0.0]),(self.n,self.n))
-        sumv = array([0.0]*self.n)
-        for i in range(self.n):
-            for j in range(self.n):
-                if i != j:
-                    if self.dist[i][j] > 1e-6:
-                        s = self.W[i][j]/self.dist[i][j]
-                    else:
-                        s = 0.0
-                    t = self.O[i][j]*s
-                    # the closer the value to 1.0, the better it is
-                    #t = (self.W[i][j]*self.O[i][j] + (1-self.W[i][j])*self.dist[i][j])/max(1e-6,self.dist[i][j])
-                    #t = self.O[i][j]/max(1e-6,self.dist[i][j])
-                    R[i][j] = -t
-                    sumv[i] += t
-                    
-        for i in range(self.n):
-            R[i][i] = sumv[i]
-        # compute the iteration step
-        self.X = matrixmultiply(self.iV,matrixmultiply(self.M,matrixmultiply(R,self.X)))
-        self.freshD = 0
-        self.getDistance()
+"""
+        
