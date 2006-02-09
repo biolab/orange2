2cb3d1d994f32d8708450f904144dc5c5459b5ed
Gregor <Gregor@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2006-02-09 17:56:48 +0000

    *** empty log message ***

--- orange/OrangeCanvas/orngDoc.py
+++ orange/OrangeCanvas/orngDoc.py
@@ -54,7 +54,7 @@ class SchemaDoc(QMainWindow):
 
         if not self.canSave or self.canvasDlg.dontAskBeforeClose:
             if newSettings:
-                self.clearWidgetSignals()
+                self.synchronizeContexts()
                 self.saveDocument()
             self.clear()
             ce.accept()
@@ -64,7 +64,7 @@ class SchemaDoc(QMainWindow):
         #QMainWindow.closeEvent(self, ce)
         res = QMessageBox.information(self,'Orange Canvas','Do you want to save changes made to schema?','&Yes','&No','&Cancel',0)
         if res == 0:
-            self.clearWidgetSignals()
+            self.synchronizeContexts()
             self.saveDocument()
             ce.accept()
             self.clear()
@@ -84,10 +84,9 @@ class SchemaDoc(QMainWindow):
         self.canvasDlg.enableSave(self.canSave)
 
     # called to properly close all widget contexts
-    def clearWidgetSignals(self):
+    def synchronizeContexts(self):
         for widget in self.widgets[::-1]:
-            widget.clearAllInputSignals()
-            widget.processSignals()
+            widget.synchronizeContexts()
 
     # add line connecting widgets outWidget and inWidget
     # if necessary ask which signals to connect
@@ -572,7 +571,7 @@ if os.path.exists(widgetDir):
         loadSett = ""
         saveSett = ""
         signals = "#set event and progress handler\n"+t+t
-        clearData = ""
+        synchronizeContexts = ""
 
         sepCount = 1
         # gui for shown widgets
@@ -601,7 +600,7 @@ if os.path.exists(widgetDir):
                 loadSett += """self.ow%s.loadSettingsStr(strSettings["%s"])\n""" % (name, widget.caption) +t+t
                 loadSett += """self.ow%s.activateLoadedSettings()\n""" % (name) +t+t
                 saveSett += """strSettings["%s"] = self.ow%s.saveSettingsStr()\n""" % (widget.caption, name) +t+t
-                clearData = "self.ow%s.clearAllInputSignals(); self.ow%s.processSignals()\n" % (name, name) +t+t + clearData
+                synchronizeContexts = "self.ow%s.synchronizeContexts()\n" % (name) +t+t + synchronizeContexts
             else:
                 buttons += "frameSpace%s = QFrame(self);  frameSpace%s.setMinimumHeight(10); frameSpace%s.setMaximumHeight(10)\n" % (str(sepCount), str(sepCount), str(sepCount)) +t+t
                 sepCount += 1
@@ -707,7 +706,7 @@ if os.path.exists(widgetDir):
     def saveSettings(self):
         %s
         strSettings = {}
-        """ % (clearData) + saveSett + "\n" + t+t + """file = open("%s", "w")
+        """ % (synchronizeContexts) + saveSett + "\n" + t+t + """file = open("%s", "w")
         cPickle.dump(strSettings, file)
         file.close()
         """ % (fileName + ".sav")
