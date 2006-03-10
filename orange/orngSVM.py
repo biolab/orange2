f78196e3dcb9c6948de0d68b126dc8ae1dfb4fea
janezd <janez.demsar@fri.uni-lj.si>
2006-03-10 16:24:02 +0000

    - added copyright notice

--- orange/orngSVM.py
+++ orange/orngSVM.py
@@ -1,3 +1,11 @@
+# ORANGE Support Vector Machines
+# This module was written by Ales Erjavec
+# and supersedes an earlier one written by Alex Jakulin (jakulin@acm.org),
+# based on: Chih-Chung Chang and Chih-Jen Lin's
+# LIBSVM : a library for support vector machines
+#  (http://www.csie.ntu.edu.tw/~cjlin/papers/libsvm.ps.gz)
+
+
 import orange, orngTest, orngStat, sys, orngSVM_Jakulin
 
 def SVMLearner(examples=None, weightID=0, **kwds):
