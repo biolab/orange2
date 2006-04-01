958e8c1bc7a7a6c7ab2510a83923757c0c807202
janezd <janez.demsar@fri.uni-lj.si>
2006-04-01 14:34:32 +0000

    - classes for tuning parameters are now derived from orange.Learner - their constructors can be given examples - they now use orngMisc.BestOnTheFly - the pseudo-random is now based on the examples' crc
    - ThresholdLearner_Class is eliminated (the __new__ trick)
    - its constructor is now learner-like: previously, it expected the wrapped-to-be learner as an argument, now it can accept examples

--- orange/orngWrap.py
+++ orange/orngWrap.py
@@ -1,12 +1,27 @@
-# orngWrap.Tune1Parameter(object=bayes, parameter='m', values=[0,10,20,30])
 import orange
 
-class TuneParameters:
+# The class needs to be given
+#   object     - the learning algorithm to be fitted
+#   evaluate   - statistics to evaluate (default: orngStat.CA)
+#   folds      - the number of folds for internal cross validation
+#   compare    - function to compare (default: cmp - the bigger the better)
+#   returnWhat - tells whether to return values of parameters, a fitted
+#                learner, the best classifier or None. "object" is left
+#                with optimal parameters in any case
+class TuneParameters(orange.Learner):
     returnNone=0
     returnParameters=1
     returnLearner=2
     returnClassifier=3
     
+    def __new__(cls, examples = None, weightID = 0, **argkw):
+        self = orange.Learner.__new__(cls, **argkw)
+        self.__dict__.update(argkw)
+        if examples:
+            return self.__call__(examples, weightID)
+        else:
+            return self
+
     def findobj(self, name):
         import string
         names=string.split(name, ".")
@@ -15,68 +30,42 @@ class TuneParameters:
             lastobj=getattr(lastobj, i)
         return lastobj, names[-1]
         
-
-# The class needs to be given the following attributes
-#   object     - the learning algorithm to be fitter
+# Same arguments as TuneParameters, plus:
 #   parameter  - a string or a list of strings with parameter(s) to fit
 #   values     - possible values of the parameter
 #                (eg <object>.<parameter> = <value>[i])
-#   evaluate   - statistics to evaluate (default: orngStat.CA)
-#   compare    - function to compare (default: cmp - the bigger the better)
-#   returnWhat - tells whether to return values of parameters, a fitted
-#                learner, the best classifier or None. "object" is left
-#                with optimal parameters in any case
 class Tune1Parameter(TuneParameters):
-    def __init__(self, **keyw):
-        self.__dict__=keyw
-
-    def __call__(self, table, weight=None, folds=5, verbose=0):
-        import types, whrandom
-        import orange, orngTest, orngStat
+    def __call__(self, table, weight=None, verbose=0):
+        import orngTest, orngStat, orngMisc
 
         verbose = verbose or getattr(self, "verbose", 0)
-        
-        if (type(self.parameter)==types.ListType) or (type(self.parameter)==types.TupleType):
+        evaluate = getattr(self, "evaluate", orngStat.CA)
+        folds = getattr(self, "folds", 5)
+        compare = getattr(self, "compare", cmp)
+        returnWhat = getattr(self, "returnWhat", Tune1Parameter.returnClassifier)
+
+        if (type(self.parameter)==list) or (type(self.parameter)==tuple):
             to_set = [self.findobj(ld) for ld in self.parameter]
         else:
             to_set = [self.findobj(self.parameter)]
 
         cvind = orange.MakeRandomIndicesCV(table, folds)
-        bestres = None
-
-        evaluate = getattr(self, "evaluate", orngStat.CA)
-        compare = getattr(self, "compare", cmp)
-
-        # now, we need to set <lastobj>.<fieldname> to values in self.parameter[1]
+        findBest = orngMisc.BestOnTheFly(seed = table.checksum(), callCompareOn1st = True)
+        tableAndWeight = weight and (table, weight) or table
         for par in self.values:
             for i in to_set:
                 setattr(i[0], i[1], par)
-
-            if weight:
-                res = evaluate(orngTest.testWithIndices([self.object], (table, weight), cvind))
-            else:
-                res = evaluate(orngTest.testWithIndices([self.object], (table), cvind))
+            res = evaluate(orngTest.testWithIndices([self.object], tableAndWeight, cvind))
+            findBest.candidate((res, par))
             if verbose==2:
-                print 'orngWrap:\n', par, res
+                print '*** orngWrap  %s: %s:' % (par, res)
 
-            if not bestres:
-                bestres, bestpar, wins = res, par, 1
-            else:
-                r=compare(res, bestres)
-                if (r>0):
-                    bestres, bestpar, wins = res, par, 1
-                elif (r==0):
-                    if not whrandom.randint(0, wins):
-                        bestres, bestpar = res, par
-                    wins=wins+1
-
-        returnWhat = getattr(self, "returnWhat", Tune1Parameter.returnClassifier)
+        bestpar = findBest.winner()[1]
+        for i in to_set:
+            setattr(i[0], i[1], bestpar)
 
         if verbose:
             print "*** Optimal parameter: %s = %s" % (self.parameter, bestpar)
-            
-        for i in to_set:
-            setattr(i[0], i[1], bestpar)
 
         if returnWhat==Tune1Parameter.returnNone:
             return None
@@ -90,42 +79,34 @@ class Tune1Parameter(TuneParameters):
             return classifier
 
 
-# The class needs to be given the following attributes
-#   object     - the learning algorithm to be fitter
+# Same arguments as TuneParameters, plus
 #   parameters - a list of tuples with parameters to be fitted and the
 #                corresponding possible values, [(parameter(s), values), ...]
 #                (eg <object>.<parameter[j]> = <value[j]>[i])
-#   evaluate   - statistics to evaluate (default: orngStat.CA)
-#   compare    - function to compare (default: cmp - the bigger the better)
-#   returnWhat - tells whether to return values of parameters, a fitted
-#                learner, the best classifier or None. "object" is left
-#                with optimal parameters in any case
 class TuneMParameters(TuneParameters):
-    def __init__(self, **keyw):
-        self.__dict__ = keyw
+    def __call__(self, table, weight=None, verbose=0):
+        import orngTest, orngStat, orngMisc
 
-    def __call__(self, table, weight=None, folds=5, verbose=0):
-        import types, whrandom
-        import orange, orngTest, orngStat, orngMisc
-
+        evaluate = getattr(self, "evaluate", orngStat.CA)
+        folds = getattr(self, "folds", 5)
+        compare = getattr(self, "compare", cmp)
         verbose = verbose or getattr(self, "verbose", 0)
+        returnWhat = getattr(self, "returnWhat", Tune1Parameter.returnClassifier)
         
         to_set = []
         parnames = []
         for par in self.parameters:
-            if (type(par[0])==types.ListType) or (type(par[0])==types.TupleType):
+            if (type(par[0])==list) or (type(par[0])==tuple):
                 to_set.append([self.findobj(ld) for ld in par[0]])
                 parnames.append(par[0])
             else:
                 to_set.append([self.findobj(par[0])])
                 parnames.append([par[0]])
 
+
         cvind = orange.MakeRandomIndicesCV(table, folds)
-        bestres = None
-
-        evaluate = getattr(self, "evaluate", orngStat.CA)
-        compare = getattr(self, "compare", cmp)
-
+        findBest = orngMisc.BestOnTheFly(seed = table.checksum(), callCompareOn1st = True)
+        tableAndWeight = weight and (table, weight) or table
         for valueindices in orngMisc.LimitedCounter([len(x[1]) for x in self.parameters]):
             values = [self.parameters[i][1][x] for i,x in enumerate(valueindices)]
             for pi, value in enumerate(values):
@@ -134,26 +115,12 @@ class TuneMParameters(TuneParameters):
                     if verbose==2:
                         print "%s: %s" % (parnames[pi][i], value)
                         
-            if weight==None:
-                res = evaluate(orngTest.testWithIndices([self.object], (table), cvind))
-            else:
-                res = evaluate(orngTest.testWithIndices([self.object], (table, weight), cvind))
+            res = evaluate(orngTest.testWithIndices([self.object], tableAndWeight, cvind))
+            findBest.candidate((res, values))
             if verbose==2:
                 print "===> Result: %s\n" % res
 
-            if not bestres:
-                bestres, bestpar, wins = res, values, 1
-            else:
-                r = compare(res, bestres)
-                if r > 0:
-                    bestres, bestpar, wins = res, values, 1
-                elif r == 0:
-                    if not whrandom.randint(0, wins):
-                        bestres, bestpar = res, values
-                    wins = wins+1
-
-        returnWhat = getattr(self, "returnWhat", Tune1Parameter.returnClassifier)
-
+        bestpar = findBest.winner()[1]
         if verbose:
             print "*** Optimal set of parameters: ",
             for pi, value in enumerate(bestpar):
@@ -176,18 +143,15 @@ class TuneMParameters(TuneParameters):
 
 
 
-def ThresholdLearner(learner, examples = None, weightId = 0, **kwds):
-    lr = apply(ThresholdLearner_Class, (learner, ), kwds)
-    if examples:
-        return lr(examples, weightId)
-    else:
-        return lr
 
-class ThresholdLearner_Class(orange.Learner):
-    def __init__(self, learner, **kwds):
-        self.learner = learner
-        for k, e in kwds.items():
-            self.setattr(k, e)
+class ThresholdLearner(orange.Learner):
+    def __new__(cls, examples = None, weightID = 0, **kwds):
+        self = orange.Learner.__new__(cls, **kwds)
+        self.__dict__.update(kwds)
+        if examples:
+            return self.__call__(examples, weightID)
+        else:
+            return self
 
     def __call__(self, examples, weightID = 0):
         if not hasattr(self, "learner"):
@@ -195,7 +159,6 @@ class ThresholdLearner_Class(orange.Learner):
         
         classifier = self.learner(examples, weightID)
         threshold, optCA, curve = orange.ThresholdCA(classifier, examples, weightID)
-#        print "Threshold %5.3f, CA %5.3f" % (threshold, optCA)
         if getattr(self, "storeCurve", 0):
             return ThresholdClassifier(classifier, threshold, curve = curve)
         else:
@@ -205,8 +168,7 @@ class ThresholdClassifier(orange.Classifier):
     def __init__(self, classifier, threshold, **kwds):
         self.classifier = classifier
         self.threshold = threshold
-        for k, e in kwds.items():
-            self.setattr(k, e)
+        self.__dict__.update(kwds)
 
     def __call__(self, example, what = orange.Classifier.GetValue):
         probs = self.classifier(example, self.GetProbabilities)
@@ -225,16 +187,20 @@ def ThresholdLearner_fixed(learner, threshold, examples = None, weightId = 0, **
     else:
         return lr
     
-class ThresholdLearner_fixed_Class(orange.Learner):
-    def __init__(self, learner, threshold, **kwds):
-        self.learner = learner
-        self.threshold = threshold
-        for k, e in kwds.items():
-            self.setattr(k, e)
+class ThresholdLearner_fixed(orange.Learner):
+    def __new__(cls, examples = None, weightID = 0, **kwds):
+        self = orange.Learner.__new__(cls, **kwds)
+        self.__dict__.update(kwds)
+        if examples:
+            return self.__call__(examples, weightID)
+        else:
+            return self
 
     def __call__(self, examples, weightID = 0):
         if not hasattr(self, "learner"):
             raise "learner not set"
+        if not hasattr(self, "threshold"):
+            raise "threshold not set"
         if len(examples.domain.classVar.values)!=2:
             raise "ThresholdLearner handles binary classes only"
         
