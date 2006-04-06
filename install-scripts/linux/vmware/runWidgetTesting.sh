5c0f0c7af0af42387a468b0bed9b33e2c5cc92b4
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2006-04-06 12:03:12 +0000

    *** empty log message ***

--- install-scripts/linux/vmware/runWidgetTesting.sh
+++ install-scripts/linux/vmware/runWidgetTesting.sh
@@ -7,5 +7,5 @@ echo "\"testWidgetImage/Windows\ XP\ Professional.vmx\"" > runThis.lst
 
 # kill after 30min
 ulimit -t 1800
-/usr/local/bin/vmware -geometry 1200x1000+0+0 -x -k runThis.lst
+/usr/local/bin/vmware -geometry 1200x1000+0+0 -x -q -k runThis.lst
 
