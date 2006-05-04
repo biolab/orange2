a6a115aaa04e49f8e9df420d57ab94039029b4de
blaz <blaz.zupan@fri.uni-lj.si>
2006-05-04 14:23:38 +0000

    an example script for orngMDS

---
+++ orange/doc/modules/mds2.py
@@ -0,0 +1,29 @@
+import orange, orngMDS, math
+
+data=orange.ExampleTable("../datasets/iris.tab")
+euclidean = orange.ExamplesDistanceConstructor_Euclidean(data)
+distance = orange.SymMatrix(len(data))
+for i in range(len(data)):
+   for j in range(i+1):
+       distance[i, j] = euclidean(data[i], data[j])
+
+mds=orngMDS.MDS(distance)
+mds.run(100)
+
+from pylab import *
+colors = ["red", "yellow", "blue"]
+
+points = []
+for (i,d) in enumerate(data):
+   points.append((mds.points[i][0], mds.points[i][1], d.getclass()))
+for c in range(len(data.domain.classVar.values)):
+    sel = filter(lambda x: x[-1]==c, points)
+    x = [s[0] for s in sel]
+    y = [s[1] for s in sel]
+    scatter(x, y, c=colors[c])
+show()
+
+try:
+    from pylab import *
+except ImportError:
+    print "Can't import pylab (matplotlib)"
