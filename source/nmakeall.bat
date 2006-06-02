ea04f529ad4e84dff5e73db74a560e0f17d1c496
janezd <janez.demsar@fri.uni-lj.si>
2006-06-02 13:16:39 +0000

    *** empty log message ***

--- source/nmakeall.bat
+++ source/nmakeall.bat
@@ -4,29 +4,34 @@ set PYTHON24=c:\python24
 cd Orange
 ..\pyxtract\fixmak.py Orange.mak
 nmake /f  temp.mak CFG="Orange - Win32 Python 24"
+nmake /f  temp.mak CFG="Orange - Win32 Release"
 del temp.mak
 cd ..
 
 cd Corn
 ..\pyxtract\fixmak.py Corn.mak
 nmake /f  temp.mak CFG="Corn - Win32 Python 24"
+nmake /f  temp.mak CFG="Corn - Win32 Release"
 del temp.mak
 cd ..
 
 cd Orangeom
-..\pyxtract\fixmak.py Orangeom
+..\pyxtract\fixmak.py Orangeom.mak
 nmake /f  temp.mak CFG="orangeom - Win32 Python 24"
+nmake /f  temp.mak CFG="orangeom - Win32 Release"
 del temp.mak
 cd ..
 
 cd Orangene
-..\pyxtract\fixmak.py Orangene.mak.mak
+..\pyxtract\fixmak.py Orangene.mak
 nmake /f  temp.mak CFG="orangene - Win32 Python 24"
+nmake /f  temp.mak CFG="orangene - Win32 Release"
 del temp.mak
 cd ..
 
 cd Statc
 ..\pyxtract\fixmak.py Statc.mak
 nmake /f  temp.mak CFG="Statc - Win32 Python 24"
+nmake /f  temp.mak CFG="orangene - Win32 Release"
 del temp.mak
 cd ..
