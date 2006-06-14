fe75447c0dc337a4e7d1c7393a2bfc4aad35f2e0
janezd <janez.demsar@fri.uni-lj.si>
2006-06-14 12:04:38 +0000

    *** empty log message ***

--- orange/doc/reference/MeasureAttribute1a.py
+++ orange/doc/reference/MeasureAttribute1a.py
@@ -4,8 +4,15 @@
 # Uses:        iris
 # Referenced:  MeasureAttribute.htm
 
-import orange, random
+import orange
 data = orange.ExampleTable("iris")
 
 d1 = orange.EntropyDiscretization("petal length", data)
-print orange.MeasureAttribute_info(d1, data)
+print orange.MeasureAttribute_relief(d1, data)
+
+meas = orange.MeasureAttribute_relief()
+for t in meas.thresholdFunction("petal length", data):
+    print "%5.3f: %5.3f" % t
+
+thresh, score, distr = meas.bestThreshold("petal length", data)
+print "\nBest threshold: %5.3f (score %5.3f)" % (thresh, score)
