77cb54076c933e33f4c05de5c12adc325f4c71a4
tomazc <tomazc@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2006-06-23 09:39:41 +0000

    no message

--- install-scripts/linux/vmware/runWidgetTesting.sh
+++ install-scripts/linux/vmware/runWidgetTesting.sh
@@ -6,7 +6,7 @@ cp -R /vmware/VMWAREimages/winXP.widgetTesting testWidgetImage
 echo "\"testWidgetImage/Windows\ XP\ Professional.vmx\"" > runThis.lst
 
 # kill after 1 hour
-ulimit -t 3600
+ulimit -t 7200
 /usr/local/bin/vmware -geometry 1200x1000+0+0 -x -q -k runThis.lst
 date
 echo done
