657849eac2b128da54fa11a6103f14832bbecffc
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2006-06-23 09:41:06 +0000

    *** empty log message ***

--- install-scripts/linux/vmware/restartVMWARE.sh
+++ install-scripts/linux/vmware/restartVMWARE.sh
@@ -14,7 +14,7 @@ while true; do
 	echo
 	echo "COPY NEW IMAGES"
 	mkdir vmware
-	cp -vR /vmware/VMWAREimages/* vmware/.
+	nice cp -vR /vmware/VMWAREimages/* vmware/.
 	echo
 	echo "EXIT VMWARE TO RESTART WITH CLEAN IMAGES"
 	/usr/local/bin/vmware -geometry 1200x1000+100+0
