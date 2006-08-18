546883f19fe67046ef74c5e4772685766b3da473
janezd <janez.demsar@fri.uni-lj.si>
2006-08-18 21:26:58 +0000

    - added -l ../orange/ppp/lists to pyprops, so there should be no more warnings about the missing list classes

--- source/orangene/_pyxtract.bat
+++ source/orangene/_pyxtract.bat
@@ -1,2 +1,2 @@
-python ../pyxtract/pyprops.py -l ../orange/ppp/stamp -n orangene
+python ../pyxtract/pyprops.py -l ../orange/ppp/stamp -l ../orange/ppp/lists -n orangene
 python ../pyxtract/pyxtract.py -m -n orangene -l ../orange/px/stamp orangene.cpp
