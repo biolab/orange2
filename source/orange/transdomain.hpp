f6e32ba9bdef95f80be0610fdd17a20790310636
janezd <janez.demsar@fri.uni-lj.si>
2006-08-18 21:21:57 +0000

    - exported TDomainTransformerConstructor to DLL (don't know why this wasn't done before)

--- source/orange/transdomain.hpp
+++ source/orange/transdomain.hpp
@@ -27,12 +27,12 @@
 WRAPPER(ExampleGenerator)
 WRAPPER(Domain)
 
-class TDomainTransformerConstructor : public TOrange {
+class ORANGE_API TDomainTransformerConstructor : public TOrange {
 public:
   __REGISTER_ABSTRACT_CLASS
 
-  bool preservesOrder; //PR a flag, telling that original attributes have corresponding attribute in the same order
-  bool preservesAttributes; //PR a flag, telling whether each original has a corresponding new attribute
+  bool preservesOrder; //PR a flag telling that original attributes have corresponding attribute in the same order
+  bool preservesAttributes; //PR a flag telling whether each original has a corresponding new attribute
 
   TDomainTransformerConstructor(const bool &po, const bool &pa);
 
