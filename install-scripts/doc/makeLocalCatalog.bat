2a517f3ee7d7483d51beebf2dfa53b1e77f51821
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2006-11-02 21:42:31 +0000

    no message

--- install-scripts/doc/makeLocalCatalog.bat
+++ install-scripts/doc/makeLocalCatalog.bat
@@ -1,4 +1,3 @@
 cd ..\..\doc\widgets\catalog
 copy default-chm.htm default.htm
-..\..\..\scripts\doc\widgetCatalogs.py html ..\..\..\orangeCanvas\widgetRegistry.xml . >> default.htm
-pause
+python ..\..\..\scripts\doc\widgetCatalogs.py html ..\..\..\orangeCanvas\widgetRegistry.xml . >> default.htm
--- install-scripts/doc/scheduledDocUpdate.bat
+++ install-scripts/doc/scheduledDocUpdate.bat
@@ -1,4 +1,5 @@
 cd ..\..
 "c:\program files\wincvs 1.3\cvsnt\cvs.exe" -d :pserver:cvso@estelle.fri.uni-lj.si:/CVS update -d doc
-cd scripts\doc
-call makeLocalCatalog.bat
+cd doc\widgets\catalog
+move default-web.htm default.htm
+python ..\..\..\scripts\doc\widgetCatalogs.py html widgetRegistry.xml . >> default.htm
