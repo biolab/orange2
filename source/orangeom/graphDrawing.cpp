30eb62848c6f2f727ae242074823b28ec46a8f7b
miha <miha.stajdohar@gmail.com>
2006-11-27 00:10:34 +0000

    Prva delujoca verzija F-R algoritma.

--- source/orangeom/graphDrawing.cpp
+++ source/orangeom/graphDrawing.cpp
@@ -30,52 +30,48 @@ double attractiveForce(double x)
 double repulsiveForce(double x)
 {
 	if (x == 0)
-		return k2 / 0.1;
+		return k2 / 1;
 
-	return k2 / x; 
+	return   k2 / x; 
 }
 
-int cool(int t)
+double cool(double t)
 {
-	return t;
+	return t * 0.98;
 }
 
-
-PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_VARARGS, "(Graph, steps, coorX, coorY, width|def=100, height|def=100) -> None")
+PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_VARARGS, "(Graph, steps, coorX, coorY, temperature, width|def=100, height|def=100) -> None")
 {
   PyTRY
     int steps;
-	int width = 100;
-	int height = 100;
-	double temperature = 10;
+	int width = 1000;
+	int height = 1000;
+	double temperature = 0;
     PyObject *pygraph;
 	PyObject *pycoorX;
 	PyObject *pycoorY;
 
-	if (!PyArg_ParseTuple(args, "OiOO|ii:graphOptimization", &pygraph, &steps, &pycoorX, &pycoorY, &width, &height))
+	//cout <<"v003" << endl;
+
+	if (!PyArg_ParseTuple(args, "OiOOd|ii:graphOptimization", &pygraph, &steps, &pycoorX, &pycoorY, &temperature, &width, &height))
 	{
 		//PyErr_Clear
       return NULL;
 	}
-
-	/*
-	if (pycoorY == Py_None)
-		// pycoorX ima dva stolpca
-	else
-	    // vsak po enega
-	*/
 
     TGraphAsList *graph = &dynamic_cast<TGraphAsList &>(PyOrange_AsOrange(pygraph).getReference());
 
     if (graph->nVertices < 2)
       PYERROR(PyExc_AttributeError, "graph has less than two nodes", NULL);
 
-	cout << "Vertices: " << graph->nVertices << endl;
+	// temperatura je 1/10 diagonale risalne povrsine
+	if (temperature == 0)
+		temperature = sqrt((double)(width*width + height*height)) / 10;
 
 	int rows;
 	double *coorX;
 	double *coorY;
-
+	
 	numericToDouble(pycoorX, coorX, rows);
 	numericToDouble(pycoorY, coorY, rows);
 
@@ -105,11 +101,8 @@ PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_
 
 		pos[i][0] = (double)coorX[i];
 		pos[i][1] = (double)coorY[i];
-
-		disp[i][0] = 0;
-		disp[i][1] = 0;
 	}
-	
+
 	int area = width * height;
 	k = sqrt((double)(area / graph->nVertices));
 	k2 = area / graph->nVertices;
@@ -117,24 +110,31 @@ PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_
 	// iterations
 	for (int i = 0; i < steps; i++)
 	{
+		// reset disp
+		for (int i = 0; i < rows; i++)
+		{
+			disp[i][0] = 0;
+			disp[i][1] = 0;
+		}
+
 		// calculate repulsive force
-		for (int v = 0; v < graph->nVertices -1 ; v++)
+		for (int v = 0; v < graph->nVertices - 1; v++)
 		{
 			for (int u = v + 1; u < graph->nVertices; u++)
 			{
 				double difX = pos[v][0] - pos[u][0];
 				double difY = pos[v][1] - pos[u][1];
 
-				double dif = sqrt(difX*difX + difY*difY);
+				double dif = sqrt(difX * difX + difY * difY);
 
 				if (dif == 0)
 					dif = 1;
 
-				disp[v][0] = disp[v][0] + (difX / dif * repulsiveForce(dif));
-				disp[v][1] = disp[v][1] + (difY / dif * repulsiveForce(dif));
+				disp[v][0] = disp[v][0] + ((difX / dif) * repulsiveForce(dif));
+				disp[v][1] = disp[v][1] + ((difY / dif) * repulsiveForce(dif));
 
-				disp[u][0] = disp[u][0] - (difX / dif * repulsiveForce(dif));
-				disp[u][1] = disp[u][1] - (difY / dif * repulsiveForce(dif));
+				disp[u][0] = disp[u][0] - ((difX / dif) * repulsiveForce(dif));
+				disp[u][1] = disp[u][1] - ((difY / dif) * repulsiveForce(dif));
 			}
 		}
 
@@ -146,6 +146,9 @@ PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_
 			if (edge != NULL)
 			{
 				int u = edge->vertex;
+
+				//cout << "v: " << v << " u: " << u << endl;
+
 				// cout << "     v: " << v << " u: " << u << " w: " << edge->weights << endl;
 				
 				double difX = pos[v][0] - pos[u][0];
@@ -156,16 +159,18 @@ PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_
 				if (dif == 0)
 					dif = 1;
 
-				disp[v][0] = disp[v][0] - (difX / dif * attractiveForce(dif));
-				disp[v][1] = disp[v][1] - (difY / dif * attractiveForce(dif));
+				disp[v][0] = disp[v][0] - ((difX / dif) * attractiveForce(dif));
+				disp[v][1] = disp[v][1] - ((difY / dif) * attractiveForce(dif));
 
-				disp[u][0] = disp[u][0] + (difX / dif * attractiveForce(dif));
-				disp[u][1] = disp[u][1] + (difY / dif * attractiveForce(dif));
+				disp[u][0] = disp[u][0] + ((difX / dif) * attractiveForce(dif));
+				disp[u][1] = disp[u][1] + ((difY / dif) * attractiveForce(dif));
 	
 				TGraphAsList::TEdge *next = edge->next;
 				while (next != NULL)
 				{
 					int u = next->vertex;
+
+					//cout << "v: " << v << " u: " << u << endl;
 
 					double difX = pos[v][0] - pos[u][0];
 					double difY = pos[v][1] - pos[u][1];
@@ -175,11 +180,11 @@ PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_
 					if (dif == 0)
 						dif = 1;
 
-					disp[v][0] = disp[v][0] - (difX / dif * attractiveForce(dif));
-					disp[v][1] = disp[v][1] - (difY / dif * attractiveForce(dif));
+					disp[v][0] = disp[v][0] - ((difX / dif) * attractiveForce(dif));
+					disp[v][1] = disp[v][1] - ((difY / dif) * attractiveForce(dif));
 
-					disp[u][0] = disp[u][0] + (difX / dif * attractiveForce(dif));
-					disp[u][1] = disp[u][1] + (difY / dif * attractiveForce(dif));
+					disp[u][0] = disp[u][0] + ((difX / dif) * attractiveForce(dif));
+					disp[u][1] = disp[u][1] + ((difY / dif) * attractiveForce(dif));
 
 					//cout << "next v: " << v << " u: " << u << " w: " << next->weights << endl;
 					next = next->next;
@@ -196,29 +201,24 @@ PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_
 			if (dif == 0)
 				dif = 1;
 
-			pos[v][0] = pos[v][0] + disp[v][0] / dif * min(abs(disp[v][0]), temperature);
-			pos[v][1] = pos[v][1] + disp[v][1] / dif * min(abs(disp[v][1]), temperature);
+			pos[v][0] = pos[v][0] + ((disp[v][0] / dif) * min(abs(disp[v][0]), temperature));
+			pos[v][1] = pos[v][1] + ((disp[v][1] / dif) * min(abs(disp[v][1]), temperature));
 
-			pos[v][0] = min((double)width / 2, max((double)(-width) / 2, pos[v][0]));
-			pos[v][1] = min((double)height / 2, max((double)(-height) / 2, pos[v][1]));
+			//pos[v][0] = min((double)width,  max((double)0, pos[v][0]));
+			//pos[v][1] = min((double)height, max((double)0, pos[v][1]));
 		}
-
-		temperature = cool(temperature);
-		dumpCoordinates(pos, 2, rows);
-		cout << endl;
-
-
 		
+		temperature = cool(temperature);
 	}
+
 	PyArrayObject *arrayX = (PyArrayObject *)(pycoorX);
 	PyArrayObject *arrayY = (PyArrayObject *)(pycoorY);
 
 	for (int i = 0; i < rows; i++)
 	{
-		*(arrayX->data + i * arrayX->strides[0]) = pos[i][0];
-		*(arrayY->data + i * arrayY->strides[0]) = pos[i][1];
+		*(double *)(arrayX->data + i * arrayX->strides[0]) = pos[i][0];
+		*(double *)(arrayY->data + i * arrayY->strides[0]) = pos[i][1];
 	}
-	
 
 	// free space
 	for (int i = 0; i < rows; i++)
@@ -230,6 +230,7 @@ PyObject *graphOptimization(PyObject *, PyObject *args, PyObject *) PYARGS(METH_
 	free(pos);
 	free(disp);
 		
-    return Py_BuildValue("ii", graph->getEdge(0, 1) ? 1 : 0, (int)(graph->edges));
+    //return Py_BuildValue("ii", graph->getEdge(0, 1) ? 1 : 0, (int)(graph->edges));
+	return Py_BuildValue("OOd", arrayX, arrayY, temperature);
   PyCATCH
 }
