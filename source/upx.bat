f6195143660b05ef20e5604a9ba8efc283e54e23
janezd <janez.demsar@fri.uni-lj.si>
2007-01-10 07:49:11 +0000

    *** empty log message ***

--- source/upx.bat
+++ source/upx.bat
@@ -1,3 +1,3 @@
 if exist ..\..\%1.pyd del ..\..\%1.pyd
 "c:\program files\upx" obj\release\%1.pyd -o ..\..\%1.pyd
-copy obj\Release\%1.lib -o ..\..\lib\%1.lib
+copy obj\Release\%1.lib ..\..\lib\%1.lib
