6fc2d72bcb6f5c6d8221301dec0bc89129b86f20
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2007-01-16 13:31:43 +0000

    common web dir for results from all platforms

--- install-scripts/linux/copyLogsToWeb.sh
+++ install-scripts/linux/copyLogsToWeb.sh
@@ -2,24 +2,23 @@
 /home/orange/mount_estelleDownload
 
 # remove old logs
-W=/home/orange/estelleDownload/regressionResults/linux
-rm $W/*.log
+W=/home/orange/estelleDownload/regressionResults
+rm $W/linux.*.log
 
 # copy new ones
 cd /home/orange/daily/orange
-cp -p *.log ../*.log $W
+cp compiling.log $W/linux.compiling.log
+cp ../output.log $W/linux.output.log
+cp ../regress.log $W/linux.regress.log
 
 cd /home/orange/daily/test_install/orange
-rm -Rf $W/tests
-mkdir $W/tests
 
 for f in *-output; do
 	echo copying $f
-	mkdir $W/tests/$f
-	cp -p $f/*.txt $W/tests/$f
+	mkdir $W/$f
+	rm $W/$f/*linux2.*.txt
+	cp $f/*crash.txt $f/*error.txt $f/*new.txt $f/*changed.txt $f/*random1.txt $f/*random2.txt $W/$f
 done
-
-cp testresults.xml $W/tests
 
 /home/orange/umount_estelleDownload
 
