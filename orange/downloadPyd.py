6be26bd9f0f6272dd454910773f87275feb2d9b8
janezd <janez.demsar@fri.uni-lj.si>
2007-01-17 07:44:38 +0000

    *** empty log message ***

---
+++ orange/downloadPyd.py
@@ -0,0 +1,37 @@
+import urllib, sys, os, sys
+
+files = "orange", "corn", "statc", "orangeom", "orangene"
+baseurl = "http://www.ailab.si/orange/download/binaries/%i%i/" % sys.version_info[:2]
+fleurl = baseurl + "%s.pyd"
+
+op = filter(lambda x:x[-7:].lower() in ["\\orange", "/orange"], sys.path)
+if not op:
+	print "Orange is not found on the Python's path"
+
+print "Downloading to %s (for Python %i.%i)" % (op[0], sys.version_info[0], sys.version_info[1])
+os.chdir(op[0])
+
+def rep(blk_cnt, blk_size, tot_size):
+	print "\rDownloading %s: %i of %i" % (fle, min(tot_size, blk_cnt*blk_size), tot_size),
+
+if 1:
+	local_stamps = dict([tuple(x.split()) for x in file("stamps_pyd.txt")])
+else:
+	local_stamps = None
+
+try:
+    repository_stamps = dict([tuple(x.split()) for x in urllib.urlopen(baseurl + "stamps_pyd.txt") if x.strip()])
+except:
+	local_stamps = repository_stamps = None
+
+for fle in files:
+    if local_stamps and local_stamps[fle+".pyd"] == repository_stamps[fle+".pyd"]:
+		print "\nSkipping %s" % fle,
+    else:
+	    print "\nDownloading %s" % fle,
+	    urllib.urlretrieve(fleurl % fle, fle+".temp", rep)
+	    os.remove(fle+".pyd")
+	    os.rename(fle+".temp", fle+".pyd")
+
+if repository_stamps:
+	file("stamps_pyd.txt", "wt").write("\n".join(["%s\t%s" % x for x in repository_stamps.items()]))
