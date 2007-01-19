7c657cffaa71187302022950fd27857d2b11716f
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2007-01-19 08:08:36 +0000

    *** empty log message ***

--- install-scripts/linux/copyLogsToWeb.sh
+++ install-scripts/linux/copyLogsToWeb.sh
@@ -17,9 +17,9 @@ fi
 if cd /home/orange/daily/test_install/orange ; then
 	for f in *-output; do
 		echo copying $f
-		mkdir $W/$f
-		rm $W/$f/*linux2.*.txt
-		cp $f/*crash.txt $f/*error.txt $f/*new.txt $f/*changed.txt $f/*random1.txt $f/*random2.txt $W/$f
+		mkdir $W/regressionTests/$f
+		rm $W/regressionTests/$f/*linux2.*.txt
+		cp $f/*crash.txt $f/*error.txt $f/*new.txt $f/*changed.txt $f/*random1.txt $f/*random2.txt $W/regressionTests/$f
 	done
 else
 	echo "regression test were not run"
