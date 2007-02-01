61cf6b054e5e443aadd6033c9261da096bd9a2f2
cvso <cvso@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2007-02-01 19:05:18 +0000

    *** empty log message ***

--- install-scripts/linux/vmware/run_dailyBuild_regressionTest_widgetTesting.sh
+++ install-scripts/linux/vmware/run_dailyBuild_regressionTest_widgetTesting.sh
@@ -1,8 +1,30 @@
 DISPLAY=:1
 export DISPLAY
 cd /home/vmware
-L=( winXP.dailyBuild winXP.regressionTests winXP.widgetTesting )
+#L=( winXP.dailyBuild winXP.regressionTests winXP.widgetTesting )
 
+# build orange
+L=( noreset.winXP.dailyBuild )
+for f in "${L[@]}"; do
+	echo "########################################################"
+	echo "running VM $f" 
+
+	# run it
+	echo "\"$f/Windows\ XP\ Professional.vmx\"" > runThis.lst
+	# kill after 1 hour
+	ulimit -t 7200
+	echo "started at"
+	date
+	/usr/local/bin/vmware -geometry 1200x1000+0+0 -x -q -k runThis.lst
+	echo done at
+	date
+	echo
+
+	# remove temp VM image
+	rm -Rf cron.$f
+done
+
+L=( winXP.regressionTests winXP.widgetTesting )
 for f in "${L[@]}"; do
 	echo "########################################################"
 	echo "running VM $f" 
