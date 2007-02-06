5d8f6eff4ef567393a1b9a2dd43cc6c89c8e8831
janezd <janez.demsar@fri.uni-lj.si>
2007-02-06 06:06:19 +0000

    - fixed the #include for arrayobject.h

--- source/orange/numeric_interface.hpp
+++ source/orange/numeric_interface.hpp
@@ -23,36 +23,43 @@
 #ifndef __NUMERIC_INTERFACE
 #define __NUMERIC_INTERFACE
 
-  #include "Python.h"
+#include "Python.h"
+
+#ifdef _MSC_VER
+  /* nicer to do some ifdefing here than needing to define a special
+     include in every project that includes this header */
+  #include "../lib/site-packages/numpy/core/include/numpy/arrayobject.h"
+#else
+  #include <numpy/arrayobject.h>
+#endif
 
 extern PyObject *moduleNumeric, *moduleNumarray, *moduleNumpy;
 extern PyTypeObject *PyNumericArrayType, *PyNumarrayArrayType, *PyNumpyArrayType;
 extern PyObject *numericMaskedArray, *numarrayMaskedArray, *numpyMaskedArray;
 
-#include<numpy/arrayobject.h>
 
-  extern bool importarray_called;
+extern bool importarray_called;
 
-  bool isSomeNumeric(PyObject *);
+bool isSomeNumeric(PyObject *);
 
-  // avoids unnecessarily importing the numeric modules
-  bool isSomeNumeric_wPrecheck(PyObject *args);
+// avoids unnecessarily importing the numeric modules
+bool isSomeNumeric_wPrecheck(PyObject *args);
 
 
-  void initializeNumTypes();
+void initializeNumTypes();
   
-  char getArrayType(PyObject *);
+char getArrayType(PyObject *);
   
-  inline char getArrayType(PyArrayObject *args)
-  { return getArrayType((PyObject *)(args)); }
+inline char getArrayType(PyArrayObject *args)
+{ return getArrayType((PyObject *)(args)); }
   
-  inline void prepareNumeric()
-  { if (!importarray_called)
-      initializeNumTypes();
-  }
+inline void prepareNumeric()
+{ if (!importarray_called)
+    initializeNumTypes();
+}
 
-  void numericToDouble(PyObject *num, double *&table, int &columns, int &rows);
-  void numericToDouble(PyObject *num, double *&table, int &rows);
+void numericToDouble(PyObject *num, double *&table, int &columns, int &rows);
+void numericToDouble(PyObject *num, double *&table, int &rows);
 
 static char supportedNumericTypes[] = "bBhHiIlLfdc";
 
