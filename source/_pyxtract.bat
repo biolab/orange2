c710d3b23ae3d6b9eeb00869167279dbbeb055b1
janezd <janez.demsar@fri.uni-lj.si>
2007-02-08 16:01:13 +0000

    - fixed the for loop (hopefully runs on george the same as in Janez's shell)

--- source/_pyxtract.bat
+++ source/_pyxtract.bat
@@ -2,4 +2,4 @@
 cd orange
 python ..\pyxtract\defvectors.py
 cd ..
-for %%d in (orange orangene orangeom) do pushd %%d & call _pyxtract.bat & popd
+for %d in (orange orangene orangeom) (pushd %d %+ call _pyxtract.bat %+ popd)
