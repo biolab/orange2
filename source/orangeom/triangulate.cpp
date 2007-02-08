35ed9b10ea5150b64020e1c07c02914e5d1e980c
janezd <janez.demsar@fri.uni-lj.si>
2007-02-08 16:00:17 +0000

    - added orangeom.qing with a placeholder for the true qing algorithm

--- source/orangeom/triangulate.cpp
+++ source/orangeom/triangulate.cpp
@@ -1,70 +1,4 @@
-#include "wml/WmlVector2.h"
-#include "wml/WmlDelaunay2a.h"
-using namespace Wml;
-
 #include "orange_api.hpp"
-#include "graph.hpp"
-#include "examplegen.hpp"
-
-PyObject *triangulate(PyObject *, PyObject *args, PyObject *) PYARGS(METH_VARARGS, "(examples[, attr1, attr2, nEdgeTypes]) -> Graph")
-{
-  PyTRY
-    PExampleGenerator egen;
-    int nEdgeTypes = 1;
-    PyObject *pyvar1 = NULL, *pyvar2 = NULL;
-    int attr1 = 0, attr2 = 1;
-    if (   !PyArg_ParseTuple(args, "O&|OOi:triangulate", pt_ExampleGenerator, &egen, &pyvar1, &pyvar2, &nEdgeTypes)
-        || pyvar1 && !varNumFromVarDom(pyvar1, egen->domain, attr1)
-        || pyvar2 && !varNumFromVarDom(pyvar2, egen->domain, attr2))
-      return NULL;
-
-    if (  (egen->domain->attributes->at(attr1)->varType != TValue::FLOATVAR)
-	     || (egen->domain->attributes->at(attr2)->varType != TValue::FLOATVAR))
-      PYERROR(PyExc_TypeError, "triangulate expects continuous attributes", NULL);
-
-    const int nofex = egen->numberOfExamples();
-
-    Vector2<float> *points = new Vector2<float>[nofex];
-    Vector2<float> *pi = points;
-
-    PEITERATE(ei, egen) {
-      if ((*ei)[attr1].isSpecial() || (*ei)[attr2].isSpecial())
-        PYERROR(PyExc_AttributeError, "triangulate cannod handle unknown values", NULL);
-
-      *(pi++) = Vector2<float>((*ei)[attr1].floatV, (*ei)[attr2].floatV);
-    }
-
-    int nTriangles;
-    int *triangles, *adjacent;
-    Delaunay2a<float> delaunay(nofex, points, nTriangles, triangles, adjacent);
-    delete adjacent;
-
-    TGraph *graph = new TGraphAsList(nofex, nEdgeTypes, 0);
-    PGraph wgraph = graph;
-    try {
-      for(int *ti = triangles, *te = ti+nTriangles*3; ti!=te; ti+=3) {
-        for(int se = 3; se--; ) {
-          double *gedge = graph->getOrCreateEdge(ti[se], ti[(se+1) % 3]);
-          for(int et = nEdgeTypes; et--; *gedge++ = 1.0);
-        }
-      }
-    }
-    catch (...) {
-      delete triangles;
-      throw;
-    }
-    delete triangles;
-
-    PyObject *res = WrapOrange(wgraph);
-
-    PyObject *objs = PyString_FromString("objects");
-    Orange_setattrLow((TPyOrange *)res, objs, PyTuple_GET_ITEM(args, 0), false);
-    Py_DECREF(objs);
-    return res;
-  PyCATCH
-}
-
-
 
 PyObject *dist(PyObject *, PyObject *args) PYARGS(METH_VARARGS, "(p, t)")
 {
@@ -182,4 +116,89 @@ PyObject *qhull(PyObject *, PyObject *arg) PYARGS(METH_O, "(array) -> ?")
 
 
 
+void qing_f(int ndimension, int npoints, double *points, double *values, int nsimplices, int *simplices,
+            int &npairs, int *&pairs, int &ncritical, int *&critical, double *corrected)
+{
+  npairs = ncritical = 2;
+
+  pairs =  new int[4];
+  memcpy(pairs, simplices, 4*sizeof(int));
+
+  critical = new int[2];
+  critical[0] = critical[1] = 42;
+  
+  double *vi = values, *ci = corrected;
+  for(int i = npoints; i--; *ci++ = *vi++ +1);
+}
+
+PyObject *qing(PyObject *, PyObject *args) PYARGS(METH_VARARGS, "(points, func_values, simplices) -> (pairs, critical, corrected)")
+{
+  PyObject *pypoints, *pyvalues, *pysimplices;
+ 
+  if (   !PyArg_UnpackTuple(args, "orangeom.qing", 3, 3, &pypoints, &pyvalues, &pysimplices)
+      || !isSomeNumeric_wPrecheck(pypoints)
+      || !isSomeNumeric_wPrecheck(pyvalues)
+      || !isSomeNumeric_wPrecheck(pysimplices))
+    PYERROR(PyExc_AttributeError, "three numeric arrays expected", NULL);
+    
+  PyArrayObject *points = (PyArrayObject *)pypoints;
+  PyArrayObject *values = (PyArrayObject *)pyvalues;
+  PyArrayObject *simplices = (PyArrayObject *)pysimplices;
+  
+  if ((getArrayType(points) != 'd') || (points->nd != 2))
+    PYERROR(PyExc_AttributeError, "the first argument (points) must be a two-dimensional array of doubles", NULL);
+
+  if ((getArrayType(values) != 'd') || (values->nd != 1))
+    PYERROR(PyExc_AttributeError, "the second argument (values) must be a vector of doubles", NULL);
+    
+  if ((getArrayType(simplices) != 'i') || (simplices->nd != 2))
+    PYERROR(PyExc_AttributeError, "the third argument must be a two-dimensional array of integers", NULL);
+    
+  const int npoints = points->dimensions[0];
+  const int dimension = points->dimensions[1];
+  const int nsimplices = simplices->dimensions[0];
+  
+  if (npoints != values->dimensions[0])
+    PYERROR(PyExc_AttributeError, "the number of function values mismatches the number of arguments", NULL);
+
+  if (simplices->dimensions[1] != dimension + 1)
+    PYERROR(PyExc_AttributeError, "the number of function arguments does not match the dimensionality of simplices", NULL);
+    
+  PyObject *moduleDict = PyModule_GetDict(moduleNumpy);
+  PyObject *mzeros = PyDict_GetItemString(moduleDict, "zeros");
+  PyObject *pycorrected = PyObject_CallFunction(mzeros, "(i)s", values->dimensions[0], "d");
+  if (!pycorrected)
+    return NULL;
+
+  points = PyArray_GETCONTIGUOUS(points);
+  values = PyArray_GETCONTIGUOUS(values);
+  simplices = PyArray_GETCONTIGUOUS(simplices);
+
+  int npairs, *pairs;
+  int ncritical, *critical;
+  qing_f(dimension, npoints, (double *)points->data, (double *)values->data,
+         nsimplices, (int *)simplices->data,
+         npairs, pairs, ncritical, critical, (double *)((PyArrayObject *)pycorrected)->data);
+
+  Py_DECREF(points);
+  Py_DECREF(values);
+  Py_DECREF(simplices);
+    
+  int i;
+  
+  PyObject *pypairs = PyList_New(npairs);
+  for(i = 0; i < npairs; i++)
+    PyList_SetItem(pypairs, i, Py_BuildValue("ii", pairs[2*i], pairs[2*i+1]));
+    
+  PyObject *pycritical = PyList_New(ncritical);
+  for(i = 0; i < ncritical; i++)
+    PyList_SetItem(pycritical, i, PyInt_FromLong(critical[i]));
+    
+  delete pairs;
+  delete critical;
+  
+  return Py_BuildValue("OOO", pypairs, pycritical, pycorrected);
+}
+  
+  
 #include "px/triangulate.px"
