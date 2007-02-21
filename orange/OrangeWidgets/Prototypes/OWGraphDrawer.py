b6f3a484301eb8233c7f6a6d254753d0b7fad3c6
miha <miha.stajdohar@gmail.com>
2007-02-21 10:38:59 +0000

    Widget for displaying graphs. Prototype version.

---
+++ orange/OrangeWidgets/Prototypes/OWGraphDrawer.py
@@ -0,0 +1,239 @@
+"""
+<name>Graph Canvas</name>
+<description>The Graph Canvas Widget enables users to visualize graph schemas.</description>
+<icon>icons/Outlier.png</icon>
+<contact>Miha Stajdohar (miha.stajdohar(@at@)gmail.com)</contact> 
+<priority>5020</priority>
+"""
+import OWGUI
+
+from OWWidget import *
+from qwt import *
+from qt import *
+from OWGraphDrawerCanvas import *
+from OWGraphVisualizer import *
+
+class OWGraphDrawer(OWWidget):
+    def __init__(self, parent=None, signalManager=None):
+        OWWidget.__init__(self, parent, signalManager, 'GraphCanvas')
+
+        self.inputs = [("Graph", orange.Graph, self.setGraph)]
+        self.outputs = []
+        
+        self.graphShowGrid = 1  # show gridlines in the graph
+
+        # GUI       
+        self.optimizeBox = QVGroupBox("Optimize", self.controlArea)
+        OWGUI.button(self.optimizeBox, self, "Random", callback=self.random)
+        OWGUI.button(self.optimizeBox, self, "F-R", callback=self.ff)
+        OWGUI.button(self.optimizeBox, self, "Circular", callback=self.circular)
+        
+        self.styleBox = QVGroupBox("Style", self.controlArea)
+        QLabel("Color Attribute", self.styleBox)
+        self.colorCombo = OWGUI.comboBox(self.styleBox, self, "color", callback=self.setVertexColor)
+        self.colorCombo.insertItem("(none)")
+        
+        QLabel("Select attributes", self.styleBox)
+        self.attListBox = QListBox(self.styleBox, "selectAttributes")
+        self.attListBox.setMultiSelection(True)
+        self.styleBox.connect(self.attListBox, SIGNAL("clicked( QListBoxItem * )"), self.clickedAttLstBox)
+        
+        pics=pixmaps()
+        
+        self.cgb = QHGroupBox(self.controlArea)
+        self.btnZomm = OWGUI.button(self.cgb, self, "", callback=self.btnZommClicked)
+        self.btnZomm.setToggleButton(1)
+        self.btnZomm.setPixmap(QPixmap(pics.LOOKG))
+        
+        self.btnMpos = OWGUI.button(self.cgb, self, "", callback=self.btnMposClicked)
+        self.btnMpos.setToggleButton(1)
+        self.btnMpos.setPixmap(QPixmap(pics.HAND))
+        
+        self.btnRctSel = OWGUI.button(self.cgb, self, "", callback=self.btnRctSelClicked)
+        self.btnRctSel.setToggleButton(1)
+        self.btnRctSel.setPixmap(QPixmap(pics.RECTG))
+        
+        self.btnPolySel = OWGUI.button(self.cgb, self, "", callback=self.btnPolySelClicked)
+        self.btnPolySel.setToggleButton(1)
+        self.btnPolySel.setPixmap(QPixmap(pics.POLYG))
+        
+        self.graph = OWGraphDrawerCanvas(self, self.mainArea, "ScatterPlot")
+        #self.optimize = OWGraphDrawingOptimize(parent=self);
+        #start of content (right) area
+        self.box = QVBoxLayout(self.mainArea)
+        self.box.addWidget(self.graph)
+    
+    def btnZommClicked(self):
+        self.btnZomm.setOn(1)
+        self.btnMpos.setOn(0)
+        self.btnRctSel.setOn(0)
+        self.btnPolySel.setOn(0)
+        self.graph.state = ZOOMING
+        self.graph.canvas().setCursor(Qt.crossCursor)
+    
+    def btnMposClicked(self):
+        self.btnZomm.setOn(0)
+        self.btnMpos.setOn(1)
+        self.btnRctSel.setOn(0)
+        self.btnPolySel.setOn(0)
+        self.graph.state = MOVE_SELECTION
+        self.graph.canvas().setCursor(Qt.pointingHandCursor)
+        
+    def btnRctSelClicked(self):
+        self.btnZomm.setOn(0)
+        self.btnMpos.setOn(0)
+        self.btnRctSel.setOn(1)
+        self.btnPolySel.setOn(0)
+        self.graph.state = SELECT_RECTANGLE
+        self.graph.canvas().setCursor(Qt.arrowCursor)
+    
+    def btnPolySelClicked(self):
+        self.btnZomm.setOn(0)
+        self.btnMpos.setOn(0)
+        self.btnRctSel.setOn(0)
+        self.btnPolySel.setOn(1)
+        self.graph.state = SELECT_POLYGON
+        self.graph.canvas().setCursor(Qt.arrowCursor)
+    
+    def clickedAttLstBox(self, item):
+        attributes = []
+        i = self.attListBox.firstItem()
+        while i:
+            if self.attListBox.isSelected(i):
+                attributes[:0] = [i.text()]
+            i = i.next()
+            
+        self.graph.setLabelText(attributes)
+        self.updateCanvas()
+    
+    def setGraph(self, graph):
+        self.visualize = GraphVisualizer(graph, self)
+            
+        
+        self.colorCombo.clear()
+        self.attListBox.clear()
+        self.colorCombo.insertItem("(one color)")
+        
+        if isinstance(graph.items, orange.ExampleTable):
+            for var in graph.items.domain:
+                self.colorCombo.insertItem(unicode(var.name))
+                self.attListBox.insertItem(unicode(var.name))
+            
+        self.graph.addVisualizer(self.visualize)
+        self.displayRandom(firstTime = True)
+        
+    def random(self):
+        self.displayRandom(firstTime=False); 
+        
+    def ff(self):
+        if self.visualize == None:   #grafa se ni
+            return
+
+        #najprej nakljucne koordinate za vsa vozlisca
+        self.visualize.fruchtermanReingold()
+        
+    def circular(self):
+        pass
+
+    def setVertexColor(self):
+        self.graph.setVertexColor(self.colorCombo.currentText())
+        self.updateCanvas()
+        
+    def setGraphGrid(self):
+        self.graph.enableGridY(self.graphShowGrid)
+        self.graph.enableGridX(self.graphShowGrid)
+            
+    def displayRandom(self, firstTime=False):
+        if self.visualize == None:   #grafa se ni
+            return
+
+        #najprej nakljucne koordinate za vsa vozlisca
+        self.visualize.random()
+
+        #ko graf preberemo iz datoteke, upostevamo podane koordinate
+#        if firstTime == True: 
+#            for i in range(0, self.visualize.nVertices()):
+#                if self.verticesDescriptions[i].inFileDefinedCoors[0] != None:
+#                    self.visualize.xCoors[i] = self.verticesDescriptions[i].inFileDefinedCoors[0]
+#                    
+#                if self.verticesDescriptions[i].inFileDefinedCoors[1] != None:
+#                    self.visualize.yCoors[i] = self.verticesDescriptions[i].inFileDefinedCoors[1]       
+                    
+        self.updateCanvas(); 
+                    
+    def updateCanvas(self):
+        #ce imamo graf
+        if self.visualize != None:
+            self.graph.updateCanvas()#self.visualize.xCoors, self.visualize.yCoors)
+        
+if __name__=="__main__":    
+    appl = QApplication(sys.argv)
+    ow = OWGraphDrawer()
+    appl.setMainWidget(ow)
+    ow.show()
+    appl.exec_loop()
+
+#ta razred je potreben, da lahko narisemo pixmap z painterjem
+class pixmaps(QWidget):
+    def __init__(self):
+        apply(QWidget.__init__, (self,))
+
+        #risanje lupe
+        self.LOOKG=QPixmap(20,20)
+        self.LOOKG.fill()
+        painter=QPainter(self.LOOKG)
+        painter.setPen(QPen(Qt.black,1))
+        painter.setBrush(QBrush(Qt.red))
+        pa=QPointArray([2,5,2,10,5,13,10,13,13,10,13,5,10,2,5,2,2,5])
+        painter.drawPolygon(pa)
+        painter.drawLine(12,11,19,18)
+        painter.drawLine(11,12,18,19)
+        painter.drawLine(12,12,18,18)
+        painter.end()
+
+        #risanje roke
+        self.HAND=QPixmap(20,20)
+        self.HAND.fill()
+        painter=QPainter(self.HAND)
+        painter.setPen(QPen(Qt.black,1))
+        painter.setBrush(QBrush(Qt.red))
+        pa=QPointArray([1,4,1,6,4,6,4,8,6,8,6,10,8,10,8,12,11,12,15,8,15,7,12,4,1,4])
+        painter.drawPolygon(pa)
+        painter.drawLine(5,6,7,6)
+        painter.drawLine(7,8,9,8)
+        painter.drawPoint(9,10)
+        painter.setBrush(QBrush(QColor(192,192,255)))
+        pa=QPointArray([11,12,14,15,18,11,15,8,11,12])
+        painter.drawPolygon(pa)
+        painter.end()
+
+        #risanje pravokotnega izbiralnika
+        self.RECTG=QPixmap(20,20)
+        self.RECTG.fill()
+        painter=QPainter(self.RECTG)
+        painter.setPen(QPen(Qt.black,1))
+        painter.setBrush(QBrush(QColor(192,192,255)))
+        pa=QPointArray([2,2,2,17,17,17,17,2,2,2])
+        painter.drawPolygon(pa)
+        painter.setPen(QPen(Qt.white,1))
+        pa=QPointArray([2,5,2,7,2,9,2,11,2,13,2,15,4,17,6,17,8,17,10,17,12,17,14,17,17,14,17,12,17,10,17,8,17,6,17,4,15,2,13,2,11,2,9,2,7,2,5,2])
+        painter.drawPoints(pa,0,-1)
+        painter.setPen(QPen(Qt.black,1))
+        pa=QPointArray([6,6,6,13,13,13,13,6,6,6])
+        painter.drawPolygon(pa)
+        painter.drawLine(6,6,13,13)
+        painter.drawLine(6,13,13,6)
+        painter.setPen(QPen(QColor(192,192,255),1))
+        pa=QPointArray([6,9,6,10,9,13,10,13,13,10,13,9,10,6,9,6])
+        painter.drawPoints(pa,0,-1)
+        painter.end()
+
+        #risanje poligonskega izbiralnika
+        self.POLYG=QPixmap(20,20)
+        self.POLYG.fill()
+        painter=QPainter(self.POLYG)
+        painter.setPen(QPen(Qt.black,1))
+        painter.setBrush(QBrush(QColor(192,192,255)))
+        pa=QPointArray([1,5,3,15,9,13,14,18,18,12,18,11,13,2,13,1,1,5])
+        painter.drawPolygon(pa)
+        painter.end()
---
+++ orange/OrangeWidgets/Prototypes/OWGraphDrawerCanvas.py
@@ -0,0 +1,326 @@
+CIRCLE=0
+SQUARE=1
+ROUND_RECT=2
+
+NOTHING = 0
+ZOOMING = 1
+SELECT_RECTANGLE = 2
+SELECT_POLYGON = 3
+MOVE_SELECTION = 4
+
+from OWGraph import *
+from Numeric import *
+
+from orngScaleScatterPlotData import *
+
+class OWGraphDrawerCanvas(OWGraph):
+    def __init__(self, graphDrawWidget, parent = None, name = "None"):
+        OWGraph.__init__(self, parent, name)
+        self.parent = parent
+        self.labelText = []
+        self.vertices = {}         # slovar vozlisc oblike  orngIndex: vertex_objekt
+        self.edges = {}            # slovar povezav oblike  curveKey: edge_objekt
+        self.indexPairs = {}       # slovar oblike CurveKey: orngIndex   (za vozlisca)
+        self.selectedVertices = [] # seznam izbranih vozlisc (njihovih indeksov)
+        self.colorIndex = -1
+        self.visualizer = None
+        self.selectedCurve = None
+        self.selectedVertex = None
+        
+        self.vertexSize = 6
+        self.nVertices = 0
+        
+        self.enableXaxis(0)
+        self.enableYLaxis(0)
+        self.state = NOTHING  #default je rocno premikanje
+    
+    def moveVertex(self, pos):
+        # ce ni nic izbrano
+        if self.selectedCurve == None:
+            return
+
+        curve = self.curve(self.vertices[self.selectedVertex])  #self.selectedCurve je key
+
+        newX = self.invTransform(curve.xAxis(), pos.x())
+        newY = self.invTransform(curve.yAxis(), pos.y())
+
+        self.visualizer.xCoors[self.selectedVertex] = newX
+        self.visualizer.yCoors[self.selectedVertex] = newY
+
+        self.setCurveData(self.vertices[self.selectedVertex], [newX], [newY])
+
+        for e in range(self.nEdges):
+            (key,i,j) = self.edges[e]
+            
+            if i == self.selectedVertex:
+                currEdgeObj = self.curve(key)
+                self.setCurveData(key, [newX, currEdgeObj.x(1)], [newY, currEdgeObj.y(1)])                    
+            elif j == self.selectedVertex:
+                currEdgeObj = self.curve(key)
+                self.setCurveData(key, [currEdgeObj.x(0), newX], [currEdgeObj.y(0), newY])
+    
+    def onMouseMoved(self, event):
+        if self.mouseCurrentlyPressed and self.state == MOVE_SELECTION:
+            if len(self.selectedVertices) > 0:
+                border=self.vertexSize/2
+                maxx=max(take(self.visualizer.xCoors, self.selectedVertices))
+                maxy=max(take(self.visualizer.yCoors, self.selectedVertices))
+                minx=min(take(self.visualizer.xCoors, self.selectedVertices))
+                miny=min(take(self.visualizer.yCoors, self.selectedVertices))
+                #relativni premik v pikslih
+                dx=event.pos().x() - self.GMmouseStartEvent.x()
+                dy=event.pos().y() - self.GMmouseStartEvent.y()
+
+                maxx=self.transform(self.xBottom, maxx) + border + dx
+                maxy=self.transform(self.yLeft, maxy) + border + dy
+                minx=self.transform(self.xBottom, minx) - border + dx
+                miny=self.transform(self.yLeft, miny) - border + dy
+                maxx=self.invTransform(self.xBottom, maxx)
+                maxy=self.invTransform(self.yLeft, maxy)
+                minx=self.invTransform(self.xBottom, minx)
+                miny=self.invTransform(self.yLeft, miny)
+
+                if maxx >= self.axisScale(self.xBottom).hBound():
+                    return
+                if minx <= self.axisScale(self.xBottom).lBound():
+                    return
+                if maxy >= self.axisScale(self.yLeft).hBound():
+                    return
+                if miny <=self.axisScale(self.yLeft).lBound():
+                    return
+
+                for ind in self.selectedVertices:
+                    self.selectedVertex = ind
+                    self.selectedCurve = self.vertices[ind]
+
+                    vObj = self.curve(self.selectedCurve)
+                    tx = self.transform(vObj.xAxis(), vObj.x(0)) + dx
+                    ty = self.transform(vObj.yAxis(), vObj.y(0)) + dy
+                    tempPoint=QPoint(tx, ty)
+                    self.moveVertex(tempPoint)
+
+                self.GMmouseStartEvent.setX(event.pos().x())  #zacetni dogodek postane trenutni
+                self.GMmouseStartEvent.setY(event.pos().y())
+                self.replot()
+
+        else:
+            OWGraph.onMouseMoved(self, event)
+
+
+    def onMousePressed(self, event):
+        if self.state == MOVE_SELECTION:
+            self.mouseCurrentlyPressed = 1
+            if self.isPointSelected(self.invTransform(self.xBottom, event.pos().x()), self.invTransform(self.yLeft, event.pos().y())) and self.selectedVertices!=[]:
+                self.GMmouseStartEvent = QPoint(event.pos().x(), event.pos().y())
+                self.canvas().setMouseTracking(True)
+            else:
+                #pritisk na gumb izven izbranega podrocja ali pa ni izbranega podrocja
+                self.selectVertex(event.pos())
+                self.GMmouseStartEvent = QPoint(event.pos().x(), event.pos().y())
+                self.canvas().setMouseTracking(True)
+
+            self.removeAllSelections()
+        else:
+            OWGraph.onMousePressed(self, event)
+
+
+    def onMouseReleased(self, event):  
+        if self.state == MOVE_SELECTION:
+            self.mouseCurrentlyPressed = 0
+            self.canvas().setMouseTracking(False)
+            self.selectedCurve= None
+            self.selectedVertex=None
+            self.moveGroup=False
+            self.selectedVertices=[]
+            self.GMmouseStartEvent=None
+            
+        elif self.state == SELECT_RECTANGLE:
+            self.selectVertices()
+            OWGraph.onMouseReleased(self, event)
+
+        elif self.state == SELECT_POLYGON:
+                OWGraph.onMouseReleased(self, event)
+                if self.tempSelectionCurve == None:   #ce je OWVisGraph zakljucil poligon
+                    self.selectVertices()
+        else:
+            OWGraph.onMouseReleased(self, event)
+    
+    def selectVertices(self):
+        for vertexKey in self.indexPairs.keys():
+            vObj = self.curve(vertexKey)
+            
+            if self.isPointSelected(vObj.x(0), vObj.y(0)):
+                self.selectedVertices.append(self.indexPairs[vertexKey])
+                
+    def selectVertex(self, pos):
+        key, dist, xVal, yVal, index = self.closestCurve(pos.x(), pos.y())
+
+        if key >= 0 and dist < 15:
+            if key in self.indexPairs.keys():   #to se zgodi samo, ce vozlisce ni povezano
+                self.selectedVertices.append(self.indexPairs[key])
+                return
+
+            curve = self.curve(key)  #to je povezava, ker so bile te z insertCurve() dodane prej,
+                                     #da se ne vidijo skozi vozlisca
+            
+            for e in range(self.nEdges):
+                (keyTmp,i,j) = self.edges[e]
+                if keyTmp == key:
+                    ndx1 = i
+                    ndx2 = j
+
+            vOb1 = self.curve(self.vertices[ndx1])
+            vOb2 = self.curve(self.vertices[ndx2])
+
+            px = self.invTransform(curve.xAxis(), pos.x())
+            py = self.invTransform(curve.yAxis(), pos.y())
+
+            if self.dist([px, py], [vOb1.x(0), vOb1.y(0)]) <= self.dist([px, py], [vOb2.x(0), vOb2.y(0)]):
+                self.selectedVertices.append(ndx1)
+            else:
+                self.selectedVertices.append(ndx2)
+        else:
+            self.selectedVertices = []
+            
+    def dist(self, s1, s2):
+        return math.sqrt((s1[0]-s2[0])**2 + (s1[1]-s2[1])**2)
+    
+    def updateData(self):
+        self.removeDrawingCurves(removeLegendItems = 0)
+        self.removeMarkers()
+
+        # draw edges
+        for e in range(self.nEdges):
+            (key,i,j) = self.edges[e]
+            
+            x1 = self.visualizer.xCoors[i]
+            x2 = self.visualizer.xCoors[j]
+            y1 = self.visualizer.yCoors[i]
+            y2 = self.visualizer.yCoors[j]
+            
+            fillColor = Qt.blue#self.discPalette[classValueIndices[self.rawdata[i].getclass().value], 255*insideData[j]]
+            edgeColor = Qt.blue#self.discPalette[classValueIndices[self.rawdata[i].getclass().value]]
+
+            key = self.addCurve(str(e), fillColor, edgeColor, 0, style = QwtCurve.Lines, xData = [x1, x2], yData = [y1, y2])
+            self.edges[e] = (key,i,j)
+            
+        
+        
+        for v in range(self.nVertices):
+            x1 = self.visualizer.xCoors[v]
+            y1 = self.visualizer.yCoors[v]
+            
+            if self.colorIndex != -1:
+                if self.visualizer.graph.items.domain[self.colorIndex].varType == orange.VarTypes.Continuous:
+                    newColor = self.contPalette[self.noJitteringScaledData[self.colorIndex][v]]
+                elif self.visualizer.graph.items.domain[self.colorIndex].varType == orange.VarTypes.Discrete:
+                    newColor = self.discPalette[self.colorIndices[self.visualizer.graph.items[v][self.colorIndex].value]]
+            else: 
+                newColor = Qt.red #QColor(0,0,0)
+            
+            fillColor = newColor
+            edgeColor = newColor
+
+            key = self.addCurve(str(v), fillColor, edgeColor, 6, xData = [x1], yData = [y1])
+            self.vertices[v] = key
+            self.indexPairs[key] = v
+            
+        if len(self.labelText) > 0:
+            for v in range(self.nVertices):
+                x1 = self.visualizer.xCoors[v]
+                y1 = self.visualizer.yCoors[v]
+                lbl = ""
+                
+                for ndx in self.labelText:
+                    lbl = lbl + str(self.visualizer.graph.items[v][ndx]) + " "
+        
+                if lbl != '':
+                    mkey = self.insertMarker(lbl)
+                    self.marker(mkey).setXValue(float(x1))
+                    self.marker(mkey).setYValue(float(y1))
+                    self.marker(mkey).setLabelAlignment(Qt.AlignCenter + Qt.AlignBottom)            
+    
+    def setVertexColor(self, attribute):
+        if attribute == "(one color)":
+            self.colorIndex = -1
+        else:
+            i = 0
+            for var in self.visualizer.graph.items.domain.variables:
+                if var.name == attribute:
+                    self.colorIndex = i
+                    if var.varType == orange.VarTypes.Discrete: 
+                        self.colorIndices = getVariableValueIndices(self.visualizer.graph.items, self.colorIndex)
+                i += 1
+        pass
+        
+    def setLabelText(self, attributes):
+        self.labelText = []
+        if isinstance(self.visualizer.graph.items, orange.ExampleTable):
+            data = self.visualizer.graph.items
+            for att in attributes:
+                for i in range(len(data.domain)):
+                    if data.domain[i].name == att:
+                        self.labelText[:0] = [i]
+                        
+    def edgesContainsEdge(self, i, j):
+        for e in range(self.nEdges):
+            (key,iTmp,jTmp) = self.edges[e]
+            
+            if (iTmp == i and jTmp == j) or (iTmp == j and jTmp == i):
+                return True
+        return False
+        
+    def addVisualizer(self, visualizer):
+        self.visualizer = visualizer
+        self.clear()
+        
+        self.nVertices = visualizer.graph.nVertices
+        self.nEdges = 0
+        
+        #dodajanje vozlisc
+        for v in range(0, self.nVertices):
+            self.vertices[v] = None
+            
+        #dodajanje povezav
+        
+        for i in range(0, self.nVertices):
+            for j in range(0, self.nVertices):
+                try: 
+                    if visualizer.graph[i,j] > 0:
+                        if not self.edgesContainsEdge(i,j):
+                            self.edges[self.nEdges] = (None,i,j)
+                            self.nEdges += 1
+                except TypeError:
+                    pass # `visualizer.graph[i,j]' does not exist
+                else:
+                    pass # `visualizer.graph[i,j]' exists             
+    
+    def resetValues(self):
+        self.vertices={}
+        self.edges={}
+        self.indexPairs={}
+        #self.xCoors = None
+        #self.yCoors = None
+        self.n = 0
+    
+    def updateCanvas(self): #, xCoors, yCoors):
+        #self.xCoors = xCoors
+        #self.yCoors = yCoors
+        
+        self.setAxisAutoScaled()
+
+        #self.drawGraph(self.nVertices);
+        self.updateData()
+        self.replot()
+        
+        # preprecimo avtomatsko primikanje plota (kadar smo odmaknili neko skrajno tocko)
+        self.setAxisFixedScale()    
+    
+    def setAxisAutoScaled(self):
+        self.setAxisAutoScale(self.xBottom)
+        self.setAxisAutoScale(self.yLeft)
+
+    def setAxisFixedScale(self):
+        self.setAxisScale(self.xBottom, self.axisScale(self.xBottom).lBound(), self.axisScale(self.xBottom).hBound())
+        self.setAxisScale(self.yLeft, self.axisScale(self.yLeft).lBound(), self.axisScale(self.yLeft).hBound())
+        
