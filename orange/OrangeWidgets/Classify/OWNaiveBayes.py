9d457f5b7289d7d71c39e56b5790b9e9f7e0acf7
janezd <janez.demsar@fri.uni-lj.si>
2007-03-02 16:44:38 +0000

    - basically rewritten the widget; the visible changes are simplified arguments (removed unused choices for prob estimation) and use of grid layout for controls

--- orange/OrangeWidgets/Classify/OWNaiveBayes.py
+++ orange/OrangeWidgets/Classify/OWNaiveBayes.py
@@ -11,181 +11,159 @@ import OWGUI, orange
 from exceptions import Exception
 
 class OWNaiveBayes(OWWidget):
-    settingsList = ["m", "name", "probEstimation", "condProbEstimation", "condProbContEstimation", "adjustThreshold", "windowProportion"]
+    settingsList = ["m_estimator.m", "name", "probEstimation", "condProbEstimation", "adjustThreshold", "windowProportion"]
     
     def __init__(self, parent=None, signalManager = None, name='NaiveBayes'):
         OWWidget.__init__(self, parent, signalManager, name)
 
         self.inputs = [("Classified Examples", ExampleTableWithClass, self.cdata)]
         self.outputs = [("Learner", orange.Learner),("Naive Bayesian Classifier", orange.BayesClassifier)]
-                
-        # Settings
-        self.m = 2.0                        # m for probability estimation
-        self.name = 'Naive Bayes'           # name of the classifier/learner
-        self.probEstimation = 0             # relative frequency
-        self.condProbEstimation = 0         # relative frequency
-        self.condProbContEstimation = 4     # relative frequency
-        self.adjustThreshold = 0            # adjust threshold (for binary classes)
-        self.windowProportion = 0.5         # Percentage of instances taken in loess learning
-        self.loessPoints = 100              # Number of points in computation of LOESS
-
-        self.data = None                    # input data set
-        self.preprocessor = None            # no preprocessing as default
-        self.loadSettings()
-
-        self.m_estimator = orange.ProbabilityEstimatorConstructor_m()        
+            
+        self.m_estimator = orange.ProbabilityEstimatorConstructor_m()
         self.estMethods=[("Relative Frequency", orange.ProbabilityEstimatorConstructor_relative()),
                          ("Laplace", orange.ProbabilityEstimatorConstructor_Laplace()),
-                         ("m-Estimate", self.m_estimator)]
-        self.condEstMethods=[("", None),
+                         #("m-Estimate", self.m_estimator)
+                         ]
+        self.condEstMethods=[("<same as above>", None),
                              ("Relative Frequency", orange.ConditionalProbabilityEstimatorConstructor_ByRows(estimatorConstructor=orange.ProbabilityEstimatorConstructor_relative())),
                              ("Laplace", orange.ConditionalProbabilityEstimatorConstructor_ByRows(estimatorConstructor=orange.ProbabilityEstimatorConstructor_Laplace())),
                              ("m-Estimate", orange.ConditionalProbabilityEstimatorConstructor_ByRows(estimatorConstructor=self.m_estimator))]
-        self.condEstContMethods=[("", None),
-                             ("Relative Frequency", orange.ConditionalProbabilityEstimatorConstructor_ByRows(estimatorConstructor=orange.ProbabilityEstimatorConstructor_relative())),
-                             ("Laplace", orange.ConditionalProbabilityEstimatorConstructor_ByRows(estimatorConstructor=orange.ProbabilityEstimatorConstructor_Laplace())),
-                             ("m-Estimate", orange.ConditionalProbabilityEstimatorConstructor_ByRows(estimatorConstructor=self.m_estimator)),
-                             ("LOESS", orange.ConditionalProbabilityEstimatorConstructor_loess())]
 
+        self.m_estimator.m = 2.0
+        self.name = 'Naive Bayes'
+        self.probEstimation = 0
+        self.condProbEstimation = 0
+        self.adjustThreshold = 0
+        self.windowProportion = 0.5
+        self.loessPoints = 100
+        
+        self.data = None
+        self.loadSettings()
+
+        
         OWGUI.lineEdit(self.controlArea, self, 'name', box='Learner/Classifier Name', \
                  tooltip='Name to be used by other widgets to identify your learner/classifier.')
         OWGUI.separator(self.controlArea)
 
-        box = QVGroupBox(self.controlArea)
-        box.setTitle('Probability Estimation')
+        box = QGroupBox('Probability Estimation', self.controlArea)
+        glay = QGridLayout(box, 7, 3, 10, 5)
 
-        width = 150
-        itms = [e[0] for e in self.estMethods]
-        OWGUI.comboBox(box, self, 'probEstimation', items=itms, label='Unconditional:', labelWidth=width, orientation='horizontal',
-                       tooltip='Method to estimate unconditional probability.', callback=self.refreshControls)
-        itms = [e[0] for e in self.condEstMethods]
-        self.est2 = OWGUI.comboBox(box, self, 'condProbEstimation', items=itms, label='Conditional (discrete):', labelWidth=width, orientation='horizontal',
-                                   tooltip='Conditional probability estimation method used for discrete attributes.', callback=self.refreshControls)
+        glay.addWidget(OWGUI.separator(box, height=5), 0, 0)
+        
+        glay.addWidget(OWGUI.widgetLabel(box, "Unconditional"), 1, 0)
+       
+        glay.addWidget(OWGUI.comboBox(box, self, 'probEstimation', items=[e[0] for e in self.estMethods], tooltip='Method to estimate unconditional probability.'),
+                        1, 2)
+        
+        glay.addWidget(OWGUI.widgetLabel(box, "Conditional (for discrete)"), 2, 0)
+        glay.addWidget(OWGUI.comboBox(box, self, 'condProbEstimation', items=[e[0] for e in self.condEstMethods], tooltip='Conditional probability estimation method used for discrete attributes.', callback=self.refreshControls),
+                       2, 2)
 
-        itms = [e[0] for e in self.condEstContMethods]
-        self.est3 = OWGUI.comboBox(box, self, 'condProbContEstimation', items=itms, label='Conditional (continuous):', labelWidth=width, orientation='horizontal',
-                                   tooltip='Conditional probability estimation method used for continuous attributes.', callback=self.refreshControls)
-        self.est3.setEnabled(True)
-
-        OWGUI.separator(box)
-
+        glay.addWidget(OWGUI.widgetLabel(box, "     " + "Parameter for m-estimate" + " "), 3, 0)
         mValid = QDoubleValidator(self.controlArea)
         mValid.setRange(0,10000,1)
-        self.mwidget = OWGUI.lineEdit(box, self, 'm', label='Parameter for m-estimate:', labelWidth=width, orientation='horizontal', box=None, tooltip=None, callback=None, valueType = str, validator = mValid)
+        self.mwidget = OWGUI.lineEdit(box, self, 'm_estimator.m', valueType = float, validator = mValid)
+        glay.addWidget(self.mwidget, 3, 2)
 
+        glay.addWidget(OWGUI.separator(box), 4, 0)
+
+        glay.addWidget(OWGUI.widgetLabel(box, 'Size of LOESS window'), 5, 0)
         kernelSizeValid = QDoubleValidator(self.controlArea)
         kernelSizeValid.setRange(0,1,3)
-        self.loessWindow = OWGUI.lineEdit(box, self, 'windowProportion', label = 'Size of LOESS window:',
-                       labelWidth=width, orientation='horizontal', box=None, tooltip='Proportion of examples used for local learning in loess. Use 0 to learn from few local instances (3) and 1 to learn from all in the data set (this kind of learning is not local anymore).',
-                       callback=None, valueType = str, validator = kernelSizeValid)
+        glay.addWidget(OWGUI.lineEdit(box, self, 'windowProportion',
+                       tooltip='Proportion of examples used for local learning in loess.\nUse 0 to learn from few local instances (3) and 1 to learn from all in the data set (this kind of learning is not local anymore).',
+                       valueType = float, validator = kernelSizeValid),
+                       5, 2)
 
+        glay.addWidget(OWGUI.widgetLabel(box, 'LOESS sample points'), 6, 0)
         pointsValid = QIntValidator(20, 1000, self.controlArea)
-#        pointsValid.setRange(0,1,3)
-        self.loessPointsEdit = OWGUI.lineEdit(box, self, 'loessPoints', label = 'Number of points in LOESS:',
-                       labelWidth=width, orientation='horizontal', box=None, tooltip='Number of points in computation of LOESS (20-1000).',
-                       callback=None, valueType = str, validator = pointsValid)
+        glay.addWidget(OWGUI.lineEdit(box, self, 'loessPoints', 
+                       tooltip='Number of points in computation of LOESS (20-1000).',
+                       valueType = int, validator = pointsValid),
+                       6, 2)
         
         OWGUI.separator(self.controlArea)
 
         OWGUI.checkBox(self.controlArea, self, "adjustThreshold", "Adjust threshold (for binary classes)", box = "Threshold")
         
         OWGUI.separator(self.controlArea)
-        self.applyBtn = OWGUI.button(self.controlArea, self, "&Apply", callback=self.setLearner)
-        
-        self.refreshControls()
+        box = OWGUI.widgetBox(self.controlArea, "Apply", orientation=1)
+        applyButton = OWGUI.button(box, self, "&Apply", callback=self.applyLearner)
 
-        self.resize(150,100)
-        self.setLearner()                   # this just sets the learner, no data yet
+        self.refreshControls()
+        self.adjustSize()
+        self.applyLearner()
+
 
     def activateLoadedSettings(self):
-        self.setLearner()
+        self.applyLearner()
 
-    def setLearner(self):
-        if float(self.m) < 0:
-            self.error("Parameter m Out of Bounds: Parameter m should be positive!")
-            return
-        if float(self.windowProportion) < 0 or float(self.windowProportion) > 1:
-            self.error("Parameter windowProportion (for LOESS) Out of Bounds: Parameter should be between 0.0 and 1.0!")
-            return
-        self.learner = orange.BayesLearner()
-        self.learner.name = self.name
-        # set the probability estimation
-        self.m_estimator.m = float(self.m)
-        self.learner.estimatorConstructor = self.estMethods[self.probEstimation][1]
-        if self.condProbEstimation:
-            self.learner.conditionalEstimatorConstructor = self.condEstMethods[self.condProbEstimation][1]
-        if self.condProbContEstimation:
-            self.learner.conditionalEstimatorConstructorContinuous = self.condEstContMethods[self.condProbContEstimation][1]
-            if hasattr(self.learner.conditionalEstimatorConstructorContinuous, "windowProportion"):
-                setattr(self.learner.conditionalEstimatorConstructorContinuous, "windowProportion", float(self.windowProportion))
-            if hasattr(self.learner.conditionalEstimatorConstructorContinuous, "nPoints"):
-                setattr(self.learner.conditionalEstimatorConstructorContinuous, "nPoints", int(self.loessPoints))
-            
-                
-        for attr, cons in ( ("estimatorConstructor", self.estMethods[self.probEstimation][1]),
-                            ("conditionalEstimatorConstructor", self.condEstMethods[self.condProbEstimation][1]),
-                            ("conditionalEstimatorConstructorContinuous", self.condEstContMethods[self.condProbContEstimation][1])):
-            if cons:
-                setattr(self.learner, attr, cons)
-                if hasattr(cons, "m"):
-                    setattr(cons, "m", float(self.m))
 
-        self.learner.adjustThreshold = self.adjustThreshold
+    def refreshControls(self, *a):
+        self.mwidget.setEnabled(self.condProbEstimation==3)
+
+    def applyLearner(self):
+        if float(self.m_estimator.m) < 0:
+            self.error("Parameter m should be positive!")
+            self.learner = None
         
+        elif float(self.windowProportion) < 0 or float(self.windowProportion) > 1:
+            self.error("Window proportion for LOESS should be between 0.0 and 1.0!")
+            self.learner = None
+
+        else:
+            self.learner = orange.BayesLearner(name = self.name, adjustThreshold = self.adjustThreshold)
+            self.learner.estimatorConstructor = self.estMethods[self.probEstimation][1]
+            if self.condProbEstimation:
+                self.learner.conditionalEstimatorConstructor = self.condEstMethods[self.condProbEstimation][1]
+            self.learner.conditionalEstimatorConstructorContinuous = orange.ConditionalProbabilityEstimatorConstructor_loess(
+               windowProportion = self.windowProportion, nPoints = self.loessPoints)
+
         self.send("Learner", self.learner)
-        if self.data <> None:
+        self.applyData()
+        self.changed = False
+
+        
+    def applyData(self):
+        if self.data and self.learner:
             try:
-                self.classifier = self.learner(self.data)
-                self.classifier.setattr("data", self.data)
+                classifier = self.learner(self.data)
+                classifier.setattr("data", self.data)
+                classifier.name = self.name
+                self.error()
             except Exception, (errValue):
-                self.classifier = None
-                self.send("Naive Bayesian Classifier", self.classifier)
+                classifier = None
                 self.error("Naive Bayes error:"+str(errValue))
-                return            
-            self.classifier.name = self.name
-            self.send("Naive Bayesian Classifier", self.classifier)
-        self.error()
+        else:
+            classifier = None
+
+        self.send("Naive Bayesian Classifier", classifier)
+
+
+    def cdata(self,data):
+        if data and data.domain.classVar.varType != orange.VarTypes.Discrete:
+            self.error("This widget only works with discrete classes.")
+            self.data = None
+        else:
+            self.data = data
+            self.error()
+
+        self.applyData()
+        
 
     def sendReport(self):
         self.startReport(self.name)
         self.reportSection("Learning parameters")
         self.reportSettings([("Probability estimation", self.estMethods[self.probEstimation][0]),
                              ("Conditional probability", self.condEstMethods[self.condProbEstimation][0]),
-                             ("Continuous probabilities", self.condEstContMethods[self.condProbContEstimation][0]),
-                             self.mwidget.box.isEnabled and ("m for m-estimate", "%.1f" % self.m),
-                             self.loessWindow.box.isEnabled and ("LOESS window size", "%.1f" % self.windowProportion),
+                             self.mwidget.box.isEnabled and ("m for m-estimate", "%.1f" % self.m_estimator.m),
+                             ("LOESS window size", "%.1f" % self.windowProportion),
+                             ("Number of points in LOESS", "%i" % self.loessPoints)
                              ("Adjust classification threshold", OWGUI.YesNo[self.adjustThreshold])
                             ])
         self.finishReport()
             
-    def cdata(self,data):
-        if data and not data.domain.classVar:
-            self.error("This data set has no class.")
-            data = None
-        elif data and data.domain.classVar.varType != orange.VarTypes.Discrete:
-            self.error("This widget only works with discrete classes.")
-            data = None
-        else:
-            self.error()
-
-        self.data = data
-        if data:
-            self.setLearner()
-        else:
-            self.learner = None
-            self.classifier = None
-            self.send("Naive Bayesian Classifier", self.classifier)
-
-    def refreshControls(self):
-        self.mwidget.box.setEnabled(self.probEstimation==2 or self.condProbEstimation==3 or self.condProbContEstimation==3)
-        self.loessWindow.box.setEnabled(self.condProbEstimation==4)
-
-        self.est2.changeItem(QString("same (%s)" % self.estMethods[self.probEstimation][0]), 0)
-        if self.est2.currentItem(): # is est3 set to same?
-          self.est3.changeItem(QString("same (%s)" % self.condEstMethods[self.condProbEstimation][0]), 0)
-        else:
-          self.est3.changeItem(QString("same (%s)" % self.estMethods[self.probEstimation][0]), 0)
-
+            
 if __name__=="__main__":
     a=QApplication(sys.argv)
     ow=OWNaiveBayes()
