21a2ba7cdf7e96f49f84af61107f5d6e9d42fba2
miha <miha.stajdohar@gmail.com>
2007-04-06 12:01:09 +0000

    *** empty log message ***

---
+++ orange/orngNetwork.py
@@ -0,0 +1,122 @@
+import math
+import orange
+import orangeom
+
+from random import *
+from Numeric import *
+
+class GraphVisualizer:
+    def __init__(self, graph, parent = None, name = "None"):
+        
+        self.graph = graph
+        #self.graph.nVertices = len(graph.items)
+        #self.graph.nVertices = 4
+        self.parent = parent
+        self.maxWidth  = 1000
+        self.maxHeight = 1000
+        
+        self.attributeList = {}
+        self.attributeValues = {}
+        #self.data = None
+        self.xCoors = None
+        self.yCoors = None
+        
+        self.random()
+        
+        self.graphOptimization = orangeom.GraphOptimization()
+        self.graphOptimization.newData(self.graph, self.xCoors, self.yCoors)
+    
+    def getVars(self):
+        vars = []
+        if (self.graph != None):
+            if isinstance(self.graph.items, orange.ExampleTable):
+                vars[:0] = self.graph.items.domain.variables
+            
+                metas = self.graph.items.domain.getmetas()
+                for i, var in metas.iteritems():
+                    vars.append(var)
+        return vars
+    
+    def getData(self, i, j):
+        if self.graph.items is orange.ExampleTable:
+            return self.data[i][j]
+        elif self.graph.data is List:
+            return self.data[i][j]
+        
+    def nVertices(self):
+        return self.graph.nVertices
+  
+  
+    def fruchtermanReingold(self):
+        refreshRate = 150
+        tolerance = 5
+        initTemp = 100
+
+        self.graphOptimization.newData(self.graph, self.xCoors, self.yCoors)
+
+        while True:
+            (self.xCoors, self.yCoors, initTemp) = self.graphOptimization.fruchtermanReingold(refreshRate, initTemp)
+            
+            if (initTemp <= tolerance):
+                #self.postProcess()
+                self.parent.updateCanvas()
+                return
+            
+            self.parent.updateCanvas()
+        
+    def random(self):
+
+        self.xCoors = zeros(self.nVertices(), Float64) # 'f')  !!!
+        self.yCoors = zeros(self.nVertices(), Float64) # 'f')   !!!
+        
+        for i in range(0, self.nVertices()):
+            self.xCoors[i] = randint(0, self.maxWidth  - 1)
+            self.yCoors[i] = randint(0, self.maxHeight - 1)
+            
+            
+    #procedura za razporejanje nepovezanih vozlisc na kroznico okoli grafa
+    def postProcess(self):
+        UDist=20
+        pos1=where(sum(self.graph,1), 0, 1)
+        pos2=where(sum(self.graph,0), 0, 1)
+        pos=logical_and(pos1, pos2)  #iscemo SAMO TISTA, ki nimajo ne vhodnih ne izhodnih povezav!
+
+        ncCount=sum(pos)
+        if ncCount==0:  #ce je graf povezan
+            return
+
+        #else:
+        #max in min na povezanem delu grafa
+        conCoorsX=compress(logical_not(pos), self.xCoors)
+        conCoorsY=compress(logical_not(pos), self.yCoors)
+
+        if len(conCoorsX)==0:  #ce je celoten graf nepovezan
+            maxX=self.maxWidth
+            maxY=self.maxHeight
+            minX=0
+            minY=0
+        else:
+            maxX=max(conCoorsX)
+            maxY=max(conCoorsY)
+            minX=min(conCoorsX)
+            minY=min(conCoorsY)
+
+        cX=(maxX+minX)/2.0  #sredisce
+        cY=(maxY+minY)/2.0
+
+        R=max((abs(maxX)-abs(cX)), (abs(maxY)-abs(cY))) * sqrt(2) +UDist  #polmer kroga
+
+        angles=arange(0,(2*pi),2*pi/ncCount)  #radiani
+        allAngles=zeros(self.nVertices(), 'f')
+
+        #ta zanka ni v Numeric zato, ker je angles[] krajsi od allAngles[] (graf ni povezan)
+        count=0
+        for i in range(0, self.nVertices()):
+            if (pos[i]==1):
+                allAngles[i]=angles[count]
+                count+=1
+
+        ccX=R*cos(allAngles)+cX  #koordinate na kroznici
+        ccY=R*sin(allAngles)+cY
+        self.xCoors = where(pos, ccX, self.xCoors)
+        self.yCoors = where(pos, ccY, self.yCoors)
