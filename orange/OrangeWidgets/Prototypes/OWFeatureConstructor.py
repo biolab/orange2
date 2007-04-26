e3fa2519b467ba4f3b591c0e6cff0cd964042b06
janezd <janez.demsar@fri.uni-lj.si>
2007-04-26 13:15:40 +0000

    - saves contexts - other improvements

--- orange/OrangeWidgets/Prototypes/OWFeatureConstructor.py
+++ orange/OrangeWidgets/Prototypes/OWFeatureConstructor.py
@@ -1,6 +1,6 @@
 """
 <name>Feature Constructor</name>
-<description>Construct a new continuous attribute computed from existing attributes with an.</description>
+<description>Construct a new continuous attribute computed from existing attributes.</description>
 <icon>icons/FeatureConstructor.png</icon>
 <priority>11</priority>
 <contact>Janez Demsar (janez.demsar(@at@)fri.uni-lj.si)</contact>
@@ -39,6 +39,8 @@ class AttrComputer:
         
 
 class OWFeatureConstructor(OWWidget):
+    contextHandlers = {"": PerfectDomainContextHandler()}
+    
     def __init__(self,parent=None, signalManager = None):
         OWWidget.__init__(self, parent, signalManager, "FeatureConstructor")
 
@@ -53,8 +55,13 @@ class OWFeatureConstructor(OWWidget):
         
         self.selectedAttr = 0
         self.selectedFunc = 0
+        self.autosend = True
         
-        hb = OWGUI.widgetBox(self.controlArea, None, "horizontal")
+        db = OWGUI.widgetBox(self.controlArea, "Attribute definitions", addSpace = True)
+        
+        OWGUI.separator(db, 4)
+
+        hb = OWGUI.widgetBox(db, None, "horizontal")
         
         vb = OWGUI.widgetBox(hb, None, "vertical")
         self.leAttrName = OWGUI.lineEdit(vb, self, "attrname", "New attribute")
@@ -71,31 +78,38 @@ class OWFeatureConstructor(OWWidget):
         OWGUI.separator(hb, 8, 8)
         OWGUI.button(hb, self, "Clear", callback = self.clearLineEdits)
         
-        OWGUI.separator(self.controlArea, 12, 12)
+        OWGUI.separator(db)
 
-        hb = OWGUI.widgetBox(self.controlArea, None, "horizontal")
+        hb = OWGUI.widgetBox(db, None, "horizontal")
         OWGUI.button(hb, self, "Add", callback = self.addAttr)
         OWGUI.button(hb, self, "Update", callback = self.updateAttr)
         OWGUI.button(hb, self, "Remove", callback = self.removeAttr)
         OWGUI.button(hb, self, "Remove All", callback = self.removeAllAttr)
         
-        OWGUI.separator(self.controlArea)
-        self.lbDefinitions = OWGUI.listBox(self.controlArea, self, "selectedDef", "defLabels", callback=self.selectAttr)
+        OWGUI.separator(db)
+        self.lbDefinitions = OWGUI.listBox(db, self, "selectedDef", "defLabels", callback=self.selectAttr)
         self.lbDefinitions.setFixedHeight(160)
 
-        OWGUI.separator(self.controlArea)
-        OWGUI.button(self.controlArea, self, "Apply", callback = self.apply)
+        hb = OWGUI.widgetBox(self.controlArea, "Apply", "horizontal")
+        OWGUI.button(hb, self, "Apply", callback = self.apply)
+        cb = OWGUI.checkBox(hb, self, "autosend", "Apply automatically", callback=self.enableAuto)
+        cb.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
-        self.definitions = [("x", '"petal length"+2'), ("z", "x+2"), ("u", "x+z"), ("zz", '"petal length"**3+"sepal length"')]
-        self.loadDefinitions()
         self.adjustSize()
 
 
-    def loadDefinitions(self):
+    def settingsFromWidgetCallback(self, handler, context):
+        context.definitions = self.definitions
+
+
+    def settingsToWidgetCallback(self, handler, context):
+        self.definitions = getattr(context, "definitions", [])
         self.defLabels = ["%s := %s" % t for t in self.definitions]
         self.selectedDef = []
 
+
     def setData(self, data):
+        self.closeContext()
         if not self.data or self.data.domain != data.domain:
             self.clearLineEdits()
         self.data = data
@@ -104,21 +118,30 @@ class OWFeatureConstructor(OWWidget):
         if self.data:
             for attr in self.data.domain:
                 self.cbAttrs.insertItem(attr.name)
+        self.removeAllAttr()
+        self.openContext("", data)
+        self.apply()
         
         
     def clearLineEdits(self):
         self.expression = self.attrname = ""
         
+        
     def addAttr(self):
-        if not self.attrname:
+        attrname = self.attrname.strip()
+        if not attrname:
             self.leAttrName.setFocus()
             return
-        if not self.expression:
+        
+        expression = self.expression.strip()
+        if not expression:
             self.leExpression.setFocus()
             return
-        self.defLabels = self.defLabels + ["%s := %s" % (self.attrname, self.expression)]
-        self.definitions.append((self.attrname, self.expression))
+        self.defLabels = self.defLabels + ["%s := %s" % (attrname, expression)]
+        self.definitions.append((attrname, expression))
         self.expression = self.attrname = ""
+        self.applyIf()
+        
         
     def removeAttr(self):
         if self.selectedDef:
@@ -126,17 +149,25 @@ class OWFeatureConstructor(OWWidget):
             if 0 <= selected < self.lbDefinitions.count():
                 self.defLabels = self.defLabels[:selected] + self.defLabels[selected+1:]
                 del self.definitions[selected]
+                self.applyIf()
+
 
     def removeAllAttr(self):
         self.defLabels = []
         self.definitions = []
+        self.selectedDef = []
         self.clearLineEdits()
+        self.applyIf()
+        
         
     def updateAttr(self):
-        selected = self.selectedDef[0]
-        if 0 <= selected < self.lbDefinitions.count():
-            self.defLabels = self.defLabels[:selected] + ["%s := %s" % (self.attrname, self.expression)] + self.defLabels[selected+1:]
-            self.definitions[selected] = (self.attrname, self.expression)
+        if self.selectedDef:
+            selected = self.selectedDef[0]
+            if 0 <= selected < self.lbDefinitions.count():
+                self.defLabels = self.defLabels[:selected] + ["%s := %s" % (self.attrname, self.expression)] + self.defLabels[selected+1:]
+                self.definitions[selected] = (self.attrname, self.expression)
+                self.applyIf()
+    
     
     def selectAttr(self):
         selected = self.selectedDef[0]
@@ -145,6 +176,7 @@ class OWFeatureConstructor(OWWidget):
         else:
             self.attrname = self.expression = ""
         
+        
     def insertIntoExpression(self, what):
         if self.leExpression.hasMarkedText():
             self.leExpression.delChar()
@@ -152,6 +184,7 @@ class OWFeatureConstructor(OWWidget):
         cp = self.leExpression.cursorPosition()
         self.expression = self.expression[:cp] + what + self.expression[cp:]
         self.leExpression.setFocus()
+    
     
     def attrListSelected(self):
         if self.selectedAttr:
@@ -162,6 +195,7 @@ class OWFeatureConstructor(OWWidget):
             
             self.insertIntoExpression(attr)
             self.selectedAttr = 0
+        
         
     def funcListSelected(self):
         if self.selectedFunc:
@@ -178,7 +212,19 @@ class OWFeatureConstructor(OWWidget):
             self.selectedFunc = 0
         
     
+    def applyIf(self):
+        self.dataChanged = True
+        if self.autosend:
+            self.apply()
+            
+            
+    def enableAuto(self):
+        if self.dataChanged:
+            self.apply()
+
+    
     def apply(self):
+        self.dataChanged = False
         if not self.data:
             self.send("Examples", None)
             return
@@ -186,6 +232,12 @@ class OWFeatureConstructor(OWWidget):
         oldDomain = self.data.domain
 
         names = [d[0] for d in self.definitions]
+        for name in names:
+            if names.count(name)>1 or name in oldDomain > 1:
+                self.error(1, "Multiple attributes with the same name (%s)" % name)
+                self.send("Examples", None)
+                return
+            
         unknown = [[name, exp, set([id[0] or id[1] for id in re_identifier.findall(exp) if id[0] in names or id[1][1:-1] in names])] for name, exp in self.definitions]
         reinserted = {}
         replacer = IdentifierReplacer(reinserted, [n.name for n in oldDomain])
