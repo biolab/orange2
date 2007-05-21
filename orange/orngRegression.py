5ea7c0d9b73bd9216956872529b3f72130338ae6
blaz <blaz.zupan@fri.uni-lj.si>
2007-05-21 14:45:54 +0000

    some coding corrections

--- orange/orngRegression.py
+++ orange/orngRegression.py
@@ -5,6 +5,123 @@ from numpy import dot, sqrt
 import statc
 from string import join
 
+########################################################################
+# Linear Regression
+
+class LinearRegressionLearner(object):
+    def __new__(self, data=None, name='linear regression', **kwds):
+        learner = object.__new__(self, **kwds)
+        if data:
+            learner.__init__(name) # force init
+            return learner(data)
+        else:
+            return learner  # invokes the __init__
+
+    def __init__(self, name='linear regression', beta0 = True):
+        self.name = name
+        self.beta0 = beta0
+
+    def __call__(self, data, weight=None):
+        # missing values handling (impute missing)
+        imputer = orange.ImputerConstructor_model()
+        imputer.learnerContinuous = orange.MajorityLearner()
+        imputer.learnerDiscrete = orange.BayesLearner()
+        imputer = imputer(data)
+        data = imputer(data)
+ 
+        # continuization (replaces discrete with continuous attributes)
+        continuizer = orange.DomainContinuizer()
+        continuizer.multinomialTreatment = continuizer.FrequentIsBase
+        continuizer.zeroBased = True
+        domain0 = continuizer(data)
+        data = data.translate(domain0)
+ 
+        #   convertion to numpy
+        A, y, w = data.toNumpy()        # weights ??
+        n, m = numpy.shape(A)
+     
+        if self.beta0 == True:
+             X = numpy.insert(A,0,1,axis=1) # adds a column of ones
+        else:
+             X = A
+             
+        beta, resid, rank, s = numpy.linalg.lstsq(X,y)  # should also check for rank
+        
+        yEstimated = dot(X,beta)  # estimation
+ 
+        # some desriptive statistisc
+        muY, sigmaY = numpy.mean(y), numpy.std(y)
+        muX, covX = numpy.mean(A, axis = 0), numpy.cov(A, rowvar = 0)
+ 
+        # model statistics
+        SST, SSR = numpy.sum((y - muY) ** 2), numpy.sum((yEstimated - muY) ** 2)
+        SSE, RSquare = SST-SSR, SSR/SST
+        R = numpy.sqrt(RSquare) # coefficient of determination
+        RAdjusted = 1 - (1 - RSquare) * (n - 1) / (n - m - 1)
+        F = (SSR / m) / (SST - SSR / (n - m - 1)) # F statistisc
+        df = m - 1
+ 
+        sigmaSquare = SSE / (n-m-1)
+
+        # standard error of estimated coefficients
+        errCoeff = sqrt(sigmaSquare * inv(dot(X.T,X)).diagonal())
+ 
+        # t statistisc, significance
+        t = beta / errCoeff
+        significance = [statc.betai(df*0.5,0.5,df/(df+tt*tt)) for tt in t]
+ 
+        #   standardized coefficients
+        if self.beta0 == True:   
+             stdCoeff = (sqrt(covX.diagonal()) / sigmaY)  * beta[1:]
+        else:
+             stdCoeff = (sqrt(covX.diagonal()) / sigmaY)  * beta
+ 
+        model = {'descriptives': { 'meanX' : muX, 'covX' : covX, 'meanY' : muY, 'sigmaY' : sigmaY},
+                 'model' : {'estCoeff' : beta, 'stdErrorEstimation': errCoeff},
+                 'model summary': {'TotalVar' : SST, 'ExplVar' : SSE, 'ResVar' : SSR, 'R' : R, 'RAdjusted' : RAdjusted,
+                                   'F' : F, 't' : t, 'sig': significance}}
+ 
+        return LinearRegression(statistics = model, domain = data.domain, name = self.name, beta0 = self.beta0)
+
+class LinearRegression:
+    def __init__(self, **kwds):
+        self.__dict__ = kwds
+        self.beta = self.statistics['model']['estCoeff']
+
+    def __call__(self, example):
+        ex = orange.Example(self.domain, example)
+        ex = numpy.array(ex.native())
+
+        if self.beta0:
+            yhat = self.beta[0] + dot(self.beta[1:], ex[:-1])
+        else:
+            yhat = dot(self.beta, ex[:-1])
+         
+        return yhat
+
+def printLinearRegression(lr):
+    """pretty-prints linear regression model"""
+    beta = lr.beta
+    err = lr.statistics['model']['stdErrorEstimation']
+    t = lr.statistics['model summary']['t']
+    sig = lr.statistics['model summary']['sig'] 
+    beta0 = lr.beta0
+    
+    labels = ('Variable', 'Coeff Est', 'Std Error', 't-value', 'p')
+    print join(['%10s' % l for l in labels], ' ')
+
+    fmt = "%10s " + join(["%10.3f"]*4, " ")
+    if beta0 == True:
+        print fmt % ('Constant', beta[0], err[0], t[0], sig[0])
+        for i in range(len(lr.domain.attributes)-1):
+            print fmt % (lr.domain.attributes[i].name, beta[i+1], err[i+1], t[i+1], sig[i+1])
+    else:
+        for i in range(len(lr.domain.attributes)-1):
+            print fmt % (lr.domain.attributes[i].name, beta[i], err[i], t[i], sig[i])       
+
+########################################################################
+# Partial Least-Squares Regression (PLS)
+
 # Function is used in PLSRegression
 def standardize(matrix):
     mean = numpy.mean(matrix, axis = 0)
@@ -16,156 +133,27 @@ def standardize(matrix):
 def normalize(vector):
     return vector / numpy.linalg.norm(vector)
 
+class PLSRegressionLearner(object):
+    """PLSRegressionLearner(data, y, x=None, nc=None)"""
+    def __new__(self, data=None, name='PLS regression', **kwds):
+        learner = object.__new__(self, **kwds)
+        if data:
+            learner.__init__(name) # force init
+            return learner(data)
+        else:
+            return learner  # invokes the __init__
 
-def LinearRegression(examples=None, weightID=0, **kwds):
-    l=apply(LinearRegressionLearner, (), kwds)
-    if examples:
-        l=l(examples)
-    return l
+    def __init__(self, name='PLS regression', nc = None):
+        self.name = name
+        self.nc = nc
 
-class LinearRegressionLearner:
-    
-    def __init__(self, beta0 = True):
-        self.beta0 = beta0
-    
-    def __call__(self, data):
-       #   missing values
-       #
-       #   imputation
-       #   for dicrete atributes uses BayesLearner
-       #   and MajorityLearner for continuous
-       imputer = orange.ImputerConstructor_model()
-       imputer.learnerContinuous = orange.MajorityLearner()
-       imputer.learnerDiscrete = orange.BayesLearner()
-       imputer = imputer(data)
-       data = imputer(data)
+    def __call__(self, data, y, x=None, nc=None, weight=None):
+        if x == None:
+            x = [v for v in data.domain.variables if v not in y]
 
-       #   binarization
-       continuizer = orange.DomainContinuizer()
-       continuizer.multinomialTreatment = continuizer.FrequentIsBase
-       continuizer.zeroBased = True
-       domain0 = continuizer(data)
-       self.data = data.translate(domain0)
-
-       #   convertion to numpy
-       A, y, w = self.data.toNumpy()        # weights ??
-       n, m = numpy.shape(A)
-    
-       if self.beta0 == True:
-            X = numpy.insert(A,0,1,axis=1) # adds a column of ones
-            
-       else:
-            X = A
-            
-       beta, resid, rank, s = numpy.linalg.lstsq(X,y)  #ne uposteva (ne)polnosti ranga--- mozna singularnost
-       
-       yEstimated = dot(X,beta)  # estimation
-
-       #   some desriptive statistisc
-       muY, sigmaY = numpy.mean(y), numpy.std(y)
-       muX, covX = numpy.mean(A, axis = 0), numpy.cov(A, rowvar = 0)
-
-       #   some statistics
-       SST, SSR = numpy.sum((y - muY) ** 2), numpy.sum((yEstimated - muY) ** 2)
-       SSE, RSquare = SST-SSR, SSR/SST
-       R = numpy.sqrt(RSquare) # coefficient of determination
-       RAdjusted = 1 - (1 - RSquare) * (n - 1) / (n - m - 1)
-       F = (SSR / m) / (SST - SSR / (n - m - 1)) # F statistisc
-       df = m - 1
-
-       sigmaSquare = SSE / (n-m-1)
-       #   standard error of estimated coefficients
-       errCoeff = sqrt(sigmaSquare * inv(dot(X.T,X)).diagonal())
-
-       #   t statistisc, significance,...
-       t = beta / errCoeff
-       Significance = [statc.betai(df*0.5,0.5,df/(df+tt*tt)) for tt in t]
-
-       #   standardized coefficients
-       if self.beta0 == True:   
-            stdCoeff = (sqrt(covX.diagonal()) / sigmaY)  * beta[1:]
-       else:
-            stdCoeff = (sqrt(covX.diagonal()) / sigmaY)  * beta
-
-       statistics ={'descriptives': { 'meanX' : muX, 'covX' : covX, 'meanY' : muY, 'sigmaY' : sigmaY},
-                    'model' : {'estCoeff' : beta, 'stdErrorEstimation': errCoeff},
-                    'model summary': {'TotalVar' : SST, 'ExplVar' : SSE, 'ResVar' : SSR, 'R' : R, 'RAdjusted' : RAdjusted,
-                                      'F' : F, 't' : t, 'sig': Significance}
-                    }
-
-       return LinearRegressionClass(self.data, statistics, self.beta0)
-
-
-class LinearRegressionClass:
-   def __init__(self, data, statistics, beta0):
-       self.data = data
-       self.statistics = statistics
-       self.beta = statistics['model']['estCoeff']
-       self.beta0 = beta0
-
-   def __call__(self, example):
-       cexample = orange.Example(self.data.domain, example)
-       cexample = numpy.array(cexample.native())
-       if self.beta0 == True:
-           value = self.beta[0] + dot(self.beta[1:],cexample[:-1])
-       else:
-           value = dot(self.beta,cexample[:-1])
-        
-       return value
-
-   def prnt(self):
-       """prints some basic statistics of regression model"""
-       beta = self.beta
-       data = self.data
-       err = self.statistics['model']['stdErrorEstimation']
-       t = self.statistics['model summary']['t']
-       sig = self.statistics['model summary']['sig'] 
-       beta0 = self.beta0
-
-       labels = ('Variable', 'Coeff Est', 'Std Error', 't-value', 'p')
-       print join(['%10s' % l for l in labels], ' ')
-
-       fmt = "%10s " + join(["%10.3f"]*4, " ")
-       if beta0 == True:
-            print fmt % ('Constant', beta[0], err[0], t[0], sig[0])
-            for i in range(len(data[0])-1):
-                print fmt % (data.domain.attributes[i].name, beta[i+1], err[i+1], t[i+1], sig[i+1])
-       else:
-            for i in range(len(data[0])-1):
-                print fmt % (data.domain.attributes[i].name, beta[i], err[i], t[i], sig[i])       
-
-
-#
-#
-#       PLS Regression
-#
-#
-
-
-def PLSRegressionLearner(data = None, Ncomp = None, listY = None, listX = None, weightID=0, **kwds):
-    ''' Forms PLS regression model from data (example table)
-        using Ncomp components. Names of independent
-        variables are stored in list listX and names of response
-        variables are stored in list listY.
-    '''
-    l = apply(PLSRegressionLearnerClass, (), kwds)
-    if data:
-        l = l(data, Ncomp, listY, listX)
-    return l
-
-
-class PLSRegressionLearnerClass:
-    
-    def __call__(self, data, Ncomp, listY, listX = None):
-        
-        if listX == None:
-            listX = []
-            for i in data.domain.variables:
-                if listY.count(i.name) == 0:
-                    listX.append(i)
-
-        dataX = data.select(listX)
-        dataY = data.select(listY)
+        dataX = data.select(x)
+        dataY = data.select(y)
+        print y, dataY
         
         # transformation to numpy arrays
         X = dataX.toNumpy()[0]
@@ -220,57 +208,15 @@ class PLSRegressionLearnerClass:
         YE = dot(dot(T,B),C.T)*YStd + YMean
         Y = Y*numpy.std(Y, axis = 0)+ YMean
         BPls = dot(dot(numpy.linalg.pinv(P.T),B),C.T)    
-        return PLSRegression(data, BPls, YMean, YStd, XMean, XStd)
+        return PLSRegression(domain=data.domain, BPls=BPls, YMean=YMean, YStd=YStd, XMean=XMean, XStd=XStd, name=self.name)
 
 class PLSRegression:
-    
-   def __init__(self, data, BPls, YMean, YStd, XMean, XStd):
-       self.data = data
-       self.BPls = BPls
-       self.YMean = YMean
-       self.YStd = YStd
-       self.XMean = XMean
-       self.XStd = XStd
+    def __init__(self, **kwds):
+        self.__dict__ = kwds
 
-   def __call__(self, example):
-       example = numpy.array(example.native())
-       example = (example - self.XMean) / self.XStd
-       estimator = dot(example, self.BPls) * self.YStd + self.YMean        
-       return estimator
-
-    
-
-d = orange.ExampleTable('C://Delo//Python//Distance Learning//04-curatedF05.tab')
-ind = d.domain.index('smiles') 
-nd = orange.Domain(d.domain[0:ind-1] + d.domain[ind+1:], 0)
-data = orange.ExampleTable(nd, d)
-
-selY = ['growthC', 'growthE', 'dev', 'sporesC']
-
-selX = []
-for i in data.domain.variables:
-    if selY.count(i.name) == 0:
-        selX.append(i)
-        
-dataX = data.select(selX)
-dataY = data.select(selY)
-
-model = PLSRegressionLearner(data, 3, selY, selX)
-        
-X0 = dataX.toNumpy()[0]
-Y0 = dataY.toNumpy()[0]
-
-regressor = PLSRegression(model.data, model.BPls, model.YMean, model.YStd, model.XMean, model.XStd)
-est = regressor(dataX[1])
-print 'Original values:', dataY[1]
-print 'Estimated values:', est 
-print '\n'
-
-   
-
-if __name__ == "__main__":
-   data = orange.ExampleTable("C://ds//housing.tab")
-   lr = LinearRegression(data)
-   lr.prnt()
-   
- 
+    def __call__(self, example):
+       ex = orange.Example(self.domain, example)
+       ex = numpy.array(ex.native())
+       ex = (ex - self.XMean) / self.XStd
+       yhat = dot(ex, self.BPls) * self.YStd + self.YMean        
+       return yhat
