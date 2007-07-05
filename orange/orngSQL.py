50a0b0d2439a2946e4c842c0ca58ac2bccf54da7
polz <polz@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2007-07-05 08:41:46 +0000

    Merged orngSQLFile.py into orngSQL.py

--- orange/orngSQL.py
+++ orange/orngSQL.py
@@ -380,3 +380,41 @@ class SQLWriter(object):
     
     def disconnect(self):
         self.conn.disconnect()
+
+def loadSQL(filename, dontCheckStored = False, domain = None):
+    f = open(filename)
+    lines = f.readlines()
+    queryLines = []
+    discreteNames = None
+    uri = None
+    metaNames = None
+    className = None
+    for i in lines:
+        if i.startswith("--orng"):
+            (dummy, command, line) = i.split(None, 2)
+            if command == 'uri':
+                uri = eval(line)
+            elif command == 'discrete':
+                discreteNames = eval(line)
+            elif command == 'meta':
+                metaNames = eval(line)
+            elif command == 'class':
+                className = eval(line)
+            else:
+                queryLines.append(i)
+        else:
+            queryLines.append(i)
+    query = "\n".join(queryLines)
+    r = SQLReader(uri)
+    if discreteNames:
+        r.discreteNames = discreteNames
+    if className:
+        r.className = className
+    if metaNames:
+        r.metaNames = metaNames
+    r.execute(query)
+    data = r.data()
+    return data
+
+def saveSQL():
+    pass
