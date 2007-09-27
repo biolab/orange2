958e12e2795c066da228c3b6e57da43f0f0c7c7a
miha <miha.stajdohar@gmail.com>
2007-09-27 21:27:42 +0000

    added orngNetwork doc

---
+++ orange/doc/modules/network_constructor.py
@@ -0,0 +1,28 @@
+import orange
+from orngNetwork import NetworkOptimization
+from pylab import *
+
+# create graph object of type GraphAsList
+graph = orange.GraphAsList(5, 0)
+
+# set edges
+for i in range(4):
+    for j in range(i + 1, 5):
+        graph[i,j] = 1
+        
+# vertices are placed randomly in NetworkOptimization constructor
+network = NetworkOptimization(graph)
+
+# read all edges and plot a line
+for u, v in graph.getEdges():
+    x1, y1 = network.coors[u][0], network.coors[u][1]
+    x2, y2 = network.coors[v][0], network.coors[v][1]
+    plot([x1, x2], [y1, y2], 'b-')  
+              
+# read x and y coordinates to Python list
+x = [coordinate[0] for coordinate in network.coors]
+y = [coordinate[1] for coordinate in network.coors]
+
+# plot vertices
+plot(x, y, 'ro')
+show()
