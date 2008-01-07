9c12ada0fb4f79d2c528880d4086b285b8dd64a3
Gregor <Gregor@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-01-07 15:31:53 +0000

    *** empty log message ***

---
+++ orange/orngDebugging.py
@@ -0,0 +1,5 @@
+# a simple module used in orange to see whether debugging is enabled, where to write the output and how much output do you want to see 
+
+orngDebuggingEnabled = 0                            # do we want to enable debugging
+orngDebuggingFileName = "debuggingOutput.txt"       # where do we want to write output to
+orngVerbosity = 1                                   # what's the level of verbosity
