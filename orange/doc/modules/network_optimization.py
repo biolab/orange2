b45bf909b39a19e1afdaff14e8dd3bb42e975dd0
miha <miha.stajdohar@gmail.com>
2008-03-10 10:16:17 +0000

    *** empty log message ***

--- orange/doc/modules/network_optimization.py
+++ orange/doc/modules/network_optimization.py
@@ -1,15 +1,15 @@
-import orange
+import orangeom
 from orngNetwork import NetworkOptimization
 from pylab import *
 
 # create graph object of type GraphAsList
-graph = orange.GraphAsList(5, 0)
+graph = orangeom.Network(5, 0)
 
 # set edges
 for i in range(4):
     for j in range(i + 1, 5):
         graph[i,j] = 1
-        
+
 # vertices are placed randomly in NetworkOptimization constructor
 network = NetworkOptimization(graph)
 
@@ -18,13 +18,13 @@ network.fruchtermanReingold(100, 1000)
 
 # read all edges and plot a line
 for u, v in graph.getEdges():
-    x1, y1 = network.coors[u][0], network.coors[u][1]
-    x2, y2 = network.coors[v][0], network.coors[v][1]
-    plot([x1, x2], [y1, y2], 'b-')        
-        
+    x1, y1 = network.coors[0][u], network.coors[1][u]
+    x2, y2 = network.coors[0][v], network.coors[1][v]
+    plot([x1, x2], [y1, y2], 'b-')
+
 # read x and y coordinates to Python list
-x = [coordinate[0] for coordinate in network.coors]
-y = [coordinate[1] for coordinate in network.coors]
+x = network.coors[0]
+y = network.coors[1]
 
 # plot vertices
 plot(x, y, 'ro')
--- orange/doc/modules/network_read.py
+++ orange/doc/modules/network_read.py
@@ -11,13 +11,13 @@ network.readNetwork("K5.net")
 
 # read all edges and plot a line
 for u, v in network.graph.getEdges():
-    x1, y1 = network.coors[u][0], network.coors[u][1]
-    x2, y2 = network.coors[v][0], network.coors[v][1]
-    plot([x1, x2], [y1, y2], 'b-')        
-        
+    x1, y1 = network.coors[0][u], network.coors[1][u]
+    x2, y2 = network.coors[0][v], network.coors[1][v]
+    plot([x1, x2], [y1, y2], 'b-')
+
 # read x and y coordinates to Python list
-x = [coordinate[0] for coordinate in network.coors]
-y = [coordinate[1] for coordinate in network.coors]
+x = network.coors[0]
+y = network.coors[1]
 
 # plot vertices
 plot(x, y, 'ro')
