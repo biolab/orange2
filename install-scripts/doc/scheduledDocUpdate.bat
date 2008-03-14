05da3c7763d5fe89c0cec572bc7517144e29b491
tomazc <tomaz.curk@fri.uni-lj.si>
2008-03-14 17:14:14 +0000

    migration to SVN

--- install-scripts/doc/scheduledDocUpdate.bat
+++ install-scripts/doc/scheduledDocUpdate.bat
@@ -1,5 +1,5 @@
 cd ..\..
-"c:\program files\wincvs 1.3\cvsnt\cvs.exe" -d :pserver:cvso@estelle.fri.uni-lj.si:/CVS update -d doc
+svn update doc
 cd doc\widgets\catalog
 move default-web.htm default.htm
 python ..\..\..\scripts\doc\widgetCatalogs.py html widgetRegistry.xml . >> default.htm
