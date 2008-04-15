389f29822de36cfc1fd2d4c963b9d3b377133576
janezd <janez.demsar@fri.uni-lj.si>
2008-04-15 19:05:54 +0000

    - code formatting changes (tabs to spaces!)

--- orange/OrangeWidgets/Classify/OWRandomForest.py
+++ orange/OrangeWidgets/Classify/OWRandomForest.py
@@ -21,18 +21,18 @@ class OWRandomForest(OWWidget):
         self.outputs = [("Learner", orange.Learner),("Random Forest Classifier", orange.Classifier),("Choosen Tree", orange.TreeClassifier) ]
 
         self.name = 'Random Forest'
-	self.trees = 10
-	self.attributes = 0
-	self.attributesP = 5
-	self.preNodeInst = 1
-	self.preNodeInstP = 5
-	self.limitDepth = 0
-	self.limitDepthP = 3
-	self.rseed = 0
-	self.outtree = 0
-	
+        self.trees = 10
+        self.attributes = 0
+        self.attributesP = 5
+        self.preNodeInst = 1
+        self.preNodeInstP = 5
+        self.limitDepth = 0
+        self.limitDepthP = 3
+        self.rseed = 0
+        self.outtree = 0
 
-	self.maxTrees = 10000
+
+        self.maxTrees = 10000
 
         self.loadSettings()
 
@@ -46,8 +46,8 @@ class OWRandomForest(OWWidget):
         self.bBox = QVGroupBox(self.controlArea)
         self.bBox.setTitle('Basic Properties')
 
-        self.treesBox = OWGUI.spin(self.bBox, self, "trees", 1, self.maxTrees, orientation="horizontal", label="Number of trees in forest  ")
-	self.attributesBox, self.attributesPBox = OWGUI.checkWithSpin(self.bBox, self, "Consider exactly", 1, 10000, "attributes", "attributesP", " random attributes at each split.")
+        self.treesBox = OWGUI.spin(self.bBox, self, "trees", 1, self.maxTrees, orientation="horizontal", label="Number of trees in forest")
+        self.attributesBox, self.attributesPBox = OWGUI.checkWithSpin(self.bBox, self, "Consider exactly", 1, 10000, "attributes", "attributesP", " "+"random attributes at each split.")
         self.rseedBox = OWGUI.spin(self.bBox, self, "rseed", 0, 100000, orientation="horizontal", label="Seed for random generator ")
 
         OWGUI.separator(self.controlArea)
@@ -55,42 +55,42 @@ class OWRandomForest(OWWidget):
         self.pBox = QVGroupBox(self.controlArea)
         self.pBox.setTitle('Growth Control')
 
-	self.limitDepthBox, self.limitDepthPBox = OWGUI.checkWithSpin(self.pBox, self, "Maximum depth of individual trees  ", 1, 1000, "limitDepth", "limitDepthP", "")
+        self.limitDepthBox, self.limitDepthPBox = OWGUI.checkWithSpin(self.pBox, self, "Maximal depth of individual trees", 1, 1000, "limitDepth", "limitDepthP", "")
         self.preNodeInstBox, self.preNodeInstPBox = OWGUI.checkWithSpin(self.pBox, self, "Stop splitting nodes with ", 1, 1000, "preNodeInst", "preNodeInstP", " or fewer instances")
 
         OWGUI.separator(self.controlArea)
 
         #self.sBox = QVGroupBox(self.controlArea)
         #self.sBox.setTitle('Single Tree Output')
-	
-	self.streesBox = OWGUI.spin(self.controlArea, self, "outtree", -1, self.maxTrees, orientation="horizontal", label="Index of tree on the output ", callback=[self.period, self.extree])
-	#self.streesBox.setDisabled(True)
-	self.streeEnabled(False)
 
-	OWGUI.separator(self.controlArea)
+        self.streesBox = OWGUI.spin(self.controlArea, self, "outtree", -1, self.maxTrees, orientation="horizontal", label="Index of tree on the output", callback=[self.period, self.extree])
+        #self.streesBox.setDisabled(True)
+        self.streeEnabled(False)
 
-	self.btnApply = OWGUI.button(self.controlArea, self, "&Apply Changes", callback = self.setLearner, disabled=0)
+        OWGUI.separator(self.controlArea)
+
+        self.btnApply = OWGUI.button(self.controlArea, self, "&Apply Changes", callback = self.setLearner, disabled=0)
 
         self.resize(100,200)
 
-	self.setLearner()
+        self.setLearner()
 
     def period(self):
-	if self.outtree == -1: self.outtree = self.claTrees-1
-	elif self.outtree >= self.claTrees: self.outtree = 0
+        if self.outtree == -1: self.outtree = self.claTrees-1
+        elif self.outtree >= self.claTrees: self.outtree = 0
 
     def extree(self):
-	self.send("Choosen Tree", self.classifier.classifiers[self.outtree])
+        self.send("Choosen Tree", self.classifier.classifiers[self.outtree])
 
     def streeEnabled(self, status):
-	if status:
-		self.claTrees = self.trees
-		self.streesBox.setDisabled(False)
-		self.period()
-		self.extree()
-	else:
-		#a = 1
-		self.streesBox.setDisabled(True)
+        if status:
+            self.claTrees = self.trees
+            self.streesBox.setDisabled(False)
+            self.period()
+            self.extree()
+        else:
+            #a = 1
+            self.streesBox.setDisabled(True)
 
     def setLearner(self):
         pb = OWGUI.ProgressBar(self, iterations=self.trees)
@@ -98,23 +98,23 @@ class OWRandomForest(OWWidget):
         if hasattr(self, "btnApply"):
             self.btnApply.setFocus()
 
-	#assemble learner
-	rand = random.Random(self.rseed)
-	
+        #assemble learner
+        rand = random.Random(self.rseed)
+
         attrs = None
         if self.attributes: attrs = self.attributesP
 
-	self.learner = orngEnsemble.RandomForestLearner(trees = self.trees, rand=rand, attributes=attrs, callback=pb.advance)
-	
-	if self.preNodeInst: self.learner.learner.stop.minExamples = self.preNodeInstP 
+        self.learner = orngEnsemble.RandomForestLearner(trees = self.trees, rand=rand, attributes=attrs, callback=pb.advance)
+
+        if self.preNodeInst: self.learner.learner.stop.minExamples = self.preNodeInstP 
         else: self.learner.learner.stop.minExamples = 0
 
-	self.learner.learner.storeExamples = 1
-	self.learner.learner.storeNodeClassifier = 1
-	self.learner.learner.storeContingencies = 1
-	self.learner.learner.storeDistributions = 1
+        self.learner.learner.storeExamples = 1
+        self.learner.learner.storeNodeClassifier = 1
+        self.learner.learner.storeContingencies = 1
+        self.learner.learner.storeDistributions = 1
 
-	if self.limitDepth: self.learner.learner.maxDepth = self.limitDepthP
+        if self.limitDepth: self.learner.learner.maxDepth = self.limitDepthP
 
         self.learner.name = self.name
         self.send("Learner", self.learner)
@@ -125,16 +125,16 @@ class OWRandomForest(OWWidget):
             try:
                 self.classifier = self.learner(self.data)
                 self.classifier.name = self.name
-		self.streeEnabled(True)
+                self.streeEnabled(True)
             except Exception, (errValue):
                 self.error(str(errValue))
                 self.classifier = None
-		self.streeEnabled(False)
+                self.streeEnabled(False)
         else:
-		self.classifier = None
-		self.streeEnabled(False)
+            self.classifier = None
+            self.streeEnabled(False)
 
-	pb.finish()
+        pb.finish()
 
         self.send("Random Forest Classifier", self.classifier)
 
