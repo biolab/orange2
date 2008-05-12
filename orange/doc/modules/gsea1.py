067d00dadf39fcdeeb1955c89d93bd22bb858b17
markotoplak <devnull@localhost>
2008-05-12 11:47:55 +0000

    orngGsea documentation.

---
+++ orange/doc/modules/gsea1.py
@@ -0,0 +1,13 @@
+import orange, orngGsea
+
+data = orange.ExampleTable("iris")
+
+gen1 = dict([
+    ("sepal",["sepal length", "sepal width"]), 
+    ("petal",["petal length", "petal width", "petal color"])
+    ])
+
+res = orngGsea.runGSEA(data, minSize=2, geneSets=gen1)
+print "%5s  %6s %6s %s" % ("LABEL", "NES", "P-VAL", "GENES")
+for name,resu in res.items():
+    print "%5s  %6.3f %6.3f %s" % (name, resu[1], resu[2], str(resu[6]))
