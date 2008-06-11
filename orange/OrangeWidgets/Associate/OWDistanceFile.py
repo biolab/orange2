503f0738b07ae0ddc2b2ebc525b30a2e7c464e85
janezd <janez.demsar@fri.uni-lj.si>
2008-06-11 23:22:50 +0000

    - replaced checkbox with radio buttons to make the GUI nicer and more intuitive - better diagnostics when something is wrong with the file - the widget did not change labels when the ExampleTable was removed from the input. Fixed.

--- orange/OrangeWidgets/Associate/OWDistanceFile.py
+++ orange/OrangeWidgets/Associate/OWDistanceFile.py
@@ -8,6 +8,7 @@
 
 import orange
 import OWGUI
+import exceptions
 from qt import *
 from OWWidget import *
 
@@ -24,15 +25,17 @@ class OWDistanceFile(OWWidget):
         self.fileIndex = 0
         self.takeAttributeNames = False
         self.data = None
+        self.matrix = None
         self.loadSettings()
 
-        box = OWGUI.widgetBox(self.controlArea, "Data File")
+        box = OWGUI.widgetBox(self.controlArea, "Data File", addSpace=True)
         hbox = OWGUI.widgetBox(box, orientation = 0)
         self.filecombo = OWGUI.comboBox(hbox, self, "fileIndex", callback = self.loadFile)
         self.filecombo.setMinimumWidth(250)
         button = OWGUI.button(hbox, self, '...', callback = self.browseFile)
         button.setMaximumWidth(25)
-        OWGUI.checkBox(box, self, "takeAttributeNames", "Take attribute names (instead of examples)", callback = self.relabel)
+        self.rbInput = OWGUI.radioButtonsInBox(self.controlArea, self, "takeAttributeNames", ["Use examples as items", "Use attribute names"], "Items from input data", callback = self.relabel)
+        self.rbInput.setDisabled(True)
 
         self.adjustSize()            
 
@@ -68,6 +71,7 @@ class OWDistanceFile(OWWidget):
         self.filecombo.updateGeometry()
 
         self.error()
+        msg = None
         try:
             fle = open(fn)
             while 1:
@@ -75,47 +79,70 @@ class OWDistanceFile(OWWidget):
                 if lne:
                     break
             spl = lne.split()
-            dim = int(spl[0])
+            try:
+                dim = int(spl[0])
+            except:
+                msg = "Matrix dimension expected in the first line"
+                raise exceptions.Exception
+            
             labeled = len(spl) > 1 and spl[1] in ["labelled", "labeled"]
             self.matrix = matrix = orange.SymMatrix(dim)
-            self.matrix.setattr("items", [""] * dim)
+            if labeled:
+                self.labels = []
+            else:
+                self.labels = [""] * dim
             for li, lne in enumerate(fle):
+                if li > dim:
+                    if not li.strip():
+                        continue
+                    msg = "File too long"
+                    raise exceptions.IndexError
                 spl = lne.split("\t")
                 if labeled:
-                    self.matrix.items[li] = spl[0].strip()
+                    self.labels.append(spl[0].strip())
                     spl = spl[1:]
+                if len(spl) > dim:
+                    msg = "Line %i too long" % li+2
+                    raise exceptions.IndexError
                 for lj, s in enumerate(spl):
                     if s:
-                        self.matrix[li, lj] = float(s)
+                        try:
+                            self.matrix[li, lj] = float(s)
+                        except:
+                            msg = "Invalid number in line %i, column %i" % (li+2, lj)
+                            raise exceptions.Exception 
 
             self.relabel()
         except:
-            self.error("Error while reading the file")
+            self.error(msg or "Error while reading the file")
 
     def relabel(self):
         self.error()
         matrix = self.matrix
         
-        if self.data:
+        if matrix and self.data:
             if self.takeAttributeNames:
                 domain = self.data.domain
                 if matrix.dim == len(domain.attributes):
-                    matrix.items = domain.attributes
+                    matrix.setattr("items", domain.attributes)
                 elif matrix.dim == len(domain.variables):
-                    matrix.items = domain.variables
+                    matrix.setattr("items", domain.variables)
                 else:
                     self.error("The number of attributes doesn't match the matrix dimension")
 
             else:
                 if matrix.dim == len(self.data):
-                    matrix.items = self.data
+                    matrix.setattr("items", self.data)
                 else:
                     self.error("The number of examples doesn't match the matrix dimension")
+        else:
+            matrix.setattr("items", self.labels)
 
         self.send("Distance Matrix", matrix)
 
     def getExamples(self, data):
         self.data = data
+        self.rbInput.setDisabled(data is None)
         self.relabel()
 
 if __name__=="__main__":
