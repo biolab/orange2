921638b81368c60af9c9691198018e07eadef9b8
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2008-08-20 11:37:04 +0000

    -added leaf ordering function

--- orange/orngClustering.py
+++ orange/orngClustering.py
@@ -1,5 +1,99 @@
 import orange
-import math
+import math, sys
+
+def orderLeafs(tree, matrix):
+    """Order the leafs in the clustering tree (based on 'Fast optimal leaf ordering for herarchical clustering. Ziv Bar-Joseph et al.')
+    Arguments:
+        tree   --binary hierarchical clustering tree of type orange.HierarchicalCluster
+        matrix --orange.SymMatrix that was used to compute the clustering
+    """
+    objects = getattr(tree.mapping, "objects", None)
+    tree.mapping.setattr("objects", range(len(tree)))
+    M = {}
+    ordering = {}
+    def _optOrdering(tree):
+        if len(tree)==1:
+            for leaf in tree:
+                M[tree, leaf, leaf] = 0
+##                print "adding:", tree, leaf, leaf
+        else:
+            _optOrdering(tree.left)
+            _optOrdering(tree.right)
+##            print "ordering", [i for i in tree]
+            Vl = set(tree.left)
+            Vr = set(tree.right)
+            Vlr = set(tree.left.right or tree.left)
+            Vll = set(tree.left.left or tree.left)
+            Vrr = set(tree.right.right or tree.right)
+            Vrl = set(tree.right.left or tree.right)
+            other = lambda e, V1, V2: V2 if e in V1 else V1
+            for u in Vl:
+                for w in Vr:
+                    if not True:
+                        C = min([matrix[m, k] for m in other(u, Vll, Vlr) for k in other(w, Vrl, Vrr)])
+                        orderedMs = sorted(other(u, Vll, Vlr), key=lambda m: M[tree.left, u, m])
+                        orderedKs = sorted(other(w, Vrl, Vrr), key=lambda k: M[tree.right, w, k])
+                        k0 = orderedKs[0]
+                        curMin = 1e30000 
+                        curMK = ()
+                        for m in orderedMs:
+                            if M[tree.left, u, m] + M[tree.right, w, k0] + C >= curMin:
+                                break
+                            for k in  orderedKs:
+                                if M[tree.left, u, m] + M[tree.right, w, k] + C >= curMin:
+                                    break
+                                if curMin > M[tree.left, u, m] + M[tree.right, w, k] + matrix[m, k]:
+                                    curMin = M[tree.left, u, m] + M[tree.right, w, k] + matrix[m, k]
+                                    curMK = (m, k)
+                        M[tree, u, w] = M[tree, w, u] = curMin
+                        ordering[tree, u, w] = (tree.left, u, curMK[0], tree.right, w, curMK[1])
+                        ordering[tree, w, u] = (tree.right, w, curMK[1], tree.left, u, curMK[0])
+                    else:
+                        def MFunc((m, k)):
+                            return M[tree.left, u, m] + M[tree.right, w, k] + matrix[m, k]
+                        m, k = min([(m, k) for m in other(u, Vll, Vlr) for k in other(w, Vrl, Vrr)], key=MFunc)
+                        M[tree, u, w] = M[tree, w, u] = MFunc((m, k))
+                        ordering[tree, u, w] = (tree.left, u, m, tree.right, w, k)
+                        ordering[tree, w, u] = (tree.right, w, k, tree.left, u, m)
+        
+    _optOrdering(tree)
+
+    def _order(tree, u, w):
+        if len(tree)==1:
+            return
+        left, u, m, right, w, k = ordering[tree, u, w]
+        if len(left)>1 and m not in left.right:
+            left.swap()
+        _order(left, u, m)
+##        if u!=left[0] or m!=left[-1]:
+##            print "error 4:", u, m, list(left)
+        if len(right)>1 and k not in right.left:
+            right.swap()
+        _order(right, k, w)
+##        if k!=right[0] or w!=right[-1]:
+##            print "error 5:", k, w, list(right)
+    
+    u, w = min([(u, w) for u in tree.left for w in tree.right], key=lambda (u, w): M[tree, u, w])
+    
+##    print "M(v) =", M[tree, u, w]
+    
+    _order(tree, u, w)
+
+    def _check(tree, u, w):
+        if len(tree)==1:
+            return
+        left, u, m, right, w, k = ordering[tree, u, w]
+        if tree[0] == u and tree[-1] == w:
+            _check(left, u, m)
+            _check(right, k, w)
+        else:
+            print "Error:", u, w, tree[0], tree[-1]
+
+    _check(tree, u ,w)            
+        
+
+    if objects:
+        tree.mapping.setattr("objects", objects)
 
 import Image, ImageDraw, ImageFont
 
@@ -85,7 +179,7 @@ class DendrogramPlot(object):
         """
         self.clusterColors = clusterColors
         
-    def _getColorScheme(self):
+    def _getColorScheme(self, gamma=1.0):
         vals = [float(val) for ex in self.data for val in ex if not val.isSpecial() and val.variable.varType==orange.VarTypes.Continuous] or [0]
         avg = sum(vals)/len(vals)
         maxVal, minVal = max(vals), min(vals)
@@ -159,29 +253,37 @@ class DendrogramPlot(object):
                     else:
                         pass #TODO indicate a missing value
 ##        for i, (label, row) in enumerate(zip(labels, matrix)):
-        
+
+        rows = []        
         for i, el in enumerate(self.tree):
-            el = self.tree.mapping[i] # in case el mapping has objects and el is not an integer
+            el = self.tree.mapping[i] # in case mapping has objects and el is not an integer
             label = self.labels[el]
 ##            print label, el, i
-            self.painter.text((textAreaStart, topMargin+i*hAdvance), self._truncText(label, textAreaWidth), font=self.font, fill=self.defaultTextColor)
+            try:
+                self.painter.text((textAreaStart, topMargin+i*hAdvance), self._truncText(label, textAreaWidth), font=self.font, fill=self.defaultTextColor)
+            except IOError, err:
+                print err
+                print label
             if self.data:
                 row = self.data[el]
+                rows.append(row)
                 _drawMatrixRow(row, topMargin+i*hAdvance)
-
+##        if self.data:
+##            import orangene
+##            map = orangene.HeatmapConstructor(orange.ExampleTable(rows), None) 
         self.image.save(file, "PNG")
     
 
 if __name__=="__main__":
-    data = orange.ExampleTable("doc//datasets//brown-selected.tab")
+##    data = orange.ExampleTable("doc//datasets//brown-selected.tab")
 ##    data = orange.ExampleTable("doc//datasets//iris.tab")
-##    data = orange.ExampleTable("doc//datasets//zoo.tab")
+    data = orange.ExampleTable("doc//datasets//zoo.tab")
 ##    data = orange.ExampleTable("doc//datasets//titanic.tab")
 ##    m = [[], [ 3], [ 2, 4], [17, 5, 4], [ 2, 8, 3, 8], [ 7, 5, 10, 11, 2], [ 8, 4, 1, 5, 11, 13], [ 4, 7, 12, 8, 10, 1, 5], [13, 9, 14, 15, 7, 8, 4, 6], [12, 10, 11, 15, 2, 5, 7, 3, 1]]
 ##    matrix = orange.SymMatrix(m)
     dist = orange.ExamplesDistanceConstructor_Euclidean(data)
     matrix = orange.SymMatrix(len(data))
-    matrix.setattr('items', data)
+##    matrix.setattr('items', data)
     for i in range(len(data)):
         for j in range(i+1):
             matrix[i, j] = dist(data[i], data[j])
@@ -189,8 +291,14 @@ if __name__=="__main__":
 ##    root.mapping.objects = [str(ex.getclass()) for ex in data]
     d = DendrogramPlot(root, data=data, labels=[str(ex.getclass()) for ex in data], width=500, height=2000)
     d.SetMatrixColorScheme((0, 255, 0), (255, 0, 0))
-    d.SetClusterColors({root.left:(0,255,0), root.right:(0,0,255)})
-    d.Plot()
+##    d.SetClusterColors({root.left:(0,255,0), root.right:(0,0,255)})
+    d.Plot("graph.png")
+    print "Sum:", sum([matrix[root.mapping[i], root.mapping[i+1]] for i in range(len(root.mapping)-1)])
+    orderLeafs(root, matrix)
+    print "Sum:", sum([matrix[root.mapping[i], root.mapping[i+1]] for i in range(len(root.mapping)-1)])
+    d = DendrogramPlot(root, data=data, labels=[str(ex.getclass()) for ex in data], width=500, height=2000)
+    d.SetMatrixColorScheme((0, 255, 0), (255, 0, 0))
+    d.Plot("graphOrdered.png")
     
     
         
