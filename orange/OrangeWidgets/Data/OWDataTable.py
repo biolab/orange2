d0badfbd529c7a9f22acebcbff6c708d89b17725
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2008-08-28 09:25:33 +0000

    -fixed table sorting

--- orange/OrangeWidgets/Data/OWDataTable.py
+++ orange/OrangeWidgets/Data/OWDataTable.py
@@ -20,6 +20,8 @@ import math
 
 ##############################################################################
 
+OrangeValueRole = Qt.UserRole + 1
+
 class OWDataTable(OWWidget):
     settingsList = ["showDistributions", "showMeta", "distColorRgb"]
 
@@ -212,8 +214,10 @@ class OWDataTable(OWWidget):
                 bgColor = white
 
             for i in range(numEx):
-                OWGUI.tableItem(table, i,j, str(data[i][key]), backColor = bgColor)
-##                OWGUI.tableItem(table, i,j, data[i][key].native(), backColor = bgColor)
+##                table.setItem(i, j, TableWidgetItem(data[i][key]
+##                OWGUI.tableItem(table, i,j, str(data[i][key]), backColor = bgColor)
+                item = OWGUI.tableItem(table, i,j, data[i][key].native(), backColor = bgColor)
+                item.setData(OrangeValueRole, QVariant(str(data[i][key])))
 
         table.resizeRowsToContents()
         table.resizeColumnsToContents()
@@ -334,7 +338,8 @@ class TableItemDelegate(QItemDelegate):
                     painter.fillRect(option.rect.adjusted(0,0,-smallerWidth,0), self.widget.distColor)
 ##            text = self.widget.locale.toString(value)    # we need this to convert doubles like 1.39999999909909 into 1.4
 ##        else:
-        text = index.data(Qt.DisplayRole).toString()
+##        text = index.data(Qt.DisplayRole).toString()
+        text = index.data(OrangeValueRole).toString()
 
         self.drawDisplay(painter, option, option.rect, text)
         painter.restore()
