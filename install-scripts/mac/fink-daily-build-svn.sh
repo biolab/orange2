a0ebab42b6cdcbb8bbc9fa139f0efd33af19434b
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-09-04 15:47:31 +0000

    Updated Fink packages and daily-build Fink script.

--- install-scripts/mac/fink-daily-build-svn.sh
+++ install-scripts/mac/fink-daily-build-svn.sh
@@ -16,12 +16,31 @@ svn export http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink/
 mv fink/* finkinfo/
 rm -rf fink/
 
-fink scanpackages
+# Get all official Fink package info files
 fink selfupdate
+fink scanpackages
 
+# Updates everything (probably by compiling new packages)
 fink update-all
 
-cp /sw/fink/debs/*.deb /Volumes/fink/10.5/svn/
+# Copies to repository all binary packages
+# (Maybe it would be better to rsync them? This also keeps obsolete and maybe non-existent packages in repository.)
+cp /sw/fink/debs/*.deb /Volumes/fink/dists/10.5/main/binary-darwin-i386/
 
-cd /Volumes/fink/10.5/
-dpkg-scanpackages svn /dev/null | gzip - > svn/Packages.gz
+# Removes old binary packages
+# (Versions of packages which have more then 5 versions and those old versions are more than one month old.)
+cd /Volumes/fink/dists/10.5/main/binary-darwin-i386/
+perl -e '
+for (<*.deb>) {
+	m/(.*?)_/;
+	$fs{$1}++;
+}
+while (($f,$n) = each(%fs)) {
+	next if $n <= 5;
+	unlink for grep {-M > 30} <$f_*.deb>;
+}
+'
+
+# Makes new packages list
+cd /Volumes/fink/
+perl -mFink::Scanpackages -e 'Fink::Scanpackages->scan("dists/10.5/main/binary-darwin-i386/");' | gzip - > dists/10.5/main/binary-darwin-i386/Packages.gz
