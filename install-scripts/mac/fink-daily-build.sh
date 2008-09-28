c8f8064b189f00d95a921e1d1cf01e51c595ec35
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-09-28 10:19:25 +0000

    Updated daily-build Fink script.

--- install-scripts/mac/fink-daily-build.sh
+++ install-scripts/mac/fink-daily-build.sh
@@ -4,7 +4,15 @@
 #
 
 STABLE_PACKAGES="orange-py25 orange orange-genomics-py25 orange-genomics"
-TRUNK_PACKAGES="orange-svn-py25 orange-svn orange-genomics-svn-py25 orange-genomics-svn"
+DAILY_PACKAGES="orange-svn-py25 orange-svn orange-genomics-svn-py25 orange-genomics-svn"
+
+# A list of packages (dependencies) from which user can choose upon installing our packages
+# We would like to build all those so that it does not need to compile anything whichever packages he or she chooses
+# The problem is that they are often mutually conflicting so we cannot have them simply installed (so that update-all
+# would update them) but have to build them explicitly
+OTHER_PACKAGES="giflib libungif ghostscript ghostscript-esp ghostscript6 ghostscript-nox ghostscript6-nox ptex-base ptex-nox-base jadetex docbook-utils tetex-base tetex-nox-base"
+
+FINK_ARGS="--yes --build-as-nobody"
 
 ((`id -u` == 0)) || { echo "Must run as root user (use sudo)."; exit 1; }
 
@@ -21,10 +29,10 @@ rm -f /sw/fink/dists/ailab/main/finkinfo/*
 svn export --force --non-interactive http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink/ /sw/fink/dists/ailab/main/finkinfo/
 
 STABLE_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/branches/ver1.0/ | grep 'Last Changed Rev:' | cut -d ' ' -f 4`
-TRUNK_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/trunk/ | grep 'Last Changed Rev:' | cut -d ' ' -f 4`
+DAILY_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/trunk/ | grep 'Last Changed Rev:' | cut -d ' ' -f 4`
 
 # Inject revision versions into templates
-perl -pi -e "s/__REVISION__/$TRUNK_REVISION/g" /sw/fink/dists/ailab/main/finkinfo/*svn*.info
+perl -pi -e "s/__REVISION__/$DAILY_REVISION/g" /sw/fink/dists/ailab/main/finkinfo/*svn*.info
 perl -pi -e "s/__REVISION__/$STABLE_REVISION/g" /sw/fink/dists/ailab/main/finkinfo/*.info
 
 # Adds our info files repository to local Fink configuration
@@ -34,18 +42,29 @@ grep '^Trees:' /sw/etc/fink.conf | grep -q 'ailab/main' || perl -p -i -l -e '$_ 
 grep -q 'deb http://www.ailab.si/orange/fink 10.5 main' /sw/etc/apt/sources.list || echo 'deb http://www.ailab.si/orange/fink 10.5 main' >> /sw/etc/apt/sources.list
 
 # Get all official Fink package info files
-fink --yes selfupdate
-fink --yes scanpackages
+fink $FINK_ARGS selfupdate
+fink $FINK_ARGS scanpackages
 
 # Updates everything (probably by compiling new packages)
-fink --yes update-all
+fink $FINK_ARGS update-all
 
-# Builds our packages if they have not been rebuilt already
-fink --yes purge $TRUNK_PACKAGES
-fink --yes build $STABLE_PACKAGES
-fink --yes purge $STABLE_PACKAGES
-fink --yes build $TRUNK_PACKAGES
-fink --yes purge $TRUNK_PACKAGES
+# Stores current packages status
+dpkg --get-selections '*' > /tmp/dpkg-selections.list
+
+for package in $OTHER_PACKAGES $STABLE_PACKAGES $DAILY_PACKAGES ; do
+	# Restores intitial packages status
+	dpkg --set-selections < /tmp/dpkg-selections.list
+	
+	# Builds a package if it has not been rebuilt already (for example, as a dependency)
+	fink $FINK_ARGS build $package
+done
+
+# Restores intitial packages status
+dpkg --set-selections < /tmp/dpkg-selections.list
+rm -f /tmp/dpkg-selections.list
+
+# Cleans unncessary files
+fink $FINK_ARGS cleanup --all
 
 # Prepares our public Fink package info and binary files repository
 mkdir -p /Volumes/fink/dists/10.5/main/binary-darwin-i386/
