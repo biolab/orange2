562a1c1c657e460c1919a6fc2972684345ecddd5
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-09-28 11:04:23 +0000

    Updated daily-build Fink script.

--- install-scripts/mac/fink-daily-build.sh
+++ install-scripts/mac/fink-daily-build.sh
@@ -21,27 +21,33 @@ FINK_ARGS="--yes --build-as-nobody"
 # Configures environment for Fink
 test -r /sw/bin/init.sh && . /sw/bin/init.sh
 
-# Prepares our Fink package info files repository
+echo "Preparing local ailab Fink info files repository."
 mkdir -p /sw/fink/dists/ailab/main/finkinfo/
 rm -f /sw/fink/dists/ailab/main/finkinfo/*
 
-# Gets latest Fink package info files from SVN to our info files repository 
+# Gets latest Fink package info files from SVN
 svn export --force --non-interactive http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink/ /sw/fink/dists/ailab/main/finkinfo/
 
 STABLE_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/branches/ver1.0/ | grep 'Last Changed Rev:' | cut -d ' ' -f 4`
 DAILY_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/trunk/ | grep 'Last Changed Rev:' | cut -d ' ' -f 4`
 
-# Inject revision versions into templates
+# Injects revision versions into templates
 perl -pi -e "s/__REVISION__/$DAILY_REVISION/g" /sw/fink/dists/ailab/main/finkinfo/*svn*.info
 perl -pi -e "s/__REVISION__/$STABLE_REVISION/g" /sw/fink/dists/ailab/main/finkinfo/*.info
 
-# Adds our info files repository to local Fink configuration
-grep '^Trees:' /sw/etc/fink.conf | grep -q 'ailab/main' || perl -p -i -l -e '$_ = "$_ ailab/main" if /^Trees/' /sw/etc/fink.conf
+if ! grep '^Trees:' /sw/etc/fink.conf | grep -q 'ailab/main'; then
+	echo "Adding local ailab Fink info files repository to Fink configuration."
+	perl -p -i -l -e '$_ = "$_ ailab/main" if /^Trees/' /sw/etc/fink.conf
+fi
 
-# Adds our binary repository to local Fink (APT) configuration (so it does not rebuild packages unnecessarily if local copies were removed)
-grep -q 'deb http://www.ailab.si/orange/fink 10.5 main' /sw/etc/apt/sources.list || echo 'deb http://www.ailab.si/orange/fink 10.5 main' >> /sw/etc/apt/sources.list
+# Adds our binary repository to local Fink (APT) configuration
+if ! grep -q 'deb http://www.ailab.si/orange/fink 10.5 main' /sw/etc/apt/sources.list; then
+	echo "Adding ailab Fink binary packages repository to Fink configuration."
+	echo 'deb http://www.ailab.si/orange/fink 10.5 main' >> /sw/etc/apt/sources.list
+fi
 
 # Get all official Fink package info files
+echo "Updating official Fink packages."
 fink $FINK_ARGS selfupdate
 fink $FINK_ARGS scanpackages
 
@@ -52,6 +58,8 @@ fink $FINK_ARGS update-all
 dpkg --get-selections '*' > /tmp/dpkg-selections.list
 
 for package in $OTHER_PACKAGES $STABLE_PACKAGES $DAILY_PACKAGES ; do
+	echo "Specially building package $package."
+	
 	# Restores intitial packages status
 	dpkg --set-selections < /tmp/dpkg-selections.list
 	
@@ -59,21 +67,21 @@ for package in $OTHER_PACKAGES $STABLE_PACKAGES $DAILY_PACKAGES ; do
 	fink $FINK_ARGS build $package
 done
 
-# Restores intitial packages status
+echo "Restoring initial packages status and cleaning."
 dpkg --set-selections < /tmp/dpkg-selections.list
 rm -f /tmp/dpkg-selections.list
 
 # Cleans unncessary files
 fink $FINK_ARGS cleanup --all
 
-# Prepares our public Fink package info and binary files repository
+echo "Preparing public ailab Fink info and binary files repository."
 mkdir -p /Volumes/fink/dists/10.5/main/binary-darwin-i386/
 mkdir -p /Volumes/fink/dists/10.5/main/finkinfo/
 
-# Copies to repository all binary packages
+echo "Copying to repository all binary packages."
 rsync --times --copy-links /sw/fink/debs/*.deb /Volumes/fink/dists/10.5/main/binary-darwin-i386/
 
-# Removes old binary packages
+echo "Removing old binary packages."
 # (Versions of packages which have more then 5 versions and those old versions are more than one month old.)
 cd /Volumes/fink/dists/10.5/main/binary-darwin-i386/
 perl -e '
@@ -87,7 +95,7 @@ while (($f,$n) = each(%fs)) {
 }
 '
 
-# Makes new packages list
+echo "Making packages list."
 cd /Volumes/fink/
 perl -mFink::Scanpackages -e 'Fink::Scanpackages->scan("dists/10.5/main/binary-darwin-i386/");' | gzip - > dists/10.5/main/binary-darwin-i386/Packages.gz
 
@@ -97,10 +105,10 @@ Component: main
 Architecture: darwin-i386
 Label: Fink' > dists/10.5/main/binary-darwin-i386/Release
 
-# Copies to repository all info files
+echo "Copying to repository all info files."
 rm -f /Volumes/fink/dists/10.5/main/finkinfo/*
 cp /sw/fink/dists/ailab/main/finkinfo/* /Volumes/fink/dists/10.5/main/finkinfo/
 
-# Makes an archive of all info files
+echo "Making an archive of all info files."
 cd /Volumes/fink/dists/10.5/main/finkinfo/
 tar -czf all.tgz *.info
--- install-scripts/mac/fink-selfsync-orange.sh
+++ install-scripts/mac/fink-selfsync-orange.sh
@@ -8,19 +8,35 @@
 # Configures environment for Fink
 test -r /sw/bin/init.sh && . /sw/bin/init.sh
 
-# Prepares our Fink package info files repository
+echo "Preparing local ailab Fink info files repository."
 mkdir -p /sw/fink/dists/ailab/main/finkinfo/
 rm -f /sw/fink/dists/ailab/main/finkinfo/*
 
-# Gets current (daily) Fink package info files from SVN to our info files repository 
+# Gets current (daily) info files from SVN
+echo "Updating local ailab Fink info files repository."
 curl http://www.ailab.si/orange/fink/dists/10.5/main/finkinfo/all.tgz --output /sw/fink/dists/ailab/main/finkinfo/all.tgz
 tar -xzf /sw/fink/dists/ailab/main/finkinfo/all.tgz -C /sw/fink/dists/ailab/main/finkinfo/
 rm -f /sw/fink/dists/ailab/main/finkinfo/all.tgz
 
-# Adds our info files repository to local Fink configuration
-grep '^Trees:' /sw/etc/fink.conf | grep -q 'ailab/main' || perl -p -i -l -e '$_ = "$_ ailab/main" if /^Trees/' /sw/etc/fink.conf
+if ! grep '^Trees:' /sw/etc/fink.conf | grep -q 'ailab/main'; then
+	echo "Adding local ailab Fink info files repository to Fink configuration."
+	perl -p -i -l -e '$_ = "$_ ailab/main" if /^Trees/' /sw/etc/fink.conf
+fi
 
-# Adds our binary repository to local Fink (APT) configuration (so it does not rebuild packages unnecessarily if local copies were removed)
-grep -q 'deb http://www.ailab.si/orange/fink 10.5 main' /sw/etc/apt/sources.list || echo 'deb http://www.ailab.si/orange/fink 10.5 main' >> /sw/etc/apt/sources.list
+# Adds our binary repository to local Fink (APT) configuration
+if ! grep -q 'deb http://www.ailab.si/orange/fink 10.5 main' /sw/etc/apt/sources.list; then
+	echo "Adding ailab Fink binary packages repository to Fink configuration."
+	echo 'deb http://www.ailab.si/orange/fink 10.5 main' >> /sw/etc/apt/sources.list
+fi
 
+# Refreshes packages lists
 fink --all scanpackages
+
+cat <<-EOF
+	
+	Information about ailab packages have been updated. You can now update installed
+	packages using commands like 'fink update-all'.
+	
+	You can list available ailab packages using commands like 'fink list orange' and
+	you can install them using commands like 'fink install orange'.
+EOF
