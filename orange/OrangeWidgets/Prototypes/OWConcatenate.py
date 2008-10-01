1d3c2d934729a824c252f92f3a957284b0779507
miha <miha.stajdohar@gmail.com>
2008-10-01 12:13:27 +0000

    add class attribute

--- orange/OrangeWidgets/Prototypes/OWConcatenate.py
+++ orange/OrangeWidgets/Prototypes/OWConcatenate.py
@@ -8,9 +8,10 @@
 
 from OWWidget import *
 import OWGUI
+from itertools import izip
 
 class OWConcatenate(OWWidget):
-    settingsList = ["mergeAttributes"]
+    settingsList = ["mergeAttributes", "dataSourceSelected", "addIdAs", "dataSourceName"]
     
     def __init__(self,parent=None, signalManager = None):
         OWWidget.__init__(self, parent, signalManager, "FeatureConstructor")
@@ -18,15 +19,31 @@ class OWConcatenate(OWWidget):
         self.outputs = [("Examples", ExampleTable)]
 
         self.mergeAttributes = 0
-
+        self.dataSourceSelected = 1
+        self.addIdAs = 0
+        self.dataSourceName = "clusterId"
+        
         self.primary = None
         self.additional = {}
+        
+        self.loadSettings()
         
         bg = self.bgMerge = OWGUI.radioButtonsInBox(self.controlArea, self, "mergeAttributes", [], "Domains merging", callback = self.apply)
         OWGUI.widgetLabel(bg, "When there is no primary table, the domain should be")
         OWGUI.appendRadioButton(bg, self, "mergeAttributes", "Union of attributes appearing in all tables")
         OWGUI.appendRadioButton(bg, self, "mergeAttributes", "Intersection of attributes in all tables")
         OWGUI.widgetLabel(bg, "The resulting table will have class only if there is no conflict betwen input classes.")
+        
+        box = OWGUI.widgetBox(self.controlArea, "Data source IDs")
+        cb = OWGUI.checkBox(box, self, "dataSourceSelected", "Append data source IDs")
+        self.classificationBox = ib = OWGUI.widgetBox(box)
+        le = OWGUI.lineEdit(ib, self, "dataSourceName", "Name" + "  ", orientation='horizontal', valueType = str)
+        OWGUI.separator(ib, height = 4)
+        aa = OWGUI.comboBox(ib, self, "addIdAs", label = "Place" + "  ", orientation = 'horizontal', items = ["Class attribute", "Attribute", "Meta attribute"])
+        cb.disables.append(ib)
+        cb.makeConsistent()
+        OWGUI.separator(box)
+        OWGUI.button(box, self, "Apply Changes", callback = self.apply)
 
         self.adjustSize()
 
@@ -47,14 +64,23 @@ class OWConcatenate(OWWidget):
         
     
     def apply(self):
+        dataSourceIDs = []
+        currentID = 1
+        
         if self.primary:
             if not self.additional:
                 newTable = self.primary
-
+                dataSourceIDs.extend([currentID] * len(self.primary))
+                currentID += 1
             else:
                 newTable = orange.ExampleTable(self.primary)
+                dataSourceIDs.extend([currentID] * len(self.primary))
+                currentID += 1
+                
                 for additional in self.additional.values():
                     newTable.extend(additional)
+                    dataSourceIDs.extend([currentID] * len(additional))
+                    currentID += 1
 
         else:
             if not self.additional:
@@ -96,5 +122,42 @@ class OWConcatenate(OWWidget):
                     newTable = orange.ExampleTable(orange.Domain(attributes, classVar))
                     for additional in self.additional.values():
                         newTable.extend(additional)
+                        dataSourceIDs.extend([currentID] * len(additional))
+                        currentID += 1
+        
+        if newTable != None:
+            tableCount = 0
+            if self.primary:
+                tableCount += 1
+            if self.additional:
+                tableCount += len(self.additional)
+                
+            dataSourceVar = orange.EnumVariable(self.dataSourceName, values = [str(x) for x in range(1, 1 + tableCount)])
+            
+            origDomain = newTable.domain
+            if self.addIdAs == 0:
+                domain = orange.Domain(origDomain.attributes, dataSourceVar)
+                if origDomain.classVar:
+                    domain.addmeta(orange.newmetaid(), origDomain.classVar)
+                aid = -1
+            elif self.addIdAs == 1:
+                domain=orange.Domain(origDomain.attributes+[dataSourceVar], origDomain.classVar)
+                aid = len(origDomain.attributes)
+            else:
+                domain=orange.Domain(origDomain.attributes, origDomain.classVar)
+                aid=orange.newmetaid()
+                domain.addmeta(aid, dataSourceVar)
+                
+            domain.addmetas(origDomain.getmetas())
+            
+            table1 = orange.ExampleTable(domain)
+            table1.extend(newTable)
+            
+            for ex, id in izip(table1, dataSourceIDs):
+                ex[aid] = dataSourceVar(str(id))
+            
+            newTable = table1
+        #for ex, midx in izip(table1, self.mc.mapping):
+        #    ex[aid] = clustVar(str(midx))
 
         self.send("Examples", newTable)
