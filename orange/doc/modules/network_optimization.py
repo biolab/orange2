f528fecfee26149514807924a1600d2c22e58c5e
miha <miha.stajdohar@gmail.com>
2008-10-16 13:15:18 +0000

    updated doc

--- orange/doc/modules/network_optimization.py
+++ orange/doc/modules/network_optimization.py
@@ -3,28 +3,28 @@ from orngNetwork import NetworkOptimization
 from pylab import *
 
 # create graph object of type GraphAsList
-graph = orangeom.Network(5, 0)
+net = orangeom.Network(5, 0)
 
 # set edges
 for i in range(4):
     for j in range(i + 1, 5):
-        graph[i,j] = 1
+        net[i,j] = 1
 
 # vertices are placed randomly in NetworkOptimization constructor
-network = NetworkOptimization(graph)
+networkOptimization = NetworkOptimization(net)
 
 # optimize verices layout with one of included algorithms
-network.fruchtermanReingold(100, 1000)
+networkOptimization.fruchtermanReingold(100, 1000)
 
 # read all edges and plot a line
-for u, v in graph.getEdges():
-    x1, y1 = network.coors[0][u], network.coors[1][u]
-    x2, y2 = network.coors[0][v], network.coors[1][v]
+for u, v in net.getEdges():
+    x1, y1 = net.coors[0][u], net.coors[1][u]
+    x2, y2 = net.coors[0][v], net.coors[1][v]
     plot([x1, x2], [y1, y2], 'b-')
 
 # read x and y coordinates to Python list
-x = network.coors[0]
-y = network.coors[1]
+x = net.coors[0]
+y = net.coors[1]
 
 # plot vertices
 plot(x, y, 'ro')
--- orange/doc/modules/network_read.py
+++ orange/doc/modules/network_read.py
@@ -4,20 +4,20 @@ from pylab import *
 
         
 # vertices are placed randomly in NetworkOptimization constructor
-network = NetworkOptimization()
+networkOptimization = NetworkOptimization()
 
 # read network from file
-network.readNetwork("K5.net")
+net = networkOptimization.readNetwork("K5.net")
 
 # read all edges and plot a line
-for u, v in network.graph.getEdges():
-    x1, y1 = network.coors[0][u], network.coors[1][u]
-    x2, y2 = network.coors[0][v], network.coors[1][v]
+for u, v in net.getEdges():
+    x1, y1 = net.coors[0][u], net.coors[1][u]
+    x2, y2 = net.coors[0][v], net.coors[1][v]
     plot([x1, x2], [y1, y2], 'b-')
 
 # read x and y coordinates to Python list
-x = network.coors[0]
-y = network.coors[1]
+x = net.coors[0]
+y = net.coors[1]
 
 # plot vertices
 plot(x, y, 'ro')
