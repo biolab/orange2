b2fd36fd5ece945a03aa852c349f366502f0db37
miha <miha.stajdohar@gmail.com>
2008-10-20 07:19:52 +0000

    new

---
+++ orange/OrangeWidgets/Unsupervised/OWSymMatrixTransform.py
@@ -0,0 +1,94 @@
+"""
+<name>Matrix Transformation</name>
+<description>Tranforms matrix according to selected criteria</description>
+<contact>Miha Stajdohar</contact>
+<icon>icons/DistanceFile.png</icon>
+<priority>1110</priority>
+"""
+
+from OWWidget import *
+import OWGUI
+import orange
+import exceptions
+import os.path
+import pickle
+import copy
+            
+class OWSymMatrixTransform(OWWidget):
+    settingsList = ["normalizeMethod", "invertMethod"]
+
+    def __init__(self, parent=None, signalManager = None):
+        self.callbackDeposit = [] # deposit for OWGUI callback functions
+        OWWidget.__init__(self, parent, signalManager, "Matrix Transformation", wantMainArea = 0, resizingEnabled = 0)
+        
+        self.inputs = [("Matrix", orange.SymMatrix, self.setMatrix, Default)]
+        self.outputs = [("Matrix", orange.SymMatrix)]
+
+        self.matrix = None
+        self.normalizeMethod = 0
+        self.invertMethod = 0
+        self.loadSettings()
+                
+        ribg = OWGUI.radioButtonsInBox(self.controlArea, self, "normalizeMethod", [], "Normalize method", callback = self.setNormalizeMode)
+        OWGUI.appendRadioButton(ribg, self, "normalizeMethod", "None", callback = self.setNormalizeMode)
+        OWGUI.appendRadioButton(ribg, self, "normalizeMethod", "To interval [0,1]", callback = self.setNormalizeMode)
+        OWGUI.appendRadioButton(ribg, self, "normalizeMethod", "Sigmoid function: 1 / (1 + e^x)", callback = self.setNormalizeMode)
+        
+        ribg = OWGUI.radioButtonsInBox(self.controlArea, self, "invertMethod", [], "Invert method", callback = self.setInvertMode)
+        OWGUI.appendRadioButton(ribg, self, "invertMethod", "None", callback = self.setInvertMode)
+        OWGUI.appendRadioButton(ribg, self, "invertMethod", "-X", callback = self.setInvertMode)
+        OWGUI.appendRadioButton(ribg, self, "invertMethod", "1 - X", callback = self.setInvertMode)
+        OWGUI.appendRadioButton(ribg, self, "invertMethod", "Max - X", callback = self.setInvertMode)
+        OWGUI.appendRadioButton(ribg, self, "invertMethod", "1 / X", callback = self.setInvertMode)
+        
+        self.adjustSize()
+
+    def setNormalizeMode(self):
+        self.transform()
+    
+    def setInvertMode(self):
+        self.transform()
+        
+    def setMatrix(self, matrix):
+        self.matrix = matrix
+        self.transform()
+            
+    def transform(self):
+        #print "transform"
+        #print "self.invertMethod:", self.invertMethod
+        #print "self.normalizeMethod:", self.normalizeMethod
+        self.error()
+        
+        matrix = None 
+        if self.invertMethod > 0 or self.normalizeMethod > 0:
+            matrix = copy.deepcopy(self.matrix)
+            
+            if self.normalizeMethod == 1:
+                matrix.normalize(0)
+            elif self.normalizeMethod == 2:
+                matrix.normalize(1)
+            
+            if self.invertMethod == 1:
+                matrix.invert(0)
+            elif self.invertMethod == 2:
+                matrix.invert(1)
+            elif self.invertMethod == 3:
+                matrix.invert(2)
+            elif self.invertMethod == 4:
+                try:                
+                    matrix.invert(3)
+                except:
+                    self.error("Division by zero")
+                    matrix = None
+                        
+        if matrix != None:
+            self.send("Matrix", matrix)
+        else:
+            self.send("Matrix", self.matrix)
+
+if __name__=="__main__":
+    a = QApplication(sys.argv)
+    ow = OWSymMatrixTransform()
+    ow.show()
+    a.exec_()
+    ow.saveSettings()
