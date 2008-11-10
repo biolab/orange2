cb6a443cb011dfdf5ee51bf62ca4368490e5ca49
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-11-10 12:26:49 +0000

    Updated daily-build scripts.

--- install-scripts/mac/dailyrun.sh
+++ install-scripts/mac/dailyrun.sh
@@ -8,4 +8,3 @@ DAILY_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/trunk/
 
 /Users/ailabc/bundle-daily-build.sh $STABLE_REVISION $DAILY_REVISION &> /private/tmp/bundle-daily-build.log || cat /private/tmp/bundle-daily-build.log
 /Users/ailabc/fink-daily-build.sh $STABLE_REVISION $DAILY_REVISION &> /private/tmp/fink-daily-build.log || cat /private/tmp/fink-daily-build.log
-
