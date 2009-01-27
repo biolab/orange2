b5dd22f405afbeda058eda02cd8a6350565915ed
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2009-01-27 14:06:52 +0000

    Updated VMware daily script.

--- install-scripts/mac/update-all-scripts.sh
+++ install-scripts/mac/update-all-scripts.sh
@@ -5,12 +5,12 @@
 
 cd /Users/ailabc/
 
-curl --silent --output  update-all-scripts.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/update-all-scripts.sh
-curl --silent --output  bundle-64bit-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/bundle-64bit-daily-build.sh
-curl --silent --output  bundle-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/bundle-daily-build.sh
-curl --silent --output  dailyrun.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/dailyrun.sh
-curl --silent --output  fink-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-daily-build.sh
-curl --silent --output  fink-restore-selections.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-restore-selections.sh
-curl --silent --output  fink-selfupdate-orange.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-selfupdate-orange.sh
+curl --silent --output update-all-scripts.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/update-all-scripts.sh
+curl --silent --output bundle-64bit-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/bundle-64bit-daily-build.sh
+curl --silent --output bundle-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/bundle-daily-build.sh
+curl --silent --output dailyrun.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/dailyrun.sh
+curl --silent --output fink-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-daily-build.sh
+curl --silent --output fink-restore-selections.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-restore-selections.sh
+curl --silent --output fink-selfupdate-orange.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-selfupdate-orange.sh
 
 chmod +x *.sh
