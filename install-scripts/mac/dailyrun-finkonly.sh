8bc978b03e83da267920feb47ffef1c84eaa3f2d
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2009-01-31 02:19:00 +0000

    Updated PPC daily build script.

--- install-scripts/mac/dailyrun-finkonly.sh
+++ install-scripts/mac/dailyrun-finkonly.sh
@@ -8,4 +8,4 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 [ -e /Volumes/fink/ ] || { open "smb://orange@estelle.fri.uni-lj.si/fink/"; sleep 10; }
 
 echo "Running fink-daily-build.sh" > /private/tmp/bundle-daily-build.log
-/Users/imac/fink-daily-build.sh &> /private/tmp/fink-daily-build.log || cat /private/tmp/fink-daily-build.log
+/Users/mitar/fink-daily-build.sh &> /private/tmp/fink-daily-build.log || cat /private/tmp/fink-daily-build.log
