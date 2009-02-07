3c07158fd668f7dae14afb8f665228ae79f87858
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2009-02-07 09:57:28 +0000

    Increased mount wait time in daily build scripts.

--- install-scripts/mac/dailyrun-finkonly.sh
+++ install-scripts/mac/dailyrun-finkonly.sh
@@ -5,7 +5,7 @@
 
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
-[ -e /Volumes/fink/ ] || { open "smb://orange@estelle.fri.uni-lj.si/fink/"; sleep 10; }
+[ -e /Volumes/fink/ ] || { open "smb://orange@estelle.fri.uni-lj.si/fink/"; sleep 30; }
 
 echo "Running fink-daily-build.sh" > /private/tmp/fink-daily-build.log
 /Users/mitar/fink-daily-build.sh &> /private/tmp/fink-daily-build.log || cat /private/tmp/fink-daily-build.log
--- install-scripts/mac/dailyrun.sh
+++ install-scripts/mac/dailyrun.sh
@@ -13,8 +13,8 @@ DAILY_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/trunk/
 
 defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 
-[ -e /Volumes/fink/ ] || { open "smb://orange@estelle.fri.uni-lj.si/fink/"; sleep 10; }
-[ -e /Volumes/download/ ] || { open "smb://orange@estelle.fri.uni-lj.si/download/"; sleep 10; }
+[ -e /Volumes/fink/ ] || { open "smb://orange@estelle.fri.uni-lj.si/fink/"; sleep 30; }
+[ -e /Volumes/download/ ] || { open "smb://orange@estelle.fri.uni-lj.si/download/"; sleep 30; }
 
 echo "Running bundle-daily-build.sh" > /private/tmp/bundle-daily-build.log
 /Users/ailabc/bundle-daily-build.sh $STABLE_REVISION $DAILY_REVISION &> /private/tmp/bundle-daily-build.log || cat /private/tmp/bundle-daily-build.log
