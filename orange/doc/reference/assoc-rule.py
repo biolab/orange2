1fa79eb8267726f85ec73ef24665afee7ad85025
blaz <blaz.zupan@fri.uni-lj.si>
2009-03-14 17:13:07 +0000

    split the script to two parts (this one is just like it used to be)

--- orange/doc/reference/assoc-rule.py
+++ orange/doc/reference/assoc-rule.py
@@ -2,7 +2,8 @@ import orange
 
 data = orange.ExampleTable("lenses")
 
-rules = orange.AssociationRulesInducer(data, support = 0.3, storeExamples = True)
+rules = orange.AssociationRulesInducer(data, support = 0.3,
+                                       storeExamples = True)
 rule = rules[0]
 
 print
@@ -20,13 +21,3 @@ for example in data:
     if rule.appliesLeft(example) and not rule.appliesRight(example):
         print example
 print
-
-print rule
-print "Match left: "
-print "\n".join(str(rule.examples[i]) for i in rule.matchLeft)
-print "\nMatch both: "
-print "\n".join(str(rule.examples[i]) for i in rule.matchBoth)
-
-inducer = orange.AssociationRulesInducer(support = 0.3, storeExamples = True)
-itemsets = inducer.getItemsets(data)
-print itemsets[8]
