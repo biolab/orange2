2cefaa69925469c47a4ab77c1e2de2c475707d07
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2009-04-23 08:47:35 +0000

    Fixed path in a dailybuild script.

--- install-scripts/mac/dailyrun-finkonly.sh
+++ install-scripts/mac/dailyrun-finkonly.sh
@@ -8,4 +8,4 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores true
 [ -e /Volumes/fink/ ] || { open "smb://orange@estelle.fri.uni-lj.si/fink/"; sleep 30; }
 
 echo "Running fink-daily-build.sh" > /private/tmp/fink-daily-build.log
-/Users/mitar/fink-daily-build.sh &> /private/tmp/fink-daily-build.log || cat /private/tmp/fink-daily-build.log
+/Users/ailabc/fink-daily-build.sh &> /private/tmp/fink-daily-build.log || cat /private/tmp/fink-daily-build.log
