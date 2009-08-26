0211404363a76e6cdac557e85fca0a32cf951a3f
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2009-08-26 10:42:47 +0000

    - added updateOMIM.py and updateHomoloGene.py

--- install-scripts/orngServer/serverUpdateScripts/updateAll.py
+++ install-scripts/orngServer/serverUpdateScripts/updateAll.py
@@ -12,7 +12,8 @@ password = opt.get("-p", opt.get("--password", "password"))
 
 age = datetime.now() - datetime.fromtimestamp(0) ## age of the universe
 
-files = ["updateTaxonomy.py", "updateGO.py", "updateKEGG.py", "updateMeSH.py", "updateNCBI_geneinfo.py"]
+files = ["updateTaxonomy.py", "updateGO.py", "updateKEGG.py", "updateMeSH.py", "updateNCBI_geneinfo.py",
+         "updateOMIM.py", "updateHomoloGene.py"]
 
 for filename in files:
     options = dict([line[3:].split("=") for line in open(filename).readlines() if line.startswith("##!")])
