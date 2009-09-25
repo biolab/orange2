760b263ffe7920ab87d40a198da3d94f2f65c47c
polz <polz@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2009-09-25 13:20:49 +0000

    Added maxDepth to OWClassificationTree.

--- orange/OrangeWidgets/Classify/OWClassificationTree.py
+++ orange/OrangeWidgets/Classify/OWClassificationTree.py
@@ -20,7 +20,8 @@ class OWClassificationTree(OWWidget):
                     "bin", "subset",
                     "preLeafInst", "preNodeInst", "preNodeMaj",
                     "preLeafInstP", "preNodeInstP", "preNodeMajP",
-                    "postMaj", "postMPruning", "postM"]
+                    "postMaj", "postMPruning", "postM", 
+                    "limitDepth", "maxDepth"]
 
     measures = (("Information Gain", "infoGain"), ("Gain Ratio", "gainRatio"), ("Gini Index", "gini"), ("ReliefF", "relief"))
     binarizationOpts = ["No binarization", "Exhaustive search for optimal split", "One value against others"]
@@ -37,7 +38,7 @@ class OWClassificationTree(OWWidget):
         self.preLeafInstP = 2; self.preNodeInstP = 5; self.preNodeMajP = 95
         self.preLeafInst = 1; self.preNodeInst = 0; self.preNodeMaj = 0
         self.postMaj = 1; self.postMPruning = 1; self.postM = 2.0
-
+        self.limitDepth = False; self.maxDepth = 100
         self.loadSettings()
 
         self.data = None
@@ -57,6 +58,7 @@ class OWClassificationTree(OWWidget):
         self.cbLimitRef, self.hbxRel1 = OWGUI.checkWithSpin(b2, self, "Limit the number of reference examples to ", 1, 1000, "limitRef", "relM")
         OWGUI.separator(b2)
         self.hbxRel2 = OWGUI.spin(b2, self, "relK", 1, 50, orientation="horizontal", label="Number of neighbours in ReliefF  ")
+ 
         OWGUI.separator(self.controlArea)
 
         OWGUI.radioButtonsInBox(self.controlArea, self, 'bin', self.binarizationOpts, "Binarization")
@@ -69,7 +71,7 @@ class OWClassificationTree(OWWidget):
         self.preLeafInstBox, self.preLeafInstPBox = OWGUI.checkWithSpin(self.pBox, self, "Min. instances in leaves ", 1, 1000, "preLeafInst", "preLeafInstP")
         self.preNodeInstBox, self.preNodeInstPBox = OWGUI.checkWithSpin(self.pBox, self, "Stop splitting nodes with less instances than ", 1, 1000, "preNodeInst", "preNodeInstP")
         self.preNodeMajBox, self.preNodeMajPBox = OWGUI.checkWithSpin(self.pBox, self, "Stop splitting nodes with a majority class of (%)", 1, 100, "preNodeMaj", "preNodeMajP")
-
+        self.cbLimitDepth, self.maxDepthBox = OWGUI.checkWithSpin(self.pBox, self, "Stop splitting nodes at depth", 0, 1000, "limitDepth", "maxDepth")
         OWGUI.separator(self.controlArea)
         self.mBox = OWGUI.widgetBox(self.controlArea, 'Post-Pruning')
 
@@ -89,7 +91,8 @@ class OWClassificationTree(OWWidget):
                              ("Pruning", ", ".join(s for s, c in (
                                                  ("%i instances in leaves" % self.preLeafInstP, self.preLeafInst),
                                                  ("%i instance in node" % self.preNodeInstP, self.preNodeInst),
-                                                 ("stop on %i%% purity" % self.preNodeMajP, self.preNodeMaj)) if c)
+                                                 ("stop on %i%% purity" % self.preNodeMajP, self.preNodeMaj),
+                                                 ("maximum depth %i" % self.maxDepth, self.limitDepth)) if c)
                                           or "None"),
                              ("Recursively merge leaves with same majority class", OWGUI.YesNo[self.postMaj]),
                              ("Pruning with m-estimate", ["No", "m=%i" % self.postM][self.postMPruning])])
@@ -98,6 +101,10 @@ class OWClassificationTree(OWWidget):
     def setLearner(self):
         if hasattr(self, "btnApply"):
             self.btnApply.setFocus()
+        if not self.limitDepth:
+            mDepth = {}
+        else:
+            mDepth = {'maxDepth': self.maxDepth}
         self.learner = orngTree.TreeLearner(measure = self.measures[self.estim][1],
             reliefK = self.relK, reliefM = self.limitRef and self.relM or -1,
             binarization = self.bin,
@@ -106,7 +113,7 @@ class OWClassificationTree(OWWidget):
             maxMajority = self.preNodeMaj and self.preNodeMajP/100.0 or 1.0,
             sameMajorityPruning = self.postMaj,
             mForPruning = self.postMPruning and self.postM,
-            storeExamples = 1)
+            storeExamples = 1, **mDepth)
 
         self.learner.name = self.name
         self.send("Learner", self.learner)
