14cbde55d5a45efb22bd874665bbf3c07585a8b6
janezd <janez.demsar@fri.uni-lj.si>
2009-10-07 12:05:36 +0000

    - replaced the tree with a list

--- orange/OrangeWidgets/OWReport.py
+++ orange/OrangeWidgets/OWReport.py
@@ -1,180 +1,3 @@
-from OWWidget import *
-from OWWidget import *
-from PyQt4.QtWebKit import *
-
-import os, time, tempfile, shutil, re, shutil, pickle
-
-import orngEnviron
-from orngEnviron import reportsDir
-
-report = None
-def escape(s):
-    return s.replace("\\", "\\\\").replace("\n", "\\n").replace("'", "\\'")
-
-
-class MyTreeWidget(QTreeWidget):
-    def __init__(self, parent, widget):
-        QTreeWidget.__init__(self, parent)
-        self.widget = widget
-        
-    def dropEvent(self, ev):
-        QTreeWidget.dropEvent(self, ev)
-        self.widget.rebuildHtml()
-
-    def mousePressEvent(self, ev):
-        QTreeWidget.mousePressEvent(self, ev)
-        node = self.currentItem() 
-        if ev.button() == Qt.RightButton and node and self.indexOfTopLevelItem(node) > -1:
-            self.widget.nodePopup.popup(ev.globalPos())
-
-    
-class ReportWindow(OWWidget):
-    def __init__(self):
-        OWWidget.__init__(self, None, None, "Report")
-        self.dontScroll = False
-        global report
-        report = self
-        self.counter = 0
-        
-        self.tempdir = tempfile.mkdtemp("", "orange-report-")
-
-        self.tree = MyTreeWidget(self.controlArea, self)
-        self.tree.setDragEnabled(True)
-        self.tree.setDragDropMode(QAbstractItemView.InternalMove)
-        self.controlArea.layout().addWidget(self.tree)
-        self.tree.setAllColumnsShowFocus(1)
-        self.tree.header().hide()
-        QObject.connect(self.tree, SIGNAL("currentItemChanged(QTreeWidgetItem *, QTreeWidgetItem *)"), self.itemChanged)
-        QObject.connect(self.tree, SIGNAL("itemActivated ( QTreeWidgetItem *, int)"), self.raiseWidget)
-        QObject.connect(self.tree, SIGNAL("itemDoubleClicked ( QTreeWidgetItem *, int)"), self.raiseWidget)
-
-        self.treeItems = {}
-
-        self.reportBrowser = QWebView(self.mainArea)
-        self.mainArea.layout().addWidget(self.reportBrowser)
-        self.reportBrowser.setUrl(QUrl.fromLocalFile(os.path.join(orngEnviron.widgetDir, "report", "index.html")))
-        frame = self.reportBrowser.page().mainFrame()
-        self.javascript = frame.evaluateJavaScript
-        frame.setScrollBarPolicy(Qt.Vertical, Qt.ScrollBarAsNeeded)
-
-        saveButton = OWGUI.button(self.controlArea, self, "&Save", self.saveReport)
-        saveButton.setAutoDefault(0)
-        
-        self.nodePopup = QMenu("Widget")
-        self.nodePopup.addAction( "Show widget",  self.showActiveNodeWidget)
-        self.nodePopup.addSeparator()
-        self.renameAction = self.nodePopup.addAction( "&Rename", self.renameActiveNode, Qt.Key_F2)
-        self.deleteAction = self.nodePopup.addAction("Remove", self.removeActiveNode, Qt.Key_Delete)
-        self.nodePopup.setEnabled(1)
-
-        self.resize(900, 850)
-       
-    # this should have been __del__, but it doesn't get called!
-    def removeTemp(self):
-        try:
-            shutil.rmtree(self.tempdir)
-        except:
-            pass
-
-
-    entry = """
-    <div id="%s">
-        <h1>%s<span class="timestamp">%s</span></h1>
-        <div class="insideh1">
-            %s
-        </div>
-    </div>
-    """
-
-    re_h = re.compile(r"<\s*[hH](?P<level>[2-5])\s*>\s*(?P<name>.*?)\s*<\s*/[hH]\1\s*>")
-    
-    class HIdentificator:
-        def __init__(self, node, id, treeItems):
-            self.id = id
-            self.subid = 0
-            self.stack = [(1, node)]
-            self.treeItems  = treeItems
-            
-        def __call__(self, mo):
-            self.subid += 1
-            elid = "N%03iS%03i" % (self.id, self.subid)
-
-            level = int(mo.group("level"))
-            beg = ""
-            while self.stack[-1][0] >= level:
-                beg += "</div>"
-                self.stack.pop()
-            beg += """<a name="%s"/><div id="%s" onClick="myself.changeItem(this.id);">""" % (elid, elid)
-            
-            node = QTreeWidgetItem(self.stack[-1][1], [mo.group("name")])
-            node.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-            self.stack[-1][1].setExpanded(True)
-            node.elementId = elid
-            self.treeItems[elid] = node
-            self.stack.append((level, node))
-
-            return beg+mo.group(0)
-            
-            
-    def __call__(self, name, data, widgetId):#, settings):
-        if not self.isVisible():
-            self.show()
-        else:
-            self.raise_()
-        self.counter += 1
-        elid = "N%03i" % self.counter
-
-        widnode = QTreeWidgetItem(self.tree, [name])
-        widnode.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled)
-        widnode.elementId = elid
-        widnode.widgetId = widgetId
-#        widnode.settings = settings 
-        self.tree.addTopLevelItem(widnode)
-        self.treeItems[elid] = widnode
-        
-        hiden = self.HIdentificator(widnode, self.counter, self.treeItems)
-        data = self.re_h.sub(hiden, data)
-        data += "</div>" * len(hiden.stack)
-        newreport = self.entry % (elid, name, time.strftime("%a %b %d %y, %H:%M:%S"), data)
-        widnode.data = newreport
-        self.javascript("""
-            document.body.innerHTML += '%s';
-            document.getElementById('%s').scrollIntoView();
-        """ % (escape(newreport), elid))
-        self.reportBrowser.page().mainFrame().addToJavaScriptWindowObject("myself", self)
-        widnode.setExpanded(False)
-        
-    def itemChanged(self, current, previous):
-        if current:
-#            if self.dontScroll:
-#                self.javascript("document.getElementById('%s').className = 'selected';" % current.elementId)
-#            else:
-#                self.javascript("""
-#                    var newsel = document.getElementById('%s');
-#                    newsel.className = 'selected';
-#                    newsel.scrollIntoView();""" % current.elementId)
-            if not self.dontScroll:
-                self.javascript("newsel.scrollIntoView(document.getElementById('%s'));" % current.elementId)
-#        if previous:
-#            self.javascript("document.getElementById('%s').className = '';" % previous.elementId)
-        
-    def rebuildHtml(self):
-        tt = "\n".join(self.tree.topLevelItem(i).data for i in range(self.tree.topLevelItemCount()))
-        print "\n".join(self.tree.topLevelItem(i).elementId for i in range(self.tree.topLevelItemCount()))
-        self.javascript("document.body.innerHTML = '%s'" % escape(tt))
-        selected = self.tree.selectedItems()
-        if selected:
-            self.itemChanged(selected[0], None)
-        
-        
-    @pyqtSignature("QString") 
-    def changeItem(self, elid):
-        self.dontScroll = True
-        item = self.treeItems[str(elid)]
-        self.tree.setCurrentItem(item)
-        self.tree.scrollToItem(item)
-        self.dontScroll = False
- 
  # Widgets cannot be reset to the settings they had at the time of reporting.
  # The reason lies in the OWGUI callback mechanism: callbacks are triggered only
  # when the controls are changed by the user. If the related widget's attribute
@@ -193,39 +16,176 @@ class ReportWindow(OWWidget):
  # could even be unfeasible. For instance, there are widget which get the data, compute
  # something and discard the data. This is good since it is memory efficient, but it
  # may prohibit the widget from implementing the update-from-the-scratch method.  
+ 
+ 
+from OWWidget import *
+from OWWidget import *
+from PyQt4.QtWebKit import *
+
+import os, time, tempfile, shutil, re, shutil, pickle
+
+import orngEnviron
+from orngEnviron import reportsDir
+
+report = None
+def escape(s):
+    return s.replace("\\", "\\\\").replace("\n", "\\n").replace("'", "\\'")
+
+
+class MyListWidget(QListWidget):
+    def __init__(self, parent, widget):
+        QListWidget.__init__(self, parent)
+        self.widget = widget
+        
+    def dropEvent(self, ev):
+        QListWidget.dropEvent(self, ev)
+        self.widget.rebuildHtml()
+
+    def mousePressEvent(self, ev):
+        QListWidget.mousePressEvent(self, ev)
+        node = self.currentItem() 
+        if ev.button() == Qt.RightButton and node:
+            self.widget.nodePopup.popup(ev.globalPos())
+
     
-    def raiseWidget(self, node, column):
+class ReportWindow(OWWidget):
+    indexfile = os.path.join(orngEnviron.widgetDir, "report", "index.html")
+    
+    def __init__(self):
+        OWWidget.__init__(self, None, None, "Report")
+        self.dontScroll = False
+        global report
+        report = self
+        self.counter = 0
+        
+        self.tempdir = tempfile.mkdtemp("", "orange-report-")
+
+        self.tree = MyListWidget(self.controlArea, self)
+        self.tree.setDragEnabled(True)
+        self.tree.setDragDropMode(QAbstractItemView.InternalMove)
+        self.controlArea.layout().addWidget(self.tree)
+        QObject.connect(self.tree, SIGNAL("currentItemChanged(QListWidgetItem *, QListWidgetItem *)"), self.selectionChanged)
+        QObject.connect(self.tree, SIGNAL("itemActivated ( QListWidgetItem *)"), self.raiseWidget)
+        QObject.connect(self.tree, SIGNAL("itemDoubleClicked ( QListWidgetItem *)"), self.raiseWidget)
+        QObject.connect(self.tree, SIGNAL("itemChanged ( QListWidgetItem *)"), self.itemChanged)
+
+        self.treeItems = {}
+
+        self.reportBrowser = QWebView(self.mainArea)
+        self.mainArea.layout().addWidget(self.reportBrowser)
+        self.reportBrowser.setUrl(QUrl.fromLocalFile(self.indexfile))
+        frame = self.reportBrowser.page().mainFrame()
+        self.javascript = frame.evaluateJavaScript
+        frame.setScrollBarPolicy(Qt.Vertical, Qt.ScrollBarAsNeeded)
+
+        saveButton = OWGUI.button(self.controlArea, self, "&Save", self.saveReport)
+        saveButton.setAutoDefault(0)
+        
+        self.nodePopup = QMenu("Widget")
+        self.nodePopup.addAction( "Show widget",  self.showActiveNodeWidget)
+        self.nodePopup.addSeparator()
+#        self.renameAction = self.nodePopup.addAction( "&Rename", self.renameActiveNode, Qt.Key_F2)
+        self.deleteAction = self.nodePopup.addAction("Remove", self.removeActiveNode, Qt.Key_Delete)
+        self.nodePopup.setEnabled(1)
+
+        self.resize(900, 850)
+       
+    # this should have been __del__, but it doesn't get called!
+    def removeTemp(self):
+        try:
+            shutil.rmtree(self.tempdir)
+        except:
+            pass
+
+
+    entry = """
+    <div id="%s" onClick="myself.changeItem(this.id);">
+        <h1>%s<span class="timestamp">%s</span></h1>
+        <div class="insideh1">
+            %s
+        </div>
+    </div>
+    """
+
+    def __call__(self, name, data, widgetId):
+        if not self.isVisible():
+            self.show()
+        else:
+            self.raise_()
+        self.counter += 1
+        elid = "N%03i" % self.counter
+
+        widnode = QListWidgetItem(name, self.tree)
+        widnode.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsDragEnabled | Qt.ItemIsEditable)
+        widnode.elementId = elid
+        widnode.widgetId = widgetId
+        self.tree.addItem(widnode)
+        self.treeItems[elid] = widnode
+        
+        newreport = self.entry % (elid, name, time.strftime("%a %b %d %y, %H:%M:%S"), data)
+        widnode.content = newreport
+        self.javascript("""
+            document.body.innerHTML += '%s';
+            document.getElementById('%s').scrollIntoView();
+        """ % (escape(newreport), elid))
+        self.reportBrowser.page().mainFrame().addToJavaScriptWindowObject("myself", self)
+        
+    def selectionChanged(self, current, previous):
+        if current:
+            if self.dontScroll:
+                self.javascript("document.getElementById('%s').className = 'selected';" % current.elementId)
+            else:
+                self.javascript("""
+                    var newsel = document.getElementById('%s');
+                    newsel.className = 'selected';
+                    newsel.scrollIntoView();""" % current.elementId)
+#            if not self.dontScroll:
+#                self.javascript("newsel.scrollIntoView(document.getElementById('%s'));" % current.elementId)
+        if previous:
+            self.javascript("document.getElementById('%s').className = '';" % previous.elementId)
+        
+        
+    def rebuildHtml(self):
+        tt = "\n".join(self.tree.item(i).content for i in range(self.tree.count()))
+        self.javascript("document.body.innerHTML = '%s'" % escape(tt))
+        selected = self.tree.selectedItems()
+        if selected:
+            self.selectionChanged(selected[0], None)
+        
+        
+    @pyqtSignature("QString") 
+    def changeItem(self, elid):
+        self.dontScroll = True
+        item = self.treeItems[str(elid)]
+        self.tree.setCurrentItem(item)
+        self.tree.scrollToItem(item)
+        self.dontScroll = False
+ 
+    def raiseWidget(self, node):
         for widget in self.widgets:
             if widget.instance.widgetId == node.widgetId:
                 break
         else:
             return
-#        widget.instance.setSettings(node.settings)
         widget.instance.reshow()
         
-
     def showActiveNodeWidget(self):
         node = self.tree.currentItem()
         if node:
-            self.raiseWidget(node, 0)
+            self.raiseWidget(node)
             
-    def renameActiveNode(self):
-        node = self.tree.currentItem()
-        if not node:
-            return
-        node.setFlags(node.flags() | Qt.ItemIsEditable)
-        self.tree.editItem(node, 0)
-        node.setFlags(node.flags() ^ ~Qt.ItemIsEditable)
+    re_h1 = re.compile(r'<h1>(?P<name>.*?)<span class="timestamp">')
+    def itemChanged(self, node):
+        print node
+        print type(node.content)
+        be, en = self.re_h1.search(node.content).span("name")
+        node.content = node.content[:be] + str(node.text()) + node.content[en:]
+        self.rebuildHtml()
 
     def removeActiveNode(self):
         node = self.tree.currentItem()
-        while node:
-            itop = self.tree.indexOfTopLevelItem(node)
-            print itop
-            if itop > -1:
-                self.tree.takeTopLevelItem(itop)
-                break
-            node = node.parent()
+        if node:
+            self.tree.takeItem(self.tree.row(node))
         self.rebuildHtml()
 
         
@@ -240,15 +200,6 @@ class ReportWindow(OWWidget):
             if not os.path.exists(fn):
                 return "file:///"+fn, fn
 
-    def saveTreeView(self, node):
-        s = '<li><a href="#%s">%s</a>\n\n' % (node.elementId, node.text(0))
-        if node.childCount():
-            s += "<ul>\n" + \
-                 "\n".join(self.saveTreeView(node.child(i)) for i in range(node.childCount())) + \
-                 "</ul>\n"
-        s += '</li>'
-        return s
-        
     img_re = re.compile(r'<IMG.*?\ssrc="(?P<imgname>[^"]*)"', re.DOTALL+re.IGNORECASE)
     def saveReport(self):
         filename = QFileDialog.getSaveFileName(self, "Save Report", reportsDir, "Web page (*.html *.htm)")
@@ -263,18 +214,18 @@ class ReportWindow(OWWidget):
             except:
                 QMessageBox.error(None, "Error", "Cannot create directory "+path)
 
-        treeIndex = "<ul>" + \
-            "".join(self.saveTreeView(self.tree.topLevelItem(i)) for i in range(self.tree.topLevelItemCount())) + \
-            "</ul>"
+        tt = file(self.indexfile, "rt").read()
         
-        tt = unicode(self.reportBrowser.page().mainFrame().toHtml())
-        tt = tt.replace("<body>", '<body><table width="100%"><tr><td valign="top"><p>Index</p>'+treeIndex+"</td><td>") \
-               .replace("</body", '</td></tr></table></body>')
+        index = "<ul>%s</ul>" % \
+            "".join("<li>%s</li>" % self.re_h1.search(self.tree.item(i).content).group("name") for i in range(self.tree.count()))
+            
+        data = "\n".join(self.tree.item(i).content for i in range(self.tree.count()))
+        
+        tt = tt.replace("<body>", '<body><table width="100%"><tr><td valign="top"><p>Index</p>%s</td><td>%s</td></tr></table>' % (index, data))
         
         filepref = "file:///"+self.tempdir
         if filepref[-1] != os.sep:
             filepref += os.sep
-        print filepref
         lfilepref = len(filepref)
         imspos = -1
         subdir = None
@@ -299,18 +250,9 @@ class ReportWindow(OWWidget):
             shutil.copy(os.path.join(filepref[8:], imname), os.path.join(absubdir, imname))
         if subdir:
             tt = tt.replace(filepref, subdir+"/")
-        tt = tt.replace('<div class="selected"', '<div')
         file(filename, "wb").write(tt.encode("utf8"))
-                       
-
-#        file(filename, "wb").write("""
-#        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-#    
-#    <link rel="stylesheet" type="text/css" href="ext-2.2/resources/css/ext-all.css">
-#    <link rel="stylesheet" type="text/css" href="ext-2.2/resources/css/xtheme-gray.css">
-#    <link rel="stylesheet" type="text/css" href="index.css">
-#""" + tt.encode("utf8"))
-
+ 
+       
 def getDepth(item, expanded=True):
     ccount = item.childCount()
     return 1 + (ccount and (not expanded or item.isExpanded()) and max(getDepth(item.child(cc), expanded) for cc in range(ccount)))
@@ -327,6 +269,7 @@ def printTree(item, level, depthRem, visibleColumns, expanded=True):
             res += printTree(item.child(i), level+1, depthRem-1, visibleColumns, expanded)
     return res
     
+                    
 def reportTree(tree, expanded=True):
     tops = tree.topLevelItemCount()
     header = tree.headerItem()
