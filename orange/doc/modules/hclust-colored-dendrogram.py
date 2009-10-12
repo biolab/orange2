070fc4fb4030ea673bd35c827238c0e0d79f92e5
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2009-10-12 09:00:22 +0000

    - updated orngClustering dendrogram drawing documentation

--- orange/doc/modules/hclust-colored-dendrogram.py
+++ orange/doc/modules/hclust-colored-dendrogram.py
@@ -9,8 +9,7 @@ reduced = orange.ExampleTable(orange.Domain(sample.domain[:2], False), sample)
 my_colors = [(255,0,0), (0,255,0), (0,0,255)]
 cls = orngClustering.hierarchicalClustering_topClusters(root, 3)
 colors = dict([(cl, col) for cl, col in zip(cls, my_colors)])
+print data.native(2)
+orngClustering.dendrogram_draw("hclust-colored-dendrogram.png", root, data = reduced, labels=[str(d.getclass()) for d in sample],
+    cluster_colors=colors, color_palette=[(0, 255, 0), (0, 0, 0), (255, 0, 0)], gamma=0.5, minv=2.0, maxv=7.0)
 
-dendrogram = orngClustering.DendrogramPlot(root, reduced, labels=[str(d.getclass()) for d in sample],
-    clusterColors=colors)
-dendrogram.setMatrixColorScheme((0, 255, 0), (255, 0, 0))
-dendrogram.plot("hclust-colored-dendrogram.png")
