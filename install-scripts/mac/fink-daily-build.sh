922ac2ed4a1b927fe2baffa143dda74c2041008a
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2009-10-20 08:53:31 +0000

    Updated conflicting Fink packages.

--- install-scripts/mac/fink-daily-build.sh
+++ install-scripts/mac/fink-daily-build.sh
@@ -25,7 +25,7 @@ DAILY_SOURCE_DIRS="install-scripts/mac/bundle-lite/ add-ons/Bioinformatics/ add-
 # We would like to build all those so that it does not need to compile anything whichever packages he or she chooses
 # The problem is that they are often mutually conflicting so we cannot have them simply installed (so that update-all
 # would update them) but have to build them explicitly
-OTHER_PACKAGES="db44 db44-aes giflib libungif ghostscript ghostscript-esp ghostscript6 ghostscript-nox ghostscript6-nox ptex-base ptex-nox-base jadetex docbook-utils tetex-base tetex-nox-base"
+OTHER_PACKAGES="db44 db44-aes giflib libungif ghostscript ghostscript-esp ghostscript6 ghostscript-nox ghostscript6-nox ptex-base ptex-nox-base jadetex docbook-utils tetex-base tetex-nox-base texlive-base texlive-nox-base"
 
 FINK_ARGS="--yes --build-as-nobody"
 FINK_SELFUPDATE_ARGS="--yes"
