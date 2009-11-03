259460c67e16f04a1908919cc1fae2e99a16af0b
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2009-11-03 18:24:56 +0000

    Minor Fink packages references updates.

--- install-scripts/mac/fink-daily-build.sh
+++ install-scripts/mac/fink-daily-build.sh
@@ -26,7 +26,7 @@ DAILY_SOURCE_DIRS="install-scripts/mac/bundle-lite/ add-ons/Bioinformatics/ add-
 # The problem is that they are often mutually conflicting so we cannot have them simply installed (so that update-all
 # would update them) but have to build them explicitly
 # Temporary removed from a list because packages and/or dependencies are broken: jadetex docbook-utils
-OTHER_PACKAGES="db44 db44-aes giflib libungif ghostscript ghostscript-esp ghostscript6 ghostscript-nox ghostscript6-nox ptex-base ptex-nox-base tetex-base tetex-nox-base texlive-base texlive-nox-base lynx links w3m elinks"
+OTHER_PACKAGES="db47 db47-aes db44 db44-aes giflib libungif ghostscript ghostscript-esp ghostscript6 ghostscript-nox ghostscript6-nox ptex-base ptex-nox-base tetex-base tetex-nox-base texlive-base texlive-nox-base lynx links w3m elinks"
 
 FINK_ARGS="--yes --build-as-nobody"
 FINK_SELFUPDATE_ARGS="--yes"
