2e847801eb8d2f198d6ebc89827680076612e7c0
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2010-02-25 17:52:02 +0000

    Added root privileges check.

--- install-scripts/mac/fink-restore-selections.sh
+++ install-scripts/mac/fink-restore-selections.sh
@@ -6,6 +6,8 @@
 # Sets error handler
 trap "echo \"Script failed\"" ERR
 
+((`id -u` == 0)) || { echo "Must run as root user (use sudo)."; exit 1; }
+
 dpkg --get-selections '*' | cut -f 1 | xargs -n 1 -J % echo % purge | dpkg --set-selections
 curl http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-selections.list | dpkg --set-selections
 apt-get --assume-yes dselect-upgrade
