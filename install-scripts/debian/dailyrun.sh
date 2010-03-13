f48e6806e77370d824ccef0a0a3ad6c48fe9b09a
Mitar <Mitar@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2010-03-13 10:32:37 +0000

    Support for 64bit Debian packages.

--- install-scripts/debian/dailyrun.sh
+++ install-scripts/debian/dailyrun.sh
@@ -7,14 +7,16 @@ DAILY_REVISION=`svn info --non-interactive http://www.ailab.si/svn/orange/trunk/
 # svn info does not return proper exit status on an error so we check it this way
 [ "$DAILY_REVISION" ] || exit 1
 
+ARCH=`dpkg --print-architecture`
+
 mount | grep -q /mnt/debian || { mount /mnt/debian; sleep 10; }
 mount | grep -q /mnt/download || { mount /mnt/download; sleep 10; }
 
 /root/debian-daily-build.sh $DAILY_REVISION &> /tmp/debian-daily-build.log
 EXIT_VALUE=$?
-echo "Orange (Debian) [$EXIT_VALUE]" > "/mnt/download/buildLogs/debian/debian-daily-build.log"
-date >> "/mnt/download/buildLogs/debian/debian-daily-build.log"
-cat /tmp/debian-daily-build.log >> "/mnt/download/buildLogs/debian/debian-daily-build.log"
+echo "Orange (Debian $ARCH) [$EXIT_VALUE]" > "/mnt/download/buildLogs/debian/debian-$ARCH-daily-build.log"
+date >> "/mnt/download/buildLogs/debian/debian-$ARCH-daily-build.log"
+cat /tmp/debian-daily-build.log >> "/mnt/download/buildLogs/debian/debian-$ARCH-daily-build.log"
 (($EXIT_VALUE)) && echo "Running debian-daily-build.sh failed"
 
 # Zero exit value
