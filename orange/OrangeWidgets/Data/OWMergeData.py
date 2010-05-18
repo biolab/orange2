da566944e0dd73773891b4ac5d8ea18081c5a9bb
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2010-05-18 13:40:07 +0000

    - changed the way data is merged (does not clone attributes, in A+B A attributes (and values) take precedent and reverse in B+A

--- orange/OrangeWidgets/Data/OWMergeData.py
+++ orange/OrangeWidgets/Data/OWMergeData.py
@@ -9,17 +9,12 @@ import orange
 from OWWidget import *
 import OWGUI
 
-########################################################
-## TODO: crashes if the same data is sent to both inputs
-########################################################
-
 class OWMergeData(OWWidget):
 
-    settingsList = ["varA", "varB"]
+#    settingsList = ["varA", "varB"]
 
-    contextHandlers = {"A": DomainContextHandler("A", ["varA"]),
-                       "B": DomainContextHandler("B", ["varB"])}
-                                                 
+    contextHandlers = {"A": DomainContextHandler("A", [ContextField("varA")], syncWithGlobal=False),
+                       "B": DomainContextHandler("B", [ContextField("varB")], syncWithGlobal=False)}                                            
 
     def __init__(self, parent = None, signalManager = None, name = "Merge data"):
         OWWidget.__init__(self, parent, signalManager, name, wantMainArea = 0)  #initialize base class
@@ -86,6 +81,8 @@ class OWMergeData(OWWidget):
             return True, varList.index(var)
         elif var and var.varType == orange.Variable.String and var.name in [v.name for v in varList]:
             return True, [v.name for v in varList].index(var.name)
+        elif var and (var.name, var.varType) in [(v.name, v.varType) for v in varList]:
+            return True, [(v.name, v.varType) for v in varList].index((var.name, var.varType))
         else:
             return False, -1
         
@@ -93,75 +90,85 @@ class OWMergeData(OWWidget):
     def onDataAInput(self, data):
         self.closeContext("A")
         # set self.dataA, generate new domain if it is the same as of self.dataB.domain
-        if data and self.dataB and data.domain == self.dataB.domain:
-            if data.domain.classVar:
-                classVar = data.domain.classVar.clone()
-            else:
-                classVar = None
-            dc = orange.Domain([x.clone() for x in data.domain], classVar)
-            for i, a in enumerate(dc):
-                a.getValueFrom = lambda ex,f,i=i: ex[i]
-            # no need to clone meta attributes: dc.addmetas(dict([(orange.newmetaid(), x.clone()) for x in data.domain.getmetas().values()])); for i,id,a in enumerate(dc.getmetas().items()): ...
-            dc.addmetas(data.domain.getmetas())
-            self.dataA = orange.ExampleTable(dc, data)
-        else:
-            self.dataA = data
+#        if data and self.dataB and data.domain == self.dataB.domain:
+#            if data.domain.classVar:
+#                classVar = data.domain.classVar.clone()
+#            else:
+#                classVar = None
+#            dc = orange.Domain([x.clone() for x in data.domain], classVar)
+#            for i, a in enumerate(dc):
+#                a.getValueFrom = lambda ex,f,i=i: ex[i]
+##             no need to clone meta attributes: dc.addmetas(dict([(orange.newmetaid(), x.clone()) for x in data.domain.getmetas().values()])); for i,id,a in enumerate(dc.getmetas().items()): ...
+#            dc.addmetas(data.domain.getmetas())
+#            self.dataA = orange.ExampleTable(dc, data)
+#            self.dataA = data
+#        else:
+#            self.dataA = data
+        self.dataA = data
         # update self.varListA and self.varA
         if self.dataA:
-            self.varListA = self.dataA.domain.variables.native() + self.dataA.domain.getmetas().values()
+            self.varListA = list(self.dataA.domain.variables) + self.dataA.domain.getmetas().values()
         else:
             self.varListA = []
-        if not self.varA in self.varListA:
-            self.varA = None
+#        if not self.varA in self.varListA:
+#            self.varA = None
         # update info
         self.updateInfoA()
         # update attribute A listbox
         self.lbAttrA.clear()
         for var in self.varListA:
             self.lbAttrA.addItem(QListWidgetItem(self.icons[var.varType], var.name))
-        self.openContext("A", self.dataA)
+        if self.dataA:
+            self.openContext("A", self.dataA)
         match, index = self.inVarList(self.varListA, self.varA)
         if match:
             self.varA = self.varListA[index]
             self.lbAttrA.setCurrentItem(self.lbAttrA.item(index))
+    #        elif self.dataA:
+    #            self.varA = None
         self.sendData()
-
 
     def onDataBInput(self, data):
         # set self.dataB, generate new domain if it is the same as of self.dataA.domain
         self.closeContext("B")
-        if data and self.dataA and data.domain == self.dataA.domain:
-            if data.domain.classVar:
-                classVar = data.domain.classVar.clone()
-            else:
-                classVar = None
-            dc = orange.Domain([x.clone() for x in data.domain.attributes], classVar)
-            for i, a in enumerate(dc):
-                a.getValueFrom = lambda ex,f,i=i: ex[i]
-            # no need to clone meta attributes: dc.addmetas(dict([(orange.newmetaid(), x.clone()) for x in data.domain.getmetas().values()])); for i,id,a in enumerate(dc.getmetas().items()): ...
-            dc.addmetas(data.domain.getmetas())
-            self.dataB = orange.ExampleTable(dc, data)
-        else:
-            self.dataB = data
+#        if data and self.dataA and data.domain == self.dataA.domain:
+#            if data.domain.classVar:
+#                classVar = data.domain.classVar.clone()
+#            else:
+#                classVar = None
+#            dc = orange.Domain([x.clone() for x in data.domain.attributes], classVar)
+#            for i, a in enumerate(dc):
+#                a.getValueFrom = lambda ex,f,i=i: ex[i]
+#            # no need to clone meta attributes: dc.addmetas(dict([(orange.newmetaid(), x.clone()) for x in data.domain.getmetas().values()])); for i,id,a in enumerate(dc.getmetas().items()): ...
+#            dc.addmetas(data.domain.getmetas())
+#            self.dataB = orange.ExampleTable(dc, data)
+#            self.dataB = data
+#        else:
+#            self.dataB = data
+        self.dataB = data
         # update self.varListB and self.varB
         if self.dataB:
-            self.varListB = self.dataB.domain.variables.native() + self.dataB.domain.getmetas().values()
+            self.varListB = list(self.dataB.domain.variables) + self.dataB.domain.getmetas().values()
         else:
             self.varListB = []
-        if not self.varB in self.varListB:
-            self.varB = None
+#        if not self.varB in self.varListB:
+#            self.varB = None
         # update info
         self.updateInfoB()
         # update attribute B listbox
         self.lbAttrB.clear()
         for var in self.varListB:
             self.lbAttrB.addItem(QListWidgetItem(self.icons[var.varType], var.name))
-            
-        self.openContext("B", self.dataB)
+        
+        if self.dataB:
+            self.openContext("B", self.dataB)
         match, index = self.inVarList(self.varListB, self.varB)
         if match:
             self.varB = self.varListB[index]
             self.lbAttrB.setCurrentItem(self.lbAttrB.item(index))
+#        elif self.dataB:
+#            self.varB = None
+            
         self.sendData()
 
 
@@ -187,86 +194,93 @@ class OWMergeData(OWWidget):
             self.lblDataBAttributes.setText("")
 
 
-    def sendData(self):
-        """Sends out data.
-        """
-        if self.varA and self.varB and self.dataA and self.dataB:
-            # create dictionaries: attribute values -> example index
-            val2idxDictA = {}
-            for eIdx, e in enumerate(self.dataA):
-                val2idxDictA[e[self.varA].native()] = eIdx
-            val2idxDictB = {}
-            for eIdx, e in enumerate(self.dataB):
-                val2idxDictB[e[self.varB].native()] = eIdx
-            # remove DC and DK from dictionaries (change when bug 62 is fixed)
-##            if val2idxDictA.has_key(orange.Value(self.varA.varType, orange.ValueTypes.DC).native()):
-##                val2idxDictA.pop(orange.Value(self.varA.varType, orange.ValueTypes.DC).native())
-##            if val2idxDictA.has_key(orange.Value(self.varA.varType, orange.ValueTypes.DK).native()):
-##                val2idxDictA.pop(orange.Value(self.varA.varType, orange.ValueTypes.DK).native())
-##            if val2idxDictB.has_key(orange.Value(self.varB.varType, orange.ValueTypes.DC).native()):
-##                val2idxDictB.pop(orange.Value(self.varB.varType, orange.ValueTypes.DC).native())
-##            if val2idxDictB.has_key(orange.Value(self.varB.varType, orange.ValueTypes.DK).native()):
-##                val2idxDictB.pop(orange.Value(self.varB.varType, orange.ValueTypes.DK).native())
-            if val2idxDictA.has_key("?"):
-                val2idxDictA.pop("?")
-            if val2idxDictA.has_key("~"):
-                val2idxDictA.pop("~")
-            if val2idxDictA.has_key(""):
-                val2idxDictA.pop("")
-            if val2idxDictB.has_key("?"):
-                val2idxDictB.pop("?")
-            if val2idxDictB.has_key("~"):
-                val2idxDictB.pop("~")
-            if val2idxDictB.has_key(""):
-                val2idxDictB.pop("")
-            # example table names
-            nameA = self.dataA.name
-            if not nameA: nameA = "Examples A"
-            nameB = self.dataB.name
-            if not nameB: nameB = "Examples B"
-            # create example B with all values unknown
-            exBDK = orange.Example(self.dataB[0])
-            for var in self.varListB:
-##                exBDK[var] = orange.Value(var.varType, orange.ValueTypes.DK)
-                exBDK[var] = "?"
-            # build example table to append to the right of A
-            vlBreduced = list(self.varListB)
-            vlBreduced.remove(self.varB)
-            domBreduced = orange.Domain(vlBreduced, None)
-            etBreduced = orange.ExampleTable(domBreduced)
-            for e in self.dataA:
-                dataBidx = val2idxDictB.get(e[self.varA].native(), None)
-                if dataBidx <> None:
-                    etBreduced.append(self.dataB[dataBidx])
-                else:
-                    etBreduced.append(orange.Example(domBreduced, exBDK))
-            etAB = orange.ExampleTable([self.dataA, etBreduced])
-            etAB.name = "%(nameA)s (merged with %(nameB)s)" % vars()
-            self.send("Merged Examples A+B", etAB)
+#    def sendData(self):
+#        """Sends out data.
+#        """
+#        if self.varA and self.varB and self.dataA and self.dataB:
+#            # create dictionaries: attribute values -> example index
+#            val2idxDictA = {}
+#            for eIdx, e in enumerate(self.dataA):
+#                val2idxDictA[e[self.varA].native()] = eIdx
+#            val2idxDictB = {}
+#            for eIdx, e in enumerate(self.dataB):
+#                val2idxDictB[e[self.varB].native()] = eIdx
+#            # remove DC and DK from dictionaries (change when bug 62 is fixed)
+###            if val2idxDictA.has_key(orange.Value(self.varA.varType, orange.ValueTypes.DC).native()):
+###                val2idxDictA.pop(orange.Value(self.varA.varType, orange.ValueTypes.DC).native())
+###            if val2idxDictA.has_key(orange.Value(self.varA.varType, orange.ValueTypes.DK).native()):
+###                val2idxDictA.pop(orange.Value(self.varA.varType, orange.ValueTypes.DK).native())
+###            if val2idxDictB.has_key(orange.Value(self.varB.varType, orange.ValueTypes.DC).native()):
+###                val2idxDictB.pop(orange.Value(self.varB.varType, orange.ValueTypes.DC).native())
+###            if val2idxDictB.has_key(orange.Value(self.varB.varType, orange.ValueTypes.DK).native()):
+###                val2idxDictB.pop(orange.Value(self.varB.varType, orange.ValueTypes.DK).native())
+#            if val2idxDictA.has_key("?"):
+#                val2idxDictA.pop("?")
+#            if val2idxDictA.has_key("~"):
+#                val2idxDictA.pop("~")
+#            if val2idxDictA.has_key(""):
+#                val2idxDictA.pop("")
+#            if val2idxDictB.has_key("?"):
+#                val2idxDictB.pop("?")
+#            if val2idxDictB.has_key("~"):
+#                val2idxDictB.pop("~")
+#            if val2idxDictB.has_key(""):
+#                val2idxDictB.pop("")
+#            # example table names
+#            nameA = self.dataA.name
+#            if not nameA: nameA = "Examples A"
+#            nameB = self.dataB.name
+#            if not nameB: nameB = "Examples B"
+#            # create example B with all values unknown
+#            exBDK = orange.Example(self.dataB[0])
+#            for var in self.varListB:
+###                exBDK[var] = orange.Value(var.varType, orange.ValueTypes.DK)
+#                exBDK[var] = "?"
+#            # build example table to append to the right of A
+#            vlBreduced = list(self.varListB)
+#            vlBreduced.remove(self.varB)
+#            domBreduced = orange.Domain(vlBreduced, None)
+#            etBreduced = orange.ExampleTable(domBreduced)
+#            for e in self.dataA:
+#                dataBidx = val2idxDictB.get(e[self.varA].native(), None)
+#                if dataBidx <> None:
+#                    etBreduced.append(self.dataB[dataBidx])
+#                else:
+#                    etBreduced.append(orange.Example(domBreduced, exBDK))
+#            etAB = orange.ExampleTable([self.dataA, etBreduced])
+#            etAB.name = "%(nameA)s (merged with %(nameB)s)" % vars()
+#            self.send("Merged Examples A+B", etAB)
+#
+#            # create example A with all values unknown
+#            exADK = orange.Example(self.dataA[0])
+#            for var in self.varListA:
+###                exADK[var] = orange.Value(var.varType, orange.ValueTypes.DK)
+#                exADK[var] = "?"
+#            # build example table to append to the right of B
+#            vlAreduced = list(self.varListA)
+#            vlAreduced.remove(self.varA)
+#            domAreduced = orange.Domain(vlAreduced, None)
+#            etAreduced = orange.ExampleTable(domAreduced)
+#            for e in self.dataB:
+#                dataAidx = val2idxDictA.get(e[self.varB].native(), None)
+#                if dataAidx <> None:
+#                    etAreduced.append(self.dataA[dataAidx])
+#                else:
+#                    etAreduced.append(orange.Example(domAreduced, exADK))
+#            etBA = orange.ExampleTable([self.dataB, etAreduced])
+#            etBA.name = nameB + " (merged with %s)" % nameA
+#            self.send("Merged Examples B+A", etBA)
+#        else:
+#            self.send("Merged Examples A+B", None)
+#            self.send("Merged Examples B+A", None)
 
-            # create example A with all values unknown
-            exADK = orange.Example(self.dataA[0])
-            for var in self.varListA:
-##                exADK[var] = orange.Value(var.varType, orange.ValueTypes.DK)
-                exADK[var] = "?"
-            # build example table to append to the right of B
-            vlAreduced = list(self.varListA)
-            vlAreduced.remove(self.varA)
-            domAreduced = orange.Domain(vlAreduced, None)
-            etAreduced = orange.ExampleTable(domAreduced)
-            for e in self.dataB:
-                dataAidx = val2idxDictA.get(e[self.varB].native(), None)
-                if dataAidx <> None:
-                    etAreduced.append(self.dataA[dataAidx])
-                else:
-                    etAreduced.append(orange.Example(domAreduced, exADK))
-            etBA = orange.ExampleTable([self.dataB, etAreduced])
-            etBA.name = nameB + " (merged with %s)" % nameA
-            self.send("Merged Examples B+A", etBA)
+    def sendData(self):
+        if self.dataA and self.dataB and self.varA and self.varB:
+            self.send("Merged Examples A+B", self.merge(self.dataA, self.dataB, self.varA, self.varB))
+            self.send("Merged Examples B+A", self.merge(self.dataB, self.dataA, self.varB, self.varA))
         else:
             self.send("Merged Examples A+B", None)
             self.send("Merged Examples B+A", None)
-
 
     ############################################################################################################################################################
     ## Event handlers
@@ -314,8 +328,69 @@ class OWMergeData(OWWidget):
         else:
             return str(n), 's'
 
+    def merge(self, dataA, dataB, varA, varB):
+        """ Merge two tables
+        """
+        
+        val2idx = dict([(e[varB].native(), i) for i, e in reversed(list(enumerate(dataB)))])
+        
+        for key in ["?", "~", ""]:
+            if key in val2idx:
+                val2idx.pop(key)
+                 
+        metasA = dataA.domain.getmetas().items()
+        metasB = dataB.domain.getmetas().items()
+        
+        includedAttsB = [attrB for attrB in dataB.domain if attrB not in dataA.domain]
+        includedMetaB = [(mid, meta) for mid, meta in metasB if (mid, meta) not in metasA]
+        includedClassVarB = dataB.domain.classVar and dataB.domain.classVar not in dataA.domain
+        
+        reducedDomainB = orange.Domain(includedAttsB, includedClassVarB)
+        reducedDomainB.addmetas(dict(includedMetaB))
+        
+        
+        mergingB = orange.ExampleTable(reducedDomainB)
+        
+        for ex in dataA:
+            ind = val2idx.get(ex[varA].native(), None)
+            if ind is not None:
+                mergingB.append(orange.Example(reducedDomainB, dataB[ind]))
+                
+            else:
+                mergingB.append(orange.Example(reducedDomainB, ["?"] * len(reducedDomainB)))
+                
+        return orange.ExampleTable([dataA, mergingB])
 
-
+    
+#    def setVarA(self, var):
+#        if var in self.varListA:
+#            ind = self.varListA.index(var)
+#            self.lbAttrA.setCurrentItem(self.lbAttrA.item(ind))
+#        
+#    def varA(self):
+#        selectedItems = self.lbAttrB.selectedItems()
+#        if selectedItems:
+#            ind = self.lbAttrB.row(selectedItems[0])
+#            return self.varListA[ind]
+#        else:
+#            return None
+#    varA = property(varA, setVarA)
+#    
+#    def setVarB(self, var):
+#        if var in self.varListB:
+#            ind = self.varListB.index(var)
+#            self.lbAttrB.setCurrentItem(self.lbAttrB.item(ind))
+#            
+#    def varB(self):
+#        selectedItems = self.lbAttrB.selectedItems()
+#        if selectedItems:
+#            ind = self.lbAttrB.row(selectedItems[0])
+#            return self.varListB[ind]
+#        else:
+#            return None
+#    
+#    varB = property(varB, setVarB)
+    
 if __name__=="__main__":
     """
     import sys
