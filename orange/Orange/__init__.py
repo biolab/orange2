de44dc74fc1de219eb2d337317432c7fe6f6c80f
janezd <janez.demsar@fri.uni-lj.si>
2010-07-15 02:05:12 +0000

    - undid the previous "fix"

---
+++ orange/Orange/__init__.py
@@ -0,0 +1,47 @@
+import orange
+core = orange
+
+import data
+import data.io
+import data.example
+import data.sample
+import data.value
+import data.variable
+import data.domain
+
+import graph
+
+import stat
+
+import probability
+import probability.estimate
+import probability.distributions
+import probability.evd
+
+import classify
+import classify.trees
+import classify.rules
+import classify.lookup
+import classify.bayes
+import classify.svm
+import classify.logreg
+import classify.knn
+import classify.majority
+
+import regress
+
+import associate
+
+import preprocess
+import preprocess.value
+import preprocess.data
+
+import distances
+
+import wrappers
+
+import featureConstruction
+import featureConstruction.univariate
+import featureConstruction.functionDecomposition
+
+import cluster
