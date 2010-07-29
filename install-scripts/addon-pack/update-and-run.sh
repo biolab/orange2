bae0fae76620e9540668ee02496428b40f6fd2d8
matija <matija.polajnar@fri.uni-lj.si>
2010-07-29 09:03:47 +0000

    VMware script: poweroff after packing the add-ons.

--- install-scripts/addon-pack/update-and-run.sh
+++ install-scripts/addon-pack/update-and-run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Matija Polajnar, 28. 7. 2010
+# matija.polajnar@fri.uni-lj.si
+#
+# This script issues an update of the packaging scripts and runs the packaging. 
+#
+
 cd `dirname $0`
 ./update-scripts.sh
 ./pack-addons.sh
+poweroff
