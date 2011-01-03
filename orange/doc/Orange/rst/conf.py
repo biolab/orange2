a41f76bdfddf9f1959ac0416924ab31c7a2a38d9
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-01-03 15:19:22 +0000

    - Using docutils Transform and CSS styling for decorating list items (with stamp pixmaps)

--- orange/doc/Orange/rst/conf.py
+++ orange/doc/Orange/rst/conf.py
@@ -96,7 +96,7 @@ html_theme = 'orange_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"collapsiblesidebar": "true"}
+html_theme_options = {"collapsiblesidebar": "false"}
 
 if html_theme == "orange_theme":
         html_theme_options.update({"orangeheaderfooter": "false"})
