f5fe03c5d78ca0fab5f0046bc38c2a0f0ee9cddd
markotoplak <devnull@localhost>
2011-02-02 11:48:07 +0000

    Fixed indent.

--- orange/Orange/classify/knn.py
+++ orange/Orange/classify/knn.py
@@ -1,8 +1,7 @@
-from orange import \
-              kNNLearner, \
-         FindNearest, \
-              FindNearest_BruteForce, \
-         FindNearestConstructor, \
-              FindNearestConstructor_BruteForce, \
-         kNNClassifier, \
+from orange import kNNLearner, \
+         FindNearest,\
+         FindNearest_BruteForce,\
+         FindNearestConstructor,\
+         FindNearestConstructor_BruteForce,\
+         kNNClassifier,\
          P2NN
