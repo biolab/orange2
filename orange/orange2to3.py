dda9c79bd14182ae2b6852758d44f7b707652ef3
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-02-03 17:49:19 +0000

    - removed unused fixer - orange2to25 runs from any directory

--- orange/orange2to3.py
+++ orange/orange2to3.py
@@ -6,6 +6,9 @@ from lib2to3.main import diff_texts, StdoutRefactoringTool, warn
 
 #fixes = refactor.get_fixers_from_package("lib2to3.fixes")    
 
+dir = os.path.dirname(__file__)
+if dir not in sys.path:
+    sys.path.insert(0, dir)
 sys.exit(main.main("fixes", sys.argv))
 
 
