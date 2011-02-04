0c7090666ee6928a269a77bea918a4ba7b2db7f2
blaz <blaz.zupan@fri.uni-lj.si>
2011-02-04 17:38:48 +0000

    mapped to new clustering modules

--- orange/orngClustering.py
+++ orange/orngClustering.py
@@ -1,2 +1,23 @@
-from Orange.cluster import * 
+from Orange.clustering.kmeans import * 
+# from Orange.cluster.hierarchical import * 
+
+
+from Orange.clustering.kmeans import Clustering as KMeans
+from Orange.clustering.kmeans import init_random as kmeans_init_random
+from Orange.clustering.kmeans import init_diversity as kmeans_init_diversity
+from Orange.clustering.kmeans import init_hclustering as KMeans_init_hierarchicalClustering
+from Orange.clustering.kmeans import data_center as data_center
+from Orange.clustering.kmeans import plot_silhouette as plot_silhouette
+from Orange.clustering.kmeans import score_distance_to_centroids as score_distance_to_centroids
+from Orange.clustering.kmeans import score_silhouette as score_silhouette
+from Orange.clustering.kmeans import score_fast_silhouette as score_fastsilhouette
+
+from Orange.clustering.hierarchical import clustering as hierarchicalClustering
+from Orange.clustering.hierarchical import clustering_features as hierarchicalClustering_attributes
+from Orange.clustering.hierarchical import cluster_to_list as hierarchicalClustering_clusterList
+from Orange.clustering.hierarchical import top_clusters as hierarchicalClustering_topClusters
+from Orange.clustering.hierarchical import top_cluster_membership as hierarhicalClustering_topClustersMembership
+from Orange.clustering.hierarchical import order_leaves as orderLeaves
+
 #left for backward compatibility
+hierarchicalClustering_attributes
