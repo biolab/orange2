6b8e41ed9f53862af96cd2419f535139d51b8cdd
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-02-04 21:26:38 +0000

    - fix_changed_names_aggressive checks for a proper unqualified 'from module import *' before transforming the code

--- orange/fixes/fix_changed_names_aggressive.py
+++ orange/fixes/fix_changed_names_aggressive.py
@@ -1,10 +1,18 @@
 from lib2to3 import pytree
-from lib2to3.fixer_util import Name, Dot, Node, attr_chain, touch_import
+from lib2to3.fixer_util import Name, Dot, Node, attr_chain, touch_import, find_root
 from lib2to3 import fixer_base
+from lib2to3 import patcomp
 from collections import defaultdict
 
 from .fix_changed_names import FixChangedNames, MAPPING
 
+def find_matches(pattern, tree):
+    for node in tree.pre_order():
+        res = {}
+        match = pattern.match(node, res)
+        if match:
+            yield res
+    
 def build_pattern(mapping=MAPPING):
     PATTERN = """
     power< local=(%s)
@@ -13,6 +21,15 @@ def build_pattern(mapping=MAPPING):
     """ 
     return PATTERN % "|".join("'%s'" % key.split(".")[-1] for key in mapping.keys())
 
+
+## Pattern for finding from module import * 
+from_import_pattern = """import_from<'from' module=(%s) 'import' star='*'>"""
+module_names = set(["'%s'" % key.split(".", 1)[0] for key in MAPPING.keys()])
+from_import_pattern = from_import_pattern % "|".join(module_names)
+print from_import_pattern
+
+from_import_expr = patcomp.compile_pattern(from_import_pattern)
+ 
 class FixChangedNamesAggressive(FixChangedNames):
     mapping = MAPPING 
     run_order = 2
@@ -21,7 +38,6 @@ class FixChangedNamesAggressive(FixChangedNames):
         # We override this, so MAPPING can be pragmatically altered and the
         # changes will be reflected in PATTERN.
         self.PATTERN = build_pattern(self.mapping)
-#        self._modules_to_change = [key.split(".", 1)[0] for key in self.mapping.keys()]
         name2mod = defaultdict(set)
         for key in self.mapping.keys():
             mod, name = key.split(".", 1)
@@ -29,31 +45,44 @@ class FixChangedNamesAggressive(FixChangedNames):
         self._names_to_modules = name2mod
         
         fixer_base.BaseFix.compile_pattern(self)
+        
+    def start_tree(self, tree, filename):
+        super(FixChangedNamesAggressive, self).start_tree(tree, filename)
+        ## Find unqualified imports
+        self._import_matches = list(find_matches(from_import_expr, tree))
+        
+    def finish_tree(self, tree, filename):
+        del self._import_matches
                 
     def transform(self, node, results):
         local = results.get("local")
         tail = results.get("tail")
         if local:
             local = local[0]
-            tail = tail[0]
             local_name = local.value
             modules = self._names_to_modules[local_name]
             if len(modules) > 1:
-                import warnings
-                warnings.warn("Conflicting name '%s' is present in %s! Ignoring transformation!" % local_name, modules)
+                self.warnings(node, "Conflicting name '%s' is present in %s! Ignoring transformation!" % local_name, modules)
                 return
             
-            ## TODOL check if tree contains a from module import * statement. if not ignore the transformation
             module = list(modules)[0]
+            
+            if all("module" not in res for res in self._import_matches): 
+                self.warning(node, "Aggressive name matched '%s' but no corresponding import! Fix manualy." % local)
+                return
+                
             new_name = unicode(self.mapping[module + "." + local_name])
             
             syms = self.syms
             
-            tail = tail.clone()
+            if tail:
+                tail = [t.clone() for t in tail]
+#            tail = tail.clone()
             new = self.package_tree(new_name)
-            new = pytree.Node(syms.power, new + [tail])
+            new = pytree.Node(syms.power, new + tail, prefix=local.prefix)
             
             # Make sure the proper package is imported
             package = new_name.rsplit(".", 1)[0]
             touch_import(None, package, node)
-            return new    
+            return new
+        
