e6adf4bed6170b16c1340cda0bf8077c2bf74bd7
matija <matija.polajnar@fri.uni-lj.si>
2011-02-04 23:51:47 +0000

    orngClustering: Add distance bindings.

--- orange/orngClustering.py
+++ orange/orngClustering.py
@@ -1,6 +1,36 @@
 from Orange.clustering.kmeans import * 
 # from Orange.cluster.hierarchical import * 
 
+from Orange.distances import \
+     AlignmentList, \
+     DistanceMap, \
+     DistanceMapConstructor, \
+     ExampleDistConstructor, \
+     ExampleDistBySorting, \
+     ExampleDistVector, \
+     ExamplesDistance, \
+     ExamplesDistance_Normalized, \
+     ExamplesDistanceConstructor
+
+from Orange.distances import Hamming as ExamplesDistance_Hamming
+from Orange.distances import DTW as ExamplesDistance_DTW
+from Orange.distances import Euclidean as ExamplesDistance_Euclidean
+from Orange.distances import Manhattan as ExamplesDistance_Manhattan
+from Orange.distances import Maximal as ExamplesDistance_Maximal
+from Orange.distances import Relief as ExamplesDistance_Relief
+
+from Orange.distances import DTWConstructor as ExamplesDistanceConstructor_DTW
+from Orange.distances import EuclideanConstructor as ExamplesDistanceConstructor_Euclidean
+from Orange.distances import HammingConstructor as ExamplesDistanceConstructor_Hamming
+from Orange.distances import ManhattanConstructor as ExamplesDistanceConstructor_Manhattan
+from Orange.distances import MaximalConstructor as ExamplesDistanceConstructor_Maximal 
+from Orange.distances import ReliefConstructor as ExamplesDistanceConstructor_Relief
+
+from Orange.distances import PearsonRConstructor as ExamplesDistanceConstructor_PearsonR
+from Orange.distances import PearsonR as ExamplesDistance_PearsonR
+from Orange.distances import SpearmanRConstructor as ExamplesDistanceConstructor_SpearmanR
+from Orange.distances import SpearmanR as ExamplesDistance_SpearmanR
+
 
 from Orange.clustering.kmeans import Clustering as KMeans
 from Orange.clustering.kmeans import init_random as kmeans_init_random
