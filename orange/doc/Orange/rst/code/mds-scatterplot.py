47faa445d3108956409af01a5dab27d1cf378303
matija <matija.polajnar@fri.uni-lj.si>
2011-02-04 23:52:06 +0000

    MDS regression tests/documentation: Add Orange.distances bindings.

--- orange/doc/Orange/rst/code/mds-scatterplot.py
+++ orange/doc/Orange/rst/code/mds-scatterplot.py
@@ -10,7 +10,7 @@ import Orange
 table = Orange.data.Table("iris.tab")
 
 # Construct a distance matrix using Euclidean distance
-euclidean = Orange.core.ExamplesDistanceConstructor_Euclidean(table)
+euclidean = Orange.distances.EuclideanConstructor(table)
 distance = Orange.core.SymMatrix(len(table))
 for i in range(len(table)):
    for j in range(i+1):
