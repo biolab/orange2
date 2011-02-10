f2b049348333e13f3c8fb4762bcbc2b19ea4e4c3
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-02-10 09:25:23 +0000

    - removed function constructor, added __new__ method instead

--- orange/Orange/classification/wrappers.py
+++ orange/Orange/classification/wrappers.py
@@ -1,15 +1,26 @@
-import Orange.core as orange
+import Orange.core
+import Orange.evaluation.scoring as scoring
+import Orange.data
+import Orange.evaluation.testing
+import Orange.evaluation.scoring
 
-class StepwiseLearner_Class:
+class StepwiseLearner(Orange.core.Learner):
+  def __new__(cls, data=None, weightId=None, **kwargs):
+      self = Orange.core.Learner.__new__(cls, **kwargs)
+      if data is not None:
+          self.__init__(**kwargs)
+          return self(data, weightId)
+      else:
+          return self
+      
   def __init__(self, **kwds):
-    import orngStat
     self.removeThreshold = 0.3
     self.addThreshold = 0.2
-    self.stat, self.statsign = orngStat.CA, 1
+    self.stat, self.statsign = scoring.CA, 1
     self.__dict__.update(kwds)
 
   def __call__(self, examples, weightID = 0, **kwds):
-    import orngTest, orngStat, statc
+    import Orange.evaluation.testing, Orange.evaluation.scoring, statc
     
     self.__dict__.update(kwds)
 
@@ -18,13 +29,13 @@ class StepwiseLearner_Class:
 
     classVar = examples.domain.classVar
     
-    indices = orange.MakeRandomIndicesCV(examples, folds = getattr(self, "folds", 10))
-    domain = orange.Domain([], classVar)
+    indices = Orange.core.MakeRandomIndicesCV(examples, folds = getattr(self, "folds", 10))
+    domain = Orange.data.Domain([], classVar)
 
-    res = orngTest.testWithIndices([self.learner], orange.ExampleTable(domain, examples), indices)
+    res = Orange.evaluation.testing.testWithIndices([self.learner], Orange.data.Table(domain, examples), indices)
     
     oldStat = self.stat(res)[0]
-    oldStats = [self.stat(x)[0] for x in orngStat.splitByIterations(res)]
+    oldStats = [self.stat(x)[0] for x in Orange.evaluation.scoring.splitByIterations(res)]
     print ".", oldStat, domain
     stop = False
     while not stop:
@@ -32,11 +43,11 @@ class StepwiseLearner_Class:
         if len(domain.attributes)>=2:
             bestStat = None
             for attr in domain.attributes:
-                newdomain = orange.Domain(filter(lambda x: x!=attr, domain.attributes), classVar)
-                res = orngTest.testWithIndices([self.learner], (orange.ExampleTable(newdomain, examples), weightID), indices)
+                newdomain = Orange.data.Domain(filter(lambda x: x!=attr, domain.attributes), classVar)
+                res = Orange.evaluation.testing.testWithIndices([self.learner], (Orange.data.Table(newdomain, examples), weightID), indices)
                 
                 newStat = self.stat(res)[0]
-                newStats = [self.stat(x)[0] for x in orngStat.splitByIterations(res)] 
+                newStats = [self.stat(x)[0] for x in Orange.evaluation.scoring.splitByIterations(res)] 
                 print "-", newStat, newdomain
                 ## If stat has increased (ie newStat is better than bestStat)
                 if not bestStat or cmp(newStat, bestStat) == self.statsign:
@@ -45,7 +56,7 @@ class StepwiseLearner_Class:
                     elif statc.wilcoxont(oldStats, newStats)[1] > self.removeThreshold:
                             bestStat, bestAttr, bestStats = newStat, newStats, attr
             if bestStat:
-                domain = orange.Domain(filter(lambda x: x!=bestAttr, domain.attributes), classVar)
+                domain = Orange.data.Domain(filter(lambda x: x!=bestAttr, domain.attributes), classVar)
                 oldStat, oldStats = bestStat, bestStats
                 stop = False
                 print "removed", bestAttr.name
@@ -53,27 +64,21 @@ class StepwiseLearner_Class:
         bestStat, bestAttr = oldStat, None
         for attr in examples.domain.attributes:
             if not attr in domain.attributes:
-                newdomain = orange.Domain(domain.attributes + [attr], classVar)
-                res = orngTest.testWithIndices([self.learner], (orange.ExampleTable(newdomain, examples), weightID), indices)
+                newdomain = Orange.data.Domain(domain.attributes + [attr], classVar)
+                res = Orange.evaluation.testing.testWithIndices([self.learner], (Orange.data.Table(newdomain, examples), weightID), indices)
                 
                 newStat = self.stat(res)[0]
-                newStats = [self.stat(x)[0] for x in orngStat.splitByIterations(res)] 
+                newStats = [self.stat(x)[0] for x in Orange.evaluation.scoring.splitByIterations(res)] 
                 print "+", newStat, newdomain
 
                 ## If stat has increased (ie newStat is better than bestStat)
                 if cmp(newStat, bestStat) == self.statsign and statc.wilcoxont(oldStats, newStats)[1] < self.addThreshold:
                     bestStat, bestStats, bestAttr = newStat, newStats, attr
         if bestAttr:
-            domain = orange.Domain(domain.attributes + [bestAttr], classVar)
+            domain = Orange.data.Domain(domain.attributes + [bestAttr], classVar)
             oldStat, oldStats = bestStat, bestStats
             stop = False
             print "added", bestAttr.name
 
-    return self.learner(orange.ExampleTable(domain, examples), weightID)
+    return self.learner(Orange.data.Table(domain, examples), weightID)
 
-def StepwiseLearner(examples = None, weightID = None, **argkw):
-    sl = apply(StepwiseLearner_Class, (), argkw)
-    if examples:
-        return sl(examples, weightID)
-    else:
-        return sl
