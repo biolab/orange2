edbfc3508e426dbf8dcefe6dad228aadde135b28
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-02-11 11:58:09 +0000

    - reassign attsAbovethreshold = attsAboveThreshold for backwards compatibility

--- orange/orngFSS.py
+++ orange/orngFSS.py
@@ -2,3 +2,6 @@ from Orange.feature.scoring import *
 from Orange.feature.selection import *
 from Orange.classification.wrappers import StepwiseLearner
 StepwiseLearner_Class = StepwiseLearner
+
+#This was in the old module
+attsAbovethreshold = attsAboveThreshold
