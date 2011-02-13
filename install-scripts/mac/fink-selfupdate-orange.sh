61fe1d8fe24308efd26688b86077be63b066f381
mitar <devnull@localhost>
2011-02-13 14:55:48 +0000

    Made `fink-selfupdate-orange.sh` script cleanup old repository.

--- install-scripts/mac/fink-selfupdate-orange.sh
+++ install-scripts/mac/fink-selfupdate-orange.sh
@@ -18,19 +18,29 @@ test -r $FINK_ROOT/bin/init.sh || { echo "Fink cannot be found." exit 2; }
 # Configures environment for Fink
 . $FINK_ROOT/bin/init.sh
 
-echo "Preparing local ailab Fink info files repository."
-mkdir -p $FINK_ROOT/fink/dists/ailab/main/finkinfo/
-rm -f $FINK_ROOT/fink/dists/ailab/main/finkinfo/*
+if [ -e $FINK_ROOT/fink/dists/ailab/ ]; then
+	echo "Removing obsolete local ailab Fink info files repository."
+	rm -rf "$FINK_ROOT/fink/dists/ailab/"
+fi
+
+echo "Preparing local biolab Fink info files repository."
+mkdir -p $FINK_ROOT/fink/dists/biolab/main/finkinfo/
+rm -f $FINK_ROOT/fink/dists/biolab/main/finkinfo/*
 
 # Gets current (daily) info files from SVN
-echo "Updating local ailab Fink info files repository."
-curl "http://orange.biolab.si/fink/dists/10.$MAC_VERSION/main/finkinfo/all.tgz" --output $FINK_ROOT/fink/dists/ailab/main/finkinfo/all.tgz
-tar -xzf $FINK_ROOT/fink/dists/ailab/main/finkinfo/all.tgz -C $FINK_ROOT/fink/dists/ailab/main/finkinfo/
-rm -f $FINK_ROOT/fink/dists/ailab/main/finkinfo/all.tgz
+echo "Updating local biolab Fink info files repository."
+curl "http://orange.biolab.si/fink/dists/10.$MAC_VERSION/main/finkinfo/all.tgz" --output $FINK_ROOT/fink/dists/biolab/main/finkinfo/all.tgz
+tar -xzf $FINK_ROOT/fink/dists/biolab/main/finkinfo/all.tgz -C $FINK_ROOT/fink/dists/biolab/main/finkinfo/
+rm -f $FINK_ROOT/fink/dists/biolab/main/finkinfo/all.tgz
 
-if ! grep '^Trees:' $FINK_ROOT/etc/fink.conf | grep -q 'ailab/main'; then
-	echo "Adding local ailab Fink info files repository to Fink configuration."
-	perl -p -i -l -e '$_ = "$_ ailab/main" if /^Trees/' $FINK_ROOT/etc/fink.conf
+if grep '^Trees:' $FINK_ROOT/etc/fink.conf | grep -q 'ailab/main'; then
+	echo "Removing obsolete local ailab Fink info files repository from Fink configuration."
+	perl -p -i -l -e 's| ?ailab/main||g if /^Trees/' $FINK_ROOT/etc/fink.conf	
+fi
+
+if ! grep '^Trees:' $FINK_ROOT/etc/fink.conf | grep -q 'biolab/main'; then
+	echo "Adding local biolab Fink info files repository to Fink configuration."
+	perl -p -i -l -e '$_ = "$_ biolab/main" if /^Trees/' $FINK_ROOT/etc/fink.conf
 fi
 
 if ! grep -q '^UseBinaryDist:.*true' $FINK_ROOT/etc/fink.conf; then
@@ -39,8 +49,15 @@ if ! grep -q '^UseBinaryDist:.*true' $FINK_ROOT/etc/fink.conf; then
 fi
 
 # Adds our binary repository to local Fink (APT) configuration if on Mac OS X versions we provide binary packages for
+if grep -q "ailab" $FINK_ROOT/etc/apt/sources.list; then
+	echo "Removing obsolete ailab Fink binary packages repository from Fink configuration."
+	grep -v "ailab" $FINK_ROOT/etc/apt/sources.list > $FINK_ROOT/etc/apt/sources.list.new
+	mv $FINK_ROOT/etc/apt/sources.list.new $FINK_ROOT/etc/apt/sources.list
+	apt-get update
+fi
+
 if [[ "$MAC_VERSION" -eq 5 || "$MAC_VERSION" -eq 6 ]] && ! grep -q "deb http://orange.biolab.si/fink 10.$MAC_VERSION main" $FINK_ROOT/etc/apt/sources.list; then
-	echo "Adding ailab Fink binary packages repository to Fink configuration."
+	echo "Adding biolab Fink binary packages repository to Fink configuration."
 	echo "deb http://orange.biolab.si/fink 10.$MAC_VERSION main" >> $FINK_ROOT/etc/apt/sources.list
 fi
 
@@ -50,12 +67,12 @@ fink --yes index
 
 cat <<-EOMSG
 	
-	Information about ailab packages have been updated. You can now update installed
+	Information about biolab packages have been updated. You can now update installed
 	packages using:
 	
 	    fink update-all
 	
-	You can list available ailab packages using commands like:
+	You can list available biolab packages using commands like:
 	
 	    fink list --tab orange
 	
@@ -69,7 +86,7 @@ if ! grep '^Trees:' $FINK_ROOT/etc/fink.conf | grep -q 'unstable/main' || grep '
 		
 		WARNING: Your local Fink installation does not seem to use unstable Fink
 		         packages tree with rsync or CVS updating. This means that it could
-		         happen that some package on which ailab packages depend will not be
+		         happen that some package on which biolab packages depend will not be
 		         found or possible to install. In this case please configure Fink to
 		         use unstable tree using:
 		         
@@ -99,7 +116,7 @@ if [ $MAC_VERSION -lt "5" ] && [ ! -x /usr/X11/bin/X ]; then
 	cat <<-EOMSG
 		
 		WARNING: It seems you do not have X11 installed on a system. This means that it
-		         could happen that some package on which ailab packages depend will fail
+		         could happen that some package on which biolab packages depend will fail
 		         to run or compile. In this case please install it from your Mac OS X
 		         installation disk.
 	EOMSG
@@ -108,7 +125,7 @@ elif [ $MAC_VERSION -ge "5" ] && [ ! "`/usr/X11/bin/X -version 2>&1 | grep '^X.O
 		
 		WARNING: It seems you do not have X11 version 2.3.0 or later installed on a
 		         system. This means that it could happen that some package on which
-		         ailab packages depend will fail to run or compile. In this case
+		         biolab packages depend will fail to run or compile. In this case
 		         please install it from your Mac OS X installation disk or from:
 		         
 		             http://xquartz.macosforge.org/trac/wiki
