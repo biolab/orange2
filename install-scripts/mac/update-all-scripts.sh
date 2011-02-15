ee51d10ea7638180bb7aef28c23c5a3c8ef8ce15
mitar <devnull@localhost>
2011-02-15 14:15:57 +0000

    Updated script for updating daily building scripts.

--- install-scripts/mac/update-all-scripts.sh
+++ install-scripts/mac/update-all-scripts.sh
@@ -3,17 +3,17 @@
 # Should be run as: ./update-all-scripts.sh
 #
 
-curl --silent --output update-all-scripts.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/update-all-scripts.sh
-curl --silent --output bundle-64bit-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/bundle-64bit-daily-build.sh
-curl --silent --output bundle-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/bundle-daily-build.sh
-curl --silent --output dailyrun.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/dailyrun.sh
-curl --silent --output dailyrun-finkonly-withsource.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/dailyrun-finkonly-withsource.sh
-curl --silent --output dailyrun-finkonly.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/dailyrun-finkonly.sh
-curl --silent --output dailyrun-bundleonly.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/dailyrun-bundleonly.sh
-curl --silent --output fink-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-daily-build.sh
-curl --silent --output fink-restore-selections.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-restore-selections.sh
-curl --silent --output fink-selfupdate-orange.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/fink-selfupdate-orange.sh
-curl --silent --output force-fink-daily-build.sh http://www.ailab.si/svn/orange/trunk/install-scripts/mac/force-fink-daily-build.sh
+curl --silent --output update-all-scripts.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/update-all-scripts.sh
+curl --silent --output bundle-64bit-daily-build.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/bundle-64bit-daily-build.sh
+curl --silent --output bundle-daily-build.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/bundle-daily-build.sh
+curl --silent --output dailyrun.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/dailyrun.sh
+curl --silent --output dailyrun-finkonly-withsource.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/dailyrun-finkonly-withsource.sh
+curl --silent --output dailyrun-finkonly.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/dailyrun-finkonly.sh
+curl --silent --output dailyrun-bundleonly.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/dailyrun-bundleonly.sh
+curl --silent --output fink-daily-build.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/fink-daily-build.sh
+curl --silent --output fink-restore-selections.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/fink-restore-selections.sh
+curl --silent --output fink-selfupdate-orange.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/fink-selfupdate-orange.sh
+curl --silent --output force-fink-daily-build.sh http://orange.biolab.si/svn/orange/trunk/install-scripts/mac/force-fink-daily-build.sh
 
 chmod +x *.sh
 
