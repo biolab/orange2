7f1bb0a94d2cb3cc5eead252f7bc3ed332d4bdd1
mitar <devnull@localhost>
2011-02-20 16:00:09 +0000

    Working on daily building.

--- install-scripts/mac/mount-dirs.sh
+++ install-scripts/mac/mount-dirs.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 umount /Volumes/fink/ 2> /dev/null || true
+sleep 5
 mkdir -p /Volumes/fink/
 /Users/ailabc/Downloads/sshfs-binaries/sshfs-static-leopard -o reconnect,workaround=nonodelay,uid=$(id -u),gid=$(id -g) fink@biolab.si:/files /Volumes/fink/
 
 umount /Volumes/download/ 2> /dev/null || true
+sleep 5
 mkdir -p /Volumes/download/
 /Users/ailabc/Downloads/sshfs-binaries/sshfs-static-leopard -o reconnect,workaround=nonodelay,uid=$(id -u),gid=$(id -g) download@biolab.si:/files /Volumes/download/
