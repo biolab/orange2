b2f9022a0871ef625ceda419be3bd014d07c58d6
jzbontar <jure.zbontar@fri.uni-lj.si>
2011-03-15 16:37:40 +0000

    Rename camel case identifiers in Orange.evaluation.testing

--- orange/doc/Orange/rst/code/testing-test.py
+++ orange/doc/Orange/rst/code/testing-test.py
@@ -1,5 +1,7 @@
 import Orange
+import orngTest
 import random
+import Orange.evaluation.testing
 
 table = Orange.data.Table("voting")
 
@@ -16,14 +18,14 @@ def printResults(res):
 
 print "\nproportionsTest that will always give the same results"
 for i in range(3):
-    res = Orange.evaluation.testing.proportionTest(learners, table, 0.7)
+    res = Orange.evaluation.testing.proportion_test(learners, table, 0.7)
     printResults(res)
 
 print "\nproportionsTest that will give different results, \
 but the same each time the script is run"
 myRandom = Orange.core.RandomGenerator()
 for i in range(3):
-    res = Orange.evaluation.testing.proportionTest(learners, table, 0.7,
+    res = Orange.evaluation.testing.proportion_test(learners, table, 0.7,
         randomGenerator=myRandom)
     printResults(res)
 # End
@@ -31,24 +33,24 @@ for i in range(3):
 if "NO_RANDOMNESS" not in vars():
     print "\nproportionsTest that will give different results each time it is run"
     for i in range(3):
-        res = Orange.evaluation.testing.proportionTest(learners, table, 0.7,
+        res = Orange.evaluation.testing.proportion_test(learners, table, 0.7,
             randseed=random.randint(0, 100))
         printResults(res)
 # End
 
 print "\nproportionsTest + storing classifiers"
-res = Orange.evaluation.testing.proportionTest(learners, table, 0.7, 100,
+res = Orange.evaluation.testing.proportion_test(learners, table, 0.7, 100,
     storeClassifiers=1)
 print "#iter %i, #classifiers %i" % \
     (len(res.classifiers), len(res.classifiers[0]))
 
 print "\nGood old 10-fold cross validation"
-res = Orange.evaluation.testing.crossValidation(learners, table)
+res = Orange.evaluation.testing.cross_validation(learners, table)
 printResults(res)
 
 print "\nLearning curve"
 prop = Orange.core.frange(0.2, 1.0, 0.2)
-res = Orange.evaluation.testing.learningCurveN(learners, table, folds=5,
+res = Orange.evaluation.testing.learning_curve_n(learners, table, folds=5,
     proportions=prop)
 for i in range(len(prop)):
     print "%5.3f:" % prop[i],
@@ -59,7 +61,7 @@ print "\nLearning curve with pre-separated data"
 indices = Orange.core.MakeRandomIndices2(table, p0=0.7)
 train = table.select(indices, 0)
 test = table.select(indices, 1)
-res = Orange.evaluation.testing.learningCurveWithTestData(learners, train,
+res = Orange.evaluation.testing.learning_curve_with_test_data(learners, train,
     test, times=5, proportions=prop)
 for i in range(len(prop)):
     print "%5.3f:" % prop[i],
@@ -67,9 +69,9 @@ for i in range(len(prop)):
 # End
 
 print "\nLearning and testing on pre-separated data"
-res = Orange.evaluation.testing.learnAndTestOnTestData(learners, train, test)
+res = Orange.evaluation.testing.learn_and_test_on_test_data(learners, train, test)
 printResults(res)
 
 print "\nLearning and testing on the same data"
-res = Orange.evaluation.testing.learnAndTestOnLearnData(learners, table)
+res = Orange.evaluation.testing.learn_and_test_on_learn_data(learners, table)
 printResults(res)
