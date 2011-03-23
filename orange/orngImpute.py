c75a7db24b0a1a91242759b8416d935330ef87a4
markotoplak <devnull@localhost>
2011-03-23 15:46:55 +0000

    orngImpute uses Ales's scripts.

--- orange/orngImpute.py
+++ orange/orngImpute.py
@@ -1,30 +1,14 @@
-#FOR COMPATIBILITY (orange 2.5)
-import orange
+import Orange
+from Orange.feature.imputation import *
 
-class ImputeLearner(orange.Learner):
-    def __new__(cls, examples = None, weightID = 0, **keyw):
-        self = orange.Learner.__new__(cls, **keyw)
-        self.dontImputeClassifier = False
-        self.__dict__.update(keyw)
-        if examples:
-            return self.__call__(examples, weightID)
-        else:
-            return self
-        
-    def __call__(self, data, weight=0):
-        trained_imputer = self.imputerConstructor(data, weight)
-        imputed_data = trained_imputer(data, weight)
-        baseClassifier = self.baseLearner(imputed_data, weight)
-        if self.dontImputeClassifier:
-            return baseClassifier
-        else:
-            return ImputeClassifier(baseClassifier, trained_imputer)
+ImputeLearner = Orange.misc.deprecated_members(
+  {
+      "dontImputeClassifier": "dont_impute_classifier",
+      "imputerConstructor": "imputer_constructor",
+      "baseLearner": "base_learner"
+  })(ImputeLearner)
 
-class ImputeClassifier(orange.Classifier):
-    def __init__(self, baseClassifier, imputer, **argkw):
-        self.baseClassifier = baseClassifier
-        self.imputer = imputer
-        self.__dict__.update(argkw)
-
-    def __call__(self, ex, what=orange.GetValue):
-        return self.baseClassifier(self.imputer(ex), what)
+ImputeClassifier = Orange.misc.deprecated_members(
+  {
+      "baseClassifier": "base_classifier"
+  })(ImputeClassifier)
