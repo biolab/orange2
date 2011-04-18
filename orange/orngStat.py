be8769b36c403310b69f31b38b0f7229699e6217
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-04-18 09:21:33 +0000

    Fixed import of AROCFromCDT.

--- orange/orngStat.py
+++ orange/orngStat.py
@@ -25,7 +25,7 @@ from Orange.evaluation.scoring import \
     is_CDT_empty as isCDTempty, \
     compute_CDT as computeCDT, \
     ROCs_from_CDT as ROCsFromCDT, \
-    AROC_from_CDT as AROCfromCDT, \
+    AROC_from_CDT as AROCFromCDT, \
     McNemar_of_two as McNemarOfTwo, \
     Wilcoxon_pairs as WilcoxonPairs, \
     plot_learning_curve_learners as plotLearningCurveLearners, \
