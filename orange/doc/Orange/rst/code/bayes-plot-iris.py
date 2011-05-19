4284cf0e3e42838695c572146b43aa4bf21430ff
anze <anze.staric@fri.uni-lj.si>
2011-05-19 12:13:25 +0000

    Clean-up of Naive Bayes examples to remove horizontal scrollbar in documentation.

--- orange/doc/Orange/rst/code/bayes-plot-iris.py
+++ orange/doc/Orange/rst/code/bayes-plot-iris.py
@@ -2,16 +2,16 @@ import Orange
 import pylab
 
 iris = Orange.data.Table("iris.tab")
-bayes = Orange.classification.bayes.NaiveLearner(iris)
+nb = Orange.classification.bayes.NaiveLearner(iris)
 
-sepal_length, probabilities = zip(*bayes.conditional_distributions[0].items())
-probability_setosa, probability_versicolor, probability_virginica = zip(*probabilities)
+sepal_length, probabilities = zip(*nb.conditional_distributions[0].items())
+p_setosa, p_versicolor, p_virginica = zip(*probabilities)
 
 pylab.xlabel("sepal length")
 pylab.ylabel("probability")
-pylab.plot(sepal_length, probability_setosa, label="setosa", linewidth=2)
-pylab.plot(sepal_length, probability_versicolor, label="versicolor", linewidth=2)
-pylab.plot(sepal_length, probability_virginica, label="virginica", linewidth=2)
+pylab.plot(sepal_length, p_setosa, label="setosa", linewidth=2)
+pylab.plot(sepal_length, p_versicolor, label="versicolor", linewidth=2)
+pylab.plot(sepal_length, p_virginica, label="virginica", linewidth=2)
 
 pylab.legend(loc="best")
 pylab.savefig("bayes-iris.png")
