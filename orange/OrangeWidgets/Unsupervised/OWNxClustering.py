92456f9cc8077d9afa90af6f536e3c560123d565
miha <miha.stajdohar@gmail.com>
2011-06-02 21:13:00 +0000

    Added another community detection algorithm of Loung et al, 2009

--- orange/OrangeWidgets/Unsupervised/OWNxClustering.py
+++ orange/OrangeWidgets/Unsupervised/OWNxClustering.py
@@ -7,13 +7,14 @@
 """
 
 import Orange
+import Orange.network.community as cd
 import OWGUI
 
 from OWWidget import *
 
 class OWNxClustering(OWWidget):
     
-    settingsList = ['method', 'iterationHistory', "autoApply"]
+    settingsList = ['method', 'iterationHistory', 'autoApply', 'iterations', 'hop_attenuation']
     
     def __init__(self, parent=None, signalManager=None):
         OWWidget.__init__(self, parent, signalManager, 'Nx Clustering')
@@ -25,13 +26,20 @@ class OWNxClustering(OWWidget):
         self.method = 0
         self.iterationHistory = 0
         self.autoApply = 0
-        
+        self.iterations = 1000
+        self.hop_attenuation = 0.1
         self.loadSettings()
         
+        OWGUI.spin(self.controlArea, self, "iterations", 1, 100000, 1, label="Iterations: ")
         ribg = OWGUI.radioButtonsInBox(self.controlArea, self, "method", [], "Method", callback = self.cluster)
         OWGUI.appendRadioButton(ribg, self, "method", "Label propagation clustering (Raghavan et al., 2007)", callback = self.cluster)
-        OWGUI.checkBox(OWGUI.indentedBox(ribg), self, "iterationHistory", "Append clustering data on each iteration", callback = self.cluster)
+        
+        OWGUI.appendRadioButton(ribg, self, "method", "Label propagation clustering (Loung et al., 2009)", callback = self.cluster)
+        OWGUI.doubleSpin(OWGUI.indentedBox(ribg), self, "hop_attenuation", 0, 1, 0.01, label="Hop attenuation (delta): ")
+        
+        
         self.info = OWGUI.widgetLabel(self.controlArea, ' ')
+        OWGUI.checkBox(self.controlArea, self, "iterationHistory", "Append clustering data on each iteration")
         autoApplyCB = OWGUI.checkBox(self.controlArea, self, "autoApply", "Commit automatically")
         OWGUI.button(self.controlArea, self, "Commit", callback=self.cluster)
         
@@ -43,10 +51,21 @@ class OWNxClustering(OWWidget):
     def cluster(self):
         self.info.setText(' ')
         
-        if self.net == None:
+        if self.net is None:
             self.send("Network", None)
             return
         
-        labels = Orange.network.community.label_propagation(self.net, results2items=1, resultHistory2items=self.iterationHistory)
-        self.info.setText('%d clusters found' % len(set(labels)))        
+        if self.method == 0:
+            labels = cd.label_propagation(self.net, results2items=1, 
+                                    resultHistory2items=self.iterationHistory, 
+                                    iterations=self.iterations)
+        if self.method == 1:
+            labels = cd.label_propagation_hop_attenuation(
+                                    self.net, 
+                                    results2items=1, 
+                                    resultHistory2items=self.iterationHistory, 
+                                    iterations=self.iterations,
+                                    delta=self.hop_attenuation)
+        
+        self.info.setText('%d clusters found' % len(set(labels.items())))        
         self.send("Network", self.net)
