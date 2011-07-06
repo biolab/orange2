e3842e646168f1b8c34f6e3315cf7b789be41fa4
Noughmad <Noughmad@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2011-07-06 08:43:01 +0000

    Rename the graph elements files to owelement.py

---
+++ orange/OrangeWidgets/Graph/__init__.py
@@ -0,0 +1,1 @@
+__all__ = [ "owcurve", "owpoint", "owlegend", "owaxis", "owtools" ]
---
+++ orange/OrangeWidgets/Graph/owcurve.py
@@ -0,0 +1,33 @@
+
+from OWBaseWidget import *
+from owconstants import *
+import orangeplot
+
+"""
+    This class represents a curve on a graph.
+    
+    .. attribute:: pen_color
+    
+    .. attribute:: brush_color
+        
+    .. attribute:: data
+        A list of pairs (x,y)
+        
+    .. attribute:: point_size
+        
+    .. attribute:: continuous
+        If true, the curve is drawn as a continuous line. Otherwise, it's drawn as a series of points
+        
+    .. method:: symbol(x,y,s=None,parent=None)
+        Returns a QGraphicsItem with this curve's symbol at position ``x'',``y'' with size ``s'' and parent ``parent''
+        
+"""
+
+class OWCurve(orangeplot.Curve):
+    def __init__(self, xData=[], yData=[], x_axis_key=xBottom, y_axis_key=yLeft, tooltip=None, parent=None, scene=None):
+        orangeplot.Curve.__init__(self, xData, yData, parent, scene)
+        self.setAxes(x_axis_key, y_axis_key)
+        if tooltip:
+            self.setToolTip(tooltip)
+
+            
