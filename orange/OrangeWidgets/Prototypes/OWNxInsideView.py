7a86e78a4257995a6945fe521eb00aa7ae43b834
miha <miha.stajdohar@gmail.com>
2011-07-06 14:34:06 +0000

    Added a new network view functionality.

--- orange/OrangeWidgets/Prototypes/OWNxInsideView.py
+++ orange/OrangeWidgets/Prototypes/OWNxInsideView.py
@@ -21,7 +21,6 @@ class NxInsideView(Orange.network.NxView):
         
         self._nhops = nhops
         self._center_node = None
-        self._network = None
         
     def init_network(self, graph):
         self._network = graph
@@ -29,25 +28,34 @@ class NxInsideView(Orange.network.NxView):
         if graph is None:
             return None
         
-        for node in graph.nodes_iter():
-            self._center_node = node
-            break
-         
+        self._center_node = graph.nodes_iter().next()
+        nodes = self._get_neighbors()
+        return self._network.subgraph(nodes)
+    
+    def update_network(self):
+        nodes = self._get_neighbors()
+        subnet = self._network.subgraph(nodes)
+        
+        if self._nx_explorer is not None:
+            self._nx_explorer.change_graph(subnet)
+        
+    def set_nhops(self, nhops):
+        self._nhops = nhops
+        
+    def nodes_selected(self):
+        selection = self._nx_explorer.networkCanvas.get_selected_nodes()
+        if len(selection) == 1:
+            self._center_node = selection[0]
+            self.update_network()
+        
+    def _get_neighbors(self):
         nodes = set([self._center_node])
         for n in range(self._nhops):
             neighbors = set()
             for node in nodes:
-                neighbors.update(graph.neighbors(node))
+                neighbors.update(self._network.neighbors(node))
             nodes.update(neighbors)
-            
-        return graph.subgraph(nodes)
-    
-    def update_network(self):
-        pass
-    
-    def set_nhops(self, nhops):
-        self._nhops = nhops
-        
+        return nodes
         
 class OWNxInsideView(OWWidget):
     
@@ -64,12 +72,12 @@ class OWNxInsideView(OWWidget):
         self.loadSettings()
         
         ib = OWGUI.widgetBox(self.controlArea, "Preferences", orientation="vertical")
-        OWGUI.spin(ib, self, "_nhops", 1, 6, 1, label="Number of hops: ", callback = self._update_view)
+        OWGUI.spin(ib, self, "_nhops", 1, 6, 1, label="Number of hops: ", callback=self.update_view)
 
         self.inside_view = NxInsideView(self._nhops)
         self.send("Nx View", self.inside_view)
     
-    def _update_view(self):
+    def update_view(self):
         self.inside_view.set_nhops(self._nhops)
         
         self.inside_view.update_network()
