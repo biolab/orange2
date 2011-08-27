c6b4faa85db3b8cb0e37ec53254613e4694b3b35
matejd <matejd@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2011-08-27 14:42:32 +0000

    Moved selections processing code to c++, improved drawing

--- source/orangeqt/plot3d.cpp
+++ source/orangeqt/plot3d.cpp
@@ -1,5 +1,6 @@
 #include "plot3d.h"
 #include <iostream>
+#include <limits>
 #include <QGLFormat>
 #include <GL/glx.h>
 #include <GL/glxext.h> // TODO: Windows?
@@ -14,18 +15,18 @@ typedef void (APIENTRYP PFNGLGETVERTEXATTRIBPOINTERPROC) (GLuint index, GLint si
 PFNGLGETVERTEXATTRIBPOINTERPROC glVertexAttribPointer = NULL;
 typedef GLint (APIENTRYP PFNGLGETUNIFORMLOCATIONPROC) (GLuint program, const GLchar *name);
 PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation = NULL;
-//typedef void (APIENTRYP PFNGLUNIFORM2FPROC) (GLint location, GLfloat v0, GLfloat v1);
 PFNGLUNIFORM2FPROC glUniform2f = NULL;
 
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
 Plot3D::Plot3D(QWidget* parent) : QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
 {
-    vbo_generated = false;
+    vbos_generated = false;
 }
 
 Plot3D::~Plot3D()
 {
+    // TODO: delete vbos
 }
 
 template<class T>
@@ -64,6 +65,7 @@ void Plot3D::set_data(quint64 array_address, int num_examples, int example_size)
     this->data_array = reinterpret_cast<float*>(array_address); // 32-bit systems, endianness?
     this->num_examples = num_examples;
     this->example_size = example_size;
+    this->selected_indices = QVector<bool>(num_examples);
 
     // Load required extensions (OpenGL context should be up by now).
 #ifdef _WIN32
@@ -85,13 +87,24 @@ void Plot3D::update_data(int x_index, int y_index, int z_index,
                          const QList<QColor>& colors, int num_symbols_used,
                          bool x_discrete, bool y_discrete, bool z_discrete, bool use_2d_symbols)
 {
+    // TODO: delete vbos
+    this->x_index = x_index;
+    this->y_index = y_index;
+    this->z_index = z_index;
+
     const float scale = 0.001;
-    float* vbo_data = new float[num_examples * 144 * 13];
-    float* vbo_edges_data = new float[num_examples * 144 * 13];
-    float* dest = vbo_data;
-    float* dest_edges = vbo_edges_data;
-    int size_in_bytes = 0;
-    int size_in_bytes_edges = 0;
+
+    float* vbo_selected_data   = new float[num_examples * 144 * 13]; // TODO: better size approximation
+    float* vbo_unselected_data = new float[num_examples * 144 * 13];
+    float* vbo_edges_data      = new float[num_examples * 144 * 13];
+    float* dests = vbo_selected_data;
+    float* destu = vbo_unselected_data;
+    float* deste = vbo_edges_data;
+    // Sizes in bytes.
+    int sib_selected   = 0;
+    int sib_unselected = 0;
+    int sib_edges      = 0;
+
     QMap<int, QList<QVector3D> >& geometry = use_2d_symbols ? geometry_data_2d : geometry_data_3d;
     QMap<int, QList<QVector3D> >& geometry_edges = use_2d_symbols ? geometry_data_edges_2d : geometry_data_edges_3d;
 
@@ -121,8 +134,14 @@ void Plot3D::update_data(int x_index, int y_index, int z_index,
         else
             color = QColor(0., 0., 0.8);
 
+        float*& dest = selected_indices[index] ? dests : destu;
+
+        // TODO: make sure symbol is in geometry map
         for (int i = 0; i < geometry[symbol].count(); i += 6) {
-            size_in_bytes += 3*13*4;
+            if (selected_indices[index])
+                sib_selected += 3*13*4;
+            else
+                sib_unselected += 3*13*4;
 
             for (int j = 0; j < 3; ++j)
             {
@@ -151,60 +170,77 @@ void Plot3D::update_data(int x_index, int y_index, int z_index,
             }
         }
 
+        // No need for edges in selected examples (those are drawn fully opaque)
+        if (this->selected_indices[index])
+            continue;
+
         for (int i = 0; i < geometry_edges[symbol].count(); i += 2) {
-            size_in_bytes_edges += 2*13*4;
+            sib_edges += 2*13*4;
 
             for (int j = 0; j < 2; ++j)
             {
-                *dest_edges = x_pos; dest_edges++; 
-                *dest_edges = y_pos; dest_edges++; 
-                *dest_edges = z_pos; dest_edges++; 
+                *deste = x_pos; deste++; 
+                *deste = y_pos; deste++; 
+                *deste = z_pos; deste++; 
 
-                *dest_edges = geometry_edges[symbol][i+j].x()*size*scale; dest_edges++;
-                *dest_edges = geometry_edges[symbol][i+j].y()*size*scale; dest_edges++;
-                *dest_edges = geometry_edges[symbol][i+j].z()*size*scale; dest_edges++;
+                *deste = geometry_edges[symbol][i+j].x()*size*scale; deste++;
+                *deste = geometry_edges[symbol][i+j].y()*size*scale; deste++;
+                *deste = geometry_edges[symbol][i+j].z()*size*scale; deste++;
 
-                *dest_edges = color.redF(); dest_edges++;
-                *dest_edges = color.greenF(); dest_edges++;
-                *dest_edges = color.blueF(); dest_edges++;
+                *deste = color.redF(); deste++;
+                *deste = color.greenF(); deste++;
+                *deste = color.blueF(); deste++;
 
-                // Just use offset
-                *dest_edges = geometry_edges[symbol][i+j].x(); dest_edges++;
-                *dest_edges = geometry_edges[symbol][i+j].y(); dest_edges++;
-                *dest_edges = geometry_edges[symbol][i+j].z(); dest_edges++;
+                // Just use offset as the normal for now
+                *deste = geometry_edges[symbol][i+j].x(); deste++;
+                *deste = geometry_edges[symbol][i+j].y(); deste++;
+                *deste = geometry_edges[symbol][i+j].z(); deste++;
 
-                *dest_edges = index; dest_edges++;
+                *deste = index; deste++;
             }
         }
     }
 
-    glGenBuffers(1, &vbo_id);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
-    glBufferData(GL_ARRAY_BUFFER, size_in_bytes, vbo_data, GL_STATIC_DRAW);
+    glGenBuffers(1, &vbo_selected_id);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_selected_id);
+    glBufferData(GL_ARRAY_BUFFER, sib_selected, vbo_selected_data, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    delete [] vbo_data;
+    delete [] vbo_selected_data;
+
+    glGenBuffers(1, &vbo_unselected_id);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_unselected_id);
+    glBufferData(GL_ARRAY_BUFFER, sib_unselected, vbo_unselected_data, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    delete [] vbo_unselected_data;
 
     glGenBuffers(1, &vbo_edges_id);
     glBindBuffer(GL_ARRAY_BUFFER, vbo_edges_id);
-    glBufferData(GL_ARRAY_BUFFER, size_in_bytes_edges, vbo_edges_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sib_edges, vbo_edges_data, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     delete [] vbo_edges_data;
 
-    num_vertices = size_in_bytes / (13*4);
-    num_edges_vertices = size_in_bytes_edges / (13*4);
-    vbo_generated = true;
+    num_selected_vertices = sib_selected / (13*4);
+    num_unselected_vertices = sib_unselected / (13*4);
+    num_edges_vertices = sib_edges / (13*4);
+
+    vbos_generated = true;
+}
+
+void Plot3D::draw_data_solid()
+{
+    // TODO
 }
 
 void Plot3D::draw_data(GLuint shader_id, float alpha_value)
 {
-    if (!vbo_generated)
+    if (!vbos_generated)
         return;
 
-    glUniform2f(glGetUniformLocation(shader_id, "alpha_value"), alpha_value-0.4, alpha_value-0.4);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
+    // Draw opaque selected examples first.
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_selected_id);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(0));
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(3*4));
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(6*4));
@@ -216,7 +252,7 @@ void Plot3D::draw_data(GLuint shader_id, float alpha_value)
     glEnableVertexAttribArray(3);
     glEnableVertexAttribArray(4);
 
-    glDrawArrays(GL_TRIANGLES, 0, num_vertices);
+    glDrawArrays(GL_TRIANGLES, 0, num_selected_vertices);
 
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
@@ -224,9 +260,32 @@ void Plot3D::draw_data(GLuint shader_id, float alpha_value)
     glDisableVertexAttribArray(3);
     glDisableVertexAttribArray(4);
 
-    glUniform2f(glGetUniformLocation(shader_id, "alpha_value"), alpha_value, alpha_value);
+    // Draw transparent unselected examples (triangles and then edges).
+    glUniform2f(glGetUniformLocation(shader_id, "alpha_value"), alpha_value-0.6, alpha_value-0.6);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_unselected_id);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(0));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(3*4));
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(6*4));
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(9*4));
+    glVertexAttribPointer(4, 1, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(12*4));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    glEnableVertexAttribArray(3);
+    glEnableVertexAttribArray(4);
+
+    glDrawArrays(GL_TRIANGLES, 0, num_unselected_vertices);
+
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+    glDisableVertexAttribArray(2);
+    glDisableVertexAttribArray(3);
+    glDisableVertexAttribArray(4);
 
     // Edges
+    glUniform2f(glGetUniformLocation(shader_id, "alpha_value"), alpha_value, alpha_value);
+
     glBindBuffer(GL_ARRAY_BUFFER, vbo_edges_id);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(0));
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 13*4, BUFFER_OFFSET(3*4));
@@ -248,6 +307,119 @@ void Plot3D::draw_data(GLuint shader_id, float alpha_value)
     glDisableVertexAttribArray(4);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+QList<double> Plot3D::get_min_max_selected(const QList<int>& area,
+                                          const QMatrix4x4& mvp,
+                                          const QList<int>& viewport,
+                                          const QVector3D& plot_scale,
+                                          const QVector3D& plot_translation)
+{
+    float x_min = std::numeric_limits<float>::max();
+    float x_max = std::numeric_limits<float>::min();
+    float y_min = x_min;
+    float y_max = x_max;
+    float z_min = x_min;
+    float z_max = x_max;
+
+    bool any_point_selected = false;
+    for (int index = 0; index < num_examples; ++index)
+    {
+        float* example = data_array + index*example_size;
+        float x_pos = *(example + x_index);
+        float y_pos = *(example + y_index);
+        float z_pos = *(example + z_index);
+
+        QVector3D position(x_pos, y_pos, z_pos);
+        position += plot_translation;
+        position *= plot_scale;
+
+        QVector4D projected = mvp * QVector4D(position, 1.0f);
+        projected /= projected.z();
+        int winx = viewport[0] + (1 + projected.x()) * viewport[2] / 2;
+        int winy = viewport[1] + (1 + projected.y()) * viewport[3] / 2;
+        winy = viewport[3] - winy;
+
+        if (winx >= area[0] && winx <= area[0]+area[2] && winy <= area[1]+area[3] && winy >= area[1])
+        {
+            any_point_selected = true;
+
+            if (x_pos < x_min) x_min = x_pos;
+            if (x_pos > x_max) x_max = x_pos;
+
+            if (y_pos < y_min) y_min = y_pos;
+            if (y_pos > y_max) y_max = y_pos;
+
+            if (z_pos < z_min) z_min = z_pos;
+            if (z_pos > z_max) z_max = z_pos;
+        }
+    }
+
+    if (any_point_selected)
+    {
+        QList<double> min_max;
+        min_max << x_min << x_max;
+        min_max << y_min << y_max;
+        min_max << z_min << z_max;
+        return min_max;
+    }
+    else
+    {
+        QList<double> min_max;
+        min_max << 0. << 1.;
+        min_max << 0. << 1.;
+        min_max << 0. << 1.;
+        return min_max;
+    }
+}
+
+void Plot3D::select_points(const QList<int>& area,
+                           const QMatrix4x4& mvp,
+                           const QList<int>& viewport,
+                           const QVector3D& plot_scale,
+                           const QVector3D& plot_translation,
+                           Plot::SelectionBehavior behavior)
+{
+    if (behavior == Plot::ReplaceSelection)
+        selected_indices.fill(false);//selected_indices = QVector<bool>(num_examples);
+
+    for (int index = 0; index < num_examples; ++index)
+    {
+        float* example = data_array + index*example_size;
+        float x_pos = *(example + x_index);
+        float y_pos = *(example + y_index);
+        float z_pos = *(example + z_index);
+
+        QVector3D position(x_pos, y_pos, z_pos);
+        position += plot_translation;
+        position *= plot_scale;
+
+        QVector4D projected = mvp * QVector4D(position, 1.0f);
+        projected /= projected.z();
+        int winx = viewport[0] + (1 + projected.x()) * viewport[2] / 2;
+        int winy = viewport[1] + (1 + projected.y()) * viewport[3] / 2;
+        winy = viewport[3] - winy;
+
+        if (winx >= area[0] && winx <= area[0]+area[2] && winy <= area[1]+area[3] && winy >= area[1])
+        {
+            if (behavior == Plot::AddSelection || behavior == Plot::ReplaceSelection)
+                selected_indices[index] = true;
+            else if (behavior == Plot::RemoveSelection)
+                selected_indices[index] = false;
+            else if (behavior == Plot::ToggleSelection)
+                selected_indices[index] = !selected_indices[index];
+        }
+    }
+}
+
+void Plot3D::unselect_all_points()
+{
+    this->selected_indices = QVector<bool>(num_examples);
+}
+
+QList<bool> Plot3D::get_selected_indices()
+{
+    return this->selected_indices.toList();
 }
 
 #include "plot3d.moc"
--- source/orangeqt/plot3d.h
+++ source/orangeqt/plot3d.h
@@ -1,8 +1,11 @@
 #ifndef PLOT_3D_H
 #define PLOT_3D_H
 
+#include "plot.h"
 #include <QtOpenGL/QGLWidget>
 #include <QVector3D>
+#include <QVector4D>
+#include <QMatrix4x4>
 #include <GL/gl.h>
 
 class Plot3D : public QGLWidget {
@@ -12,24 +15,54 @@ public:
     explicit Plot3D(QWidget* parent = 0);
     virtual ~Plot3D();
 
-    void set_symbol_geometry(int symbol, int type, const QList<QVector3D>& geometry);
+    void set_symbol_geometry(int symbol,
+                             int type,
+                             const QList<QVector3D>& geometry);
 
-    void set_data(quint64 array_address, int num_examples, int example_size);
+    void set_data(quint64 array_address,
+                  int num_examples,
+                  int example_size);
     void update_data(int x_index, int y_index, int z_index,
-                int color_index, int symbol_index, int size_index, int label_index,
-                const QList<QColor>& colors, int num_symbols_used,
-                bool x_discrete, bool y_discrete, bool z_discrete, bool use_2d_symbols);
-    void draw_data(GLuint shader_id, float alpha_value);
+                     int color_index, int symbol_index, int size_index, int label_index,
+                     const QList<QColor>& colors, int num_symbols_used,
+                     bool x_discrete, bool y_discrete, bool z_discrete, bool use_2d_symbols);
+    void draw_data(GLuint shader_id,
+                   float alpha_value);
+    void draw_data_solid(); // (only draws solid geometry as a performance optimization)
+
+    QList<double> get_min_max_selected(const QList<int>& area,
+                                      const QMatrix4x4& mvp,
+                                      const QList<int>& viewport,
+                                      const QVector3D& plot_scale,
+                                      const QVector3D& plot_translation);
+
+    void select_points(const QList<int>& area,
+                       const QMatrix4x4& mvp,
+                       const QList<int>& viewport,
+                       const QVector3D& plot_scale,
+                       const QVector3D& plot_translation,
+                       Plot::SelectionBehavior behavior = Plot::AddSelection);
+
+    void unselect_all_points();
+
+    QList<bool> get_selected_indices();
 
 private:
     float* data_array;
+    QVector<bool> selected_indices; // Array of length num_examples
     int num_examples;
     int example_size;
-    int num_vertices;
+    int num_selected_vertices;
+    int num_unselected_vertices;
     int num_edges_vertices;
-    GLuint vbo_id;
-    GLuint vbo_edges_id;
-    bool vbo_generated;
+    GLuint vbo_selected_id;   // Triangles (drawn opaque)
+    GLuint vbo_unselected_id; // Triangles (drawn slightly transparent)
+    GLuint vbo_edges_id;      // Edges are in a separated VBO (but drawn together with vbo_unselected)
+    bool vbos_generated;
+
+    int x_index;
+    int y_index;
+    int z_index;
 
     QMap<int, QList<QVector3D> > geometry_data_2d;
     QMap<int, QList<QVector3D> > geometry_data_3d;
