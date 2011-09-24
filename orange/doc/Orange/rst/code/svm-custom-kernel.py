91b8811d0bd65ebe3e8f6f1261ff9c246ad32c10
miha <miha.stajdohar@gmail.com>
2011-09-24 09:32:35 +0000

    Refactored to Orange25.

--- orange/doc/Orange/rst/code/svm-custom-kernel.py
+++ orange/doc/Orange/rst/code/svm-custom-kernel.py
@@ -1,37 +1,37 @@
-from Orange import core
 from Orange import data
-from Orange.classification import svm
+from Orange import evaluation
+
+from Orange.classification.svm import SVMLearner, kernels
+from Orange.distance.instances import EuclideanConstructor
+from Orange.distance.instances import HammingConstructor
 
 table = data.Table("iris.tab")
-l1=svm.SVMLearner()
-l1.kernel_func=svm.kernels.RBFKernelWrapper(core.ExamplesDistanceConstructor_Euclidean(table), gamma=0.5)
-l1.kernel_type=svm.SVMLearner.Custom
-l1.probability=True
-c1=l1(table)
-l1.name="SVM - RBF(Euclidean)"
+l1 = SVMLearner()
+l1.kernel_func = kernels.RBFKernelWrapper(EuclideanConstructor(table), gamma=0.5)
+l1.kernel_type = SVMLearner.Custom
+l1.probability = True
+c1 = l1(table)
+l1.name = "SVM - RBF(Euclidean)"
 
-l2=svm.SVMLearner()
-l2.kernel_func=svm.kernels.RBFKernelWrapper(
-    core.ExamplesDistanceConstructor_Hamming(table), gamma=0.5)
-l2.kernel_type=svm.SVMLearner.Custom
-l2.probability=True
-c2=l2(table)
-l2.name="SVM - RBF(Hamming)"
+l2 = SVMLearner()
+l2.kernel_func = kernels.RBFKernelWrapper(HammingConstructor(table), gamma=0.5)
+l2.kernel_type = SVMLearner.Custom
+l2.probability = True
+c2 = l2(table)
+l2.name = "SVM - RBF(Hamming)"
 
-l3=svm.SVMLearner()
-l3.kernel_func = svm.kernels.CompositeKernelWrapper(
-    svm.kernels.RBFKernelWrapper(
-    core.ExamplesDistanceConstructor_Euclidean(table), gamma=0.5),
-    svm.kernels.RBFKernelWrapper(
-    core.ExamplesDistanceConstructor_Hamming(table), gamma=0.5), l=0.5)
-l3.kernel_type=svm.SVMLearner.Custom
-l3.probability=True
-c3=l1(table)
-l3.name="SVM - Composite"
-        
-from Orange import evaluation
+l3 = SVMLearner()
+l3.kernel_func = kernels.CompositeKernelWrapper(
+    kernels.RBFKernelWrapper(EuclideanConstructor(table), gamma=0.5),
+    kernels.RBFKernelWrapper(HammingConstructor(table), gamma=0.5), l=0.5)
+l3.kernel_type = SVMLearner.Custom
+l3.probability = True
+c3 = l1(table)
+l3.name = "SVM - Composite"
+
 tests = evaluation.testing.cross_validation([l1, l2, l3], table, folds=5)
-[ca1, ca2, ca3]=evaluation.scoring.CA(tests)
+[ca1, ca2, ca3] = evaluation.scoring.CA(tests)
+
 print l1.name, "CA:", ca1
 print l2.name, "CA:", ca2
 print l3.name, "CA:", ca3
--- orange/doc/Orange/rst/code/svm-easy.py
+++ orange/doc/Orange/rst/code/svm-easy.py
@@ -7,9 +7,9 @@ svm_easy = svm.SVMLearnerEasy(name="svm easy", folds=3)
 svm_normal = svm.SVMLearner(name="svm")
 learners = [svm_easy, svm_normal]
 
-from Orange import evaluation
+from Orange.evaluation import testing, scoring
 
-results = evaluation.testing.cross_validation(learners, table, folds=5)
+results = testing.cross_validation(learners, table, folds=5)
 print "Name     CA        AUC"
-for learner, CA, AUC in zip(learners, evaluation.scoring.CA(results), evaluation.scoring.AUC(results)):
+for learner,CA,AUC in zip(learners, scoring.CA(results), scoring.AUC(results)):
     print "%-8s %.2f      %.2f" % (learner.name, CA, AUC)
