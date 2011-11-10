56feb0134b38fa257d131a49b9205a44e8f630c0
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-11-10 14:42:06 +0000

    Fixed widget description (removed linear svm, added CN2-EVC)

--- orange/OrangeWidgets/Classify/OWNomogram.py
+++ orange/OrangeWidgets/Classify/OWNomogram.py
@@ -1,6 +1,6 @@
 """
 <name>Nomogram</name>
-<description>Nomogram viewer for Naive Bayesian, logistic regression or linear SVM classifiers.</description>
+<description>Nomogram viewer for Naive Bayesian, logistic regression or CN2 (EVC only) classifiers.</description>
 <icon>icons/Nomogram.png</icon>
 <contact>Martin Mozina (martin.mozina(@at@)fri.uni-lj.si)</contact>
 <priority>2500</priority>
