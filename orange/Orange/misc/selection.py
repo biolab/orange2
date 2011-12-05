d467106178a89c1a5c373cf3d629002ae293f16f
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-12-05 17:07:43 +0000

    Renamed members, functions in Orange.misc.selection underscore style and fixed documentation. Added name mappings to fix_changed_names fixer.

--- orange/Orange/misc/selection.py
+++ orange/Orange/misc/selection.py
@@ -7,13 +7,13 @@ Selection (``selection``)
 .. index::
    single: misc; selection
 
-Many machine learning techniques generate a set different solutions or have to
-choose, as for instance in classification tree induction, between different
-features. The most trivial solution is to iterate through the candidates,
-compare them and remember the optimal one. The problem occurs, however, when
-there are multiple candidates that are equally good, and the naive approaches
-would select the first or the last one, depending upon the formulation of
-the if-statement.
+Many machine learning techniques generate a set of different solutions or
+have to choose, as for instance in classification tree induction, between
+different features. The most trivial solution is to iterate through the 
+candidates, compare them and remember the optimal one. The problem occurs,
+however, when there are multiple candidates that are equally good, and the
+naive approaches would select the first or the last one, depending upon the
+formulation of the if-statement.
 
 :class:`Orange.misc.selection` provides a class that makes a random choice
 in such cases. Each new candidate is compared with the currently optimal
@@ -22,7 +22,8 @@ one is chosen by random. The number of competing optimal candidates is stored,
 so in this random choice the probability to select the new candidate (over the
 current one) is 1/w, where w is the current number of equal candidates,
 including the present one. One can easily verify that this gives equal
-chances to all candidates, independent of the order in which they are presented.
+chances to all candidates, independent of the order in which they are
+presented.
 
 Example
 --------
@@ -36,8 +37,9 @@ part of `misc-selection-bestonthefly.py`_ (uses `lymphography.tab`_)
   :lines: 7-16
 
 Our candidates are tuples gain ratios and features, so we set
-:obj:`callCompareOn1st` to make the compare function compare the first element
-(gain ratios). We could achieve the same by initializing the object like this:
+:obj:`call_compare_on_1st` to make the compare function compare the first
+element (gain ratios). We could achieve the same by initializing the object
+like this:
 
 part of `misc-selection-bestonthefly.py`_ (uses `lymphography.tab`_)
 
@@ -55,12 +57,34 @@ The other way to do it is through indices.
 .. _misc-selection-bestonthefly.py: code/misc-selection-bestonthefly.py.py
 .. _lymphography.tab: code/lymphography.tab
 
-Here we only give gain ratios to :obj:`bestOnTheFly`, so we don't have to specify a
-special compare operator. After checking all features we get the index of the 
-optimal one by calling :obj:`winnerIndex`.
+Here we only give gain ratios to :obj:`BestOnTheFly`, so we don't have
+to specify a special compare operator. After checking all features we
+get the index of the  optimal one by calling :obj:`winner_index`.
+
+.. autoclass:: BestOnTheFly
+
+.. autofunction:: select_best
+
+.. autofunction:: select_best_index
+
+.. autofunction:: compare_first_bigger
+
+.. autofunction:: compare_first_smaller
+
+.. autofunction:: compare_last_bigger
+
+.. autofunction:: compare_last_smaller
+
+.. autofunction:: compare_bigger
+
+.. autofunction:: compare_smaller
+
 """
 
 import random
+
+from Orange.misc import deprecated_members, deprecated_function_name, \
+                        deprecated_keywords
 
 class BestOnTheFly:
     """
@@ -74,25 +98,26 @@ class BestOnTheFly:
     the same experiment always gives the same results, despite\
     pseudo-randomness.random seed.
     :type seed: int
-    :param callCompareOn1st: If set, :obj:`BestOnTheFly` will suppose\
+    :param call_compare_on_1st: If set, :obj:`BestOnTheFly` will suppose\
     that the candidates are lists are tuples, and it will call compare\
     with the first element of the tuple.
-    :type callCompareOn1st: bool
+    :type call_compare_on_1st: bool
     """
     
-    def __init__(self, compare=cmp, seed = 0, callCompareOn1st = False):
+    def __init__(self, compare=cmp, seed = 0, call_compare_on_1st = False):
         self.randomGenerator = random.Random(seed)
         self.compare=compare
         self.wins=0
         self.bestIndex, self.index = -1, -1
         self.best = None
-        self.callCompareOn1st = callCompareOn1st
+        self.call_compare_on_1st = call_compare_on_1st
 
     def candidate(self, x):
         """Add new candidate.
         
         :param x: new candidate.
-        :type x: object"""
+        :type x: object
+        """
         self.index += 1
         if not self.wins:
             self.best=x
@@ -100,7 +125,7 @@ class BestOnTheFly:
             self.bestIndex=self.index
             return 1
         else:
-            if self.callCompareOn1st:
+            if self.call_compare_on_1st:
                 cmpr=self.compare(x[0], self.best[0])
             else:
                 cmpr=self.compare(x, self.best)
@@ -121,20 +146,29 @@ class BestOnTheFly:
         """Return (currently) optimal object. This function can be called
         any number of times, even when the candidates are still coming.
         
-        :rtype: object"""
+        :rtype: object
+        """
         return self.best
 
-    def winnerIndex(self):
+    def winner_index(self):
         """Return the index of the optimal object within the sequence of
         the candidates.
         
-        :rtype: int"""
+        :rtype: int
+        """
         if self.best is not None:
             return self.bestIndex
         else:
             return None
 
-def selectBest(x, compare=cmp, seed = 0, callCompareOn1st = False):
+BestOnTheFly = deprecated_members({"callCompareOn1st": "call_compare_on_1st",
+                                   "winnerIndex": "winner_index",
+                                   },
+                                   wrap_methods=["__init__"])(BestOnTheFly)
+
+
+@deprecated_keywords({"callCompareOn1st": "call_compare_on_1st"})
+def select_best(x, compare=cmp, seed = 0, call_compare_on_1st = False):
     """Return the optimal object from list x. The function is used if the candidates
     are already in the list, so using the more complicated :obj:`BestOnTheFly` directly is
     not needed.
@@ -142,8 +176,8 @@ def selectBest(x, compare=cmp, seed = 0, callCompareOn1st = False):
     To demonstrate the use of :obj:`BestOnTheFly` see the implementation of
     :obj:`selectBest`::
     
-      def selectBest(x, compare=cmp, seed = 0, callCompareOn1st = False):
-          bs=BestOnTheFly(compare, seed, callCompareOn1st)
+      def selectBest(x, compare=cmp, seed = 0, call_compare_on_1st = False):
+          bs=BestOnTheFly(compare, seed, call_compare_on_1st)
           for i in x:
               bs.candidate(i)
           return bs.winner()
@@ -151,89 +185,100 @@ def selectBest(x, compare=cmp, seed = 0, callCompareOn1st = False):
     :param x: list of existing candidates.
     :type x: list
     :param compare: compare function.
-    :param seed: If not given, a random seed of 0 is used to ensure that\
-    the same experiment always gives the same results, despite\
-    pseudo-randomness.random seed.
+    :param seed: If not given, a random seed of 0 is used to ensure that
+        the same experiment always gives the same results, despite
+        pseudo-randomness.random seed.
     :type seed: int
-    :param callCompareOn1st: If set, :obj:`BestOnTheFly` will suppose\
-    that the candidates are lists are tuples, and it will call compare\
-    with the first element of the tuple.
-    :type callCompareOn1st: bool
-    :rtype: object"""
-    bs=BestOnTheFly(compare, seed, callCompareOn1st)
+    :param call_compare_on_1st: If set, :obj:`BestOnTheFly` will suppose
+        that the candidates are lists are tuples, and it will call compare
+        with the first element of the tuple.
+    :type call_compare_on_1st: bool
+    :rtype: object
+    
+    """
+    bs=BestOnTheFly(compare, seed, call_compare_on_1st)
     for i in x:
         bs.candidate(i)
     return bs.winner()
 
-def selectBestIndex(x, compare=cmp, seed = 0, callCompareOn1st = False):
+selectBest = deprecated_function_name(select_best)
+
+
+@deprecated_keywords({"callCompareOn1st": "call_compare_on_1st"})
+def select_best_index(x, compare=cmp, seed = 0, call_compare_on_1st = False):
     """Similar to :obj:`selectBest` except that it doesn't return the best object
-    but its index in the list x."""
-    bs=BestOnTheFly(compare, seed, callCompareOn1st)
+    but its index in the list x.
+    """
+    bs=BestOnTheFly(compare, seed, call_compare_on_1st)
     for i in x:
         bs.candidate(i)
-    return bs.winnerIndex()
+    return bs.winner_index()
 
-# def compare2_firstBigger(x, y):
-def compareFirstBigger(x, y):
+selectBestIndex = deprecated_function_name(select_best_index)
+
+
+def compare_first_bigger(x, y):
     """Function takes two lists and compares first elements.
     
     :param x: list of values.
     :type x: list
     :param y: list of values.
     :type y: list
-    :rtype:  cmp(x[0], y[0])"""
+    :rtype:  cmp(x[0], y[0])
+    """
     return cmp(x[0], y[0])
 
-#def compare2_firstSmaller(x, y):
-def compareFirstSmaller(x, y):
+def compare_first_smaller(x, y):
     """Function takes two lists and compares first elements.
     
     :param x: list of values.
     :type x: list
     :param y: list of values.
     :type y: list
-    :rtype:  -cmp(x[0], y[0])"""
+    :rtype:  -cmp(x[0], y[0])
+    """
     return -cmp(x[0], y[0])
 
-#     def compare2_lastBigger(x, y):
-def compareLastBigger(x, y):
+def compare_last_bigger(x, y):
     """Function takes two lists and compares last elements.
     
     :param x: list of values.
     :type x: list
     :param y: list of values.
     :type y: list
-    :rtype:  cmp(x[0], y[0])"""
+    :rtype:  cmp(x[0], y[0])
+    """
     return cmp(x[-1], y[-1])
 
-#    def compare2_lastSmaller(x, y):
-def compareLastSmaller(x, y):
+def compare_last_smaller(x, y):
     """Function takes two lists and compares last elements.
     
     :param x: list of values.
     :type x: list
     :param y: list of values.
     :type y: list
-    :rtype:  -cmp(x[0], y[0])"""
+    :rtype:  -cmp(x[0], y[0])
+    """
     return -cmp(x[-1], y[-1])
     
-#     def compare2_bigger(x, y):
-def compareBigger(x, y):
+def compare_bigger(x, y):
     """Function takes and compares two numbers.
     
     :param x: value.
     :type x: int
     :param y: value.
     :type y: int
-    :rtype:  cmp(x, y)"""
+    :rtype:  cmp(x, y)
+    """
     return cmp(x, y)
-#     def compare2_smaller(x, y):
-def compareSmaller(x, y):
+
+def compare_smaller(x, y):
     """Function takes and compares two numbers.
     
     :param x: value.
     :type x: int
     :param y: value.
     :type y: int
-    :rtype: cmp(x, y)"""
+    :rtype: cmp(x, y)
+    """
     return -cmp(x, y)
--- orange/doc/Orange/rst/code/misc-selection-bestonthefly.py
+++ orange/doc/Orange/rst/code/misc-selection-bestonthefly.py
@@ -8,24 +8,24 @@ import Orange
 
 table = Orange.data.Table("lymphography")
 
-findBest = Orange.misc.selection.BestOnTheFly(callCompareOn1st = True)
+find_best = Orange.misc.selection.BestOnTheFly(call_compare_on_1st = True)
 
 for attr in table.domain.attributes:
-    findBest.candidate((Orange.feature.scoring.GainRatio(attr, table), attr))
+    find_best.candidate((Orange.feature.scoring.GainRatio(attr, table), attr))
 
-print "%5.3f: %s" % findBest.winner()
+print "%5.3f: %s" % find_best.winner()
 
-findBest = Orange.misc.selection.BestOnTheFly(Orange.misc.selection.compareFirstBigger)
-
-for attr in table.domain.attributes:
-    findBest.candidate((Orange.feature.scoring.GainRatio(attr, table), attr))
-
-print "%5.3f: %s" % findBest.winner()
-
-findBest = Orange.misc.selection.BestOnTheFly()
+find_best = Orange.misc.selection.BestOnTheFly(Orange.misc.selection.compare_first_bigger)
 
 for attr in table.domain.attributes:
-    findBest.candidate(Orange.feature.scoring.GainRatio(attr, table))
+    find_best.candidate((Orange.feature.scoring.GainRatio(attr, table), attr))
 
-bestIndex = findBest.winnerIndex()
-print "%5.3f: %s" % (findBest.winner(), table.domain[bestIndex])
+print "%5.3f: %s" % find_best.winner()
+
+find_best = Orange.misc.selection.BestOnTheFly()
+
+for attr in table.domain.attributes:
+    find_best.candidate(Orange.feature.scoring.GainRatio(attr, table))
+
+best_index = find_best.winner_index()
+print "%5.3f: %s" % (find_best.winner(), table.domain[best_index])
