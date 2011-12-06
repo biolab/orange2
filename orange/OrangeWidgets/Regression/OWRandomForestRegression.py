24a6a39a8d8f4c0f11da3f6f3f92e68f8581127c
ales_erjavec <ales.erjavec@fri.uni-lj.si>
2011-12-06 12:42:39 +0000

    Overload setData method (for class type check).

--- orange/OrangeWidgets/Regression/OWRandomForestRegression.py
+++ orange/OrangeWidgets/Regression/OWRandomForestRegression.py
@@ -14,6 +14,33 @@ class OWRandomForestRegression(OWRandomForest):
     def __init__(self, parent=None, signalManager=None, title="Random forest regression"):
         OWRandomForest.__init__(self, parent, signalManager, title)
         
-        self.inputs = [("Examples", ExampleTable, self.setData), ("Preprocess", PreprocessedLearner, self.setPreprocessor)]
-        self.outputs = [("Learner", orange.Learner),("Random Forest Classifier", orange.Classifier),("Choosen Tree", orange.TreeClassifier) ]
+        self.inputs = [("Examples", ExampleTable, self.setData),
+                       ("Preprocess", PreprocessedLearner, self.setPreprocessor)]
+        
+        self.outputs = [("Learner", orange.Learner),
+                        ("Random Forest Classifier", orange.Classifier),
+                        ("Choosen Tree", orange.TreeClassifier)]
+        
+
+    def setData(self, data):
+        self.data = self.isDataWithClass(data, orange.VarTypes.Continuous, checkMissing=True) and data or None
+        
+        if self.data:
+            learner = self.constructLearner()
+            pb = OWGUI.ProgressBar(self, iterations=self.trees)
+            learner.callback = pb.advance
+            try:
+                self.classifier = learner(self.data)
+                self.classifier.name = self.name
+                self.streeEnabled(True)
+            except Exception, (errValue):
+                self.error(str(errValue))
+                self.classifier = None
+                self.streeEnabled(False)
+            pb.finish()
+        else:
+            self.classifier = None
+            self.streeEnabled(False)
+
+        self.send("Random Forest Classifier", self.classifier)
         
