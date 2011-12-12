79e4cdd77e4103f5379fef7b8dd9566f9e21b0eb
markotoplak <devnull@localhost>
2011-12-12 20:19:30 +0000

    Removed eval from accuracy8.py

--- orange/doc/ofb-rst/code/accuracy8.py
+++ orange/doc/ofb-rst/code/accuracy8.py
@@ -20,19 +20,19 @@ res = orngTest.crossValidation(learners, data, folds=10)
 cm = orngStat.computeConfusionMatrices(res,
         classIndex=data.domain.classVar.values.index('democrat'))
 
-stat = (('CA', 'CA(res)'),
-        ('Sens', 'sens(cm)'),
-        ('Spec', 'spec(cm)'),
-        ('AUC', 'AUC(res)'),
-        ('IS', 'IS(res)'),
-        ('Brier', 'BrierScore(res)'),
-        ('F1', 'F1(cm)'),
-        ('F2', 'Falpha(cm, alpha=2.0)'),
-        ('MCC', 'MCC(cm)'),
-        ('sPi', 'scottsPi(cm)'),
+stat = (('CA', lambda res,cm: orngStat.CA(res)),
+        ('Sens', lambda res,cm: orngStat.sens(cm)),
+        ('Spec', lambda res,cm: orngStat.spec(cm)),
+        ('AUC', lambda res,cm: orngStat.AUC(res)),
+        ('IS', lambda res,cm: orngStat.IS(res)),
+        ('Brier', lambda res,cm: orngStat.BrierScore(res)),
+        ('F1', lambda res,cm: orngStat.F1(cm)),
+        ('F2', lambda res,cm: orngStat.Falpha(cm, alpha=2.0)),
+        ('MCC', lambda res,cm: orngStat.MCC(cm)),
+        ('sPi', lambda res,cm: orngStat.scottsPi(cm)),
         )
 
-scores = [eval("orngStat."+s[1]) for s in stat]
+scores = [s[1](res,cm) for s in stat]
 print
 print "Learner  " + "".join(["%-7s" % s[0] for s in stat])
 for (i, l) in enumerate(learners):
