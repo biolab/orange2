ac5a8d3d9d9ed12355e85c6f80e602033a4d0a19
Miha Stajdohar <miha.stajdohar@gmail.com>
2012-02-04 19:01:15 +0100

    Fixed a bug when appending clustering results multiple times.

--- orange/OrangeWidgets/Unsupervised/OWNxClustering.py
+++ orange/OrangeWidgets/Unsupervised/OWNxClustering.py
@@ -2,11 +2,11 @@
 <name>Net Clustering</name>
 <description>Orange widget for community detection in networks</description>
 <icon>icons/Network.png</icon>
-<contact>Miha Stajdohar (miha.stajdohar(@at@)gmail.com)</contact> 
+<contact>Miha Stajdohar (miha.stajdohar(@at@)gmail.com)</contact>
 <priority>6430</priority>
 """
 
-import Orange
+import Orange.network
 import Orange.network.community as cd
 import OWGUI
 
@@ -15,12 +15,14 @@ from OWWidget import *
 
 class OWNxClustering(OWWidget):
 
-    settingsList = ['method', 'iterationHistory', 'autoApply', 'iterations', 'hop_attenuation']
+    settingsList = ['method', 'iterationHistory', 'autoApply', 'iterations',
+                    'hop_attenuation']
 
     def __init__(self, parent=None, signalManager=None):
         OWWidget.__init__(self, parent, signalManager, 'Nx Clustering')
 
-        self.inputs = [("Network", Orange.network.Graph, self.setNetwork, Default)]
+        self.inputs = [("Network", Orange.network.Graph,
+                        self.setNetwork, Default)]
         self.outputs = [("Network", Orange.network.Graph)]
 
         self.net = None
@@ -31,25 +33,38 @@ class OWNxClustering(OWWidget):
         self.hop_attenuation = 0.1
         self.loadSettings()
 
-        OWGUI.spin(self.controlArea, self, "iterations", 1, 100000, 1, label="Iterations: ")
-        ribg = OWGUI.radioButtonsInBox(self.controlArea, self, "method", [], "Method", callback=self.cluster)
-        OWGUI.appendRadioButton(ribg, self, "method", "Label propagation clustering (Raghavan et al., 2007)", callback=self.cluster)
+        OWGUI.spin(self.controlArea, self, "iterations", 1,
+                   100000, 1, label="Iterations: ")
+        ribg = OWGUI.radioButtonsInBox(self.controlArea, self, "method",
+                                       [], "Method", callback=self.cluster)
+        OWGUI.appendRadioButton(ribg, self, "method",
+                        "Label propagation clustering (Raghavan et al., 2007)",
+                        callback=self.cluster)
 
-        OWGUI.appendRadioButton(ribg, self, "method", "Label propagation clustering (Leung et al., 2009)", callback=self.cluster)
-        OWGUI.doubleSpin(OWGUI.indentedBox(ribg), self, "hop_attenuation", 0, 1, 0.01, label="Hop attenuation (delta): ")
-
+        OWGUI.appendRadioButton(ribg, self, "method",
+                        "Label propagation clustering (Leung et al., 2009)",
+                        callback=self.cluster)
+        OWGUI.doubleSpin(OWGUI.indentedBox(ribg), self, "hop_attenuation",
+                         0, 1, 0.01, label="Hop attenuation (delta): ")
 
         self.info = OWGUI.widgetLabel(self.controlArea, ' ')
-        OWGUI.checkBox(self.controlArea, self, "iterationHistory", "Append clustering data on each iteration")
-        autoApplyCB = OWGUI.checkBox(self.controlArea, self, "autoApply", "Commit automatically")
-        OWGUI.button(self.controlArea, self, "Commit", callback=self.cluster)
+        OWGUI.checkBox(self.controlArea, self, "iterationHistory",
+                       "Append clustering data on each iteration",
+                       callback=self.cluster)
+        OWGUI.checkBox(self.controlArea, self, "autoApply",
+                       "Commit automatically")
+        OWGUI.button(self.controlArea, self, "Commit",
+                     callback=lambda b=True: self.cluster(b))
 
     def setNetwork(self, net):
         self.net = net
         if self.autoApply:
             self.cluster()
 
-    def cluster(self):
+    def cluster(self, btn=False):
+        if not btn and not self.autoApply:
+            return
+
         self.info.setText(' ')
 
         if self.net is None:
@@ -68,5 +83,5 @@ class OWNxClustering(OWWidget):
                                     iterations=self.iterations,
                                     delta=self.hop_attenuation)
 
-        self.info.setText('%d clusters found' % len(set(labels.items())))
+        self.info.setText('%d clusters found' % len(set(labels.values())))
         self.send("Network", self.net)
