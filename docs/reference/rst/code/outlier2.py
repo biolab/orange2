1f25692a9050e46d586fcf082c6822f008eb5e88
markotoplak <devnull@localhost>
2012-02-07 10:00:07 +0100

    Fixed 2.5 examples that used Orange.distance.instances

--- docs/reference/rst/code/outlier2.py
+++ docs/reference/rst/code/outlier2.py
@@ -2,7 +2,7 @@ import Orange
 
 bridges = Orange.data.Table("bridges")
 outlier_det = Orange.preprocess.outliers.OutlierDetection()
-outlier_det.set_examples(bridges, Orange.distance.instances.EuclideanConstructor(bridges))
+outlier_det.set_examples(bridges, Orange.distance.Euclidean(bridges))
 outlier_det.set_knn(3)
 z_values = outlier_det.z_values()
 for ex,zv in sorted(zip(bridges, z_values), key=lambda x: x[1])[-5:]:
