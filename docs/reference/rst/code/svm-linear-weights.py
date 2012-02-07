6cf2dd15a672a05da4af50175b6da74f8bac7578
Miha Stajdohar <miha.stajdohar@gmail.com>
2012-02-07 23:36:43 +0100

    Fixed tests.

--- docs/reference/rst/code/svm-linear-weights.py
+++ docs/reference/rst/code/svm-linear-weights.py
@@ -1,14 +1,13 @@
-from Orange import data 
+from Orange import data
 from Orange.classification import svm
 
 brown = data.Table("brown-selected")
-classifier = svm.SVMLearner(brown, 
-                            kernel_type=svm.kernels.Linear, 
+classifier = svm.SVMLearner(brown,
+                            kernel_type=svm.kernels.Linear,
                             normalization=False)
 
 weights = svm.get_linear_svm_weights(classifier)
-print weights
+print sorted(weights)
 
 import pylab as plt
 plt.hist(weights.values())
- 
