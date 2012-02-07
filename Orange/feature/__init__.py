e23029008f934f849ad96e37841f2be968ae4356
markotoplak <devnull@localhost>
2012-02-07 10:25:02 +0100

    added descriptos to Orange.feature

--- Orange/feature/__init__.py
+++ Orange/feature/__init__.py
@@ -9,4 +9,20 @@ import discretization
 import continuization
 import imputation
 
+from Orange.core import Variable as Descriptor
+from Orange.core import EnumVariable as Discrete
+from Orange.core import FloatVariable as Continuous
+from Orange.core import PythonVariable as Python
+from Orange.core import StringVariable as String
+
+from Orange.core import VarList as Descriptors
+
+from Orange.core import newmetaid as new_meta_id
+
+from Orange.core import Variable as V
+make = V.make
+retrieve = V.get_existing
+MakeStatus = V.MakeStatus
+del V
+
 __docformat__ = 'restructuredtext'
