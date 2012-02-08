b7f7f6e77c980c50035dd73799f02506efadb651
ales_erjavec <devnull@localhost>
2012-02-08 15:25:25 +0100

    Added unittest for Orange.misc.selection.

---
+++ Orange/testing/unit/tests/test_selection.py
@@ -0,0 +1,37 @@
+import unittest
+
+from Orange.misc import selection
+
+
+class TestBestOnTheFly(unittest.TestCase):
+    def test_compare_on_1st(self): 
+        best = selection.BestOnTheFly(call_compare_on_1st=True)
+        test = [1, 2, 0, 5, 4, 5, 4]
+        # Test on integers
+        for t in test:
+            best.candidate((t, str(t)))
+            
+        winner = best.winner()
+        self.assertTrue(winner == (5, "5"))
+        self.assertIsInstance(winner, tuple)
+        
+        index = best.winner_index()
+        self.assertIsInstance(index, int)
+        
+        
+    def test_compare_first_bigger(self): 
+        best = selection.BestOnTheFly(selection.compare_first_bigger)
+        test = [1, 2, 0, 5, 4, 5, 4]
+        # Test on integers
+        for t in test:
+            best.candidate((t, str(t)))
+            
+        winner = best.winner()
+        self.assertTrue(winner == (5, "5"))
+        self.assertIsInstance(winner, tuple)
+        
+        index = best.winner_index()
+        self.assertIsInstance(index, int)
+        
+    
+        
