13c5b53ac3f2f0b8a4cf0e7693680998ff6906f2
ales_erjavec <devnull@localhost>
2012-02-09 00:57:53 +0100

    Skip C45 tests if the C45 dynamic library not found.

--- Orange/testing/unit/tests/test_c45.py
+++ Orange/testing/unit/tests/test_c45.py
@@ -6,4 +6,19 @@ import orange
 class TestC45(testing.LearnerTestCase):
     def setUp(self):
         self.learner = orange.C45Learner
+        
+    @testing.test_on_data
+    def test_learner_on(self, dataset):
+        try:
+            orange.C45Learner()
+        except orange.KernelException:
+            raise unittest.SkipTest("C45 dll not found")
+        testing.LearnerTestCase.test_learner_on(self, dataset)
        
+    @testing.test_on_data
+    def test_pickling_on(self, dataset):
+        try:
+            orange.C45Learner()
+        except orange.KernelException:
+            raise unittest.SkipTest("C45 dll not found")
+        testing.LearnerTestCase.test_pickling_on(self, dataset)
