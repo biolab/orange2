a4596cf652dc61340133eaa192054023db4eb938
Miha Stajdohar <miha.stajdohar@gmail.com>
2012-02-13 13:49:01 +0100

    Test all unit tests.

---
+++ Orange/testing/unit/test_all.py
@@ -0,0 +1,10 @@
+import os.path
+import unittest
+
+
+def load_tests(loader, standard_tests, pattern):
+    # top level directory cached on loader instance
+    this_dir = os.path.join(os.path.dirname(__file__), "tests")
+    package_tests = loader.discover(start_dir=this_dir, pattern="test*.py")
+    standard_tests.addTests(package_tests)
+    return standard_tests
