e950d162a9a5b83e740da63efa2858b5e44da932
ales_erjavec <devnull@localhost>
2012-02-15 13:38:48 +0100

    Added test_display_name_mapping.

---
+++ Orange/testing/unit/tests/test_display_name_mapping.py
@@ -0,0 +1,21 @@
+import unittest 
+
+import orange, Orange
+
+class TestNameMapping(unittest.TestCase):
+    def test_qualified_names(self):
+        """ Test that qualified names of core C++ objects 
+        map to the correct name in the Orange.* hierarchy.
+          
+        """
+        for cls in orange.__dict__.values():
+            if type(cls) == type:
+                try:
+                    cls2 = eval(cls.__module__ + "." + cls.__name__)
+                except AttributeError as err:
+                    self.assertTrue(False, cls.__module__ + "." + \
+                                    cls.__name__ + " does not exist")
+                
+                self.assertEqual(cls2, cls)
+#                if cls2 != cls:
+#                    print cls.__module__+"."+cls.__name__
