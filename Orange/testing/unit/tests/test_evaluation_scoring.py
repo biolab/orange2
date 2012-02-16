d213cd111710a758cba0be52d5b4ec10f3bd085c
anzeh <anze.staric@gmail.com>
2012-02-16 12:54:34 +0100

    Removed debuging outputs.

--- Orange/testing/unit/tests/test_evaluation_scoring.py
+++ Orange/testing/unit/tests/test_evaluation_scoring.py
@@ -24,11 +24,9 @@ class TestAuc(unittest.TestCase):
 
         auc = scoring.AUC(cv)
         self.assertEqual(len(auc), 1)
-        print auc
 
         auc = scoring.AUC(pt)
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_on_iris(self):
         ds = data.Table("iris")
@@ -36,7 +34,6 @@ class TestAuc(unittest.TestCase):
         auc = scoring.AUC(test_results)
 
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_on_iris_by_pairs(self):
         ds = data.Table("iris")
@@ -44,7 +41,6 @@ class TestAuc(unittest.TestCase):
         auc = scoring.AUC.by_pairs(test_results)
 
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_on_iris_by_weighted_pairs(self):
         ds = data.Table("iris")
@@ -52,7 +48,6 @@ class TestAuc(unittest.TestCase):
         auc = scoring.AUC.by_weighted_pairs(test_results)
 
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_on_iris_one_against_all(self):
         ds = data.Table("iris")
@@ -60,7 +55,6 @@ class TestAuc(unittest.TestCase):
         auc = scoring.AUC.one_against_all(test_results)
 
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_on_iris_weighted_one_against_all(self):
         ds = data.Table("iris")
@@ -68,36 +62,28 @@ class TestAuc(unittest.TestCase):
         auc = scoring.AUC.weighted_one_against_all(test_results)
 
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_on_iris_single_class(self):
         ds = data.Table("iris")
         test_results = testing.cross_validation([self.learner], ds, folds=5)
         auc = scoring.AUC.single_class(test_results)
         self.assertEqual(len(auc), 1)
-        print auc
         auc = scoring.AUC.single_class(test_results, 0)
         self.assertEqual(len(auc), 1)
-        print auc
         auc = scoring.AUC.single_class(test_results, 1)
         self.assertEqual(len(auc), 1)
-        print auc
         auc = scoring.AUC.single_class(test_results, 2)
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_on_iris_pair(self):
         ds = data.Table("iris")
         test_results = testing.cross_validation([self.learner], ds, folds=5)
         auc = scoring.AUC.pair(test_results, 0, 1)
         self.assertEqual(len(auc), 1)
-        print auc
         auc = scoring.AUC.pair(test_results, 0, 2)
         self.assertEqual(len(auc), 1)
-        print auc
         auc = scoring.AUC.pair(test_results, 1, 2)
         self.assertEqual(len(auc), 1)
-        print auc
 
     def test_auc_matrix_on_iris(self):
         ds = data.Table("iris")
@@ -105,7 +91,6 @@ class TestAuc(unittest.TestCase):
         auc = scoring.AUC.matrix(test_results)
         self.assertEqual(len(auc), 1)
         self.assertEqual(len(auc[0]), 3)
-        print auc
 
 
 if __name__ == '__main__':
