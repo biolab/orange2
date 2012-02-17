68d876012dec7ece251c3e94fb57e91f7ab436ed
ales_erjavec <devnull@localhost>
2012-02-17 11:39:52 +0100

    Changed back the update-all-scripts scripts to use curl but pull the scripts from bitbucket.

--- install-scripts/mac/update-all-scripts.sh
+++ install-scripts/mac/update-all-scripts.sh
@@ -3,19 +3,23 @@
 # Should be run as: ./update-all-scripts.sh
 #
 
-if [ ! -e /private/tmp/repos ]; then
-	mkdir /private/tmp/repos
-fi
+curl --silent --output update-all-scripts.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/update-all-scripts.sh
+curl --silent --output bundle-64bit-daily-build.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/bundle-64bit-daily-build.sh
+curl --silent --output bundle-daily-build.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/bundle-daily-build.sh
+curl --silent --output dailyrun.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/dailyrun.sh
+curl --silent --output dailyrun-finkonly-withsource.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/dailyrun-finkonly-withsource.sh
+curl --silent --output dailyrun-finkonly.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/dailyrun-finkonly.sh
+curl --silent --output dailyrun-bundleonly.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/dailyrun-bundleonly.sh
+curl --silent --output fink-daily-build.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/fink-daily-build.sh
+curl --silent --output fink-restore-selections.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/fink-restore-selections.sh
+curl --silent --output fink-selfupdate-orange.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/fink-selfupdate-orange.sh
+curl --silent --output force-fink-daily-build.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/force-fink-daily-build.sh
+curl --silent --output mount-dirs.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/mount-dirs.sh
 
-LOCAL_REPO=/private/tmp/repos/orange
-
-if [ ! -e $LOCAL_REPO ]; then
-	hg clone https://bitbucket.org/biolab/orange $LOCAL_REPO
-fi
-
-hg pull --update -R $LOCAL_REPO
-
-cp ${LOCAL_REPO}/install-scripts/mac/*.sh ./
+curl --silent --output bundle-build-hg.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/bundle-build-hg.sh
+curl --silent --output bundle-daily-build-hg.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/bundle-daily-build-hg.sh
+curl --silent --output bundle-inject-hg.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/bundle-inject-hg.sh
+curl --silent --output dailyrun-bundleonly-hg.sh https://bitbucket.org/biolab/orange/raw/tip/install-scripts/mac/dailyrun-bundleonly-hg.sh
 
 chmod +x *.sh
 
