1579d993a9b4cfe49a0286beb52206fc9395d345
Miha Stajdohar <miha.stajdohar@gmail.com>
2012-02-18 15:08:56 +0100

    Trimmed number of decimals.

--- docs/reference/rst/code/outlier1.py
+++ docs/reference/rst/code/outlier1.py
@@ -3,4 +3,4 @@ import Orange
 bridges = Orange.data.Table("bridges")
 outlierDet = Orange.data.outliers.OutlierDetection()
 outlierDet.set_examples(bridges)
-print outlierDet.z_values()
+print ", ".join("%.8f" % val for val in outlierDet.z_values())
