2d1db6bdcdf2d3a6f81e40023877b0fb11b5e9ca
Lan Zagar <lan.zagar@fri.uni-lj.si>
2012-03-21 14:38:49 +0000

    Updated linear regression for the improved to_numpy.

--- Orange/regression/linear.py
+++ Orange/regression/linear.py
@@ -228,16 +228,10 @@ class LinearRegressionLearner(base.BaseRegressionLearner):
 
         # convertion to numpy
         A, y, w = table.to_numpy()
-        if A is None:
-            n, m = len(table), 0
-        else:
-            n, m = numpy.shape(A)
+        n, m = numpy.shape(A)
      
         if self.intercept:
-            if A is None:
-                X = numpy.ones([n, 1])
-            else:
-                X = numpy.insert(A, 0, 1, axis=1) # adds a column of ones
+            X = numpy.insert(A, 0, 1, axis=1) # adds a column of ones
         else:
             X = A
              
@@ -260,7 +254,7 @@ class LinearRegressionLearner(base.BaseRegressionLearner):
         coefficients = dot(D, y)
 
         mu_y, sigma_y = numpy.mean(y), numpy.std(y)
-        if A is not None:
+        if m > 0:
             cov_x = numpy.cov(X, rowvar=0)
 
             # standardized coefficients
