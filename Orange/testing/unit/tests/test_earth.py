b0d9b81fc9b455151da988e169c5ce5cd1196d0c
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2012-03-27 12:36:51 +0200

    Removed old commented out code.

--- Orange/testing/unit/tests/test_earth.py
+++ Orange/testing/unit/tests/test_earth.py
@@ -101,13 +101,16 @@ class TestEarthMultitarget(unittest.TestCase):
         self.assertTrue(all(c1 == c2 for c1, c2 in \
                             zip(predictor.class_vars, domain.class_vars)))
         
-    
 #@datasets_driven(datasets=testing.REGRESSION_DATASETS,)
 #class TestScoreRSS(testing.MeasureAttributeTestCase):
 #    def setUp(self):
 #        from Orange.regression.earth import ScoreRSS
 #        self.measure = ScoreRSS()
 
+def load_tests(loader, tests, ignore):
+    import doctest
+    loader.addTests(doctest.DocTestSuite(earth, **earth._doctest_args()))
+    return loader
 
 if __name__ == "__main__":
     unittest.main()
