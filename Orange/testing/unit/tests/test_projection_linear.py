6b07f2f970dad6c0c09bb70a8d445d554e1754fd
anze <anze.staric@gmail.com>
2012-04-03 10:08:35 +0200

    Improved pca unittests.

--- Orange/testing/unit/tests/test_projection_linear.py
+++ Orange/testing/unit/tests/test_projection_linear.py
@@ -9,6 +9,8 @@ import random
 from Orange import data, feature
 from Orange.projection import linear
 
+random.seed(0)
+
 def normalize(a):
     a = a if isinstance(a, np.ndarray) else np.array(a)
     return a / np.linalg.norm(a)
@@ -26,16 +28,24 @@ def prepare_dataset(components=((),), n=150):
     domain = data.Domain([feature.Continuous("A%d" % i) for i in range(m)], False)
     return domain, d
 
+def create_base(ncomponents, m):
+    vector = np.random.randint(0,5,m)
+    while not vector.any():
+        vector = np.random.randint(0,5,m)
+    principal_components = np.zeros((ncomponents, m))
+    k = float(m) / ncomponents
+    to = 0
+    for i in range(1,ncomponents):
+        from_, to = int(k*(i-1)), int(k*i)
+        principal_components[i-1,from_:to] = vector[from_:to]
+    principal_components[ncomponents-1,to:m] = normalize(vector[to:m])
+    return principal_components
 
 
 class TestPca(unittest.TestCase):
-    def create_normal_dataset(self):
-        self.principal_component = normalize([random.randint(0, 5) for _ in range(10)])
-        self.dataset = data.Table(*prepare_dataset(components=[self.principal_component]))
-
-    def create_wide_dataset(self):
-        self.principal_component = normalize([random.randint(0, 5) for _ in range(250)])
-        self.dataset = data.Table(*prepare_dataset(components=[self.principal_component]))
+    def create_dataset(self, ncomponents=3, m=10):
+        self.principal_components = create_base(ncomponents, m)
+        self.dataset = data.Table(*prepare_dataset(components=self.principal_components))
 
     def create_empty_dataset(self):
         self.dataset = data.Table(*prepare_dataset(components=([0, 0, 0, 0, 0],), n=0))
@@ -53,57 +63,61 @@ class TestPca(unittest.TestCase):
                     ex[i] = "?"
 
 
-    def test_pca_on_normal_data(self):
-        self.create_normal_dataset()
+    def test_pca(self):
+        for m in (10, 250):
+            self.create_dataset(m=m)
 
-        pca = linear.Pca(standardize=False)(self.dataset)
-        self.assertIsInstance(pca, linear.PcaProjector)
+            pca = linear.Pca(standardize=False)(self.dataset)
 
-        absolute_error = (np.abs(pca.projection[0]) - np.abs(self.principal_component)).sum()
-        self.assertAlmostEqual(absolute_error, 0.)
+            self.assertInCorrectSpace(pca.projection[pca.variances > 0.01, :])
+            for v in pca.projection:
+                # projections vectors should be normalized
+                self.assertAlmostEqual(np.linalg.norm(v), 1.)
 
-    def test_pca_on_wide_data(self):
-        self.create_wide_dataset()
-
-        pca = linear.Pca(standardize=False)(self.dataset)
-        self.assertIsInstance(pca, linear.PcaProjector)
-
-        absolute_error = (np.abs(pca.projection[0]) - np.abs(self.principal_component)).sum()
-        self.assertAlmostEqual(absolute_error, 0., 1)
+            # Components should have decreasing variants
+            self.assertListEqual(pca.variances.tolist(), sorted(pca.variances, reverse=True))
 
     def test_pca_with_standardization(self):
-        self.create_normal_dataset()
+        self.create_dataset(ncomponents=1)
 
         pca = linear.Pca(standardize=True)(self.dataset)
-        eigen_vector = pca.projection[0]
-        non_zero_elements = eigen_vector[eigen_vector.nonzero()]
+        projection = pca.projection[0]
+        non_zero_elements = projection[projection.nonzero()]
 
         # since values in all dimensions are normally distributed, dimensions should be treated as equally important
         self.assertAlmostEqual(non_zero_elements.min(), non_zero_elements.max())
 
     def test_pca_with_variance_covered(self):
-        self.create_normal_dataset()
+        ncomponents = 3
+        self.create_dataset(ncomponents=ncomponents)
 
         pca = linear.Pca(variance_covered=.99)(self.dataset)
-        # all data points lie in one dimension, one component should cover all the variance
+
         nvectors, vector_dimension = pca.projection.shape
-        self.assertEqual(nvectors, 1)
+        self.assertEqual(nvectors, ncomponents)
 
     def test_pca_with_max_components(self):
-        self.create_normal_dataset()
         max_components = 3
+        self.create_dataset(ncomponents = max_components + 3)
 
         pca = linear.Pca(max_components=max_components)(self.dataset)
-        # all data points lie in one dimension, one component should cover all the variance
+
         nvectors, vector_dimension = pca.projection.shape
         self.assertEqual(nvectors, max_components)
 
     def test_pca_handles_unknowns(self):
         self.create_dataset_with_unknowns()
 
-        pca = linear.Pca()(self.dataset)
+        linear.Pca()(self.dataset)
 
+    def test_total_variance_remains_the_same(self):
+        for m in (10, 250):
+            self.create_dataset(m=m)
 
+            pca = linear.Pca()(self.dataset)
+
+            self.assertAlmostEqual(pca.variance_sum, pca.variances.sum())
+            self.assertAlmostEqual(pca.variance_sum, (self.principal_components != 0).sum())
 
     def test_pca_on_empty_data(self):
         self.create_empty_dataset()
@@ -116,6 +130,17 @@ class TestPca(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             linear.Pca()(self.dataset)
+
+    def assertInCorrectSpace(self, vectors):
+        vectors = vectors.copy()
+        for component in self.principal_components:
+            i = component.nonzero()[0][0]
+            coef = vectors[:,i] / component[i]
+            vectors -= np.dot(coef.reshape(-1, 1), component.reshape(1, -1))
+
+        for vector in vectors:
+            for value in vector:
+                self.assertAlmostEqual(value, 0.)
 
 
 class TestProjector(unittest.TestCase):
