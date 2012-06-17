4606be7401dfc96390ec89d119714046b2a8044d
mstajdohar <devnull@localhost>
2012-06-17 13:39:41 +0200

    Plot is a pointer.

--- source/orangeqt/networkcurve.cpp
+++ source/orangeqt/networkcurve.cpp
@@ -985,14 +985,14 @@ int NetworkCurve::fr(int steps, bool weighted, bool smooth_cooling)
 		QTime before_refresh_time = QTime::currentTime();
 		if (before_refresh_time > refresh_time && i % 2 == 0)
 		{
-		    QRectF r2 = QRectF(plot().contentsRect());
+		    QRectF r2 = QRectF(plot()->contentsRect());
 		    r2.adjust(1, 1, -1, -1);
             double t = r2.top();
             r2.setTop(r2.bottom());
             r2.setBottom(t);
 
             QPair<int, int> axes = axes();
-		    QRectF r1 = plot().data_rect_for_axes(axes.first, axes.second);
+		    QRectF r1 = plot()->data_rect_for_axes(axes.first, axes.second);
 
             QTransform tr1 = QTransform().translate(-r1.left(), -r1.top());
             QTransform ts = QTransform().scale(r2.width()/r1.width(), r2.height()/r1.height());
