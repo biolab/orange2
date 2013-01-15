71bfa9d229af4e4c98836c52b507c5119a636cde
astaric <devnull@localhost>
2013-01-15 11:02:34 +0100

    Set version to 2.6.

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ from distutils.extension import Extension
 
 NAME = 'Orange'
 
-VERSION = '2.6a2'
-ISRELEASED = False
+VERSION = '2.6'
+ISRELEASED = True
 
 DESCRIPTION = 'Orange, a component-based data mining framework.'
 LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
