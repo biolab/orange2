d2a6aa2698a43164fe601b5af1a1e9e7a6f4b209
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2013-01-21 13:55:02 +0100

    Fixed log message.

--- Orange/OrangeCanvas/canvas/scene.py
+++ Orange/OrangeCanvas/canvas/scene.py
@@ -336,7 +336,7 @@ class CanvasScene(QGraphicsScene):
         self.link_item_added.emit(item)
 
         log.info("Added link %r -> %r to '%s'" % \
-                 (item.sourceItem.title, item.sinkItem.title, self))
+                 (item.sourceItem.title(), item.sinkItem.title(), self))
 
         self.__anchor_layout.invalidateLink(item)
 
