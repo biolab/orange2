3fc210669ca5db2cbca6d7eec194f16117499314
markotoplak <devnull@localhost>
2013-01-25 17:47:14 +0100

    Removed obsolete files from Orange/doc (old reference, tutorial, widget documentation, web page properties, a copy of GPL).

--- Orange/doc/modules/tuningm.py
+++
@@ -1,9 +0,0 @@
-import orange, orngTree, orngStat, orngWrap
-
-learner = orngTree.TreeLearner()
-data = orange.ExampleTable("voting")
-tuner = orngWrap.TuneMParameters(object=learner,
-                                 parameters=[("minSubset", [2, 5, 10, 20]),
-                                             ("measure", [orange.MeasureAttribute_gainRatio(), orange.MeasureAttribute_gini()])],
-                                 evaluate = orngStat.AUC)
-classifier = tuner(data)
