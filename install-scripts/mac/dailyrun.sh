d14091c27da9e445a596f5fef8ff8c84957d7308
Miha Stajdohar <miha.stajdohar@gmail.com>
2013-04-02 12:04:41 +0200

    Removed fink from install scripts.

--- install-scripts/mac/dailyrun.sh
+++ install-scripts/mac/dailyrun.sh
@@ -103,102 +103,102 @@ cat $WORK_DIR/bundle-daily-build.log >> "$LOG_DIR/bundle-$MAC_VERSION-daily-buil
 (($EXIT_VALUE)) && echo "Running bundle-daily-build-hg.sh failed"
 
 
-MAC_VERSION=`sw_vers -productVersion | cut -d '.' -f 2`
-ARCH=`perl -MFink::FinkVersion -e 'print Fink::FinkVersion::get_arch'`
-
-FINK_ROOT=/sw
-
-if [ ! $LOCAL ]; then
-	# Compare with the published info files
-	BASE="http://orange.biolab.si/fink/dists/$ARCH/main/finkinfo"
-else
-	# Compare with the local info files
-	BASE="file://$FINK_ROOT/fink/dists/local/main/finkinfo"
-fi
-
-
-OLD_ORANGE_VERSION=`curl --silent $BASE/orange-gui-dev-py.info | grep "Version: " | cut -d" " -f 2`
-OLD_BIOINFORMATICS_VERSION=`curl --silent $BASE/orange-bioinformatics-gui-dev-py.info | grep "Version: " | cut -d" " -f 2`
-OLD_TEXT_VERSION=`curl --silent $BASE/orange-text-gui-dev-py.info | grep "Version: " | cut -d" " -f 2`
-
-if [[ $OLD_ORANGE_VERSION < $ORANGE_VERSION ]]; then
-	NEW_ORANGE=1
-fi
-
-if [[ $OLD_BIOINFORMATICS_VERSION < $BIOINFORMATICS_VERSION ]]; then
-	NEW_BIOINFORMATICS=1
-fi
-
-if [[ $OLD_TEXT_VERSION < $TEXT_VERSION ]]; then
-	NEW_TEXT=1
-fi
-
-
-# Base url for sources in fink .info files
-if [ $LOCAL ]; then
-	BASE_URL="file://$PUBLISH_DIR/sources"
-else
-	BASE_URL="http://orange.biolab.si/download/sources"
-fi
-
-# Update the local finkinfo 
-# Local info files will be moved to biolab/main/finkinfo in fink-daily-build-packages.sh
-FINK_INFO_DIR="$FINK_ROOT/fink/dists/local/main/finkinfo"
-
-if [ ! -e $FINK_INFO_DIR ]; then
-	mkdir -p $FINK_INFO_DIR
-fi
-
-# Remove any old remaining local .info files
-rm -f $FINK_INFO_DIR/orange-*.info
-
-# Directory where fink .info templates are
-FINK_TEMPLATES=$WORK_DIR/orange/install-scripts/mac/fink
-
-FINK_LOG=$WORK_DIR/fink-daily-build.log
-echo "" > $FINK_LOG
-
-if [[ $NEW_ORANGE || $FORCE ]]; then
-	FINK_ORANGE_SOURCE_TEMPLATE="Orange-%v.tar.gz"
-	./fink-register-info.sh "$FINK_TEMPLATES/orange-gui-dev-py.info" $BASE_URL/$FINK_ORANGE_SOURCE_TEMPLATE $ORANGE_SOURCE_MD5 $ORANGE_VERSION $FINK_INFO_DIR/orange-gui-dev-py.info >> $FINK_LOG 2>&1
-	FINK_ORANGE_INFO_EXIT_VALUE=$?
-fi
-
-if [[ $NEW_BIOINFORMATICS || $FORCE ]]; then
-	FINK_BIOINFORMATICS_SOURCE_TEMPLATE="Orange-Bioinformatics-%v.tar.gz"
-	./fink-register-info.sh "$FINK_TEMPLATES/orange-bioinformatics-gui-dev-py.info" $BASE_URL/$FINK_BIOINFORMATICS_SOURCE_TEMPLATE $BIOINFORMATICS_SOURCE_MD5 $BIOINFORMATICS_VERSION $FINK_INFO_DIR/orange-bioinformatics-gui-dev-py.info >> $FINK_LOG 2>&1
-	FINK_BIOINFORMATICS_INFO_EXIT_VALUE=$?
-fi
-
-if [[ $NEW_TEXT || $FORCE ]]; then
-	FINK_TEXT_SOURCE_TEMPLATE="Orange-Text-%v.tar.gz"
-	./fink-register-info.sh "$FINK_TEMPLATES/orange-text-gui-dev-py.info" $BASE_URL/$FINK_TEXT_SOURCE_TEMPLATE $TEXT_SOURCE_MD5 $TEXT_VERSION $FINK_INFO_DIR/orange-text-gui-dev-py.info >> $FINK_LOG 2>&1
-	FINK_TEXT_INFO_EXIT_VALUE=$?
-fi
-
-if [ ! $LOCAL ]; then
-	/Users/ailabc/mount-dirs.sh || { echo "Mounting failed." ; exit 1 ; }
-fi
-
-EXIT_VALUE=$(($FINK_ORANGE_INFO_EXIT_VALUE + $FINK_BIOINFORMATICS_INFO_EXIT_VALUE + $FINK_TEXT_INFO_EXIT_VALUE))
-if (($EXIT_VALUE)); then
-	echo "Running fink-register-info.sh failed"
-	rm -f $FINK_INFO_DIR/orange-*.info
-fi
-
-## daily fink build
-
-./fink-daily-build-packages.sh &> $WORK_DIR/fink-daily-build-packages.log
-EXIT_VALUE=$?
-
-if [ ! $LOCAL ]; then
-	/Users/ailabc/mount-dirs.sh || { echo "Mounting failed." ; exit 1 ; }
-fi
-
-echo "Orange (fink $MAC_VERSION $ARCH) [$EXIT_VALUE]" > "$LOG_DIR/fink-$MAC_VERSION-$ARCH-daily-build.log"
-date >> "$LOG_DIR/fink-$MAC_VERSION-$ARCH-daily-build.log"
-cat $WORK_DIR/fink-daily-build-packages.log >> "$LOG_DIR/fink-$MAC_VERSION-$ARCH-daily-build.log"
-(($EXIT_VALUE)) && echo "Running fink-daily-build.sh failed"
+#MAC_VERSION=`sw_vers -productVersion | cut -d '.' -f 2`
+#ARCH=`perl -MFink::FinkVersion -e 'print Fink::FinkVersion::get_arch'`
+#
+#FINK_ROOT=/sw
+#
+#if [ ! $LOCAL ]; then
+#	# Compare with the published info files
+#	BASE="http://orange.biolab.si/fink/dists/$ARCH/main/finkinfo"
+#else
+#	# Compare with the local info files
+#	BASE="file://$FINK_ROOT/fink/dists/local/main/finkinfo"
+#fi
+#
+#
+#OLD_ORANGE_VERSION=`curl --silent $BASE/orange-gui-dev-py.info | grep "Version: " | cut -d" " -f 2`
+#OLD_BIOINFORMATICS_VERSION=`curl --silent $BASE/orange-bioinformatics-gui-dev-py.info | grep "Version: " | cut -d" " -f 2`
+#OLD_TEXT_VERSION=`curl --silent $BASE/orange-text-gui-dev-py.info | grep "Version: " | cut -d" " -f 2`
+#
+#if [[ $OLD_ORANGE_VERSION < $ORANGE_VERSION ]]; then
+#	NEW_ORANGE=1
+#fi
+#
+#if [[ $OLD_BIOINFORMATICS_VERSION < $BIOINFORMATICS_VERSION ]]; then
+#	NEW_BIOINFORMATICS=1
+#fi
+#
+#if [[ $OLD_TEXT_VERSION < $TEXT_VERSION ]]; then
+#	NEW_TEXT=1
+#fi
+#
+#
+## Base url for sources in fink .info files
+#if [ $LOCAL ]; then
+#	BASE_URL="file://$PUBLISH_DIR/sources"
+#else
+#	BASE_URL="http://orange.biolab.si/download/sources"
+#fi
+#
+## Update the local finkinfo
+## Local info files will be moved to biolab/main/finkinfo in fink-daily-build-packages.sh
+#FINK_INFO_DIR="$FINK_ROOT/fink/dists/local/main/finkinfo"
+#
+#if [ ! -e $FINK_INFO_DIR ]; then
+#	mkdir -p $FINK_INFO_DIR
+#fi
+#
+## Remove any old remaining local .info files
+#rm -f $FINK_INFO_DIR/orange-*.info
+#
+## Directory where fink .info templates are
+#FINK_TEMPLATES=$WORK_DIR/orange/install-scripts/mac/fink
+#
+#FINK_LOG=$WORK_DIR/fink-daily-build.log
+#echo "" > $FINK_LOG
+#
+#if [[ $NEW_ORANGE || $FORCE ]]; then
+#	FINK_ORANGE_SOURCE_TEMPLATE="Orange-%v.tar.gz"
+#	./fink-register-info.sh "$FINK_TEMPLATES/orange-gui-dev-py.info" $BASE_URL/$FINK_ORANGE_SOURCE_TEMPLATE $ORANGE_SOURCE_MD5 $ORANGE_VERSION $FINK_INFO_DIR/orange-gui-dev-py.info >> $FINK_LOG 2>&1
+#	FINK_ORANGE_INFO_EXIT_VALUE=$?
+#fi
+#
+#if [[ $NEW_BIOINFORMATICS || $FORCE ]]; then
+#	FINK_BIOINFORMATICS_SOURCE_TEMPLATE="Orange-Bioinformatics-%v.tar.gz"
+#	./fink-register-info.sh "$FINK_TEMPLATES/orange-bioinformatics-gui-dev-py.info" $BASE_URL/$FINK_BIOINFORMATICS_SOURCE_TEMPLATE $BIOINFORMATICS_SOURCE_MD5 $BIOINFORMATICS_VERSION $FINK_INFO_DIR/orange-bioinformatics-gui-dev-py.info >> $FINK_LOG 2>&1
+#	FINK_BIOINFORMATICS_INFO_EXIT_VALUE=$?
+#fi
+#
+#if [[ $NEW_TEXT || $FORCE ]]; then
+#	FINK_TEXT_SOURCE_TEMPLATE="Orange-Text-%v.tar.gz"
+#	./fink-register-info.sh "$FINK_TEMPLATES/orange-text-gui-dev-py.info" $BASE_URL/$FINK_TEXT_SOURCE_TEMPLATE $TEXT_SOURCE_MD5 $TEXT_VERSION $FINK_INFO_DIR/orange-text-gui-dev-py.info >> $FINK_LOG 2>&1
+#	FINK_TEXT_INFO_EXIT_VALUE=$?
+#fi
+#
+#if [ ! $LOCAL ]; then
+#	/Users/ailabc/mount-dirs.sh || { echo "Mounting failed." ; exit 1 ; }
+#fi
+#
+#EXIT_VALUE=$(($FINK_ORANGE_INFO_EXIT_VALUE + $FINK_BIOINFORMATICS_INFO_EXIT_VALUE + $FINK_TEXT_INFO_EXIT_VALUE))
+#if (($EXIT_VALUE)); then
+#	echo "Running fink-register-info.sh failed"
+#	rm -f $FINK_INFO_DIR/orange-*.info
+#fi
+#
+### daily fink build
+#
+#./fink-daily-build-packages.sh &> $WORK_DIR/fink-daily-build-packages.log
+#EXIT_VALUE=$?
+#
+#if [ ! $LOCAL ]; then
+#	/Users/ailabc/mount-dirs.sh || { echo "Mounting failed." ; exit 1 ; }
+#fi
+#
+#echo "Orange (fink $MAC_VERSION $ARCH) [$EXIT_VALUE]" > "$LOG_DIR/fink-$MAC_VERSION-$ARCH-daily-build.log"
+#date >> "$LOG_DIR/fink-$MAC_VERSION-$ARCH-daily-build.log"
+#cat $WORK_DIR/fink-daily-build-packages.log >> "$LOG_DIR/fink-$MAC_VERSION-$ARCH-daily-build.log"
+#(($EXIT_VALUE)) && echo "Running fink-daily-build.sh failed"
 
 # Zero exit value
 true
--- install-scripts/mac/mount-dirs.sh
+++ install-scripts/mac/mount-dirs.sh
@@ -4,10 +4,10 @@ if [ -e /Library/Filesystems/fusefs.fs/Support/fusefs.kext ]; then
 	kextload -q /Library/Filesystems/fusefs.fs/Support/fusefs.kext
 fi
 
-umount /Volumes/fink/ 2> /dev/null || true
-sleep 5
-mkdir -p /Volumes/fink/
-/Users/ailabc/Downloads/sshfs-binaries/sshfs-static-leopard -o reconnect,workaround=nonodelay,uid=$(id -u),gid=$(id -g) fink@biolab.si:/files /Volumes/fink/
+#umount /Volumes/fink/ 2> /dev/null || true
+#sleep 5
+#mkdir -p /Volumes/fink/
+#/Users/ailabc/Downloads/sshfs-binaries/sshfs-static-leopard -o reconnect,workaround=nonodelay,uid=$(id -u),gid=$(id -g) fink@biolab.si:/files /Volumes/fink/
 
 umount /Volumes/download/ 2> /dev/null || true
 sleep 5
