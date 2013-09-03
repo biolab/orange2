2baf8b72d9f4dcfec3cac60448285e250dae9880
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2013-09-03 14:53:58 +0200

    Refactored scheme parsing.
    Using widget description's "replaces" list to resolve widgets.

--- Orange/OrangeCanvas/application/canvasmain.py
+++ Orange/OrangeCanvas/application/canvasmain.py
@@ -50,7 +50,7 @@ from .addons import AddOnManagerDialog
 from ..document.schemeedit import SchemeEditWidget
 
 from ..scheme import widgetsscheme
-from ..scheme.readwrite import parse_scheme, sniff_version
+from ..scheme.readwrite import scheme_load, sniff_version
 
 from . import welcomedialog
 from ..preview import previewdialog, previewmodel
@@ -951,9 +951,9 @@ class CanvasMainWindow(QMainWindow):
         new_scheme = widgetsscheme.WidgetsScheme(parent=self)
         errors = []
         try:
-            parse_scheme(new_scheme, open(filename, "rb"),
-                         error_handler=errors.append,
-                         allow_pickle_data=True)
+            scheme_load(new_scheme, open(filename, "rb"),
+                        error_handler=errors.append)
+
         except Exception:
             message_critical(
                  self.tr("Could not load an Orange Scheme file"),
