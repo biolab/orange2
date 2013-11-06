55e38cc852903e2d16eb583f98ef9bd6861fc834
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2013-11-06 13:55:01 +0100

    Skip neural network tests with numpy < 1.5

--- Orange/testing/unit/tests/test_neural.py
+++ Orange/testing/unit/tests/test_neural.py
@@ -3,10 +3,16 @@ try:
 except ImportError:
     import unittest
 
+import numpy
+from distutils.version import StrictVersion
+
 from Orange.testing import testing
 from Orange.classification import neural
 
+np_version = StrictVersion(numpy.version.short_version).version
 
+
+@unittest.skipIf(np_version < (1, 5), "numpy >= 1.5 required")
 @testing.datasets_driven(datasets=["iris", "lenses", "monks-1"])
 class TestNeuralNetwork(testing.LearnerTestCase):
     LEARNER = neural.NeuralNetworkLearner
