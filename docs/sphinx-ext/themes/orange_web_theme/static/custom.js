4506f7a406bb1e6de8c989fb29692a564f11f2b6
Miha Jenko <mjenko@t-2.net>
2014-10-08 16:14:59 +0200

    Temp.

--- docs/sphinx-ext/themes/orange_web_theme/static/custom.js
+++ docs/sphinx-ext/themes/orange_web_theme/static/custom.js
@@ -15,18 +15,33 @@ $(document).ready(function() {
     });
 
     /* sidebar handling so it is aligned with the last element of the nav-bar */
-    function setSidebar() {
-        var $sphinxsidebar = $('div.sphinxsidebar').removeAttr('style');
-        if ($(window).width() > 767) {
-            var $navbar = $('ul.navbar-nav').find('li.nav-item');
-            var $communityItem = $navbar[$navbar.length - 1];
-            var sidebarRight = $(window).width() - $communityItem.offsetLeft - $communityItem.offsetWidth + 15;
-            $sphinxsidebar.css('right', sidebarRight);
+    // function setSidebar() {
+    //     var $sphinxsidebar = $('div.sphinxsidebar').removeAttr('style');
+    //     if ($(window).width() > 767) {
+    //         var $navbar = $('ul.navbar-nav').find('li.nav-item');
+    //         var $communityItem = $navbar[$navbar.length - 1];
+    //         var sidebarRight = $(window).width() - $communityItem.offsetLeft - $communityItem.offsetWidth + 15;
+    //         $sphinxsidebar.css('right', sidebarRight);
 
-            /* if sidebar is too long, return to static */
-            if ($(window).height() - $sphinxsidebar.height() - $('header#top').height() < 35 ) {
-                $sphinxsidebar.removeAttr('style');
-            }
+    //         /* if sidebar is too long, return to static */
+    //         if ($(window).height() - $sphinxsidebar.height() - $('header#top').height() < 35 ) {
+    //             $sphinxsidebar.removeAttr('style');
+    //         }
+    //     }
+    // }
+    // setSidebar();
+
+    /* nav-bar handling so it is aligned with the sidebar */
+    function setSidebar() {
+        var $mainNav = $('nav.main-nav');
+        $mainNav.removeAttr('style');
+        if ($(window).width() > 767) {
+            var $widgetArea = $('div.sphinxsidebar');
+            var widgetAreaPos = $widgetArea.offset();
+            var widgetAreaRight = $(window).width() - widgetAreaPos.left - $widgetArea.width();
+            $mainNav.css({'position': 'relative'});
+            $mainNav.css({'clear': 'both'});
+            $mainNav.css({'right': widgetAreaRight});
         }
     }
     setSidebar();
