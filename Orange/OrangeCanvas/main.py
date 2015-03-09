5bf3ecd42de94d92f0af6fc5ee3b2ce740de8670
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2015-03-09 16:35:47 +0100

    Disable the OSX font fix if using Qt 4.8.6 or later.
    Qt 4.8.6 already contains a fix for this.
    
    (transplanted from orange3/3fedb321a2061a2473cc14b2215c4284606de89a)

--- Orange/OrangeCanvas/main.py
+++ Orange/OrangeCanvas/main.py
@@ -46,10 +46,11 @@ def running_in_ipython():
 
 def fix_osx_10_9_private_font():
     """Temporary fix for QTBUG-32789."""
-    from PyQt4.QtCore import QSysInfo
+    from PyQt4.QtCore import QSysInfo, QT_VERSION
     if sys.platform == "darwin":
         try:
-            if QSysInfo.MacintoshVersion > QSysInfo.MV_10_8:
+            if QSysInfo.MacintoshVersion > QSysInfo.MV_10_8 and \
+                    QT_VERSION < 0x40806:
                 QFont.insertSubstitution(".Lucida Grande UI", "Lucida Grande")
         except AttributeError:
             pass
