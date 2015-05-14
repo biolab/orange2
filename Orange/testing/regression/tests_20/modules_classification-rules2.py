8bb30cc381d0f5d742c49d899564cdcee49830e4
Martin Mozina <martin.mozina@fri.uni-lj.si>
2015-05-14 16:58:24 +0200

    Updated regression tests.

--- Orange/testing/regression/tests_20/modules_classification-rules2.py
+++ Orange/testing/regression/tests_20/modules_classification-rules2.py
@@ -3,10 +3,10 @@
 # Classes:     CN2Learner, CN2UnorderedLearner, CN2UnorderedLearner
 # Uses:        titanic.tab
 
-import orange
+import Orange
 import orngCN2
 
-data = orange.ExampleTable("titanic.tab")
+data = Orange.data.Table("titanic.tab")
 
 # create learner
 learner = orngCN2.CN2Learner()
@@ -18,7 +18,7 @@ print "*****"
 
 learner = orngCN2.CN2UnorderedLearner()
 
-learner.ruleFinder = orange.RuleBeamFinder()
+learner.ruleFinder = Orange.core.RuleBeamFinder()
 learner.ruleFinder.evaluator = orngCN2.mEstimate(m=50)
 
 cl = learner(data)
@@ -28,8 +28,8 @@ print "****"
 
 learner = orngCN2.CN2SDUnorderedLearner()
 
-learner.ruleFinder.ruleStoppingValidator = orange.RuleValidator_LRS(alpha=0.01,min_coverage=10,max_rule_complexity = 2)
-learner.ruleFinder.ruleFilter = orange.RuleBeamFilter_Width(width = 50)
+learner.ruleFinder.ruleStoppingValidator = Orange.core.RuleValidator_LRS(alpha=0.01,min_coverage=10,max_rule_complexity = 2)
+learner.ruleFinder.ruleFilter = Orange.core.RuleBeamFilter_Width(width = 50)
 
 cl = learner(data)
 for r in cl.rules:
@@ -38,7 +38,7 @@ print "****"
 
 learner = orngCN2.CN2UnorderedLearner()
 
-learner.ruleFinder = orange.RuleBeamFinder()
+learner.ruleFinder = Orange.core.RuleBeamFinder()
 learner.ruleFinder.evaluator = orngCN2.WRACCEvaluator()
 
 cl = learner(data)
--- Orange/testing/regression/tests_20/modules_classification_rules1.py
+++ Orange/testing/regression/tests_20/modules_classification_rules1.py
@@ -1,17 +1,17 @@
-import orange
+import Orange
 import orngCN2
 
-data = orange.ExampleTable("titanic.tab")
+data = Orange.data.Table("titanic.tab")
 
 # create learner
-learner = orange.RuleLearner()
+learner = Orange.core.RuleLearner()
 
 cl = learner(data)
 for r in cl.rules:
     print orngCN2.ruleToString(r)
 print "*****"
 
-learner.ruleFinder = orange.RuleBeamFinder()
+learner.ruleFinder = Orange.core.RuleBeamFinder()
 learner.ruleFinder.evaluator = orngCN2.mEstimate(m=50)
 
 cl = learner(data)
@@ -19,8 +19,8 @@ for r in cl.rules:
     print orngCN2.ruleToString(r)
 print "****"
 
-learner.ruleFinder.ruleStoppingValidator = orange.RuleValidator_LRS(alpha=0.01,min_coverage=10,max_rule_complexity = 2)
-learner.ruleFinder.ruleFilter = orange.RuleBeamFilter_Width(width = 50)
+learner.ruleFinder.ruleStoppingValidator = Orange.core.RuleValidator_LRS(alpha=0.01,min_coverage=10,max_rule_complexity = 2)
+learner.ruleFinder.ruleFilter = Orange.core.RuleBeamFilter_Width(width = 50)
 
 cl = learner(data)
 for r in cl.rules:
